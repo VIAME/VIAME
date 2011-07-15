c7b5911e0cf04ca46a01f16d3ba22c5cafe9a11e
Ben Boeckel <ben.boeckel@kitware.com>
2011-07-15 11:34:55 -0400

    Add registration headers

---
+++ src/processes/examples/registration.h
@@ -0,0 +1,17 @@
+/*ckwg +5
+ * Copyright 2011 by Kitware, Inc. All Rights Reserved. Please refer to
+ * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
+ * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
+ */
+
+#ifndef VISTK_PROCESSES_EXAMPLES_REGISTRATION_H
+#define VISTK_PROCESSES_EXAMPLES_REGISTRATION_H
+
+extern "C"
+{
+
+void register_processes();
+
+}
+
+#endif // VISTK_PROCESSES_EXAMPLES_REGISTRATION_H
