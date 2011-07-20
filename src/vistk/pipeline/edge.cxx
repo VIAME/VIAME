98c27ab9b10b4a3a1ba1f60ad3cc8e152cc633e7
Ben Boeckel <ben.boeckel@kitware.com>
2011-07-20 09:42:07 -0400

    Move dumb_pipe_edge code into the base edge

--- src/vistk/pipeline/edge.cxx
+++ src/vistk/pipeline/edge.cxx
@@ -7,14 +7,38 @@
 #include "edge.h"
 #include "edge_exception.h"
 
+#include <boost/thread/condition_variable.hpp>
+#include <boost/thread/locks.hpp>
+#include <boost/thread/mutex.hpp>
+
+#include <queue>
+
 /**
  * \file edge.cxx
  *
- * \brief Implementation of the base class for \link edge edges\endlink.
+ * \brief Implementation of the \ref edge class.
  */
 
 namespace vistk
 {
+
+class edge::priv
+{
+  public:
+    priv();
+    ~priv();
+
+    bool required;
+
+    process_t upstream;
+    process_t downstream;
+
+    std::queue<edge_datum_t> q;
+
+    boost::condition_variable cond_have_data;
+
+    mutable boost::mutex mutex;
+};
 
 edge
 ::~edge()
@@ -35,6 +59,35 @@ edge
   return (datum_count() != 0);
 }
 
+bool
+edge
+::full_of_data() const
+{
+  return false;
+}
+
+size_t
+edge
+::datum_count() const
+{
+  boost::unique_lock<boost::mutex> lock(d->mutex);
+
+  (void)lock;
+
+  return d->q.size();
+}
+
+void
+edge
+::push_datum(edge_datum_t const& datum)
+{
+  boost::unique_lock<boost::mutex> lock(d->mutex);
+
+  (void)lock;
+
+  d->q.push(datum);
+}
+
 edge_datum_t
 edge
 ::get_datum()
@@ -46,18 +99,46 @@ edge
   return dat;
 }
 
+edge_datum_t
+edge
+::peek_datum()
+{
+  boost::unique_lock<boost::mutex> lock(d->mutex);
+
+  while (!has_data())
+  {
+    d->cond_have_data.wait(lock);
+  }
+
+  return d->q.front();
+}
+
+void
+edge
+::pop_datum()
+{
+  boost::unique_lock<boost::mutex> lock(d->mutex);
+
+  while (!has_data())
+  {
+    d->cond_have_data.wait(lock);
+  }
+
+  d->q.pop();
+}
+
 void
 edge
 ::set_required_by_downstream(bool required)
 {
-  m_required = required;
+  d->required = required;
 }
 
 bool
 edge
 ::required_by_downstream() const
 {
-  return m_required;
+  return d->required;
 }
 
 void
@@ -69,12 +150,12 @@ edge
     throw null_process_connection();
   }
 
-  if (m_upstream)
+  if (d->upstream)
   {
-    throw input_already_connected(m_upstream->name(), process->name());
+    throw input_already_connected(d->upstream->name(), process->name());
   }
 
-  m_upstream = process;
+  d->upstream = process;
 }
 
 void
@@ -86,17 +167,27 @@ edge
     throw null_process_connection();
   }
 
-  if (m_downstream)
+  if (d->downstream)
   {
-    throw output_already_connected(m_downstream->name(), process->name());
+    throw output_already_connected(d->downstream->name(), process->name());
   }
 
-  m_downstream = process;
+  d->downstream = process;
 }
 
 edge
 ::edge(config_t const& /*config*/)
-  : m_required(true)
+{
+}
+
+edge::priv
+::priv()
+  : required(true)
+{
+}
+
+edge::priv
+::~priv()
 {
 }
 
--- src/vistk/pipeline/edge.h
+++ src/vistk/pipeline/edge.h
@@ -46,60 +46,58 @@ class VISTK_PIPELINE_EXPORT edge
     /**
      * \brief Destructor.
      */
-    virtual ~edge();
+    ~edge();
 
     /**
      * \brief Whether the edge represents a dependency from upstream to downstream.
      *
      * \returns True if the edge expresses that upstream must be executed before downstream, false otherwise.
      */
-    virtual bool makes_dependency() const;
+    bool makes_dependency() const;
 
     /**
      * \brief Whether the edge has any data in it or not.
      *
      * \returns True if there is data available within the edge, false otherwise.
      */
-    virtual bool has_data() const;
+    bool has_data() const;
     /**
      * \brief Whether the edge can accept more data or not.
      *
      * \returns True if the edge can hold no more data, false otherwise.
      */
-    virtual bool full_of_data() const = 0;
+    bool full_of_data() const;
     /**
      * \brief How many results are in the edge.
      *
      * \returns The number of data items the edge holds.
      */
-    virtual size_t datum_count() const = 0;
+    size_t datum_count() const;
 
     /**
      * \brief Push a datum into the edge.
      *
-     * \note Some edge implementations may block if they are full.
-     *
      * \param datum The datum to put into the edge.
      */
-    virtual void push_datum(edge_datum_t const& datum) = 0;
+    void push_datum(edge_datum_t const& datum);
     /**
      * \brief Extract a datum from the edge.
      *
      * This calls \ref edge::peek_datum and then removes it from the edge.
      *
-     * \note Some edge implementations may block if they are empty.
-     *
      * \returns The next datum available from the edge.
      */
-    virtual edge_datum_t get_datum();
+    edge_datum_t get_datum();
     /**
      * \brief Look at the next datum in the edge.
      *
-     * \note Some edge implementations may block if they are empty.
-     *
      * \returns The next datum available from the edge.
      */
-    virtual edge_datum_t peek_datum() = 0;
+    edge_datum_t peek_datum();
+    /**
+     * \brief Removes a datum from the edge.
+     */
+    void pop_datum();
 
     /**
      * \brief Set whether the data the edge delivers is required for downstream.
@@ -122,7 +120,7 @@ class VISTK_PIPELINE_EXPORT edge
      *
      * \param process The process which can push data into the edge.
      */
-    virtual void set_upstream_process(process_t process);
+    void set_upstream_process(process_t process);
     /**
      * \brief Set the process which is connected to the output side of the edge.
      *
@@ -131,7 +129,7 @@ class VISTK_PIPELINE_EXPORT edge
      *
      * \param process The process which can pull data from the edge.
      */
-    virtual void set_downstream_process(process_t process);
+    void set_downstream_process(process_t process);
   protected:
     /**
      * \brief Constructor.
@@ -139,19 +137,9 @@ class VISTK_PIPELINE_EXPORT edge
      * \param config Contains configuration for the edge.
      */
     edge(config_t const& config);
-
-    /**
-     * \brief Removes a datum from the edge.
-     */
-    virtual void pop_datum() = 0;
-
-    /// Whether the edge is required for the downstream process to run.
-    bool m_required;
-
-    /// The process that can put data into the edge.
-    process_t m_upstream;
-    /// The process that can pull data from the edge.
-    process_t m_downstream;
+  private:
+    class priv;
+    const boost::shared_ptr<priv> d;
 };
 
 } // end namespace vistk
