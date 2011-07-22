fc560d81519174602420b276b8586a63fe5b68e7
Ben Boeckel <ben.boeckel@kitware.com>
2011-07-22 17:02:43 -0400

    Update API to make pipelines from pipe_blocks

--- src/vistk/pipeline_util/load_pipe.h
+++ src/vistk/pipeline_util/load_pipe.h
@@ -9,6 +9,8 @@
 
 #include "pipeline_util-config.h"
 
+#include "pipe_declaration_types.h"
+
 #include <vistk/pipeline/types.h>
 
 #include <boost/filesystem/path.hpp>
@@ -25,22 +27,51 @@ namespace vistk
 {
 
 /**
- * \brief Convert a pipeline description file into a pipeline.
+ * \brief Convert a pipeline description file into a collection of pipeline blocks.
  *
- * \param fname The file to load the pipeline from
+ * \param fname The file to load the pipeline blocks from.
  *
- * \returns A new pipeline.
+ * \returns A new set of pipeline blocks.
  */
-pipeline_t VISTK_PIPELINE_UTIL_EXPORT load_pipe_file(boost::filesystem::path const& fname);
+pipe_blocks VISTK_PIPELINE_UTIL_EXPORT load_pipe_blocks_from_file(boost::filesystem::path const& fname);
 
 /**
  * \brief Convert a pipeline description into a pipeline.
  *
  * \param istr The stream to load the pipeline from.
+ * \param inc_root The root directory to search for includes from.
  *
- * \returns A new pipeline.
+ * \returns A new set of pipeline blocks.
  */
-pipeline_t VISTK_PIPELINE_UTIL_EXPORT load_pipe(std::istream& istr);
+pipe_blocks VISTK_PIPELINE_UTIL_EXPORT load_pipe_blocks(std::istream& istr, boost::filesystem::path const& inc_root = "");
+
+/**
+ * \brief Convert a pipeline description file into a pipeline.
+ *
+ * \param fname The file to load the pipeline from.
+ *
+ * \returns A new pipeline baked from the given file.
+ */
+pipeline_t VISTK_PIPELINE_UTIL_EXPORT bake_pipe_from_file(boost::filesystem::path const& fname);
+
+/**
+ * \brief Bake a pipeline from a stream.
+ *
+ * \param istr The stream to load the pipeline from.
+ * \param inc_root The root directory to search for includes from.
+ *
+ * \returns A pipeline baked from the given stream.
+ */
+pipeline_t VISTK_PIPELINE_UTIL_EXPORT bake_pipe(std::istream& istr, boost::filesystem::path const& inc_root = "");
+
+/**
+ * \brief Bakes a pipeline from a collection of blocks.
+ *
+ * \param blocks The blocks to use for baking the pipeline.
+ *
+ * \returns A pipeline baked from \p blocks.
+ */
+pipeline_t VISTK_PIPELINE_UTIL_EXPORT bake_pipe_blocks(pipe_blocks const& blocks);
 
 }
 
