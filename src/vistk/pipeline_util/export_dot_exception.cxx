4e651d5132c71e09929c32a552abde5568c73c84
Ben Boeckel <ben.boeckel@kitware.com>
2011-08-04 11:20:11 -0400

    Add exceptions when exporting dot

---
+++ src/vistk/pipeline_util/export_dot_exception.cxx
@@ -0,0 +1,43 @@
+/*ckwg +5
+ * Copyright 2011 by Kitware, Inc. All Rights Reserved. Please refer to
+ * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
+ * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
+ */
+
+#include "export_dot_exception.h"
+
+#include <sstream>
+
+/**
+ * \file export_dot_exception.cxx
+ *
+ * \brief Implementation of exceptions used when exporting a pipeline to dot.
+ */
+
+namespace vistk
+{
+
+null_pipeline_export_dot_exception
+::null_pipeline_export_dot_exception() throw()
+  : export_dot_exception()
+{
+  std::stringstream sstr;
+
+  sstr << "The pipeline given to export was NULL";
+
+  m_what = sstr.str();
+}
+
+null_pipeline_export_dot_exception
+::~null_pipeline_export_dot_exception() throw()
+{
+}
+
+char const*
+null_pipeline_export_dot_exception
+::what() const throw()
+{
+  return m_what.c_str();
+}
+
+}
---
+++ src/vistk/pipeline_util/export_dot_exception.h
@@ -0,0 +1,69 @@
+/*ckwg +5
+ * Copyright 2011 by Kitware, Inc. All Rights Reserved. Please refer to
+ * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
+ * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
+ */
+
+#ifndef VISTK_PIPELINE_UTIL_EXPORT_DOT_EXCEPTION_H
+#define VISTK_PIPELINE_UTIL_EXPORT_DOT_EXCEPTION_H
+
+#include "pipeline_util-config.h"
+
+#include <vistk/pipeline/types.h>
+
+#include <string>
+
+/**
+ * \file export_dot_exception.h
+ *
+ * \brief Header for exceptions used when export a pipeline to dot.
+ */
+
+namespace vistk
+{
+
+/**
+ * \class export_dot_exception export_dot_exception.h <vistk/pipeline_util/export_dot_exception.h>
+ *
+ * \brief The base class for all exceptions thrown when exporting a pipeline to dot.
+ *
+ * \ingroup exceptions
+ */
+class VISTK_PIPELINE_UTIL_EXPORT export_dot_exception
+  : public pipeline_exception
+{
+};
+
+/**
+ * \class null_pipeline_export_dot_exception load_pipe_exception.h <vistk/pipeline_util/load_pipe_exception.h>
+ *
+ * \brief The exception thrown when a \c NULL pipeline is given to export.
+ *
+ * \ingroup exceptions
+ */
+class VISTK_PIPELINE_UTIL_EXPORT null_pipeline_export_dot_exception
+  : public export_dot_exception
+{
+  public:
+    /**
+     * \brief Constructor.
+     */
+    null_pipeline_export_dot_exception() throw();
+    /**
+     * \brief Destructor.
+     */
+    ~null_pipeline_export_dot_exception() throw();
+
+    /**
+     * \brief A description of the exception.
+     *
+     * \returns A string describing what went wrong.
+     */
+    char const* what() const throw();
+  private:
+    std::string m_what;
+};
+
+}
+
+#endif // VISTK_PIPELINE_UTIL_EXPORT_DOT_EXCEPTION_H
