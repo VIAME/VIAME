b6884b19260977b9348d3f9553addd720d9ab562
Ben Boeckel <ben.boeckel@kitware.com>
2011-08-04 17:48:54 -0400

    Add a test to test for duplicate groups

--- tests/vistk/pipeline/test_pipeline.cxx
+++ tests/vistk/pipeline/test_pipeline.cxx
@@ -47,6 +47,7 @@ static void test_add_process();
 static void test_add_group();
 static void test_duplicate_process_process();
 static void test_duplicate_process_group();
+static void test_duplicate_group_process();
 
 void
 run_test(std::string const& test_name)
@@ -70,6 +71,10 @@ run_test(std::string const& test_name)
   else if (test_name == "duplicate_process_group")
   {
     test_duplicate_process_group();
+  }
+  else if (test_name == "duplicate_group_process")
+  {
+    test_duplicate_group_process();
   }
   else
   {
@@ -238,5 +243,54 @@ test_duplicate_process_group()
   {
     std::cerr << "Error: Did not get expected exception "
               << "when adding a duplicate process to the pipeline" << std::endl;
+  }
+}
+
+void
+test_duplicate_group_process()
+{
+  vistk::load_known_modules();
+
+  vistk::process_registry_t const reg = vistk::process_registry::self();
+  vistk::process_registry::type_t const proc_type = vistk::process_registry::type_t("numbers");
+
+  vistk::config::value_t const proc_name = vistk::process::name_t("name");
+
+  vistk::config_t proc_config = vistk::config::empty_config();
+
+  proc_config->set_value(vistk::process::config_name, proc_name);
+
+  vistk::process_t const process = reg->create_process(proc_type, proc_config);
+
+  vistk::config_t const config = vistk::config::empty_config();
+
+  vistk::pipeline_t pipeline = vistk::pipeline_t(new vistk::pipeline(config));
+
+  pipeline->add_process(process);
+
+  bool got_exception = false;
+
+  try
+  {
+    pipeline->add_group(proc_name);
+  }
+  catch (vistk::duplicate_process_name_exception& e)
+  {
+    got_exception = true;
+
+    (void)e.what();
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << "Error: Unexpected exception: "
+              << e.what() << std::endl;
+
+    got_exception = true;
+  }
+
+  if (!got_exception)
+  {
+    std::cerr << "Error: Did not get expected exception "
+              << "when adding a duplicate group to the pipeline" << std::endl;
   }
 }
