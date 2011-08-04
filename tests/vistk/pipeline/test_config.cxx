c145d866d40c51680cf7a369ee1caae38068c0ef
Ben Boeckel <ben.boeckel@kitware.com>
2011-08-04 10:09:31 -0400

    Add test for getting unset configuration values

--- tests/vistk/pipeline/test_config.cxx
+++ tests/vistk/pipeline/test_config.cxx
@@ -39,6 +39,7 @@ main(int argc, char* argv[])
 
 static void test_has_value();
 static void test_get_value();
+static void test_get_value_no_exist();
 
 void
 run_test(std::string const& test_name)
@@ -50,6 +51,10 @@ run_test(std::string const& test_name)
   else if (test_name == "get_value")
   {
     test_get_value();
+  }
+  else if (test_name == "get_value_no_exist")
+  {
+    test_get_value_no_exist();
   }
   else
   {
@@ -96,5 +101,48 @@ test_get_value()
   if (valuea != get_valuea)
   {
     std::cerr << "Error: Did not retrieve value that was set" << std::endl;
+  }
+}
+
+void
+test_get_value_no_exist()
+{
+  vistk::config_t config = vistk::config::empty_config();
+
+  vistk::config::key_t const keya = vistk::config::key_t("keya");
+  vistk::config::key_t const keyb = vistk::config::key_t("keyb");
+
+  vistk::config::value_t const valuea = vistk::config::value_t("value_a");
+  vistk::config::value_t const valueb = vistk::config::value_t("value_b");
+
+  bool got_exception = false;
+
+  try
+  {
+    config->get_value<vistk::config::value_t>(keya);
+  }
+  catch (vistk::no_such_configuration_value_exception&)
+  {
+    got_exception = true;
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << "Error: Unexpected exception: "
+              << e.what() << std::endl;
+
+    got_exception = true;
+  }
+
+  if (!got_exception)
+  {
+    std::cerr << "Error: Did not get expected exception "
+              << "when retrieving an unset value" << std::endl;
+  }
+
+  vistk::config::value_t const get_valueb = config->get_value<vistk::config::value_t>(keyb, valueb);
+
+  if (valueb != get_valueb)
+  {
+    std::cerr << "Error: Did not retrieve default when requesting unset value" << std::endl;
   }
 }
