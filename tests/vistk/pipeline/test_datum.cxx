0d755a6af1dec2673d737ef5fd534c6b99f71750
Ben Boeckel <ben.boeckel@kitware.com>
2011-08-04 11:50:39 -0400

    Add datum test skeleton

---
+++ tests/vistk/pipeline/test_datum.cxx
@@ -0,0 +1,47 @@
+/*ckwg +5
+ * Copyright 2011 by Kitware, Inc. All Rights Reserved. Please refer to
+ * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
+ * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
+ */
+
+#include <vistk/pipeline/datum.h>
+
+#include <exception>
+#include <iostream>
+
+static void run_test(std::string const& test_name);
+
+int
+main(int argc, char* argv[])
+{
+  if (argc != 2)
+  {
+    std::cerr << "Error: Expected one argument" << std::endl;
+
+    return 1;
+  }
+
+  std::string const test_name = argv[1];
+
+  try
+  {
+    run_test(test_name);
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << "Error: Unexpected exception: " << e.what() << std::endl;
+
+    return 1;
+  }
+
+  return 0;
+}
+
+void
+run_test(std::string const& test_name)
+{
+  //else
+  {
+    std::cerr << "Error: Unknown test: " << test_name << std::endl;
+  }
+}
