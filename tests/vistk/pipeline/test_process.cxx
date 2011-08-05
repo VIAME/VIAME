791dc3fd3c796a85431fe40c65d41fdb8f8b567c
Ben Boeckel <ben.boeckel@kitware.com>
2011-08-05 11:14:51 -0400

    Add a test for connecting a NULL edge to an input

--- tests/vistk/pipeline/test_process.cxx
+++ tests/vistk/pipeline/test_process.cxx
@@ -4,6 +4,12 @@
  * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
  */
 
+#include <test_common.h>
+
+#include <vistk/pipeline/modules.h>
+#include <vistk/pipeline/process.h>
+#include <vistk/pipeline/process_exception.h>
+
 #include <exception>
 #include <iostream>
 #include <string>
@@ -36,11 +42,48 @@ main(int argc, char* argv[])
   return 0;
 }
 
+static void test_null_input_edge();
+
 void
 run_test(std::string const& test_name)
 {
-  //else
+  if (test_name == "null_input_edge")
+  {
+    test_null_input_edge();
+  }
+  else
   {
     std::cerr << "Error: Unknown test: " << test_name << std::endl;
   }
+}
+
+static vistk::process_t create_process(vistk::process_registry::type_t const& type, vistk::process::name_t const& name);
+
+void
+test_null_input_edge()
+{
+  vistk::process_registry::type_t const proc_type = vistk::process_registry::type_t("numbers");
+
+  vistk::process_t const process = create_process(proc_type, vistk::process::name_t());
+
+  vistk::edge_t const edge;
+
+  EXPECT_EXCEPTION(vistk::null_edge_port_connection_exception,
+                   process->connect_input_port(vistk::process::port_t(), edge),
+                   "connecting a NULL edge to an input port");
+}
+
+vistk::process_t
+create_process(vistk::process_registry::type_t const& type, vistk::process::name_t const& name)
+{
+  static bool modules_loaded = (vistk::load_known_modules(), true);
+  static vistk::process_registry_t const reg = vistk::process_registry::self();
+
+  (void)modules_loaded;
+
+  vistk::config_t config = vistk::config::empty_config();
+
+  config->set_value(vistk::process::config_name, vistk::config::value_t(name));
+
+  return reg->create_process(type, config);
 }
