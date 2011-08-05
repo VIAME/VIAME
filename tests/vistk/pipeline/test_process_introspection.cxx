ae49c4fcf6d3379eebe28a55386b0efb1828df52
Ben Boeckel <ben.boeckel@kitware.com>
2011-08-05 17:09:25 -0400

    Split up the introspection tests

--- tests/vistk/pipeline/test_process_introspection.cxx
+++ tests/vistk/pipeline/test_process_introspection.cxx
@@ -19,11 +19,11 @@
 #include <exception>
 #include <iostream>
 
+static void test_process(vistk::process_registry::type_t const& type);
+
 int
 main()
 {
-  vistk::process_registry_t reg = vistk::process_registry::self();
-
   try
   {
     vistk::load_known_modules();
@@ -43,170 +43,201 @@ main()
     return 1;
   }
 
-  vistk::process::name_t const expected_name = vistk::process::name_t("expected_name");
+  vistk::process_registry_t const reg = vistk::process_registry::self();
 
   vistk::process_registry::types_t const types = reg->types();
+
+  BOOST_FOREACH (vistk::process_registry::type_t const& type, types)
+  {
+    try
+    {
+      test_process(type);
+    }
+    catch (std::exception& e)
+    {
+      std::cerr << "Error: Unexpected exception when testing "
+                << "type \'" << type << "\': " << e.what() << std::endl;
+    }
+  }
+
+  return 0;
+}
+
+static void test_process_configuration(vistk::process_t const process);
+static void test_process_input_ports(vistk::process_t const process);
+static void test_process_output_ports(vistk::process_t const process);
+
+static void test_process_invalid_configuration(vistk::process_t const process);
+static void test_process_invalid_input_port(vistk::process_t const process);
+static void test_process_invalid_output_port(vistk::process_t const process);
+
+void
+test_process(vistk::process_registry::type_t const& type)
+{
+  static vistk::process::name_t const expected_name = vistk::process::name_t("expected_name");
 
   vistk::config_t config = vistk::config::empty_config();
   config->set_value(vistk::process::config_name, expected_name);
 
-  BOOST_FOREACH (vistk::process_registry::type_t const& type, types)
-  {
-    vistk::process_t process;
+  vistk::process_registry_t const reg = vistk::process_registry::self();
 
+  if (reg->description(type).empty())
+  {
+    std::cerr << "Error: The description is empty" << std::endl;
+  }
+
+  vistk::process_t const process = reg->create_process(type, config);
+
+  if (!process)
+  {
+    std::cerr << "Error: Received NULL process (" << type << ")" << std::endl;
+
+    return;
+  }
+
+  if (process->name() != expected_name)
+  {
+    std::cerr << "Error: Name (" << process->name() << ") "
+              << "does not match expected name: " << expected_name << std::endl;
+  }
+
+  if (process->type() != type)
+  {
+    std::cerr << "Error: The type does not match the registry type" << std::endl;
+  }
+
+  test_process_configuration(process);
+  test_process_input_ports(process);
+  test_process_output_ports(process);
+
+  test_process_invalid_configuration(process);
+  test_process_invalid_input_port(process);
+  test_process_invalid_output_port(process);
+}
+
+void
+test_process_configuration(vistk::process_t const process)
+{
+  vistk::config::keys_t const keys = process->available_config();
+
+  BOOST_FOREACH (vistk::config::key_t const& key, keys)
+  {
     try
     {
-      process = reg->create_process(type, config);
+      process->config_info(key);
     }
-    catch (vistk::no_such_process_type_exception& e)
+    catch (vistk::unknown_configuration_value_exception& e)
     {
-      std::cerr << "Error: Failed to create process: " << e.what() << std::endl;
-
-      continue;
+      std::cerr << "Error: Failed to get a default for "
+                << process->type() << vistk::config::block_sep << key
+                << ": " << e.what() << std::endl;
     }
     catch (std::exception& e)
     {
-      std::cerr << "Error: Unexpected exception when creating process: " << e.what() << std::endl;
-    }
-
-    if (!process)
-    {
-      std::cerr << "Error: Received NULL process (" << type << ")" << std::endl;
-
-      continue;
-    }
-
-    if (reg->description(type).empty())
-    {
-      std::cerr << "Error: The description is empty" << std::endl;
-    }
-
-    if (process->name() != expected_name)
-    {
-      std::cerr << "Error: Name (" << process->name() << ") "
-                << "does not match expected name: " << expected_name << std::endl;
-    }
-
-    if (process->type() != type)
-    {
-      std::cerr << "Error: The type does not match the registry type" << std::endl;
-    }
-
-    vistk::config::keys_t const keys = process->available_config();
-
-    BOOST_FOREACH (vistk::config::key_t const& key, keys)
-    {
-      try
-      {
-        process->config_info(key);
-      }
-      catch (vistk::unknown_configuration_value_exception& e)
-      {
-        std::cerr << "Error: Failed to get a default for "
-                  << type << vistk::config::block_sep << key
-                  << ": " << e.what() << std::endl;
-      }
-      catch (std::exception& e)
-      {
-        std::cerr << "Error: Unexpected exception when querying for default "
-                  << "(" << type << vistk::config::block_sep
-                  << key << "): " << e.what() << std::endl;
-      }
-    }
-
-    vistk::process::ports_t const iports = process->input_ports();
-
-    BOOST_FOREACH (vistk::process::port_t const& port, iports)
-    {
-      try
-      {
-        process->input_port_info(port);
-      }
-      catch (vistk::no_such_port_exception& e)
-      {
-        std::cerr << "Error: Failed to get a type for input port "
-                  << type << "." << port << ": " << e.what() << std::endl;
-      }
-      catch (std::exception& e)
-      {
-        std::cerr << "Error: Unexpected exception when querying for input port type "
-                  << "(" << type << "." << port << "): " << e.what() << std::endl;
-      }
-    }
-
-    vistk::process::ports_t const oports = process->output_ports();
-
-    BOOST_FOREACH (vistk::process::port_t const& port, oports)
-    {
-      try
-      {
-        process->output_port_info(port);
-      }
-      catch (vistk::no_such_port_exception& e)
-      {
-        std::cerr << "Error: Failed to get a type for output port "
-                  << type << "." << port << ": " << e.what() << std::endl;
-      }
-      catch (std::exception& e)
-      {
-        std::cerr << "Error: Unexpected exception when querying for output port type "
-                  << "(" << type << "." << port << "): " << e.what() << std::endl;
-      }
-    }
-
-    // Test for proper exceptions on invalid config requests.
-    {
-      vistk::config::key_t const non_existent_config = vistk::config::key_t("does_not_exist");
-
-      EXPECT_EXCEPTION(vistk::unknown_configuration_value_exception,
-                       process->config_info(non_existent_config),
-                       "requesting the information for a non-existent config");
-    }
-
-    // Test for proper exceptions on invalid port requests.
-    {
-      vistk::process::port_t const non_existent_port = vistk::process::port_t("does_not_exist");
-
-      // Input ports.
-      {
-        EXPECT_EXCEPTION(vistk::no_such_port_exception,
-                         process->input_port_info(non_existent_port),
-                         "requesting the info for a non-existent input port");
-
-        vistk::edge_t edge = vistk::edge_t(new vistk::edge(config));
-
-        EXPECT_EXCEPTION(vistk::no_such_port_exception,
-                         process->connect_input_port(non_existent_port, edge),
-                         "requesting a connection to a non-existent input port");
-      }
-
-      // Output ports.
-      {
-        EXPECT_EXCEPTION(vistk::no_such_port_exception,
-                         process->output_port_info(non_existent_port),
-                         "requesting the info for a non-existent output port");
-
-        vistk::edge_t edge = vistk::edge_t(new vistk::edge(config));
-
-        EXPECT_EXCEPTION(vistk::no_such_port_exception,
-                         process->connect_output_port(non_existent_port, edge),
-                         "requesting a connection to a non-existent output port");
-      }
+      std::cerr << "Error: Unexpected exception when querying for default "
+                << "(" << process->type() << vistk::config::block_sep
+                << key << "): " << e.what() << std::endl;
     }
   }
+}
 
-  // Check exceptions for unknown types.
+void
+test_process_input_ports(vistk::process_t const process)
+{
+  vistk::process::ports_t const ports = process->input_ports();
+
+  BOOST_FOREACH (vistk::process::port_t const& port, ports)
   {
-    vistk::process_registry::type_t const non_existent_process = vistk::process_registry::type_t("no_such_process");
+    vistk::process::port_info_t info;
 
-    EXPECT_EXCEPTION(vistk::no_such_process_type_exception,
-                     reg->create_process(non_existent_process, config),
-                     "requesting an non-existent process type");
+    try
+    {
+      info = process->input_port_info(port);
+    }
+    catch (vistk::no_such_port_exception& e)
+    {
+      std::cerr << "Error: Failed to get a info for input port "
+                << process->type() << "." << port << ": " << e.what() << std::endl;
+    }
+    catch (std::exception& e)
+    {
+      std::cerr << "Error: Unexpected exception when querying for input port info "
+                << "(" << process->type() << "." << port << "): " << e.what() << std::endl;
+    }
 
-    EXPECT_EXCEPTION(vistk::no_such_process_type_exception,
-                     reg->description(non_existent_process),
-                     "requesting an non-existent process type");
+    /// \todo Test port information.
   }
+}
 
-  return 0;
+void
+test_process_output_ports(vistk::process_t const process)
+{
+  vistk::process::ports_t const ports = process->output_ports();
+
+  BOOST_FOREACH (vistk::process::port_t const& port, ports)
+  {
+    vistk::process::port_info_t info;
+
+    try
+    {
+      info = process->output_port_info(port);
+    }
+    catch (vistk::no_such_port_exception& e)
+    {
+      std::cerr << "Error: Failed to get a info for output port "
+                << process->type() << "." << port << ": " << e.what() << std::endl;
+    }
+    catch (std::exception& e)
+    {
+      std::cerr << "Error: Unexpected exception when querying for output port info "
+                << "(" << process->type() << "." << port << "): " << e.what() << std::endl;
+    }
+
+    /// \todo Test port information.
+  }
+}
+
+void
+test_process_invalid_configuration(vistk::process_t const process)
+{
+  vistk::config::key_t const non_existent_config = vistk::config::key_t("does_not_exist");
+
+  EXPECT_EXCEPTION(vistk::unknown_configuration_value_exception,
+                   process->config_info(non_existent_config),
+                   "requesting the information for a non-existent config");
+}
+
+void
+test_process_invalid_input_port(vistk::process_t const process)
+{
+  static vistk::process::port_t const non_existent_port = vistk::process::port_t("does_not_exist");
+  static vistk::config_t const config = vistk::config::empty_config();
+
+  EXPECT_EXCEPTION(vistk::no_such_port_exception,
+                   process->input_port_info(non_existent_port),
+                   "requesting the info for a non-existent input port");
+
+  vistk::edge_t edge = vistk::edge_t(new vistk::edge(config));
+
+  EXPECT_EXCEPTION(vistk::no_such_port_exception,
+                   process->connect_input_port(non_existent_port, edge),
+                   "requesting a connection to a non-existent input port");
+}
+
+void
+test_process_invalid_output_port(vistk::process_t const process)
+{
+  static vistk::process::port_t const non_existent_port = vistk::process::port_t("does_not_exist");
+  static vistk::config_t const config = vistk::config::empty_config();
+
+  // Output ports.
+  EXPECT_EXCEPTION(vistk::no_such_port_exception,
+                   process->output_port_info(non_existent_port),
+                   "requesting the info for a non-existent output port");
+
+  vistk::edge_t edge = vistk::edge_t(new vistk::edge(config));
+
+  EXPECT_EXCEPTION(vistk::no_such_port_exception,
+                   process->connect_output_port(non_existent_port, edge),
+                   "requesting a connection to a non-existent output port");
 }
