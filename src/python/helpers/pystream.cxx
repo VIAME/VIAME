3548991b2f83f7c4e0b3c10972e6b5d5d4db74fd
Ben Boeckel <ben.boeckel@kitware.com>
2011-08-08 17:15:13 -0400

    Add a wrapper to treat python objects as streams

---
+++ src/python/helpers/pystream.cxx
@@ -0,0 +1,65 @@
+/*ckwg +5
+ * Copyright 2011 by Kitware, Inc. All Rights Reserved. Please refer to
+ * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
+ * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
+ */
+
+#include "pystream.h"
+
+#include <algorithm>
+#include <string>
+
+pyistream_device
+::pyistream_device(boost::python::object const& obj)
+  : m_obj(obj)
+{
+}
+
+pyistream_device
+::~pyistream_device()
+{
+}
+
+std::streamsize
+pyistream_device
+::read(char_type* s, std::streamsize n)
+{
+  boost::python::str bytes = boost::python::str(m_obj.attr("read")(n));
+
+  long const sz = boost::python::len(bytes);
+
+  if (sz)
+  {
+    std::string cppstr = boost::python::extract<std::string>(bytes);
+
+    std::copy(cppstr.begin(), cppstr.end(), s);
+
+    return sz;
+  }
+  else
+  {
+    return -1;
+  }
+}
+
+pyostream_device
+::pyostream_device(boost::python::object const& obj)
+  : m_obj(obj)
+{
+}
+
+pyostream_device
+::~pyostream_device()
+{
+}
+
+std::streamsize
+pyostream_device
+::write(char_type const* s, std::streamsize n)
+{
+  boost::python::str bytes(s, static_cast<size_t>(n));
+
+  m_obj.attr("write")(bytes);
+
+  return n;
+}
---
+++ src/python/helpers/pystream.h
@@ -0,0 +1,44 @@
+/*ckwg +5
+ * Copyright 2011 by Kitware, Inc. All Rights Reserved. Please refer to
+ * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
+ * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
+ */
+
+#ifndef VISTK_PYTHON_HELPERS_PYSTREAM_H
+#define VISTK_PYTHON_HELPERS_PYSTREAM_H
+
+#include <boost/iostreams/concepts.hpp>
+#include <boost/iostreams/stream.hpp>
+#include <boost/python.hpp>
+
+#include <iostream>
+
+class pyistream_device
+  : public boost::iostreams::source
+{
+  public:
+    pyistream_device(boost::python::object const& obj);
+    ~pyistream_device();
+
+    std::streamsize read(char_type* s, std::streamsize n);
+  private:
+    boost::python::object m_obj;
+};
+
+typedef boost::iostreams::stream<pyistream_device> pyistream;
+
+class pyostream_device
+  : public boost::iostreams::sink
+{
+  public:
+    pyostream_device(boost::python::object const& obj);
+    ~pyostream_device();
+
+    std::streamsize write(char_type const* s, std::streamsize n);
+  private:
+    boost::python::object m_obj;
+};
+
+typedef boost::iostreams::stream<pyostream_device> pyostream;
+
+#endif // VISTK_PYTHON_HELPERS_PYSTREAM_H
