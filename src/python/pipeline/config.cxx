a56911caa0e84945cde2a6e01a1ba7449b158953
Ben Boeckel <ben.boeckel@kitware.com>
2011-08-09 13:31:18 -0400

    Add docstrings to python config symbols

--- src/python/pipeline/config.cxx
+++ src/python/pipeline/config.cxx
@@ -29,28 +29,55 @@ BOOST_PYTHON_MODULE(config)
     vistk::configuration_exception>(translator);
 
   def("empty_config", &vistk::config::empty_config
-    , (arg("name") = vistk::config::key_t()));
+    , (arg("name") = vistk::config::key_t())
+    , "Returns an empty configuration.");
 
-  class_<vistk::config::key_t>("ConfigKey");
-  class_<vistk::config::keys_t>("ConfigKeys")
+  class_<vistk::config::key_t>("ConfigKey"
+    , "A key for a configuration.");
+  class_<vistk::config::keys_t>("ConfigKeys"
+    , "A collection of keys for a configuration.")
     .def(vector_indexing_suite<vistk::config::keys_t>())
   ;
-  class_<vistk::config::value_t>("ConfigValue");
+  class_<vistk::config::value_t>("ConfigValue"
+    , "A value in the configuration.");
 
   class_<vistk::config, vistk::config_t, boost::noncopyable>("Config", no_init)
-    .def("subblock", &vistk::config::subblock)
-    .def("subblock_view", &vistk::config::subblock_view)
-    .def("get_value", &get_value)
-    .def("get_value", &get_value_with_default)
-    .def("set_value", &vistk::config::set_value)
-    .def("unset_value", &vistk::config::unset_value)
-    .def("is_read_only", &vistk::config::is_read_only)
-    .def("mark_read_only", &vistk::config::mark_read_only)
-    .def("merge_config", &vistk::config::merge_config)
-    .def("available_values", &vistk::config::available_values)
-    .def("has_value", &vistk::config::has_value)
-    .def_readonly("block_sep", &vistk::config::block_sep)
-    .def_readonly("global_value", &vistk::config::global_value)
+    .def("subblock", &vistk::config::subblock
+      , (arg("name"))
+      , "Returns a subblock from the configuration.")
+    .def("subblock_view", &vistk::config::subblock_view
+      , (arg("name"))
+      , "Returns a linked subblock from the configuration.")
+    .def("get_value", &get_value
+      , (arg("key"))
+      , "Retrieve a value from the configuration.")
+    .def("get_value", &get_value_with_default
+      , (arg("key"), arg("default"))
+      , "Retrieve a value from the configuration, using a default in case of failure.")
+    .def("set_value", &vistk::config::set_value
+      , (arg("key"), arg("value"))
+      , "Set a value in the configuration.")
+    .def("unset_value", &vistk::config::unset_value
+      , (arg("key"))
+      , "Unset a value in the configuration.")
+    .def("is_read_only", &vistk::config::is_read_only
+      , (arg("key"))
+      , "Check if a key is marked as read only.")
+    .def("mark_read_only", &vistk::config::mark_read_only
+      , (arg("key"))
+      , "Mark a key as read only.")
+    .def("merge_config", &vistk::config::merge_config
+      , (arg("config"))
+      , "Merge another configuration block into the current one.")
+    .def("available_values", &vistk::config::available_values
+      , "Retrieves the list of available values in the configuration.")
+    .def("has_value", &vistk::config::has_value
+      , (arg("key"))
+      , "Returns True if the key is set.")
+    .def_readonly("block_sep", &vistk::config::block_sep
+      , "The string which separates block names from key names.")
+    .def_readonly("global_value", &vistk::config::global_value
+      , "A special key which is automatically inherited on subblock requests.")
   ;
 }
 
