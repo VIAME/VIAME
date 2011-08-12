34c2dc143e9e7603bf8ce917324368ca5bf4222f
Ben Boeckel <ben.boeckel@kitware.com>
2011-08-12 11:36:24 -0400

    Add tests for python bindings to stamp

--- tests/python/pipeline/test-stamp.py
+++ tests/python/pipeline/test-stamp.py
@@ -17,9 +17,44 @@ def test_import():
         log("Error: Failed to import the stamp module")
 
 
+def test_create():
+    from vistk.pipeline import stamp
+
+    stamp.new_stamp()
+
+
+def test_api_calls():
+    from vistk.pipeline import stamp
+
+    s = stamp.new_stamp()
+    sc = stamp.copied_stamp(s)
+    si = stamp.incremented_stamp(s)
+    t = stamp.new_stamp()
+    sr = stamp.recolored_stamp(s, t)
+
+    if s.is_same_color(t):
+        log("Error: New stamps have the same color")
+
+    if not s.is_same_color(sc):
+        log("Error: Copied stamps do not have the same color")
+
+    if s > si:
+        log("Error: A stamp is greater than its increment")
+
+    if si < s:
+        log("Error: A stamp is greater than its increment")
+
+    if s < t or t < s:
+        log("Error: Different colored stamps return True for comparison")
+
+
 def main(testname):
     if testname == 'import':
         test_import()
+    elif testname == 'create':
+        test_create()
+    elif testname == 'api_calls':
+        test_api_calls()
     else:
         log("Error: No such test '%s'" % testname)
 
