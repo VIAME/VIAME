02faa67481e0902501e9e269c17351d9b8f05afa
Ben Boeckel <ben.boeckel@kitware.com>
2011-08-12 12:01:45 -0400

    Add a test for exporting to dot from python

--- tests/python/pipeline_util/test-export_.py
+++ tests/python/pipeline_util/test-export_.py
@@ -17,9 +17,29 @@ def test_import():
         log("Error: Failed to import the export_ module")
 
 
-def main(testname):
+def test_simple_pipeline(path):
+    import os
+
+    from vistk.pipeline import pipeline
+    from vistk.pipeline import modules
+    from vistk.pipeline_util import bake
+    from vistk.pipeline_util import export_
+
+    modules.load_known_modules()
+
+    p = bake.bake_pipe_file(path)
+    _, w = os.pipe()
+
+    name = 'graph'
+
+    export_.export_dot(w, p, name)
+
+
+def main(testname, path):
     if testname == 'import':
         test_import()
+    elif testname == 'simple_pipeline':
+        test_simple_pipeline(path)
     else:
         log("Error: No such test '%s'" % testname)
 
@@ -28,8 +48,8 @@ if __name__ == '__main__':
     import os
     import sys
 
-    if not len(sys.argv) == 4:
-        log("Error: Expected three arguments")
+    if not len(sys.argv) == 5:
+        log("Error: Expected four arguments")
         sys.exit(1)
 
     testname = sys.argv[1]
@@ -38,4 +58,8 @@ if __name__ == '__main__':
 
     sys.path.append(sys.argv[3])
 
-    main(testname)
+    pipeline_dir = sys.argv[4]
+
+    path = os.path.join(pipeline_dir, '%s.pipe' % testname)
+
+    main(testname, path)
