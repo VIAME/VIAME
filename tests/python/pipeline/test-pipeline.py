b68e3bb3cde01f6d07003407b485b4c478f002ff
Ben Boeckel <ben.boeckel@kitware.com>
2011-08-12 11:25:04 -0400

    Test the python bindings for pipeline

--- tests/python/pipeline/test-pipeline.py
+++ tests/python/pipeline/test-pipeline.py
@@ -17,9 +17,94 @@ def test_import():
         log("Error: Failed to import the pipeline module")
 
 
+def test_create():
+    from vistk.pipeline import config
+    from vistk.pipeline import pipeline
+
+    c = config.empty_config()
+
+    pipeline.Pipeline(c)
+
+
+def test_api_calls():
+    from vistk.pipeline import config
+    from vistk.pipeline import edge
+    from vistk.pipeline import modules
+    from vistk.pipeline import pipeline
+    from vistk.pipeline import process
+    from vistk.pipeline import process_registry
+
+    c = config.empty_config()
+
+    p = pipeline.Pipeline(c)
+
+    proc_type1 = 'numbers'
+    proc_type2 = 'print_number'
+
+    proc_name1 = 'src'
+    proc_name2 = 'snk'
+
+    port_name1 = 'number'
+    port_name2 = 'number'
+
+    group_name = 'group'
+    group_iport = 'iport'
+    group_oport = 'oport'
+
+    modules.load_known_modules()
+
+    reg = process_registry.ProcessRegistry.self()
+
+    c.set_value(process_registry.Process.config_name, proc_name1)
+    proc1 = reg.create_process(proc_type1, c)
+
+    conf_name = 'output'
+
+    c.set_value(process_registry.Process.config_name, proc_name2)
+    c.set_value(conf_name, 'test-python-pipeline-api_calls-print_number.txt')
+    proc2 = reg.create_process(proc_type2, c)
+
+    p.add_process(proc1)
+    p.add_process(proc2)
+    p.add_group(group_name)
+    p.connect(proc_name1, port_name1,
+              proc_name2, port_name2)
+    p.map_input_port(group_name, group_iport,
+                     proc_name2, port_name2,
+                     process.PortFlags())
+    p.map_output_port(group_name, group_oport,
+                      proc_name1, port_name1,
+                      process.PortFlags())
+    p.process_names()
+    p.process_by_name(proc_name1)
+    p.upstream_for_process(proc_name2)
+    p.upstream_for_port(proc_name2, port_name2)
+    p.downstream_for_process(proc_name1)
+    p.downstream_for_port(proc_name1, port_name1)
+    p.sender_for_port(proc_name2, port_name2)
+    p.receivers_for_port(proc_name1, port_name1)
+    p.input_edges_for_process(proc_name2)
+    p.input_edge_for_port(proc_name2, port_name2)
+    p.output_edges_for_process(proc_name1)
+    p.output_edges_for_port(proc_name1, port_name1)
+    p.groups()
+    p.input_ports_for_group(group_name)
+    p.output_ports_for_group(group_name)
+    p.mapped_group_input_port_flags(group_name, group_iport)
+    p.mapped_group_output_port_flags(group_name, group_oport)
+    p.mapped_group_input_ports(group_name, group_iport)
+    p.mapped_group_output_port(group_name, group_oport)
+
+    p.setup_pipeline()
+
+
 def main(testname):
     if testname == 'import':
         test_import()
+    elif testname == 'create':
+        test_create()
+    elif testname == 'api_calls':
+        test_api_calls()
     else:
         log("Error: No such test '%s'" % testname)
 
