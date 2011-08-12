f13688406b7f6eb5da2b9f9b2fbb328370a9618d
Ben Boeckel <ben.boeckel@kitware.com>
2011-08-12 12:22:07 -0400

    Add test to bake a pipeline from python

--- tests/python/pipeline_util/test-bake.py
+++ tests/python/pipeline_util/test-bake.py
@@ -17,9 +17,31 @@ def test_import():
         log("Error: Failed to import the bake module")
 
 
-def main(testname):
+def test_simple_pipeline(path):
+    import os
+
+    from vistk.pipeline import config
+    from vistk.pipeline import pipeline
+    from vistk.pipeline import modules
+    from vistk.pipeline_util import bake
+    from vistk.pipeline_util import load
+
+    blocks = load.load_pipe_file(path)
+
+    modules.load_known_modules()
+
+    bake.bake_pipe_file(path)
+    with open(path, 'r') as fin:
+        bake.bake_pipe(fin)
+    bake.bake_pipe_blocks(blocks)
+    bake.extract_configuration(blocks)
+
+
+def main(testname, path):
     if testname == 'import':
         test_import()
+    elif testname == 'simple_pipeline':
+        test_simple_pipeline(path)
     else:
         log("Error: No such test '%s'" % testname)
 
@@ -28,8 +50,8 @@ if __name__ == '__main__':
     import os
     import sys
 
-    if not len(sys.argv) == 4:
-        log("Error: Expected three arguments")
+    if not len(sys.argv) == 5:
+        log("Error: Expected four arguments")
         sys.exit(1)
 
     testname = sys.argv[1]
@@ -38,4 +60,8 @@ if __name__ == '__main__':
 
     sys.path.append(sys.argv[3])
 
-    main(testname)
+    pipeline_dir = sys.argv[4]
+
+    path = os.path.join(pipeline_dir, '%s.pipe' % testname)
+
+    main(testname, path)
