1d2aaf8858aa9d6148714b110727de8507465c04
Ben Boeckel <ben.boeckel@kitware.com>
2011-08-19 14:31:21 -0400

    Add docstrings for python stamp bindings

--- src/python/pipeline/stamp.cxx
+++ src/python/pipeline/stamp.cxx
@@ -20,14 +20,23 @@ using namespace boost::python;
 
 BOOST_PYTHON_MODULE(stamp)
 {
-  def("new_stamp", &vistk::stamp::new_stamp);
-  def("copied_stamp", &vistk::stamp::copied_stamp);
-  def("incremented_stamp", &vistk::stamp::incremented_stamp);
-  def("recolored_stamp", &vistk::stamp::recolored_stamp);
+  def("new_stamp", &vistk::stamp::new_stamp
+    , "Creates a new stamp.");
+  def("copied_stamp", &vistk::stamp::copied_stamp
+    , (arg("stamp"))
+    , "Creates an equivalent stamp to the one given.");
+  def("incremented_stamp", &vistk::stamp::incremented_stamp
+    , (arg("stamp"))
+    , "Creates a stamp that is greater than the given stamp.");
+  def("recolored_stamp", &vistk::stamp::recolored_stamp
+    , (arg("stamp"), arg("colored_stamp"))
+    , "Creates a copy of the given stamp with a new color.");
 
   class_<vistk::stamp, vistk::stamp_t, boost::noncopyable>("Stamp"
     , no_init)
-    .def("is_same_color", &vistk::stamp::is_same_color)
+    .def("is_same_color", &vistk::stamp::is_same_color
+      , (arg("stamp"))
+      , "Returns True if the stamps are the same color, False otherwise.")
     .def(self == self)
     .def(self < self)
   ;
