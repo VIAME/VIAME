c99d89f3420409f0f33561cc4a3a137a8c008160
Ben Boeckel <ben.boeckel@kitware.com>
2011-08-19 14:31:07 -0400

    Add tests for python utils bindings

---
+++ tests/python/pipeline/test-utils.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+#ckwg +5
+# Copyright 2011 by Kitware, Inc. All Rights Reserved. Please refer to
+# KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
+# Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
+
+
+def log(msg):
+    import sys
+    sys.stderr.write("%s\n" % msg)
+
+
+def test_import():
+    try:
+        import vistk.pipeline.utils
+    except:
+        log("Error: Failed to import the modules module")
+
+
+def test_create():
+    from vistk.pipeline import utils
+
+    utils.ThreadName()
+
+
+def test_name_thread():
+    from vistk.pipeline import utils
+
+    utils.name_thread("a_name")
+
+
+def main(testname):
+    if testname == 'import':
+        test_import()
+    elif testname == 'create':
+        test_create()
+    elif testname == 'name_thread':
+        test_name_thread()
+    else:
+        log("Error: No such test '%s'" % testname)
+
+
+if __name__ == '__main__':
+    import os
+    import sys
+
+    if not len(sys.argv) == 4:
+        log("Error: Expected three arguments")
+        sys.exit(1)
+
+    testname = sys.argv[1]
+
+    os.chdir(sys.argv[2])
+
+    sys.path.append(sys.argv[3])
+
+    main(testname)
