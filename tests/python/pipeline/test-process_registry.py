bdbb4463b007120e3c12904155e0d0063279a790
Ben Boeckel <ben.boeckel@kitware.com>
2011-08-22 11:45:41 -0400

    Add a test for the process wrapper class

--- tests/python/pipeline/test-process_registry.py
+++ tests/python/pipeline/test-process_registry.py
@@ -10,6 +10,18 @@ def log(msg):
     sys.stderr.write("%s\n" % msg)
 
 
+def ensure_exception(action, func, *args):
+    got_exception = False
+
+    try:
+        func(*args)
+    except:
+        got_exception = True
+
+    if not got_exception:
+        log("Error: Did not get exception when %s" % action)
+
+
 def test_import():
     try:
         import vistk.pipeline.process_registry
@@ -76,6 +88,51 @@ def test_register():
         log("Error: Could not create newly registered process type")
 
 
+def test_wrapper_api():
+    from vistk.pipeline import config
+    from vistk.pipeline import edge
+    from vistk.pipeline import process
+    from vistk.pipeline import process_registry
+
+    reg = process_registry.ProcessRegistry.self()
+
+    proc_type = 'python_example'
+    proc_desc = 'simple description'
+
+    iport = 'no_such_iport'
+    oport = 'no_such_oport'
+    key = 'no_such_key'
+
+    reg.register_process(proc_type, proc_desc, example_process())
+
+    c = config.empty_config()
+
+    def check_process(p):
+        p.input_ports()
+        p.output_ports()
+        ensure_exception("asking for info on a non-existant input port",
+                         p.input_port_info, iport)
+        ensure_exception("asking for info on a non-existant output port",
+                         p.output_port_info, oport)
+
+        e = edge.Edge(c)
+
+        ensure_exception("connecting to a non-existant input port",
+                         p.connect_input_port, iport, e)
+        ensure_exception("connecting to a non-existant output port",
+                         p.connect_output_port, oport, e)
+
+        p.available_config()
+        ensure_exception("asking for info on a non-existant config key",
+                         p.config_info, key)
+
+        p.init()
+        p.step()
+
+    p = reg.create_process(proc_type, c)
+    check_process(p)
+
+
 def main(testname):
     if testname == 'import':
         test_import()
@@ -85,6 +142,8 @@ def main(testname):
         test_api_calls()
     elif testname == 'register':
         test_register()
+    elif testname == 'wrapper_api':
+        test_wrapper_api()
     else:
         log("Error: No such test '%s'" % testname)
 
