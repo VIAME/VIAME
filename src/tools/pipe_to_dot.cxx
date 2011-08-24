e39cf077b84783b10cd72170f2b7b422ae3ad702
Ben Boeckel <ben.boeckel@kitware.com>
2011-08-24 19:22:32 -0400

    Allow extra config and settings in pipe_to_dot

--- src/tools/pipe_to_dot.cxx
+++ src/tools/pipe_to_dot.cxx
@@ -5,18 +5,24 @@
  */
 
 #include <vistk/pipeline_util/export_dot.h>
+#include <vistk/pipeline_util/load_pipe.h>
 #include <vistk/pipeline_util/pipe_bakery.h>
 
 #include <vistk/pipeline/modules.h>
 #include <vistk/pipeline/types.h>
 
+#include <boost/algorithm/string/classification.hpp>
+#include <boost/algorithm/string/split.hpp>
 #include <boost/filesystem/operations.hpp>
+#include <boost/foreach.hpp>
 #include <boost/program_options.hpp>
 
 #include <fstream>
 #include <iostream>
 
 namespace po = boost::program_options;
+
+static std::string const split_str = "=";
 
 static po::options_description make_options();
 static void usage(po::options_description const& options);
@@ -72,7 +78,83 @@ int main(int argc, char* argv[])
 
     /// \todo Include paths?
 
-    pipe = vistk::bake_pipe(istr, boost::filesystem::current_path());
+    vistk::pipe_blocks blocks = vistk::load_pipe_blocks(istr, boost::filesystem::current_path());
+
+    // Load supplemental configuration files.
+    if (vm.count("config"))
+    {
+      vistk::paths_t const configs = vm["config"].as<vistk::paths_t>();
+
+      BOOST_FOREACH (vistk::path_t const& config, configs)
+      {
+        vistk::pipe_blocks const config_blocks = vistk::load_pipe_blocks_from_file(config);
+
+        blocks.insert(blocks.end(), config_blocks.begin(), config_blocks.end());
+      }
+    }
+
+    // Insert lone setting variables from the command line.
+    if (vm.count("setting"))
+    {
+      std::vector<std::string> const settings = vm["setting"].as<std::vector<std::string> >();
+
+      BOOST_FOREACH (std::string const& setting, settings)
+      {
+        vistk::config_pipe_block block;
+
+        vistk::config_value_t value;
+
+        size_t const split_pos = setting.find(split_str);
+
+        if (split_pos == std::string::npos)
+        {
+          std::cerr << "Error: The setting on the command line "
+                       "\'" << setting << "\' does not contain "
+                       "the \'" << split_str << "\' string which "
+                       "separates the key from the value" << std::endl;
+
+          return 1;
+        }
+
+        vistk::config::key_t setting_key = setting.substr(0, split_pos);
+        vistk::config::value_t setting_value = setting.substr(split_pos + split_str.size());
+
+        vistk::config::keys_t keys;
+
+        if (vistk::config::block_sep.size() != 1)
+        {
+          std::cerr << "Error: The block separator is not than "
+                       "one character and does not work here" << std::endl;
+
+          return 1;
+        }
+
+        /// \bug Does not work if (vistk::config::block_sep.size() != 1).
+        boost::split(keys, setting_key, boost::is_any_of(vistk::config::block_sep));
+
+        if (keys.size() < 2)
+        {
+          std::cerr << "Error: The key in the command line setting "
+                       "\'" << setting << "\' does not contain "
+                       "at least two keys in its keypath which is "
+                       "invalid" << std::endl;
+
+          return 1;
+        }
+
+        value.key.key_path.push_back(keys.back());
+        value.value = setting_value;
+
+        keys.pop_back();
+
+        block.key = keys;
+        block.values.push_back(value);
+
+        blocks.push_back(block);
+      }
+    }
+
+    pipe = vistk::bake_pipe_blocks(blocks);
   }
 
   if (!pipe)
@@ -123,6 +205,8 @@ make_options()
     ("help,h", "output help message and quit")
     ("input,i", po::value<vistk::path_t>(), "input path")
     ("output,o", po::value<vistk::path_t>()->default_value("-"), "output path")
+    ("config,c", po::value<vistk::paths_t>(), "supplemental configuration file")
+    ("setting,s", po::value<std::vector<std::string> >(), "additional configuration")
     ("include,I", po::value<vistk::paths_t>(), "configuration include path")
     ("name,n", po::value<std::string>()->default_value("(unnamed)"), "name of the graph")
   ;
