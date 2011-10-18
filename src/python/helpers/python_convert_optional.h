35391d0e2167d036015d13663848e38052953d34
Ben Boeckel <ben.boeckel@kitware.com>
2011-10-18 16:01:01 -0400

    Properly register a boost::optional binding

--- src/python/helpers/python_convert_optional.h
+++ src/python/helpers/python_convert_optional.h
@@ -7,8 +7,10 @@
 #ifndef VISTK_PYTHON_HELPERS_PYTHON_CONVERT_OPTIONAL_H
 #define VISTK_PYTHON_HELPERS_PYTHON_CONVERT_OPTIONAL_H
 
-#include <boost/optional.hpp>
 #include <boost/python/converter/registry.hpp>
+#include <boost/python/class.hpp>
+#include <boost/python/default_call_policies.hpp>
+#include <boost/optional.hpp>
 
 #include <Python.h>
 
@@ -94,13 +96,72 @@ class boost_optional_converter
     }
 };
 
-#define REGISTER_OPTIONAL_CONVERTER(T)             \
-  do                                               \
-  {                                                \
-    typedef boost_optional_converter<T> converter; \
-    typedef typename converter::optional_t opt_t;  \
-    to_python_converter<opt_t, converter>();       \
-    converter();                                   \
-  } while (false)
+template <typename T>
+void register_optional_converter(char const* name, char const* desc);
+
+template <typename T>
+class boost_optional_operations
+{
+  public:
+    typedef T type_t;
+    typedef boost::optional<T> optional_t;
+
+    static bool not_(optional_t const& opt);
+    static type_t& get(optional_t& opt);
+    static type_t const& get_default(optional_t const& opt, type_t const& def);
+};
+
+template <typename T>
+void
+register_optional_converter(char const* name, char const* desc)
+{
+  typedef boost_optional_converter<T> converter_t;
+  typedef boost::optional<T> optional_t;
+
+  boost::python::class_<optional_t>(name
+    , desc
+    , boost::python::no_init)
+    .def(boost::python::init<>())
+    .def(boost::python::init<T>())
+    .def("empty", &boost_optional_operations<T>::not_
+      , "True if there is no value, False otherwise.")
+    .def("get", &boost_optional_operations<T>::get
+      , "Returns the contained value, or None if there isn\'t one."
+      , boost::python::return_internal_reference<>())
+    .def("get", &boost_optional_operations<T>::get_default
+      , (boost::python::arg("default"))
+      , "Returns the contained value, or default if there isn\'t one."
+      , boost::python::return_internal_reference<>())
+  ;
+
+  boost::python::to_python_converter<optional_t, converter_t>();
+  converter_t();
+
+  boost::python::implicitly_convertible<T, optional_t>();
+}
+
+template <typename T>
+bool
+boost_optional_operations<T>
+::not_(optional_t const& opt)
+{
+  return !opt;
+}
+
+template <typename T>
+T&
+boost_optional_operations<T>
+::get(optional_t& opt)
+{
+  return opt.get();
+}
+
+template <typename T>
+T const&
+boost_optional_operations<T>
+::get_default(optional_t const& opt, T const& def)
+{
+  return opt.get_value_or(def);
+}
 
 #endif // VISTK_PYTHON_PIPELINE_PYTHON_CONVERT_OPTIONAL_H
