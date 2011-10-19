b2b770bb5045fcb4377bcb5ee806eeab493df104
Ben Boeckel <ben.boeckel@kitware.com>
2011-10-19 14:15:00 -0400

    Add test boilerplate for running python pipelines

---
+++ tests/python/pipeline/test-run.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+#ckwg +5
+# Copyright 2011 by Kitware, Inc. All Rights Reserved. Please refer to
+# KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
+# Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
+
+
+def log(msg):
+    import sys
+    sys.stderr.write("%s\n" % msg)
+
+
+def ensure_exception(action, func, *args):
+    got_exception = False
+
+    try:
+        func(*args)
+    except:
+        got_exception = True
+
+    if not got_exception:
+        log("Error: Did not get exception when %s" % action)
+
+
+def main(testname):
+    #else:
+    log("Error: No such test '%s'" % testname)
+
+
+if __name__ == '__main__':
+    import os
+    import sys
+
+    if not len(sys.argv) == 4:
+        log("Error: Expected three arguments")
+        sys.exit(1)
+
+    testname = sys.argv[1]
+
+    os.chdir(sys.argv[2])
+
+    sys.path.append(sys.argv[3])
+
+    try:
+        main(testname)
+    except BaseException as e:
+        log("Error: Unexpected exception: %s" % str(e))
