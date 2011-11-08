8852f86e161ec3ca35fe8c2bb6b9e63d0de89b9e
Ben Boeckel <ben.boeckel@kitware.com>
2011-11-08 13:50:03 -0500

    Add a python_threading class
    This class is to ensure that threading is enabled in Python for classes.

---
+++ src/python/helpers/python_threading.cxx
@@ -0,0 +1,23 @@
+/*ckwg +5
+ * Copyright 2011 by Kitware, Inc. All Rights Reserved. Please refer to
+ * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
+ * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
+ */
+
+#include "python_threading.h"
+
+#include <Python.h>
+
+python_threading
+::python_threading()
+{
+  if (!PyEval_ThreadsInitialized())
+  {
+    PyEval_InitThreads();
+  }
+}
+
+python_threading
+::~python_threading()
+{
+}
---
+++ src/python/helpers/python_threading.h
@@ -0,0 +1,17 @@
+/*ckwg +5
+ * Copyright 2011 by Kitware, Inc. All Rights Reserved. Please refer to
+ * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
+ * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
+ */
+
+#ifndef VISTK_PYTHON_PYTHON_THREADING_H
+#define VISTK_PYTHON_PYTHON_THREADING_H
+
+class python_threading
+{
+  public:
+    python_threading();
+    virtual ~python_threading();
+};
+
+#endif // VISTK_PYTHON_PYTHON_THREADING_H
