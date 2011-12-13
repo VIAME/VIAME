1e8e766f3261bc82d8bd6690b0219363d3edda30
Ben Boeckel <ben.boeckel@kitware.com>
2011-12-12 20:32:53 -0500

    Add a python wrapper for the modules functions

---
+++ src/python/pipeline/modules.cxx
@@ -0,0 +1,22 @@
+/*ckwg +5
+ * Copyright 2011 by Kitware, Inc. All Rights Reserved. Please refer to
+ * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
+ * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
+ */
+
+#include <vistk/pipeline/modules.h>
+
+#include <boost/python.hpp>
+
+/**
+ * \file modules.cxx
+ *
+ * \brief Python bindings for module loading.
+ */
+
+using namespace boost::python;
+
+BOOST_PYTHON_MODULE(modules)
+{
+  def("load_known_modules", &vistk::load_known_modules);
+}
