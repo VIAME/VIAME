b0b51743a4979ebbf8cb601d268dd2fe35b25b0e
Ben Boeckel <ben.boeckel@kitware.com>
2011-12-15 15:03:47 -0500

    Update function wrap for the create process macro

--- src/processes/helpers/functions/function_process_register.h
+++ src/processes/helpers/functions/function_process_register.h
@@ -16,47 +16,14 @@
  */
 
 /**
- * \def CREATE_INST
- *
- * \brief The name of the function which creates an instance of the process.
- *
- * \param name The base name of the process.
- */
-#define CREATE_INST(name) create_##name##_process
-
-/**
- * \def DECLARE_CREATE_INSTANCE
- *
- * \brief Declares the function to create the process.
- *
- * \param name The base name of the process.
- */
-#define DECLARE_CREATE_INSTANCE(name) \
-  static process_t CREATE_INST(name)(vistk::config_t const& config)
-
-/**
- * \def REGISTER_CLASS
+ * \def REGISTER_FUNCTION
  *
  * \brief Registers the class with the registry.
  *
  * \param name The base name of the process.
  * \param desc A description of the process.
  */
-#define REGISTER_CLASS(name, desc) \
-  registry->register_process(#name, vistk::process_registry::description_t(desc), CREATE_INST(name))
-
-/**
- * \def DEFINE_CREATE_INSTANCE
- *
- * \brief Implements the function which creates an instance of the process.
- *
- * \param name The base name of the process.
- */
-#define DEFINE_CREATE_INSTANCE(name)                     \
-  vistk::process_t                                       \
-  CREATE_INST(name)(vistk::config_t const& config)       \
-  {                                                      \
-    return boost::make_shared<CLASS_NAME(name)>(config); \
-  }
+#define REGISTER_FUNCTION(name, desc) \
+  registry->register_process(#name, vistk::process_registry::description_t(desc), CREATE_PROCESS(CLASS_NAME(name)))
 
 #endif // VISTK_PROCESSES_HELPER_FUNCTIONS_FUNCTION_PROCESS_REGISTER_H
