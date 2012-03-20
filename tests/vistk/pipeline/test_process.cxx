17d521e0f9410676c50823b790ed8bb16a7e19b6
Ben Boeckel <ben.boeckel@kitware.com>
2012-03-20 15:52:13 -0400

    Add tests for port type setting

--- tests/vistk/pipeline/test_process.cxx
+++ tests/vistk/pipeline/test_process.cxx
@@ -54,8 +54,16 @@ static void test_null_output_edge();
 static void test_connect_after_init();
 static void test_reinit();
 static void test_step_before_init();
+static void test_set_static_input_type();
+static void test_set_static_output_type();
+static void test_set_input_type_duplicate();
+static void test_set_output_type_duplicate();
+static void test_set_input_type_after_init();
+static void test_set_output_type_after_init();
 static void test_set_tagged_flow_dependent_port();
 static void test_set_tagged_flow_dependent_port_cascade();
+static void test_add_input_port_after_type_pin();
+static void test_add_output_port_after_type_pin();
 static void test_set_untagged_flow_dependent_port();
 static void test_null_config();
 static void test_null_input_port_info();
@@ -85,6 +93,30 @@ run_test(std::string const& test_name)
   {
     test_step_before_init();
   }
+  else if (test_name == "set_static_input_type")
+  {
+    test_set_static_input_type();
+  }
+  else if (test_name == "set_static_output_type")
+  {
+    test_set_static_output_type();
+  }
+  else if (test_name == "set_input_type_duplicate")
+  {
+    test_set_input_type_duplicate();
+  }
+  else if (test_name == "set_output_type_duplicate")
+  {
+    test_set_output_type_duplicate();
+  }
+  else if (test_name == "set_input_type_after_init")
+  {
+    test_set_input_type_after_init();
+  }
+  else if (test_name == "set_output_type_after_init")
+  {
+    test_set_output_type_after_init();
+  }
   else if (test_name == "set_tagged_flow_dependent_port")
   {
     test_set_tagged_flow_dependent_port();
@@ -92,6 +124,14 @@ run_test(std::string const& test_name)
   else if (test_name == "set_tagged_flow_dependent_port_cascade")
   {
     test_set_tagged_flow_dependent_port_cascade();
+  }
+  else if (test_name == "add_input_port_after_type_pin")
+  {
+    test_add_input_port_after_type_pin();
+  }
+  else if (test_name == "add_output_port_after_type_pin")
+  {
+    test_add_output_port_after_type_pin();
   }
   else if (test_name == "set_untagged_flow_dependent_port")
   {
@@ -120,6 +160,7 @@ run_test(std::string const& test_name)
 }
 
 static vistk::process_t create_process(vistk::process_registry::type_t const& type, vistk::process::name_t const& name = vistk::process::name_t());
+static vistk::edge_t create_edge();
 static vistk::process_t create_null_config_process(vistk::config_t const& config);
 static vistk::process_t create_null_input_port_info_process(vistk::config_t const& config);
 static vistk::process_t create_null_output_port_info_process(vistk::config_t const& config);
@@ -195,6 +236,120 @@ test_step_before_init()
   EXPECT_EXCEPTION(vistk::uninitialized_exception,
                    process->step(),
                    "stepping before initialization");
+}
+
+void
+test_set_static_input_type()
+{
+  vistk::process_registry::type_t const proc_type = vistk::process_registry::type_t("multiplication");
+
+  vistk::process::port_t const port_name = vistk::process::port_t("factor1");
+  vistk::process::port_type_t const port_type = vistk::process::port_type_t("type");
+
+  vistk::process_t const process = create_process(proc_type);
+
+  EXPECT_EXCEPTION(vistk::static_type_reset_exception,
+                   process->set_input_port_type(port_name, port_type),
+                   "setting the type of a non-dependent input port");
+}
+
+void
+test_set_static_output_type()
+{
+  vistk::process_registry::type_t const proc_type = vistk::process_registry::type_t("multiplication");
+
+  vistk::process::port_t const port_name = vistk::process::port_t("product");
+  vistk::process::port_type_t const port_type = vistk::process::port_type_t("type");
+
+  vistk::process_t const process = create_process(proc_type);
+
+  EXPECT_EXCEPTION(vistk::static_type_reset_exception,
+                   process->set_output_port_type(port_name, port_type),
+                   "setting the type of a non-dependent output port");
+}
+
+void
+test_set_input_type_duplicate()
+{
+  vistk::process_registry::type_t const proc_type = vistk::process_registry::type_t("flow_dependent");
+
+  vistk::process::port_t const port_name = vistk::process::port_t("input");
+  vistk::process::port_type_t const port_type = vistk::process::port_type_t("type");
+
+  vistk::process_t const process = create_process(proc_type);
+
+  process->set_input_port_type(port_name, port_type);
+
+  vistk::process::port_info_t const port_info_before = process->input_port_info(port_name);
+
+  process->set_input_port_type(port_name, port_type);
+
+  vistk::process::port_info_t const port_info_duplicate = process->input_port_info(port_name);
+
+  // If nothing actually changes, info pointers should still be valid.
+  if (port_info_before != port_info_duplicate)
+  {
+    TEST_ERROR("Input port information changed without a data change");
+  }
+}
+
+void
+test_set_output_type_duplicate()
+{
+  vistk::process_registry::type_t const proc_type = vistk::process_registry::type_t("flow_dependent");
+
+  vistk::process::port_t const port_name = vistk::process::port_t("output");
+  vistk::process::port_type_t const port_type = vistk::process::port_type_t("type");
+
+  vistk::process_t const process = create_process(proc_type);
+
+  process->set_output_port_type(port_name, port_type);
+
+  vistk::process::port_info_t const port_info_before = process->output_port_info(port_name);
+
+  process->set_output_port_type(port_name, port_type);
+
+  vistk::process::port_info_t const port_info_duplicate = process->output_port_info(port_name);
+
+  // If nothing actually changes, info pointers should still be valid.
+  if (port_info_before != port_info_duplicate)
+  {
+    TEST_ERROR("Output port information changed without a data change");
+  }
+}
+
+void
+test_set_input_type_after_init()
+{
+  vistk::process_registry::type_t const proc_type = vistk::process_registry::type_t("flow_dependent");
+
+  vistk::process::port_t const port_name = vistk::process::port_t("input");
+  vistk::process::port_type_t const port_type = vistk::process::port_type_t("type");
+
+  vistk::process_t const process = create_process(proc_type);
+
+  process->init();
+
+  EXPECT_EXCEPTION(vistk::set_type_on_initialized_process_exception,
+                   process->set_input_port_type(port_name, port_type),
+                   "setting an input port type after initialization");
+}
+
+void
+test_set_output_type_after_init()
+{
+  vistk::process_registry::type_t const proc_type = vistk::process_registry::type_t("flow_dependent");
+
+  vistk::process::port_t const port_name = vistk::process::port_t("output");
+  vistk::process::port_type_t const port_type = vistk::process::port_type_t("type");
+
+  vistk::process_t const process = create_process(proc_type);
+
+  process->init();
+
+  EXPECT_EXCEPTION(vistk::set_type_on_initialized_process_exception,
+                   process->set_output_port_type(port_name, port_type),
+                   "setting an output port type after initialization");
 }
 
 void
@@ -282,6 +437,78 @@ test_set_tagged_flow_dependent_port_cascade()
   if (oinfo->type != port_type)
   {
     TEST_ERROR("Setting the input port type did not also set the output port info");
+  }
+}
+
+void
+test_add_input_port_after_type_pin()
+{
+  vistk::process_registry::type_t const proc_type = vistk::process_registry::type_t("collate");
+
+  vistk::process::port_t const color = vistk::process::port_t("color/");
+  vistk::process::port_t const res = vistk::process::port_t("res/");
+  vistk::process::port_t const coll = vistk::process::port_t("coll/");
+
+  vistk::process::port_t const tag = vistk::process::port_t("test");
+  vistk::process::port_t const group = vistk::process::port_t("/group");
+
+  vistk::process::port_type_t const port_type = vistk::process::port_type_t("type");
+
+  vistk::process_t const process = create_process(proc_type);
+  vistk::edge_t const edge = create_edge();
+
+  (void)process->input_port_info(color + tag);
+
+  EXPECT_EXCEPTION(vistk::no_such_port_exception,
+                   process->connect_input_port(coll + tag + group, edge),
+                   "making sure the input port does not exist");
+
+  if (!process->set_output_port_type(res + tag, port_type))
+  {
+    TEST_ERROR("Could not set the source port type");
+  }
+
+  vistk::process::port_info_t const info = process->input_port_info(coll + tag + group);
+
+  if (info->type != port_type)
+  {
+    TEST_ERROR("The port with an input tagged dependency port was not set to the new type automatically upon creation");
+  }
+}
+
+void
+test_add_output_port_after_type_pin()
+{
+  vistk::process_registry::type_t const proc_type = vistk::process_registry::type_t("distribute");
+
+  vistk::process::port_t const color = vistk::process::port_t("color/");
+  vistk::process::port_t const src = vistk::process::port_t("src/");
+  vistk::process::port_t const dist = vistk::process::port_t("dist/");
+
+  vistk::process::port_t const tag = vistk::process::port_t("test");
+  vistk::process::port_t const group = vistk::process::port_t("/group");
+
+  vistk::process::port_type_t const port_type = vistk::process::port_type_t("type");
+
+  vistk::process_t const process = create_process(proc_type);
+  vistk::edge_t const edge = create_edge();
+
+  (void)process->output_port_info(color + tag);
+
+  EXPECT_EXCEPTION(vistk::no_such_port_exception,
+                   process->connect_output_port(dist + tag + group, edge),
+                   "making sure the output port does not exist");
+
+  if (!process->set_input_port_type(src + tag, port_type))
+  {
+    TEST_ERROR("Could not set the source port type");
+  }
+
+  vistk::process::port_info_t const info = process->output_port_info(dist + tag + group);
+
+  if (info->type != port_type)
+  {
+    TEST_ERROR("The port with an output tagged dependency port was not set to the new type automatically upon creation");
   }
 }
 
@@ -412,6 +639,15 @@ create_process(vistk::process_registry::type_t const& type, vistk::process::name
   config->set_value(vistk::process::config_name, vistk::config::value_t(name));
 
   return reg->create_process(type, config);
+}
+
+vistk::edge_t
+create_edge()
+{
+  vistk::config_t const config = vistk::config::empty_config();
+  vistk::edge_t const edge = boost::make_shared<vistk::edge>(config);
+
+  return edge;
 }
 
 class null_config_process
