4760cd1b104d7cdab65f8ed484f365c85cddbdfe
Ben Boeckel <ben.boeckel@kitware.com>
2012-04-24 16:42:00 -0400

    Separate floating-point pixtypes from integers

--- src/processes/helpers/image/macros.h
+++ src/processes/helpers/image/macros.h
@@ -21,9 +21,12 @@
     return &function<type>;                  \
   }
 
-#define SPECIFY_FUNCTION(function)         \
+#define SPECIFY_INT_FUNCTION(function)     \
   TYPE_CHECK(bool, bool, function)         \
-  else TYPE_CHECK(uint8_t, byte, function) \
+  else TYPE_CHECK(uint8_t, byte, function)
+
+#define SPECIFY_FUNCTION(function)         \
+  SPECIFY_INT_FUNCTION(function)           \
   else TYPE_CHECK(float, float, function)  \
   else TYPE_CHECK(double, double, function)
 
