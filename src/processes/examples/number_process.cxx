10c945bbe184652bde9a87a8efd30f92eabdb44c
Ben Boeckel <ben.boeckel@kitware.com>
2012-04-26 14:02:20 -0400

    Remove color ports from example processes

--- src/processes/examples/number_process.cxx
+++ src/processes/examples/number_process.cxx
@@ -9,7 +9,6 @@
 #include <vistk/pipeline/config.h>
 #include <vistk/pipeline/datum.h>
 #include <vistk/pipeline/process_exception.h>
-#include <vistk/pipeline/stamp.h>
 
 #include <boost/make_shared.hpp>
 
@@ -37,16 +36,11 @@ class number_process::priv
 
     number_t current;
 
-    bool has_color;
-
-    stamp_t output_stamp;
-
     static config::key_t const config_start;
     static config::key_t const config_end;
     static config::value_t const default_start;
     static config::value_t const default_end;
     static port_t const port_output;
-    static port_t const port_color;
 };
 
 config::key_t const number_process::priv::config_start = config::key_t("start");
@@ -54,7 +48,6 @@ config::key_t const number_process::priv::config_end = config::key_t("end");
 config::value_t const number_process::priv::default_start = config::value_t("0");
 config::value_t const number_process::priv::default_end = config::value_t("100");
 process::port_t const number_process::priv::port_output = process::port_t("number");
-process::port_t const number_process::priv::port_color = process::port_t("color");
 
 number_process
 ::number_process(config_t const& config)
@@ -71,10 +64,6 @@ number_process
 
   required.insert(flag_required);
 
-  declare_input_port(priv::port_color, boost::make_shared<port_info>(
-    type_none,
-    port_flags_t(),
-    port_description_t("If connected, uses the stamp's color for the output.")));
   declare_output_port(priv::port_output, boost::make_shared<port_info>(
     "integer",
     required,
@@ -106,13 +95,6 @@ number_process
     throw invalid_configuration_exception(name(), reason);
   }
 
-  if (input_port_edge(priv::port_color))
-  {
-    d->has_color = true;
-  }
-
-  d->output_stamp = heartbeat_stamp();
-
   process::_init();
 }
 
@@ -134,41 +116,7 @@ number_process
     ++d->current;
   }
 
-  d->output_stamp = stamp::incremented_stamp(d->output_stamp);
-
-  if (d->has_color)
-  {
-    edge_datum_t const color_dat = grab_from_port(priv::port_color);
-
-    switch (color_dat.get<0>()->type())
-    {
-      case datum::complete:
-        mark_process_as_complete();
-        dat = datum::complete_datum();
-      case datum::data:
-      case datum::empty:
-        break;
-      case datum::error:
-      {
-        static datum::error_t const err_string = datum::error_t("Error on the color edge.");
-
-        dat = datum::error_datum(err_string);
-      }
-      case datum::invalid:
-      default:
-      {
-        static datum::error_t const err_string = datum::error_t("Unrecognized datum type on the color edge.");
-
-        dat = datum::error_datum(err_string);
-      }
-    }
-
-    d->output_stamp = stamp::recolored_stamp(d->output_stamp, color_dat.get<1>());
-  }
-
-  edge_datum_t const edat = edge_datum_t(dat, d->output_stamp);
-
-  push_to_port(priv::port_output, edat);
+  push_datum_to_port(priv::port_output, dat);
 
   process::_step();
 }
@@ -178,7 +126,6 @@ number_process::priv
   : start(s)
   , end(e)
   , current(s)
-  , has_color(false)
 {
 }
 
