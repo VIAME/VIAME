8378306983bf9092182eb6e1d925ab72314cf1ef
Ben Boeckel <ben.boeckel@kitware.com>
2012-05-26 00:46:25 -0400

    Add functions to configure_file at build time

---
+++ cmake/configure.cmake
@@ -0,0 +1,79 @@
+# Configure functions for the vistk project
+# The following functions are defined:
+#   vistk_configure_file
+#   vistk_configure_pkgconfig
+# Their syntax is:
+#   vistk_configure_file(name source dest [variable ...])
+#     The first argument is the name of the file being configured. The next two
+#     parameters are the source and destination paths of the file to be
+#     configured. Any variables that need to be replaced in the file should be
+#     passed as extra arguments. The file will be added to the list of files to
+#     be cleaned.
+#   vistk_configure_pkgconfig(module)
+#     A convenience function for creating pkgconfig files.
+
+add_custom_target(configure ALL)
+
+function (vistk_configure_file name source dest)
+  set(configure_script
+    "${CMAKE_CURRENT_BINARY_DIR}/configure.${name}.cmake")
+
+  file(WRITE "${configure_script}"
+    "# Configure script for \"${source}\" -> \"${dest}\"\n")
+
+  foreach (arg ${ARGN})
+    file(APPEND "${configure_script}"
+      "set(${arg} \"${${arg}}\")\n")
+  endforeach (arg)
+
+  file(APPEND "${configure_script}" "
+configure_file(
+  \"${source}\"
+  \"${dest}\"
+  @ONLY)
+")
+
+  add_custom_command(
+    OUTPUT  "${dest}"
+    COMMAND "${CMAKE_COMMAND}"
+            -P "${configure_script}"
+    MAIN_DEPENDENCY
+            "${source}"
+    DEPENDS "${source}"
+            "${configure_script}"
+    WORKING_DIRECTORY
+            "${CMAKE_CURRENT_BINARY_DIR}"
+    COMMENT "Configuring ${name} file \"${source}\" -> \"${dest}\"")
+  add_custom_target(configure-${name}
+    DEPENDS
+      "${dest}")
+  add_dependencies(configure
+    configure-${name})
+
+  set(clean_files
+    "${dest}"
+    "${configure_script}")
+
+  set_directory_properties(
+    PROPERTIES
+      ADDITIONAL_MAKE_CLEAN_FILES "${clean_files}")
+endfunction (vistk_configure_file)
+
+function (vistk_configure_pkgconfig module)
+  if (UNIX)
+    set(pkgconfig_file "${vistk_binary_dir}/${module}.pc")
+
+    vistk_configure_file(vistk-${module}.pc
+      "${CMAKE_CURRENT_SOURCE_DIR}/${module}.pc.in"
+      "${pkgconfig_file}"
+      vistk_version
+      CMAKE_INSTALL_PREFIX
+      LIB_SUFFIX
+      ${ARGN})
+
+    install(
+      FILES       "${pkgconfig_file}"
+      DESTINATION "lib${LIB_SUFFIX}/pkgconfig"
+      COMPONENT   development)
+  endif (UNIX)
+endfunction (vistk_configure_pkgconfig module)
