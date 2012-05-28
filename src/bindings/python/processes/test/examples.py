6992b53943beb4c9495a7e68286e7e539189e4d7
Ben Boeckel <ben.boeckel@kitware.com>
2012-05-28 11:56:40 -0400

    Update modules for straight.plugin compatability

--- src/bindings/python/processes/test/examples.py
+++ src/bindings/python/processes/test/examples.py
@@ -10,18 +10,3 @@ from vistk.pipeline import process
 class TestPythonProcess(process.PythonProcess):
     def __init__(self, conf):
         process.PythonProcess.__init__(self, conf)
-
-
-def __vistk_register__():
-    from vistk.pipeline import process_registry
-
-    module_name = 'python:test.examples'
-
-    reg = process_registry.ProcessRegistry.self()
-
-    if reg.is_module_loaded(module_name):
-        return
-
-    reg.register_process('test_python_process', 'A test Python process.', TestPythonProcess)
-
-    reg.mark_module_as_loaded(module_name)
--- src/bindings/python/schedules/examples/pythread_per_process_schedule.py
+++ src/bindings/python/schedules/examples/pythread_per_process_schedule.py
@@ -89,18 +89,3 @@ class PyThreadPerProcessSchedule(schedule.PythonSchedule):
 
     def _make_monitor_edge_config(self):
         self.edge_conf = config.empty_config()
-
-
-def __vistk_register__():
-    from vistk.pipeline import schedule_registry
-
-    module_name = 'python:schedules.examples'
-
-    reg = schedule_registry.ScheduleRegistry.self()
-
-    if reg.is_module_loaded(module_name):
-        return
-
-    reg.register_schedule('pythread_per_process', 'Runs each procss in its own Python thread', PyThreadPerProcessSchedule)
-
-    reg.mark_module_as_loaded(module_name)
