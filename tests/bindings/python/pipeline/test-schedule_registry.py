8ac107244d7bea47b16913d06cfa4b00f1221521
Ben Boeckel <ben.boeckel@kitware.com>
2012-05-29 10:34:18 -0400

    Fix copyright years for files edited in 2012

--- tests/bindings/python/pipeline/test-schedule_registry.py
+++ tests/bindings/python/pipeline/test-schedule_registry.py
@@ -1,6 +1,6 @@
 #!@PYTHON_EXECUTABLE@
 #ckwg +5
-# Copyright 2011 by Kitware, Inc. All Rights Reserved. Please refer to
+# Copyright 2011-2012 by Kitware, Inc. All Rights Reserved. Please refer to
 # KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
 # Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
 
