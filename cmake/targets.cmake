545bcbbb4f567ec9ce263b53273ccbccafc4afa1
Ben Boeckel <ben.boeckel@kitware.com>
2012-06-19 12:44:37 -0400

    Flip around where the configuration directory is

--- cmake/targets.cmake
+++ cmake/targets.cmake
@@ -64,7 +64,7 @@ function (vistk_add_library name)
     set(subdir ${library_subdir})
 
     if (WIN32)
-      set(subdir "/${config}${subdir}")
+      set(subdir "${subdir}/${config}")
     endif (WIN32)
 
     string(TOUPPER "${config}" upper_config)
