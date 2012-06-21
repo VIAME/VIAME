12ce5c8da51465d22fbbee3bcbc9de81cf782df4
Ben Boeckel <ben.boeckel@kitware.com>
2012-06-21 19:25:31 -0400

    Catch exceptions in processopedia in detail mode

--- src/tools/processopedia.cxx
+++ src/tools/processopedia.cxx
@@ -83,28 +83,17 @@ main(int argc, char* argv[])
 
   BOOST_FOREACH (vistk::process::type_t const& proc_type, types)
   {
-    if (!vm.count("detail"))
-    {
-      try
-      {
-        std::cout << proc_type << ": " << reg->description(proc_type) << std::endl;
-      }
-      catch (vistk::no_such_process_type_exception const& e)
-      {
-        std::cerr << "Error: " << e.what() << std::endl;
-      }
-
-      continue;
-    }
-
-    std::cout << "Process type: " << proc_type << std::endl;
-    std::cout << "  Description: " << reg->description(proc_type) << std::endl;
-
-    vistk::process_t proc_m;
-
     try
     {
-      proc_m = reg->create_process(proc_type, vistk::process::name_t());
+      if (!vm.count("detail"))
+      {
+        std::cout << proc_type << ": " << reg->description(proc_type) << std::endl;
+
+        continue;
+      }
+
+      std::cout << "Process type: " << proc_type << std::endl;
+      std::cout << "  Description: " << reg->description(proc_type) << std::endl;
     }
     catch (vistk::no_such_process_type_exception const& e)
     {
@@ -113,7 +102,7 @@ main(int argc, char* argv[])
       continue;
     }
 
-    vistk::process_t const proc = proc_m;
+    vistk::process_t const proc = reg->create_process(proc_type, vistk::process::name_t());
 
     vistk::process::constraints_t const constraints = proc->constraints();
     std::string const constraints_str = boost::join(constraints, ", ");
