b01afdcf5d27e7705f0b184cc2bcbd07f01049d3
Ben Boeckel <ben.boeckel@kitware.com>
2012-07-17 16:50:56 -0400

    Clean up the mutex locking in the scheduler class
    This is in preperation for the pause/resume functionality.

--- src/vistk/pipeline/scheduler.cxx
+++ src/vistk/pipeline/scheduler.cxx
@@ -10,7 +10,8 @@
 #include "pipeline.h"
 
 #include <boost/thread/locks.hpp>
-#include <boost/thread/mutex.hpp>
+#include <boost/thread/reverse_lock.hpp>
+#include <boost/thread/shared_mutex.hpp>
 
 /**
  * \file scheduler.cxx
@@ -24,12 +25,22 @@ namespace vistk
 class scheduler::priv
 {
   public:
-    priv(pipeline_t const& pipeline);
+    priv(scheduler* sched, pipeline_t const& pipe);
     ~priv();
 
+    void stop();
+
+    scheduler* const q;
     pipeline_t const p;
     bool running;
-    boost::mutex mut;
+
+    typedef boost::shared_mutex mutex_t;
+    typedef boost::upgrade_lock<mutex_t> upgrade_lock_t;
+    typedef boost::unique_lock<mutex_t> unique_lock_t;
+    typedef boost::reverse_lock<unique_lock_t> reverse_unique_lock_t;
+    typedef boost::upgrade_to_unique_lock<mutex_t> upgrade_to_unique_lock_t;
+
+    mutex_t mut;
 };
 
 scheduler
@@ -39,7 +50,6 @@ scheduler
 
 scheduler
 ::scheduler(pipeline_t const& pipe, config_t const& config)
-  : d(new priv(pipe))
 {
   if (!config)
   {
@@ -50,61 +60,76 @@ scheduler
   {
     throw null_scheduler_pipeline_exception();
   }
+
+  d.reset(new priv(this, pipe));
 }
 
 void
 scheduler
 ::start()
 {
+  priv::upgrade_lock_t lock(d->mut);
+
+  if (d->running)
   {
-    boost::mutex::scoped_lock const lock(d->mut);
-
-    (void)lock;
-
-    d->p->start();
-    d->running = true;
+    /// \todo Throw exception.
   }
 
+  priv::upgrade_to_unique_lock_t const write_lock(lock);
+
+  (void)write_lock;
+
+  d->p->start();
+
   _start();
+
+  d->running = true;
 }
 
 void
 scheduler
 ::wait()
 {
-  {
-    boost::mutex::scoped_lock const lock(d->mut);
-
-    (void)lock;
-
-    if (!d->running)
-    {
-      /// \todo Throw an exception.
-    }
-  }
-
-  _wait();
-
-  stop();
-}
-
-void
-scheduler
-::stop()
-{
-  boost::mutex::scoped_lock const lock(d->mut);
-
-  (void)lock;
+  priv::unique_lock_t lock(d->mut);
 
   if (!d->running)
   {
     /// \todo Throw an exception.
   }
 
-  _stop();
+  // Allow many threads to wait on the scheduler.
+  {
+    priv::reverse_unique_lock_t const rev_lock(lock);
 
-  d->p->stop();
-  d->running = false;
+    (void)rev_lock;
+
+    _wait();
+  }
+
+  // After each thread, only one should call stop. Let threads through
+  // one-at-a-time to see if the pipeline needs to be stopped yet.
+  if (d->running)
+  {
+    d->stop();
+  }
+}
+
+void
+scheduler
+::stop()
+{
+  priv::upgrade_lock_t lock(d->mut);
+
+  if (!d->running)
+  {
+    /// \todo Throw an exception.
+  }
+
+  priv::upgrade_to_unique_lock_t const write_lock(lock);
+
+  (void)write_lock;
+
+  d->stop();
 }
 
 pipeline_t
@@ -115,8 +140,9 @@ scheduler
 }
 
 scheduler::priv
-::priv(pipeline_t const& pipe)
-  : p(pipe)
+::priv(scheduler* sched, pipeline_t const& pipe)
+  : q(sched)
+  , p(pipe)
   , running(false)
 {
 }
@@ -124,6 +150,17 @@ scheduler::priv
 scheduler::priv
 ::~priv()
 {
+}
+
+void
+scheduler::priv
+::stop()
+{
+  // Tell the subclass that we want to stop.
+  q->_stop();
+
+  p->stop();
+  running = false;
 }
 
 }
