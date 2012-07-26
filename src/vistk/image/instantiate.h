85639452a4461369702b895700d802bb325e6f3e
Ben Boeckel <ben.boeckel@kitware.com>
2012-07-26 12:41:55 -0400

    Document image instantiation macros

--- src/vistk/image/instantiate.h
+++ src/vistk/image/instantiate.h
@@ -15,14 +15,29 @@
  * \brief Types for instantiation of image library templates.
  */
 
+/**
+ * \brief Integer pixel types for image algorithms.
+ *
+ * \param cls The class to instantiate.
+ */
 #define VISTK_IMAGE_INSTANTIATE_INT(cls) \
   template class cls<bool>;              \
   template class cls<uint8_t>
 
+/**
+ * \brief Floating point pixel types for image algorithms.
+ *
+ * \param cls The class to instantiate.
+ */
 #define VISTK_IMAGE_INSTANTIATE_FLOAT(cls) \
   template class cls<float>;               \
   template class cls<double>
 
+/**
+ * \brief All pixel types for image algorithms.
+ *
+ * \param cls The class to instantiate.
+ */
 #define VISTK_IMAGE_INSTANTIATE(cls) \
   VISTK_IMAGE_INSTANTIATE_INT(cls);  \
   VISTK_IMAGE_INSTANTIATE_FLOAT(cls)
