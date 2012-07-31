7b9ed59c98fba9a1fe13242c113a80322e2a21a7
Ben Boeckel <ben.boeckel@kitware.com>
2012-07-31 18:21:42 -0400

    Factor out option parsing

--- src/tools/helpers/tool_usage.cxx
+++ src/tools/helpers/tool_usage.cxx
@@ -6,6 +6,8 @@
 
 #include "tool_usage.h"
 
+#include <boost/program_options/parsers.hpp>
+
 #include <iostream>
 
 #include <cstdlib>
@@ -16,4 +18,25 @@ tool_usage(int ret, boost::program_options::options_description const& options)
   std::cerr << options << std::endl;
 
   exit(ret);
+}
+
+boost::program_options::variables_map
+tool_parse(int argc, char* argv[], boost::program_options::options_description const& desc)
+{
+  boost::program_options::variables_map vm;
+
+  try
+  {
+    boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
+  }
+  catch (boost::program_options::unknown_option const& e)
+  {
+    std::cerr << "Error: unknown option " << e.get_option_name() << std::endl;
+
+    tool_usage(EXIT_FAILURE, desc);
+  }
+
+  boost::program_options::notify(vm);
+
+  return vm;
 }
--- src/tools/helpers/tool_usage.h
+++ src/tools/helpers/tool_usage.h
@@ -7,5 +7,8 @@
 #include <vistk/config.h>
 
 #include <boost/program_options/options_description.hpp>
+#include <boost/program_options/variables_map.hpp>
 
 void VISTK_NO_RETURN tool_usage(int ret, boost::program_options::options_description const& options);
+
+boost::program_options::variables_map tool_parse(int argc, char* argv[], boost::program_options::options_description const& desc);
