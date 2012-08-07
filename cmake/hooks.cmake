85c67e1656a2fa2381546a4cf2ab0b39cdeb403e
Ben Boeckel <ben.boeckel@kitware.com>
2012-08-07 11:41:14 -0400

    Add git hook installation targets

---
+++ cmake/hooks.cmake
@@ -0,0 +1,32 @@
+set(hooks_directory
+  "${vistk_source_dir}/.git/hooks")
+
+add_custom_command(
+  OUTPUT  "${hooks_directory}/.git"
+  COMMAND "${GIT_EXECUTABLE}"
+          init
+  COMMAND "${GIT_EXECUTABLE}"
+          remote add origin
+          ../..
+  WORKING_DIRECTORY
+          "${hooks_directory}"
+  COMMENT "Initializing the hooks repository")
+add_custom_target(hooks-init
+  DEPENDS
+    "${hooks_directory}/.git")
+
+add_custom_target(hooks-update ALL
+  DEPENDS
+    hooks-init)
+add_custom_command(
+  TARGET  hooks-update
+  COMMAND "${GIT_EXECUTABLE}"
+          fetch
+          origin
+          hooks
+  COMMAND "${GIT_EXECUTABLE}"
+          merge
+          FETCH_HEAD
+  WORKING_DIRECTORY
+          "${hooks_directory}"
+  COMMENT "Updating hooks repository")
