4338164f3d7d889092125833a0b46a776d2240a7
Ben Boeckel <ben.boeckel@kitware.com>
2012-08-24 16:43:05 -0400

    Add Python bindings for cluster blocks

--- src/bindings/python/vistk/pipeline_util/load.cxx
+++ src/bindings/python/vistk/pipeline_util/load.cxx
@@ -47,6 +47,20 @@ static object pipe_block_connect(vistk::pipe_block const& block);
 static void pipe_block_connect_set(vistk::pipe_block& block, vistk::connect_pipe_block const& connect);
 static object pipe_block_group(vistk::pipe_block const& block);
 static void pipe_block_group_set(vistk::pipe_block& block, vistk::group_pipe_block const& group);
+static object cluster_subblock_config(vistk::cluster_subblock_t const& subblock);
+static void cluster_subblock_config_set(vistk::cluster_subblock_t& subblock, vistk::cluster_config_t const& config);
+static object cluster_subblock_input(vistk::cluster_subblock_t const& subblock);
+static void cluster_subblock_input_set(vistk::cluster_subblock_t& subblock, vistk::cluster_input_t const& input);
+static object cluster_subblock_output(vistk::cluster_subblock_t const& subblock);
+static void cluster_subblock_output_set(vistk::cluster_subblock_t& subblock, vistk::cluster_output_t const& output);
+static object cluster_block_config(vistk::cluster_block const& block);
+static void cluster_block_config_set(vistk::cluster_block& block, vistk::config_pipe_block const& config);
+static object cluster_block_process(vistk::cluster_block const& block);
+static void cluster_block_process_set(vistk::cluster_block& block, vistk::process_pipe_block const& process);
+static object cluster_block_connect(vistk::cluster_block const& block);
+static void cluster_block_connect_set(vistk::cluster_block& block, vistk::connect_pipe_block const& connect);
+static object cluster_block_cluster(vistk::cluster_block const& block);
+static void cluster_block_cluster_set(vistk::cluster_block& block, vistk::cluster_pipe_block const& cluster);
 static vistk::pipe_blocks load_pipe_file(std::string const& path);
 static vistk::pipe_blocks load_pipe(object const& stream, std::string const& inc_root);
 
@@ -146,6 +160,53 @@ BOOST_PYTHON_MODULE(load)
     /// \todo Need operator == on pipe_block.
     //.def(vector_indexing_suite<vistk::pipe_blocks>())
   ;
+  class_<vistk::cluster_config_t>("ClusterConfig"
+    , "A configuration value for a cluster.")
+    .def_readwrite("description", &vistk::cluster_config_t::description)
+    .def_readwrite("config_value", &vistk::cluster_config_t::config_value)
+  ;
+  class_<vistk::cluster_input_t>("ClusterInput"
+    , "An input mapping for a cluster.")
+    .def_readwrite("description", &vistk::cluster_input_t::description)
+    .def_readwrite("from_", &vistk::cluster_input_t::from)
+    .def_readwrite("to", &vistk::cluster_input_t::to)
+  ;
+  class_<vistk::cluster_output_t>("ClusterOutput"
+    , "An output mapping for a cluster.")
+    .def_readwrite("description", &vistk::cluster_output_t::description)
+    .def_readwrite("from_", &vistk::cluster_output_t::from)
+    .def_readwrite("to", &vistk::cluster_output_t::to)
+  ;
+  class_<vistk::cluster_subblock_t>("ClusterSubblock"
+    , "A subblock within a cluster.")
+    .add_property("config", &cluster_subblock_config, &cluster_subblock_config_set)
+    .add_property("input", &cluster_subblock_input, &cluster_subblock_input_set)
+    .add_property("output", &cluster_subblock_output, &cluster_subblock_output_set)
+  ;
+  class_<vistk::cluster_subblocks_t>("ClusterSubblocks"
+    , "A collection of cluster subblocks.")
+    /// \todo Need operator == on cluster_subblock_t.
+    //.def(vector_indexing_suite<vistk::cluster_subblocks_t>())
+  ;
+  class_<vistk::cluster_pipe_block>("ClusterBlock"
+    , "A block which declares a cluster within the pipeline.")
+    .def_readwrite("name", &vistk::cluster_pipe_block::name)
+    .def_readwrite("description", &vistk::cluster_pipe_block::description)
+    .def_readwrite("type", &vistk::cluster_pipe_block::type)
+    .def_readwrite("subblocks", &vistk::cluster_pipe_block::subblocks)
+  ;
+  class_<vistk::cluster_block>("ClusterDefineBlock"
+    , "A block in a pipeline declaration file.")
+    .add_property("config", &cluster_block_config, &cluster_block_config_set)
+    .add_property("process", &cluster_block_process, &cluster_block_process_set)
+    .add_property("connect", &cluster_block_connect, &cluster_block_connect_set)
+    .add_property("cluster", &cluster_block_cluster, &cluster_block_cluster_set)
+  ;
+  class_<vistk::cluster_blocks>("ClusterDefineBlocks"
+    , "A collection of cluster blocks.")
+    /// \todo Need operator == on cluster_block.
+    //.def(vector_indexing_suite<vistk::cluster_blocks>())
+  ;
 
   def("load_pipe_file", &load_pipe_file
     , (arg("path"))
@@ -221,6 +282,7 @@ class pipe_block_visitor
       BLOCK_CONFIG,
       BLOCK_PROCESS,
       BLOCK_CONNECT,
+      BLOCK_CLUSTER,
       BLOCK_GROUP
     } block_t;
 
@@ -233,6 +295,7 @@ class pipe_block_visitor
     object operator () (vistk::process_pipe_block const& process_block) const;
     object operator () (vistk::connect_pipe_block const& connect_block) const;
     object operator () (vistk::group_pipe_block const& group_block) const;
+    object operator () (vistk::cluster_pipe_block const& cluster_block) const;
 };
 
 object
@@ -283,6 +346,111 @@ pipe_block_group_set(vistk::pipe_block& block, vistk::group_pipe_block const& gr
   block = group;
 }
 
+class cluster_subblock_visitor
+  : public boost::static_visitor<object>
+{
+  public:
+    typedef enum
+    {
+      BLOCK_CONFIG,
+      BLOCK_INPUT,
+      BLOCK_OUTPUT
+    } block_t;
+
+    cluster_subblock_visitor(block_t type);
+    ~cluster_subblock_visitor();
+
+    block_t const block_type;
+
+    object operator () (vistk::cluster_config_t const& config) const;
+    object operator () (vistk::cluster_input_t const& input) const;
+    object operator () (vistk::cluster_output_t const& output) const;
+};
+
+object
+cluster_subblock_config(vistk::cluster_subblock_t const& subblock)
+{
+  return boost::apply_visitor(cluster_subblock_visitor(cluster_subblock_visitor::BLOCK_CONFIG), subblock);
+}
+
+void
+cluster_subblock_config_set(vistk::cluster_subblock_t& subblock, vistk::cluster_config_t const& config)
+{
+  subblock = config;
+}
+
+object
+cluster_subblock_input(vistk::cluster_subblock_t const& subblock)
+{
+  return boost::apply_visitor(cluster_subblock_visitor(cluster_subblock_visitor::BLOCK_INPUT), subblock);
+}
+
+void
+cluster_subblock_input_set(vistk::cluster_subblock_t& subblock, vistk::cluster_input_t const& input)
+{
+  subblock = input;
+}
+
+object
+cluster_subblock_output(vistk::cluster_subblock_t const& subblock)
+{
+  return boost::apply_visitor(cluster_subblock_visitor(cluster_subblock_visitor::BLOCK_OUTPUT), subblock);
+}
+
+void
+cluster_subblock_output_set(vistk::cluster_subblock_t& subblock, vistk::cluster_output_t const& output)
+{
+  subblock = output;
+}
+
+object
+cluster_block_config(vistk::cluster_block const& block)
+{
+  return boost::apply_visitor(pipe_block_visitor(pipe_block_visitor::BLOCK_CONFIG), block);
+}
+
+void
+cluster_block_config_set(vistk::cluster_block& block, vistk::config_pipe_block const& config)
+{
+  block = config;
+}
+
+object
+cluster_block_process(vistk::cluster_block const& block)
+{
+  return boost::apply_visitor(pipe_block_visitor(pipe_block_visitor::BLOCK_PROCESS), block);
+}
+
+void
+cluster_block_process_set(vistk::cluster_block& block, vistk::process_pipe_block const& process)
+{
+  block = process;
+}
+
+object
+cluster_block_connect(vistk::cluster_block const& block)
+{
+  return boost::apply_visitor(pipe_block_visitor(pipe_block_visitor::BLOCK_CONNECT), block);
+}
+
+void
+cluster_block_connect_set(vistk::cluster_block& block, vistk::connect_pipe_block const& connect)
+{
+  block = connect;
+}
+
+object
+cluster_block_cluster(vistk::cluster_block const& block)
+{
+  return boost::apply_visitor(pipe_block_visitor(pipe_block_visitor::BLOCK_CLUSTER), block);
+}
+
+void
+cluster_block_cluster_set(vistk::cluster_block& block, vistk::cluster_pipe_block const& cluster)
+{
+  block = cluster;
+}
+
 vistk::pipe_blocks
 load_pipe_file(std::string const& path)
 {
@@ -295,6 +463,65 @@ load_pipe(object const& stream, std::string const& inc_root)
   pyistream istr(stream);
 
   return vistk::load_pipe_blocks(istr, vistk::path_t(inc_root));
+}
+
+cluster_subblock_visitor
+::cluster_subblock_visitor(block_t type)
+  : block_type(type)
+{
+}
+
+cluster_subblock_visitor
+::~cluster_subblock_visitor()
+{
+}
+
+object
+cluster_subblock_visitor
+::operator () (vistk::cluster_config_t const& config) const
+{
+  vistk::python::python_gil const gil;
+
+  (void)gil;
+
+  if (block_type == BLOCK_CONFIG)
+  {
+    return object(config);
+  }
+
+  return object();
+}
+
+object
+cluster_subblock_visitor
+::operator () (vistk::cluster_input_t const& input) const
+{
+  vistk::python::python_gil const gil;
+
+  (void)gil;
+
+  if (block_type == BLOCK_INPUT)
+  {
+    return object(input);
+  }
+
+  return object();
+}
+
+object
+cluster_subblock_visitor
+::operator () (vistk::cluster_output_t const& output) const
+{
+  vistk::python::python_gil const gil;
+
+  (void)gil;
+
+  if (block_type == BLOCK_OUTPUT)
+  {
+    return object(output);
+  }
+
+  return object();
 }
 
 group_subblock_visitor
@@ -434,6 +661,24 @@ pipe_block_visitor
   if (block_type == BLOCK_GROUP)
   {
     obj = object(group_block);
+  }
+
+  return obj;
+}
+
+object
+pipe_block_visitor
+::operator () (vistk::cluster_pipe_block const& cluster_block) const
+{
+  vistk::python::python_gil const gil;
+
+  (void)gil;
+
+  object obj;
+
+  if (block_type == BLOCK_CLUSTER)
+  {
+    obj = object(cluster_block);
   }
 
   return obj;
