0c4f272a2848bcacd13fab9cbc3803dca4dbf9cb
Ben Boeckel <ben.boeckel@kitware.com>
2012-09-13 12:26:27 -0400

    Use lowercase for internal variable names

--- cmake/snippets/vxl_tests.cmake
+++ cmake/snippets/vxl_tests.cmake
@@ -27,13 +27,13 @@ main()
 }
 ")
 
-    try_compile(VXL_HAS_FLOAT_DECLS_COMPILE
+    try_compile(vxl_has_float_decls_compile
       "${cmakefiles_dir}/vxl_has_float_decls"
       "${vxl_has_float_decls_path}"
       CMAKE_FLAGS
         "-DINCLUDE_DIRECTORIES=${VXL_VCL_INCLUDE_DIR}")
 
-    if (NOT VXL_HAS_FLOAT_DECLS_COMPILE)
+    if (NOT vxl_has_float_decls_compile)
       message(WARNING "VXL was compiled such that float declarations are in "
         "header files, but the `-pedantic` flag does not allow this, please "
         "continue at your own risk.")
