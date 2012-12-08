419d9462ac6f4009198c55742031487c010608f5
Ben Boeckel <ben.boeckel@kitware.com>
2012-12-07 19:40:11 -0500

    Use new macros in basic tests

--- tests/vistk/test/test_test.cxx
+++ tests/vistk/test/test_test.cxx
@@ -1,5 +1,5 @@
 /*ckwg +5
- * Copyright 2012 by Kitware, Inc. All Rights Reserved. Please refer to
+ * Copyright 2012-2013 by Kitware, Inc. All Rights Reserved. Please refer to
  * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
  * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
  */
@@ -8,87 +8,101 @@
 
 #include <vistk/pipeline/utils.h>
 
-#include <stdexcept>
 #include <iostream>
-#include <string>
+#include <stdexcept>
 
-#include <cstdlib>
-#include <cstring>
+#define TEST_ARGS ()
+
+DECLARE_TEST(return_code);
+DECLARE_TEST(error_string);
+DECLARE_TEST(error_string_mid);
+DECLARE_TEST(error_string_stdout);
+DECLARE_TEST(error_string_second_line);
+DECLARE_TEST(expected_exception);
+DECLARE_TEST(unexpected_exception);
+DECLARE_TEST(environment);
 
 int
 main(int argc, char* argv[])
 {
-  if (argc != 2)
-  {
-    TEST_ERROR("Expected one argument");
+  CHECK_ARGS(1);
 
-    return EXIT_FAILURE;
-  }
+  testname_t const testname = argv[1];
 
-  std::string const test_name = argv[1];
+  DECLARE_TEST_MAP(tests);
 
-  if (test_name == "return_code")
-  {
-    return EXIT_FAILURE;
-  }
-  else if (test_name == "error_string")
-  {
-    TEST_ERROR("an error");
-  }
-  else if (test_name == "error_string_mid")
-  {
-    std::cerr << "Test";
-    TEST_ERROR("an error");
-  }
-  else if (test_name == "error_string_stdout")
-  {
-    std::cout << "Error: an error" << std::endl;
-  }
-  else if (test_name == "error_string_second_line")
-  {
-    std::cerr << "Not an error" << std::endl;
-    TEST_ERROR("an error");
-  }
-  else if (test_name == "expected_exception")
-  {
-    EXPECT_EXCEPTION(std::logic_error,
-                     throw std::logic_error("reason"),
-                     "when throwing an exception");
-  }
-  else if (test_name == "unexpected_exception")
-  {
-    EXPECT_EXCEPTION(std::runtime_error,
-                     throw std::logic_error("reason"),
-                     "when throwing an unexpected exception");
-  }
-  else if (test_name == "environment")
-  {
-    vistk::envvar_name_t const envvar = "TEST_ENVVAR";
+  ADD_TEST(tests, return_code);
+  ADD_TEST(tests, error_string);
+  ADD_TEST(tests, error_string_mid);
+  ADD_TEST(tests, error_string_stdout);
+  ADD_TEST(tests, error_string_second_line);
+  ADD_TEST(tests, expected_exception);
+  ADD_TEST(tests, unexpected_exception);
+  ADD_TEST(tests, environment);
 
-    vistk::envvar_value_t const envvalue = vistk::get_envvar(envvar);
+  RUN_TEST(tests, testname);
+}
 
-    if (!envvalue)
-    {
-      TEST_ERROR("failed to get environment from CTest");
-    }
-    else
-    {
-      vistk::envvar_value_t const expected = vistk::envvar_value_t("test_value");
+IMPLEMENT_TEST(return_code)
+{
+  exit(EXIT_FAILURE);
+}
 
-      if (*envvalue != *expected)
-      {
-        TEST_ERROR("Did not get expected value: "
-                   "Expected: " << expected << " "
-                   "Received: " << envvalue);
-      }
-    }
+IMPLEMENT_TEST(error_string)
+{
+  TEST_ERROR("an error");
+}
+
+IMPLEMENT_TEST(error_string_mid)
+{
+  std::cerr << "Test";
+  TEST_ERROR("an error");
+}
+
+IMPLEMENT_TEST(error_string_stdout)
+{
+  std::cout << "Error: an error" << std::endl;
+}
+
+IMPLEMENT_TEST(error_string_second_line)
+{
+  std::cerr << "Not an error" << std::endl;
+  TEST_ERROR("an error");
+}
+
+IMPLEMENT_TEST(expected_exception)
+{
+  EXPECT_EXCEPTION(std::logic_error,
+                    throw std::logic_error("reason"),
+                    "when throwing an exception");
+}
+
+IMPLEMENT_TEST(unexpected_exception)
+{
+  EXPECT_EXCEPTION(std::runtime_error,
+                    throw std::logic_error("reason"),
+                    "when throwing an unexpected exception");
+}
+
+IMPLEMENT_TEST(environment)
+{
+  vistk::envvar_name_t const envvar = "TEST_ENVVAR";
+
+  vistk::envvar_value_t const envvalue = vistk::get_envvar(envvar);
+
+  if (!envvalue)
+  {
+    TEST_ERROR("failed to get environment from CTest");
   }
   else
   {
-    TEST_ERROR("Unknown test: " << test_name);
+    vistk::envvar_value_t const expected = vistk::envvar_value_t("test_value");
 
-    return EXIT_FAILURE;
+    if (*envvalue != *expected)
+    {
+      TEST_ERROR("Did not get expected value: "
+                  "Expected: " << expected << " "
+                  "Received: " << envvalue);
+    }
   }
-
-  return EXIT_SUCCESS;
 }
