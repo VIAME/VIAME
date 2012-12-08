7d16223dac10cf58e508d41fc7713b7890a8b812
Ben Boeckel <ben.boeckel@kitware.com>
2012-12-07 19:57:32 -0500

    Add test runner function for Python tests

--- src/bindings/python/test/test.py
+++ src/bindings/python/test/test.py
@@ -1,5 +1,5 @@
 #ckwg +4
-# Copyright 2012 by Kitware, Inc. All Rights Reserved. Please refer to
+# Copyright 2012-2013 by Kitware, Inc. All Rights Reserved. Please refer to
 # KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
 # Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
 
@@ -28,3 +28,17 @@ def expect_exception(action, kind, func, *args):
 
     if not got_exception:
         test_error("Did not get exception when %s" % action)
+
+
+def run_test(testname, tests, *args, **kwargs):
+    if testname not in tests:
+        import sys
+
+        test_error("No such test '%s'" % testname)
+
+        sys.exit(1)
+
+    try:
+        tests[testname](*args, **kwargs)
+    except BaseException as e:
+        test_error("Unexpected exception: %s" % str(e))
