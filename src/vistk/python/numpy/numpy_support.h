2ad8aaca599b42c9c7aaf3f9bc2a0f251c32e0a4
Ben Boeckel <ben.boeckel@kitware.com>
2013-01-21 02:13:40 -0500

    Avoid using deprecated NumPy APIs

---
+++ src/vistk/python/numpy/numpy_support.h
@@ -0,0 +1,36 @@
+/*ckwg +5
+ * Copyright 2013 by Kitware, Inc. All Rights Reserved. Please refer to
+ * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
+ * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
+ */
+
+#ifndef VISTK_PYTHON_NUMPY_NUMPY_SUPPORT_H
+#define VISTK_PYTHON_NUMPY_NUMPY_SUPPORT_H
+
+#include "numpy-config.h"
+
+#include <numpy/numpyconfig.h>
+
+/**
+ * \file numpy_support.h
+ *
+ * \brief Support macros for NumPy.
+ */
+
+/**
+ * \def NPY
+ *
+ * \brief Compatability macro for NumPy.
+ *
+ * \param x The NumPy symbol.
+ */
+
+#if NPY_API_VERSION >= 0x00000006
+// All NPY_* defines were renamed to NPY_ARRAY_* in 1.7 and the old ones
+// deprecated.
+#define NPY(x) NPY_ARRAY_##x
+#else
+#define NPY(x) NPY_##x
+#endif
+
+#endif // VISTK_PYTHON_NUMPY_SUPPORT_H
