3af2f88f23bf03d947cce8bc3f0ad9e50aec82d7
Ben Boeckel <ben.boeckel@kitware.com>
2013-02-11 18:55:21 -0500

    Swap vcl and core vxl include directory order
    The vcl headers are "lower" than the core of vxl, so they should be
    included first.

--- cmake/snippets/depends.cmake
+++ cmake/snippets/depends.cmake
@@ -38,8 +38,8 @@ include("${vistk_source_dir}/cmake/snippets/boost_tests.cmake")
 
 find_package(VXL REQUIRED)
 
-include_directories(SYSTEM ${VXL_CORE_INCLUDE_DIR})
 include_directories(SYSTEM ${VXL_VCL_INCLUDE_DIR})
+include_directories(SYSTEM ${VXL_CORE_INCLUDE_DIR})
 link_directories(${VXL_LIBRARY_DIR})
 
 include("${vistk_source_dir}/cmake/snippets/vxl_tests.cmake")
