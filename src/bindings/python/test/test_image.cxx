43350a07b20e8bb0a5d811814c2aa8adf791fc66
Ben Boeckel <ben.boeckel@kitware.com>
2013-02-22 16:26:00 -0500

    Add vil_image_view_base functions

--- src/bindings/python/test/test_image.cxx
+++ src/bindings/python/test/test_image.cxx
@@ -1,5 +1,5 @@
 /*ckwg +5
- * Copyright 2012 by Kitware, Inc. All Rights Reserved. Please refer to
+ * Copyright 2012-2013 by Kitware, Inc. All Rights Reserved. Please refer to
  * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
  * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
  */
@@ -18,6 +18,11 @@
 
 using namespace boost::python;
 
+static vil_image_view_base_sptr make_image_base(size_t width, size_t height, size_t planes);
+static size_t take_image_base(vil_image_view_base_sptr const& img);
+static bool save_image_base(vil_image_view_base_sptr const& img, std::string const& path);
+static vil_image_view_base_sptr pass_image_base(vil_image_view_base_sptr const& img);
+
 template <typename T>
 static vil_image_view<T> make_image(size_t width, size_t height, size_t planes);
 template <typename T>
@@ -29,6 +34,19 @@ static vil_image_view<T> pass_image(vil_image_view<T> const& img);
 
 BOOST_PYTHON_MODULE(test_image)
 {
+  def("make_image_base", &make_image_base
+    , (arg("width"), arg("height"), arg("planes"))
+    , "Create a base image.");
+  def("take_image_base", &take_image_base
+    , (arg("image"))
+    , "Take a base image and return its size.");
+  def("save_image_base", &save_image_base
+    , (arg("image"), arg("path"))
+    , "Take a base image and write it to a file.");
+  def("pass_image_base", &pass_image_base
+    , (arg("image"))
+    , "Take a base image and return it.");
+
 #define DEFINE_FUNCTIONS(type)                             \
   do                                                       \
   {                                                        \
@@ -52,6 +70,36 @@ BOOST_PYTHON_MODULE(test_image)
   DEFINE_FUNCTIONS(double);
 
 #undef DEFINE_FUNCTIONS
+}
+
+vil_image_view_base_sptr
+make_image_base(size_t width, size_t height, size_t planes)
+{
+  typedef uint8_t base_type;
+
+  return vil_image_view_base_sptr(new vil_image_view<base_type>(width, height, planes));
+}
+
+size_t
+take_image_base(vil_image_view_base_sptr const& img)
+{
+  size_t const ni = img->ni();
+  size_t const nj = img->nj();
+  size_t const np = img->nplanes();
+
+  return (ni * nj * np);
+}
+
+bool
+save_image_base(vil_image_view_base_sptr const& img, std::string const& path)
+{
+  return vil_save(*img, path.c_str());
+}
+
+vil_image_view_base_sptr
+pass_image_base(vil_image_view_base_sptr const& img)
+{
+  return img;
 }
 
 template <typename T>
