8291c93a10430487dc1fc57712de7ba3281d59f1
Ben Boeckel <ben.boeckel@kitware.com>
2013-02-23 00:30:24 -0500

    Fix header guards in tool helpers

--- src/tools/helpers/tool_usage.h
+++ src/tools/helpers/tool_usage.h
@@ -1,8 +1,11 @@
 /*ckwg +5
- * Copyright 2012 by Kitware, Inc. All Rights Reserved. Please refer to
+ * Copyright 2012-2013 by Kitware, Inc. All Rights Reserved. Please refer to
  * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
  * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
  */
+
+#ifndef VISTK_TOOL_HELPERS_TOOL_USAGE_H
+#define VISTK_TOOL_HELPERS_TOOL_USAGE_H
 
 #include <vistk/config.h>
 
@@ -15,3 +18,5 @@ void tool_version_message();
 boost::program_options::options_description tool_common_options();
 
 boost::program_options::variables_map tool_parse(int argc, char* argv[], boost::program_options::options_description const& desc);
+
+#endif // VISTK_TOOL_HELPERS_TOOL_USAGE_H
