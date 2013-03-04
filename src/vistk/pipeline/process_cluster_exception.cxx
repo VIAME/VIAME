5362f846a895f51ff7a05b27b613171b39d2f95d
Ben Boeckel <ben.boeckel@kitware.com>
2013-03-04 17:24:57 -0500

    Make an error message clearer

--- src/vistk/pipeline/process_cluster_exception.cxx
+++ src/vistk/pipeline/process_cluster_exception.cxx
@@ -42,7 +42,7 @@ mapping_after_process_exception
           "the process cluster \'" << m_name << "\' was "
           "requested for mapping to the \'" << m_mapped_key << "\' "
           "configuration on the process \'" << m_name << "\', "
-          "but it has already been created";
+          "but the process has already been created";
 
   m_what = sstr.str();
 }
