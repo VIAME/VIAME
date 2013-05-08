1d596bc7dd2e8c94ca6d49606a8cfa3ada79b6fe
Ben Boeckel <ben.boeckel@kitware.com>
2013-05-08 14:28:48 -0400

    Factor out fixes CMake code

--- src/fixes/python.cmake
+++ src/fixes/python.cmake
@@ -1,5 +1,5 @@
-set(__sprokit_python_fixes_targets)
-set(__sprokit_python_fixes)
+set(__sprokit_python_fixes
+  CACHE INTERNAL "Internal list of sprokit fixes")
 
 option(SPROKIT_ENABLE_BOOST_PYTHON_FIXES "Enables fixes for Boost.Python bugs (recommended)" ON)
 mark_as_advanced(SPROKIT_ENABLE_BOOST_PYTHON_FIXES)
@@ -7,50 +7,23 @@ if (SPROKIT_ENABLE_BOOST_PYTHON_FIXES)
   # Not fixed yet.
   #if (Boost_VERSION LESS 10XX00)
     # Override Boost's invoke.hpp
-    set(__sprokit_reldir "threading")
-    set(__sprokit_target_name "boost-python-invoke.hpp")
-    sprokit_configure_directory(${__sprokit_target_name}
-      "${sprokit_fix_source_dir}/${__sprokit_reldir}/boost"
-      "${sprokit_fix_binary_dir}/${__sprokit_reldir}/${BOOST_MANGLE_NAMESPACE}")
-    set(__sprokit_python_fixes
-      ${__sprokit_python_fixes}
-      "${__sprokit_reldir}")
-    set(__sprokit_python_fixes_targets
-      ${__sprokit_python_fixes_targets}
-      ${__sprokit_target_name})
+    sprokit_add_fix(threading __sprokit_python_fixes)
   #endif ()
   # Not fixed yet.
   #if (Boost_VERSION LESS 10XX00)
     # Override Boost's override.hpp
-    set(__sprokit_reldir "exceptions_in_override")
-    set(__sprokit_target_name "boost-python-override.hpp")
-    sprokit_configure_directory(${__sprokit_target_name}
-      "${sprokit_fix_source_dir}/${__sprokit_reldir}/boost"
-      "${sprokit_fix_binary_dir}/${__sprokit_reldir}/${BOOST_MANGLE_NAMESPACE}")
-    set(__sprokit_python_fixes
-      ${__sprokit_python_fixes}
-      "${__sprokit_reldir}")
-    set(__sprokit_python_fixes_targets
-      ${__sprokit_python_fixes_targets}
-      ${__sprokit_target_name})
+    sprokit_add_fix(exceptions_in_override __sprokit_python_fixes)
   #endif ()
-
-  unset(__sprokit_reldir)
-  unset(__sprokit_target_name)
 endif ()
 
 function (sprokit_use_python_fixes)
-  foreach (python_fix ${__sprokit_python_fixes})
-    if (NOT BOOST_MANGLE_NAMESPACE STREQUAL "boost")
-      include_directories(BEFORE SYSTEM "${sprokit_fix_source_dir}/${python_fix}")
-    endif ()
-    include_directories(BEFORE SYSTEM "${sprokit_fix_binary_dir}/${python_fix}")
+  foreach (fix ${__sprokit_python_fixes})
+    sprokit_use_fix(${fix})
   endforeach ()
 endfunction ()
 
 function (sprokit_require_python_fixes target)
-  if (__sprokit_python_fixes_targets)
-    add_dependencies(${target}
-      ${__sprokit_python_fixes_targets})
-  endif (__sprokit_python_fixes_targets)
+  foreach (fix ${__sprokit_python_fixes})
+    sprokit_require_fix(${target} ${fix})
+  endforeach ()
 endfunction ()
--- src/fixes/tools.cmake
+++ src/fixes/tools.cmake
@@ -1,40 +1,23 @@
-set(__sprokit_tools_fixes_targets)
-set(__sprokit_tools_fixes)
+set(__sprokit_tools_fixes
+  CACHE INTERNAL "Internal list of sprokit fixes")
 
 option(SPROKIT_ENABLE_BOOST_PROGRAM_OPTIONS_FIXES "Enables fixes for boost::program_options bugs (recommended)" ON)
 mark_as_advanced(SPROKIT_ENABLE_BOOST_PROGRAM_OPTIONS_FIXES)
 if (SPROKIT_ENABLE_BOOST_PROGRAM_OPTIONS_FIXES)
   if (Boost_VERSION LESS 105000)
     # Override Boost's value_semantic.hpp
-    set(reldir "value_name")
-    set(target_name "boost-program_options-value_semantic.hpp")
-    sprokit_configure_directory(${target_name}
-      "${sprokit_fix_source_dir}/${reldir}/boost"
-      "${sprokit_fix_binary_dir}/${reldir}/${BOOST_MANGLE_NAMESPACE}")
-    set(__sprokit_tools_fixes
-      ${__sprokit_tools_fixes}
-      "${__sprokit_reldir}")
-    set(__sprokit_tools_fixes_targets
-      ${__sprokit_tools_fixes_targets}
-      ${target_name})
+    sprokit_add_fix(value_name __sprokit_tools_fixes)
   endif ()
-
-  unset(__sprokit_reldir)
-  unset(__sprokit_target_name)
 endif ()
 
 function (sprokit_use_tools_fixes)
-  foreach (tools_fix ${__sprokit_tools_fixes})
-    if (NOT BOOST_MANGLE_NAMESPACE STREQUAL "boost")
-      include_directories(BEFORE SYSTEM "${sprokit_fix_source_dir}/${tools_fix}")
-    endif ()
-    include_directories(BEFORE SYSTEM "${sprokit_fix_binary_dir}/${tools_fix}")
+  foreach (fix ${__sprokit_tools_fixes})
+    sprokit_use_fix(${fix})
   endforeach ()
 endfunction ()
 
 function (sprokit_require_tools_fixes target)
-  if (__sprokit_tools_fixes_targets)
-    add_dependencies(${target}
-      ${__sprokit_tools_fixes_targets})
-  endif (__sprokit_tools_fixes_targets)
+  foreach (fix ${__sprokit_tools_fixes})
+    sprokit_require_fix(${target} ${fix})
+  endforeach ()
 endfunction ()
