7be49ba04793c1b30db197f01f5e010425d129d8
Ben Boeckel <ben.boeckel@kitware.com>
2013-07-19 19:15:15 -0400

    Add a test for the set_indexing_suite API

--- tests/bindings/python/sprokit/pipeline/test-process.py
+++ tests/bindings/python/sprokit/pipeline/test-process.py
@@ -112,6 +112,155 @@ def test_api_calls():
     process.PythonProcess.flag_required
 
 
+def test_flags_as_set():
+    from sprokit.pipeline import process
+
+    # TODO: Make tests more rigorous (check more than just len()).
+
+    a = process.PortFlags()
+
+    # adding to the set
+    a.add(process.PythonProcess.flag_required)
+    a.add(process.PythonProcess.flag_input_mutable)
+    a.add(process.PythonProcess.flag_input_nodep)
+    a.add(process.PythonProcess.flag_input_static)
+
+    # length
+    if not len(a) == 4:
+        test_error("len() does not work: expected 4, got %d" % len(a))
+
+    # adding duplicate values
+    a.add(process.PythonProcess.flag_required)
+
+    if not len(a) == 4:
+        test_error(".add() added a duplicate item: expected 4, got %d" % len(a))
+
+    # adding invalid objects
+    expect_exception('adding a valud of an invalid type', RuntimeError,
+                     process.PortFlags.add, a, True),
+
+    # indexing failures
+    expect_exception('getting an item by index', IndexError,
+                     process.PortFlags.__getitem__, a, 0)
+    expect_exception('deleting an item by index', IndexError,
+                     process.PortFlags.__delitem__, a, 0)
+    expect_exception('setting an item by index', IndexError,
+                     process.PortFlags.__setitem__, a, 0, process.PythonProcess.flag_input_mutable)
+
+    # 'in' keyword
+    if process.PythonProcess.flag_required not in a:
+        test_error("a value in the set is 'not in' the set")
+    if process.PythonProcess.flag_output_const in a:
+        test_error("a value not in the set is 'in' the set")
+
+    # iteration
+    for value in a:
+        pass
+
+    # boolean casting
+    if not a:
+        test_error("a non-empty set is False-like")
+
+    b = process.PortFlags()
+
+    if b:
+        test_error("an empty set is True-like")
+
+    # removal
+    expect_exception('.pop() on an empty set', KeyError,
+                     process.PortFlags.pop, b)
+    expect_exception('.remove() with an item that does not exist in the set', KeyError,
+                     process.PortFlags.remove, a, process.PythonProcess.flag_output_const)
+    a.discard(process.PythonProcess.flag_output_const)
+
+    if not len(a) == 4:
+        test_error(".discard() removed an item not in the set")
+
+    a.discard(process.PythonProcess.flag_input_static)
+
+    if not len(a) == 3:
+        test_error(".discard() did not remove an item from the set")
+
+    a.remove(process.PythonProcess.flag_input_nodep)
+
+    if not len(a) == 2:
+        test_error(".remove() did not remove an item from the set")
+
+    a.pop()
+
+    if not len(a) == 1:
+        test_error(".pop() did not remove an item from the set")
+
+    a.clear()
+
+    if a:
+        test_error(".clear() did not make a False-like set")
+
+    # copy
+    b.add(process.PythonProcess.flag_required)
+
+    c = b.copy()
+
+    b.clear()
+
+    if not c:
+        test_error(".clear() on a set modified a set created using .copy()")
+
+    c = b.copy()
+
+    b.add(process.PythonProcess.flag_required)
+
+    if c:
+        test_error(".add() on a set modified a set created using .copy()")
+
+    # set vs. set queries
+    a.add(process.PythonProcess.flag_input_nodep)
+    a.add(process.PythonProcess.flag_input_static)
+
+    if not b.isdisjoint(a):
+        test_error(".isdisjoint() does not work")
+    if b.issubset(a):
+        test_error(".issubset() does not work")
+    if a.issuperset(b):
+        test_error(".issuperset() does not work")
+
+    a.add(process.PythonProcess.flag_required)
+
+    if b.isdisjoint(a):
+        test_error(".isdisjoint() does not work")
+    if not b.issubset(a):
+        test_error(".issubset() does not work")
+    if not a.issuperset(b):
+        test_error(".issuperset() does not work")
+
+    u = a.union(b)
+
+    if not len(u) == 3:
+        test_error(".union() does not work: expected 3, got %d" % len(u))
+
+    d = a.difference(b)
+
+    if not len(d) == 2:
+        test_error(".difference() does not work: expected 2, got %d" % len(d))
+
+    i = a.intersection(b)
+
+    if not len(i) == 1:
+        test_error(".intersection() does not work: expected 1, got %d" % len(i))
+
+    b.add(process.PythonProcess.flag_output_const)
+
+    s = a.symmetric_difference(b)
+
+    if not len(s) == 3:
+        test_error(".symmetric_difference() does not work: expected 3, got %d" % len(s))
+
+    a.update(b)
+
+    if not len(a) == 4:
+        test_error(".update() does not work: expected 4, got %d" % len(a))
+
+
 if __name__ == '__main__':
     import os
     import sys
