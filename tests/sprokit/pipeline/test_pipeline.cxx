39805f6583f44396ca72c088d0914b7690dd9de7
Ben Boeckel <ben.boeckel@kitware.com>
2013-07-19 15:28:36 -0400

    Add a test which has a frequency mismatch

--- tests/sprokit/pipeline/test_pipeline.cxx
+++ tests/sprokit/pipeline/test_pipeline.cxx
@@ -1237,6 +1237,58 @@ IMPLEMENT_TEST(setup_pipeline_frequency_split_to_inputs)
   pipeline->setup_pipeline();
 }
 
+IMPLEMENT_TEST(setup_pipeline_frequency_incompatible_via_flow)
+{
+  sprokit::process::type_t const proc_typeu = sprokit::process::type_t("numbers");
+  sprokit::process::type_t const proc_typef = sprokit::process::type_t("duplicate");
+  sprokit::process::type_t const proc_typet = sprokit::process::type_t("multiplication");
+  sprokit::process::type_t const proc_typed = sprokit::process::type_t("sink");
+
+  sprokit::process::name_t const proc_nameu = sprokit::process::name_t("upstream");
+  sprokit::process::name_t const proc_namef = sprokit::process::name_t("duplicate");
+  sprokit::process::name_t const proc_namet = sprokit::process::name_t("multiply");
+  sprokit::process::name_t const proc_named = sprokit::process::name_t("sink");
+
+  sprokit::config::key_t const key = sprokit::config::key_t("copies");
+  sprokit::config::value_t const copies = boost::lexical_cast<sprokit::config::value_t>(1);
+
+  sprokit::config_t const configf = sprokit::config::empty_config();
+  configf->set_value(key, copies);
+
+  sprokit::process_t const processu = create_process(proc_typeu, proc_nameu);
+  sprokit::process_t const processf = create_process(proc_typef, proc_namef, configf);
+  sprokit::process_t const processt = create_process(proc_typet, proc_namet);
+  sprokit::process_t const processd = create_process(proc_typed, proc_named);
+
+  sprokit::pipeline_t const pipeline = create_pipeline();
+
+  pipeline->add_process(processu);
+  pipeline->add_process(processf);
+  pipeline->add_process(processt);
+  pipeline->add_process(processd);
+
+  sprokit::process::port_t const port_nameu = sprokit::process::port_t("number");
+  sprokit::process::port_t const port_namefi = sprokit::process::port_t("input");
+  sprokit::process::port_t const port_namefo = sprokit::process::port_t("duplicate");
+  sprokit::process::port_t const port_nameti1 = sprokit::process::port_t("factor1");
+  sprokit::process::port_t const port_nameti2 = sprokit::process::port_t("factor2");
+  sprokit::process::port_t const port_nameto = sprokit::process::port_t("product");
+  sprokit::process::port_t const port_named = sprokit::process::port_t("sink");
+
+  pipeline->connect(proc_nameu, port_nameu,
+                    proc_namef, port_namefi);
+  pipeline->connect(proc_namef, port_namefo,
+                    proc_namet, port_nameti1);
+  pipeline->connect(proc_nameu, port_nameu,
+                    proc_namet, port_nameti2);
+  pipeline->connect(proc_namet, port_nameto,
+                    proc_named, port_named);
+
+  EXPECT_EXCEPTION(sprokit::frequency_mismatch_exception,
+                   pipeline->setup_pipeline(),
+                   "setting up a pipeline with an invalid frequency mapping");
+}
+
 IMPLEMENT_TEST(setup_pipeline_duplicate)
 {
   sprokit::process::type_t const proc_type = sprokit::process::type_t("orphan");
