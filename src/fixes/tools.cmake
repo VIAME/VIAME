08754c67e0fd511dad64f07ff6642b8e95fda89a
Ben Boeckel <ben.boeckel@kitware.com>
2013-09-04 17:23:15 -0400

    Use properties for fix mechanisms

--- src/fixes/tools.cmake
+++ src/fixes/tools.cmake
@@ -1,23 +1,27 @@
-set(__sprokit_tools_fixes
-  CACHE INTERNAL "Internal list of sprokit fixes")
+set_property(GLOBAL
+  PROPERTY sprokit_tools_fixes)
 
 option(SPROKIT_ENABLE_BOOST_PROGRAM_OPTIONS_FIXES "Enables fixes for boost::program_options bugs (recommended)" ON)
 mark_as_advanced(SPROKIT_ENABLE_BOOST_PROGRAM_OPTIONS_FIXES)
 if (SPROKIT_ENABLE_BOOST_PROGRAM_OPTIONS_FIXES)
   if (Boost_VERSION LESS 105000)
     # Override Boost's value_semantic.hpp
-    sprokit_add_fix(value_name __sprokit_tools_fixes)
+    sprokit_add_fix(value_name sprokit_tools_fixes)
   endif ()
 endif ()
 
 function (sprokit_use_tools_fixes)
-  foreach (fix IN LISTS __sprokit_tools_fixes)
-    sprokit_use_fix(${fix})
+  get_property(fixes GLOBAL
+    PROPERTY sprokit_tools_fixes)
+  foreach (fix IN LISTS fixes)
+    sprokit_use_fix("${fix}")
   endforeach ()
 endfunction ()
 
 function (sprokit_require_tools_fixes target)
-  foreach (fix IN LISTS __sprokit_tools_fixes)
-    sprokit_require_fix(${target} ${fix})
+  get_property(fixes GLOBAL
+    PROPERTY sprokit_tools_fixes)
+  foreach (fix IN LISTS fixes)
+    sprokit_require_fix("${target}" "${fix}")
   endforeach ()
 endfunction ()
