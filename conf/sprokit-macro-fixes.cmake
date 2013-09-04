e8176ac8913ee5cabc6c9d2edfe69d5be720cdb1
Ben Boeckel <ben.boeckel@kitware.com>
2013-09-04 17:22:53 -0400

    Use a global property for available sprokit fixes

--- conf/sprokit-macro-fixes.cmake
+++ conf/sprokit-macro-fixes.cmake
@@ -7,13 +7,28 @@
 #
 #   sprokit_use_fixes([name ...])
 #     Set up future targets to use fixes for Boost used in sprokit.
-#     Does not affect parent directories.
+#     Does not affect parent directories. If called without arguments, all
+#     known fixes are used.
 
-set(SPROKIT_FIXES "@sprokit_fixes@"
-  CACHE INTERNAL "Fixes available from sprokit")
+define_property(GLOBAL
+  PROPERTY   sprokit_fixes
+  BRIEF_DOCS "A list of all fixes sprokit provides for various headers."
+  FULL_DOCS  "Some things in sprokit require a patched header (usually for "
+             "Boost) and as such, requires extra include directories for each "
+             "fix. The fixes available depends on whether sprokit was built "
+             "using them or not.")
+set_property(GLOBAL
+  PROPERTY sprokit_fixes
+  "@sprokit_fixes@")
 
 function (sprokit_use_fixes)
-  foreach (fix IN LISTS ARGN)
+  if (ARGN)
+    set(fixes "${ARGN}")
+  else ()
+    get_property(fixes GLOBAL
+      PROPERTY sprokit_fixes)
+  endif ()
+  foreach (fix IN LISTS fixes)
     foreach (includedir IN LISTS SPROKIT_INCLUDE_DIR)
       include_directories(BEFORE SYSTEM "${includedir}/fixes/${fix}")
     endforeach ()
