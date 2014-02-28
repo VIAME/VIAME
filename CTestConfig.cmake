54048559e034c2f6c03a00ac28191a24a8d207e0
Ben Boeckel <ben.boeckel@kitware.com>
2014-02-28 11:46:34 -0500

    Use the public dashboard URL

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -2,6 +2,6 @@ set(CTEST_PROJECT_NAME       "sprokit")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
 set(CTEST_DROP_METHOD     "http")
-set(CTEST_DROP_SITE       "www.kitware.com")
-set(CTEST_DROP_LOCATION   "/CDash/submit.php?project=sprokit")
+set(CTEST_DROP_SITE       "open.cdash.org")
+set(CTEST_DROP_LOCATION   "/submit.php?project=sprokit")
 set(CTEST_DROP_SITE_CDASH TRUE)
