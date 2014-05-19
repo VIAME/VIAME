12c5cdee9193e956af35187617f29ce7d185ec53
Linus Sherrill <linus.sherrill@kitware.com>
2014-05-19 15:53:27 -0400

    Move type definition files

---
+++ src/core/types/kwiver.h
@@ -0,0 +1,33 @@
+/*ckwg +5
+ * Copyright 2014 by Kitware, Inc. All Rights Reserved. Please refer to
+ * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
+ * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
+ */
+
+#ifndef _KWIVER_TYPES_KWIVER_H_
+#define _KWIVER_TYPES_KWIVER_H_
+
+#include <sprokit/pipeline/process.h>
+
+#include <vector>
+#include <geo_lat_lon.h>
+
+namespace kwiver
+{
+
+/*! \file KWIVER specific types.
+ *
+ * This file contains the canonical type names for KWIVER types used
+ * in the sprokit pipeline.
+ */
+
+static sprokit::process::type_t const kwiver_corner_points( "kwiver:corner_points_ul_ur_lr_ll" );
+static sprokit::process::type_t const kwiver_gsd( "kwiver:meters_per_pixel" );
+static sprokit::process::type_t const kwiver_timestamp( "kwiver:timestamp" );
+
+// -- concrete types --
+typedef double gsd_t;
+typedef std::vector < kwiver::geo_lat_lon > corner_points_t;
+
+} // end namespace
+#endif /* _KWIVER_TYPES_KWIVER_H_ */
---
+++ src/core/types/maptk.h
@@ -0,0 +1,21 @@
+/*ckwg +5
+ * Copyright 2014 by Kitware, Inc. All Rights Reserved. Please refer to
+ * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
+ * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
+ */
+
+#ifndef _KWIVER_TYPES_MAPTK_H_
+#define _KWIVER_TYPES_MAPTK_H_
+
+#include <sprokit/pipeline/process.h>
+
+/*! \file Type names for maptk types.
+ *
+ * This file contains the canonical type strings used by sprokit for
+ * the types defined in maptk.
+ */
+
+static sprokit::process::type_t const maptk_src_to_ref_homography( "maptk:s2r_homography" );
+static sprokit::process::type_t const maptk_image_container( "maptk:image_container_sptr" );
+
+#endif /* _KWIVER_TYPES_MAPTK_H_ */
