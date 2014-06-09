c1ba03c047b31ca558f55f1bea4228516951e537
Linus Sherrill <linus.sherrill@kitware.com>
2014-06-09 13:44:37 -0400

    Reorganize directory structure.
    Move kwiver source down one level to allow addition of
    more major modules.

--- CMake/Doxygen.cmake
+++ CMake/Doxygen.cmake
@@ -6,11 +6,18 @@ find_package(Doxygen)
 cmake_dependent_option(KWIVER_BUILD_DOC "Build documentation" OFF
   DOXYGEN_FOUND OFF)
 
-set(KWIVER_DOC_OUTPUT_DIR  "${KWIVER_BUILD_INSTALL_PREFIX}/doc")
+if (KWIVER_BUILD_DOC)
+  set(KWIVER_DOC_OUTPUT_DIR "${KWIVER_BUILD_INSTALL_PREFIX}/doc"
+    CACHE PATH  "Location for documentation" )
+endif()
 
 if (DOXYGEN_FOUND)
-  add_custom_target(doxygen)
+  add_custom_target(doxygen
+      COMMAND "${DOXYGEN_EXECUTABLE}" "${KWIVER_DOC_OUTPUT_DIR}/Doxyfile"
+      WORKING_DIRECTORY "${KWIVER_DOC_OUTPUT_DIR}"
+      ALL)
 endif ()
 
+## TODO
 # need Doxyfile.in
 # configure Doxyfile
