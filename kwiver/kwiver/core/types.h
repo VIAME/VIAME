21aca001987ff6bdc57bd12a2cc9eb2bad5bf28f
Linus Sherrill <linus.sherrill@kitware.com>
2015-04-17 12:01:43 -0400

    Move config block types to that class.

--- kwiver/kwiver/core/types.h
+++ kwiver/kwiver/core/types.h
@@ -45,14 +45,6 @@
 namespace kwiver
 {
 
-/// The type that represents a configuration value key.
-typedef std::string config_block_key_t;
-/// The type that represents a collection of configuration keys.
-typedef std::vector<config_block_key_t> config_block_keys_t;
-/// The type that represents a stored configuration value.
-typedef std::string config_block_value_t;
-/// The type that represents a description of a configuration key.
-typedef std::string config_block_description_t;
 
 /// The type to be used for file and directory paths
 typedef boost::filesystem::path path_t;
