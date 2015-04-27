95821a3a35fe7f181003cd05ac4ec24f4bcb24ef
Linus Sherrill <linus.sherrill@kitware.com>
2015-04-27 17:08:02 -0400

    Small milestone - successful build.

---
+++ CMake/kwiver-depends.cmake
@@ -0,0 +1,7 @@
+# Central location for KWIVER external dependency declaration and resolution
+
+include( kwiver-depends-Boost )
+include( kwiver-depends-Eigen )
+include( kwiver-depends-OpenCV )
+include( kwiver-depends-PROJ )
+include( kwiver-depends-VisCL )
