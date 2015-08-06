266dff2526878385dfe421f3cae74b6916c0205a
Keith Fieldhouse <keith.fieldhouse@kitware.com>
2015-08-06 15:36:25 -0400

    Honor the EIGEN3_ROOT if available

--- CMake/FindEigen3.cmake
+++ CMake/FindEigen3.cmake
@@ -63,6 +63,7 @@ else (EIGEN3_INCLUDE_DIR)
 
   find_path(EIGEN3_INCLUDE_DIR NAMES signature_of_eigen3_matrix_library
       PATHS
+	  ${EIGEN3_ROOT}/include
       ${CMAKE_INSTALL_PREFIX}/include
       ${KDE4_INCLUDE_DIR}
       PATH_SUFFIXES eigen3 eigen
