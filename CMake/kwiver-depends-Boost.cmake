bfb9826b8f96f644afde8a378311a7e8738fd9e3
Linus Sherrill <linus.sherrill@kitware.com>
2015-09-21 15:30:09 -0400

    Make tools and tests optional so we can build without boost

--- CMake/kwiver-depends-Boost.cmake
+++ CMake/kwiver-depends-Boost.cmake
@@ -1,19 +1,21 @@
 # Required Boost external dependency
 
-if(WIN32)
-  set(Boost_USE_STATIC_LIBS TRUE)
-  set(Boost_WIN_MODULES chrono)
-endif()
+if (VITAL_USE_BOOST)
+  if(WIN32)
+    set(Boost_USE_STATIC_LIBS TRUE)
+    set(Boost_WIN_MODULES chrono)
+  endif()
 
-find_package(Boost 1.55 REQUIRED
-  COMPONENTS
+  find_package(Boost 1.55 REQUIRED
+    COMPONENTS
     filesystem
     program_options
     system
     )
 
-add_definitions(-DBOOST_ALL_NO_LIB)
+  add_definitions(-DBOOST_ALL_NO_LIB)
 
-include_directories(SYSTEM ${Boost_INCLUDE_DIRS})
-link_directories(${Boost_LIBRARY_DIRS})
+  include_directories(SYSTEM ${Boost_INCLUDE_DIRS})
+  link_directories(${Boost_LIBRARY_DIRS})
+endif()
 ####
