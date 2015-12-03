2c4460c42e7679143a3b8e570406c2a4d597aa93
Linus Sherrill <linus.sherrill@kitware.com>
2015-12-03 15:59:44 -0500

    Add image container access functions to C bindings.
    Add functions to bindings that allow conversion from sptr to
    opaque handle. These functions are needed to move image containers
    from boost::python through the C interface to python.

--- vital/bindings/c/image_container.cxx
+++ vital/bindings/c/image_container.cxx
@@ -34,6 +34,7 @@
  */
 
 #include "image_container.h"
+#include "image_container.hxx"
 
 #include <vital/types/image_container.h>
 
@@ -52,20 +53,8 @@ SharedPointerCache< kwiver::vital::image_container, vital_image_container_t >
 //+ really need a way to display SPTR_CACHE.
 // need to verify that pointers are released as needed and cache does not grow without bound
 
-/// Create a new, simple image container around an image
-vital_image_container_t* vital_image_container_new_simple( vital_image_t *img )
-{
-  STANDARD_CATCH(
-    "C::image_container::new_simple", 0,
-
-    kwiver::vital::image *vital_img = reinterpret_cast<kwiver::vital::image*>(img);
-    kwiver::vital::image_container_sptr img_sptr( new kwiver::vital::simple_image_container( *vital_img ) );
-    kwiver::vital_c::IMGC_SPTR_CACHE.store( img_sptr );
-    return reinterpret_cast<vital_image_container_t*>( img_sptr.get() );
-  );
-  return 0;
-}
-
+// ==================================================================
+// These two functions support C++ access to the SPTR_CACHE.
 
 /**
  * @brief Accept shared pointer to image container.
@@ -78,15 +67,43 @@ vital_image_container_t* vital_image_container_new_simple( vital_image_t *img )
  *
  * @return Opaque object pointer/handle
  */
-vital_image_container_t* vital_image_container_from_sptr( void* sptr )
+vital_image_container_t* vital_image_container_from_sptr( kwiver::vital::image_container_sptr sptr )
 {
   STANDARD_CATCH(
     "C::image_container::from_sptr", 0,
 
-    kwiver::vital::image_container_sptr img_sptr = *reinterpret_cast< kwiver::vital::image_container_sptr* >(sptr);
+    kwiver::vital_c::IMGC_SPTR_CACHE.store( sptr );
+    return reinterpret_cast<vital_image_container_t*>( sptr.get() );
+    );
+  return 0;
+}
+
+
+kwiver::vital::image_container_sptr vital_image_container_to_sptr( vital_image_container_t* handle )
+{
+  STANDARD_CATCH(
+    "C::image_container::to_sptr", 0,
+
+    return kwiver::vital_c::IMGC_SPTR_CACHE.get( handle );
+    );
+  return kwiver::vital::image_container_sptr();
+}
+
+// ==================================================================
+// These following functions support C access to the image container
+// and associated SPTR_CACHE
+// ------------------------------------------------------------------
+// / Create a new, simple image container around an image
+vital_image_container_t* vital_image_container_new_simple( vital_image_t *img )
+{
+  STANDARD_CATCH(
+    "C::image_container::new_simple", 0,
+
+    kwiver::vital::image *vital_img = reinterpret_cast<kwiver::vital::image*>(img);
+    kwiver::vital::image_container_sptr img_sptr( new kwiver::vital::simple_image_container( *vital_img ) );
     kwiver::vital_c::IMGC_SPTR_CACHE.store( img_sptr );
     return reinterpret_cast<vital_image_container_t*>( img_sptr.get() );
-    );
+  );
   return 0;
 }
 
