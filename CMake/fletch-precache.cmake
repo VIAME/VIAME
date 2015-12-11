fc4a0401704262c1f7cf584c01f8d9c954d959c1
Linus <linus.sherrill@kitware.com>
2015-12-11 15:59:33 -0500

    Add CMake pre-cache file to specify required fletch options.
    Run cmake as follows to load initial cache values when
    configuring fletch
    
    cmake -C fletch-precache.cmake
    ccmake -C fletch-precache.cmake

---
+++ CMake/fletch-precache.cmake
@@ -0,0 +1,17 @@
+#
+# Precache settings for building fletch
+#
+# Selects required modules
+#
+(set fletch_ENABLE_ALL_PACKAGES                   OFF)
+(set fletch_ENABLE_Boost                          ON)
+(set fletch_ENABLE_Ceres                          ON)
+(set fletch_ENABLE_Eigen                          ON)
+(set fletch_ENABLE_OpenCV                         ON)
+(set fletch_ENABLE_PNG                            ON)
+(set fletch_ENABLE_SparseSuite                    ON)
+(set fletch_ENABLE_TinyXML                        ON)
+(set fletch_ENABLE_Zlib                           ON)
+(set fletch_ENABLE_libjpeg-turbo                  ON)
+(set fletch_ENABLE_libtiff                        ON)
+(set FLETCH_BUILD_WITH_PYTHON                     ON)
