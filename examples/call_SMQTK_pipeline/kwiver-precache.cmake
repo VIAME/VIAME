7c786a7871f9396452f201d1a55a601abfe0bd99
Keith Fieldhouse <keith.fieldhouse@kitware.com>
2016-02-02 11:04:28 -0500

    Update CMake variable for building PROCESSES

--- examples/call_SMQTK_pipeline/kwiver-precache.cmake
+++ examples/call_SMQTK_pipeline/kwiver-precache.cmake
@@ -4,7 +4,7 @@
 # Selects required modules
 #
 set(CMAKE_BUILD_TYPE                Release CACHE STRING "" FORCE)
-set(KWIVER_BUILD_KWIVER_PROCESSES   ON      CACHE BOOL "" FORCE)
+set(KWIVER_ENABLE_PROCESSES         ON      CACHE BOOL "" FORCE)
 set(KWIVER_BUILD_SHARED             ON      CACHE BOOL "" FORCE)
 set(KWIVER_ENABLE_MAPTK             ON      CACHE BOOL "" FORCE)
 set(KWIVER_ENABLE_OPENCV            ON      CACHE BOOL "" FORCE)
