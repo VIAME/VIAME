5850cdf2c846f0bb00decaa3d76c1369ae9b6fc9
Linus Sherrill <linus.sherrill@kitware.com>
2016-02-03 16:38:30 -0500

    Add system check for the ability to use std::random

--- CMake/kwiver-configcheck.cmake
+++ CMake/kwiver-configcheck.cmake
@@ -57,4 +57,15 @@ try_compile( success
 
 set( VITAL_USE_STD_CHRONO ${success} )
 
+##
+# see if std::random is supported
+try_compile( success
+  ${CMAKE_BINARY_DIR}
+  ${CMAKE_CURRENT_LIST_DIR}/configcheck/std_random.cxx
+  CMAKE_FLAGS
+     -DCMAKE_CXX_FLAGS:STRING=#${CMAKE_CXX_FLAGS}
+  OUTPUT_VARIABLE OUTPUT)
+
+set( VITAL_USE_STD_RANDOM ${success} )
+
 ###
