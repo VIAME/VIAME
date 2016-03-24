c444fc4a9c19dc69a7c8d05583a08cf3646a752e
Linus Sherrill <linus.sherrill@kitware.com>
2016-03-24 09:17:53 -0400

    Add process examples from SMQTK pipeline.

---
+++ arrows/processes/examples/call_SMQTK_pipeline/SMQTK_Descriptor_test.cxx
@@ -0,0 +1,71 @@
+/*ckwg +29
+ * Copyright 2015 by Kitware, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  * Neither name of Kitware, Inc. nor the names of any contributors may be used
+ *    to endorse or promote products derived from this software without specific
+ *    prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "SMQTK_Descriptor.h"
+#include <iostream>
+
+
+// ==================================================================
+
+int main(int argc, char *argv[])
+{
+
+  if (argc < 3 )
+  {
+    std::cerr << "usage: " << argv[0] << " config-file  image-file [image-file ...]\n";
+    return 1;
+  }
+
+  // loop over file names
+  for (int i = 2; i < argc; i++)
+  {
+    cv::Mat img = cv::imread( argv[i], CV_LOAD_IMAGE_COLOR );
+    if ( ! img.data )
+    {
+      std::cerr << "Could not read image from file \"" << argv[i] << "\"\n";
+      continue;
+    }
+
+    kwiver::SMQTK_Descriptor des;
+    std::vector< double > results = des.ExtractSMQTK( img, argv[1] );
+
+    std::cout << "Descriptor size: " << results.size()
+              << std::endl;
+
+    for ( unsigned i = 0; i < 50; i++)
+    {
+      std::cout << results.at(i) << " ";
+    }
+    std::cout << std::endl;
+
+  } // end for
+
+  return 0;
+}
