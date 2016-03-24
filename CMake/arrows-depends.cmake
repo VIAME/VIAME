64fc9ac3092db93b58d474a9435559639b577578
Linus Sherrill <linus.sherrill@kitware.com>
2016-03-24 09:17:04 -0400

    Add initial versions of CMake dependency finders.

---
+++ CMake/arrows-depends.cmake
@@ -0,0 +1,9 @@
+# Central location for ARROWS external dependency declaration and resolution
+
+include( arrows-depends-vital )
+include( arrows-depends-Ceres )
+##+ include( arrows-depends-Eigen )
+include( arrows-depends-OpenCV )
+include( arrows-depends-PROJ )
+include( arrows-depends-VisCL )
+include( arrows-depends-VXL )
