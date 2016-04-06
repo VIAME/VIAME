4bf66cdadfc3c119199638c638395970fccd8b1c
Linus Sherrill <linus.sherrill@kitware.com>
2016-04-06 10:42:47 -0400

    Make Find PROJ use fletch supplied PROJ4_ROOT variable.

--- CMake/kwiver-depends-PROJ.cmake
+++ CMake/kwiver-depends-PROJ.cmake
@@ -6,6 +6,12 @@ option( KWIVER_ENABLE_PROJ
   )
 
 if( KWIVER_ENABLE_PROJ )
+  set( old_prefix CMAKE_PREFIX_PATH )
+  set( CMAKE_PREFIX_PATH ${PROJ4_ROOT} )
+
   find_package( PROJ REQUIRED )
+
+  set( CMAKE_PREFIX_PATH   old_prefix )
+
   include_directories( SYSTEM ${PROJ_INCLUDE_DIR} )
 endif( KWIVER_ENABLE_PROJ )
