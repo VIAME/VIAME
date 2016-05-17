80e10b69dc9343c4fc947c8ed25da55396caa2e9
Linus Sherrill <linus.sherrill@kitware.com>
2016-05-17 12:36:47 -0400

    Moving and removing files.
    This commit moves the files around without any changes in content.

--- sprokit/cmake/snippets/version.cmake
+++
@@ -1,6 +0,0 @@
-# Set variables for the sprokit version.
-
-set(sprokit_version_major 0)
-set(sprokit_version_minor 1)
-set(sprokit_version_patch 0)
-set(sprokit_version "${sprokit_version_major}.${sprokit_version_minor}.${sprokit_version_patch}")
