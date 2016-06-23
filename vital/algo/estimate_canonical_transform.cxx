27839bcc828321b715f88a2cb805f5908434f20d
Linus Sherrill <linus.sherrill@kitware.com>
2016-06-23 15:57:47 -0400

    Add estimate_canonical_transform files from public master.
    These files are needed by the map-tk that is being converted to
    use kwiver. These files are already available on map-tk public
    master.

---
+++ vital/algo/estimate_canonical_transform.cxx
@@ -0,0 +1,58 @@
+/*ckwg +29
+ * Copyright 2013-2016 by Kitware, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  * Neither name of Kitware, Inc. nor the names of any contributors may be used
+ *    to endorse or promote products derived from this software without specific
+ *    prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * \file
+ * \brief Implementation of canonical similarity transform
+ *        estimation algorithm definition.
+ */
+
+#include <vital/algo/algorithm.txx>
+#include <vital/algo/estimate_canonical_transform.h>
+#include <vital/vital_foreach.h>
+
+
+/// \cond DoxygenSuppress
+INSTANTIATE_ALGORITHM_DEF(kwiver::vital::algo::estimate_canonical_transform);
+/// \endcond
+
+
+namespace kwiver {
+namespace vital {
+namespace algo {
+
+
+estimate_canonical_transform
+::estimate_canonical_transform()
+{
+  attach_logger( "estimate_canonical_transform" );
+}
+
+} } } // end namespace
