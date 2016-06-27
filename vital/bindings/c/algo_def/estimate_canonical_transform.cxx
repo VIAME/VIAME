6baf247e3803fff6cf138c88a682a51930d43af0
Paul Tunison <paul.tunison@kitware.com>
2016-06-27 15:09:02 -0400

    uncommented STANDARD_CATCH use

--- vital/bindings/c/algo_def/estimate_canonical_transform.cxx
+++ vital/bindings/c/algo_def/estimate_canonical_transform.cxx
@@ -55,28 +55,28 @@ vital_algorithm_estimate_canonical_transform_estimate( vital_algorithm_t *algo,
                                                        vital_landmark_map_t const *lm_map,
                                                        vital_error_handle_t *eh )
 {
-//  STANDARD_CATCH(
-//    "vital_algorithm_estimate_canonical_transform_estimate", eh,
+  STANDARD_CATCH(
+    "vital_algorithm_estimate_canonical_transform_estimate", eh,
 
-  auto algo_sptr = vital_c::ALGORITHM_estimate_canonical_transform_SPTR_CACHE.get( algo );
-  auto cam_map_sptr = vital_c::CAM_MAP_SPTR_CACHE.get( cam_map );
-  auto lm_map_sptr = vital_c::LANDMARK_MAP_SPTR_CACHE.get( lm_map );
+    auto algo_sptr = vital_c::ALGORITHM_estimate_canonical_transform_SPTR_CACHE.get( algo );
+    auto cam_map_sptr = vital_c::CAM_MAP_SPTR_CACHE.get( cam_map );
+    auto lm_map_sptr = vital_c::LANDMARK_MAP_SPTR_CACHE.get( lm_map );
 
-  vital::similarity_d sim;
-  try
-  {
-    sim = algo_sptr->estimate_transform( cam_map_sptr, lm_map_sptr );
-  }
-  catch( vital::algorithm_exception const &ex )
-  {
-    POPULATE_EH( eh, 1, ex.what() );
-    return NULL;
-  }
+    vital::similarity_d sim;
+    try
+    {
+      sim = algo_sptr->estimate_transform( cam_map_sptr, lm_map_sptr );
+    }
+    catch( vital::algorithm_exception const &ex )
+    {
+      POPULATE_EH( eh, 1, ex.what() );
+      return NULL;
+    }
 
-  return reinterpret_cast< vital_similarity_d_t* >(
-    new vital::similarity_d( sim )
+    return reinterpret_cast< vital_similarity_d_t* >(
+      new vital::similarity_d( sim )
+    );
+
   );
-
-//  );
-//  return NULL;
+  return NULL;
 }
