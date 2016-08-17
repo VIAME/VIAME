54deebd0de77289babb3c2e082d69c6356909fcb
Linus Sherrill <linus.sherrill@kitware.com>
2016-08-17 15:23:32 -0400

    Adding examples

---
+++ examples/detector1.cxx
@@ -0,0 +1,44 @@
+
+
+#include <vital/config/config_block_io.h>
+#include <arrows/ocv/image_container.h>
+#include <arrows/ocv/image_io.h>
+#include <arrows/ocv/hough_circle_detector.h>
+
+#include <string>
+
+int main( int argc, char* argv[] )
+{
+  // (1) get file name for input image
+  std::string filename = argv[1];
+
+  // (1.1) Look for name of config file as second parameter
+  kwiver::vital::config_block_sptr config;
+  if ( argc > 2 )
+  {
+    config = kwiver::vital::read_config_file( argv[2] );
+  }
+
+  // (2) create image reader
+  kwiver::vital::algo::image_io_sptr image_reader( new kwiver::arrows::ocv::image_io() );
+
+  // (3) Read the image
+  kwiver::vital::image_container_sptr the_image = image_reader->load( filename );
+
+  // (4) Create the detector
+  kwiver::vital::algo::image_object_detector_sptr detector( new kwiver::arrows::ocv::hough_circle_detector() );
+
+  // (4.1) If there was a config structure, then pass it to the algorithm.
+  if (config)
+  {
+    detector->set_configuration( config );
+  }
+
+  // (5) Send image to detector and get detections.
+  kwiver::vital::detected_object_set_sptr detections = detector->detect( the_image );
+
+  // (6) See what was detected
+  std::cout << "There were " << detections->size() << " detections in the image." << std::endl;
+
+  return 0;
+}
---
+++ examples/detector4.cxx
@@ -0,0 +1,59 @@
+
+
+#include <vital/algorithm_plugin_manager.h>
+#include <vital/config/config_block_io.h>
+#include <vital/algo/image_object_detector.h>
+#include <arrows/ocv/image_container.h>
+#include <arrows/ocv/image_io.h>
+
+#include <string>
+
+int main( int argc, char* argv[] )
+{
+  // (1) Create logger to use for reporting errors and other diagnostics.
+  kwiver::vital::logger_handle_t logger( kwiver::vital::get_logger( "detector_test" ));
+
+  // (2) Initialize and load all discoverable plugins
+  kwiver::vital::algorithm_plugin_manager::load_plugins_once();
+
+  // (3) get file name for input image
+  std::string filename = argv[1];
+
+  // (4) Look for name of config file as second parameter
+  kwiver::vital::config_block_sptr config;
+  config = kwiver::vital::read_config_file( argv[2] );
+
+  // (5) create image reader
+  kwiver::vital::algo::image_io_sptr image_reader( new kwiver::arrows::ocv::image_io() );
+
+  // (6) Read the image
+  kwiver::vital::image_container_sptr the_image = image_reader->load( filename );
+
+  // (7) Create the detector
+  kwiver::vital::algo::image_object_detector_sptr detector;
+  kwiver::vital::algo::image_object_detector::set_nested_algo_configuration( "detector", config, detector );
+
+  if ( ! detector )
+  {
+    LOG_ERROR( logger, "Unable to create detector" );
+    return 1;
+  }
+
+  kwiver::vital::algo::image_object_detector::get_nested_algo_configuration( "detector", config, detector );
+
+  // Check config so it will give run-time diagnostic of config problems
+  if ( ! kwiver::vital::algo::image_object_detector::check_nested_algo_configuration( "detector", config ) )
+  {
+    LOG_ERROR( logger, "Configuration check failed." );
+    return 1;
+  }
+
+  // (5) Send image to detector and get detections.
+  kwiver::vital::detected_object_set_sptr detections = detector->detect( the_image );
+
+  // (6) See what was detected
+  std::cout << "There were " << detections->size() << " detections in the image." << std::endl;
+
+
+  return 0;
+}
