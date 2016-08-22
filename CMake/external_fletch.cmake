6228e360a78f97167f8111448beaa723b86a3bdd
Matt Dawkins <matt.dawkins@kitware.com>
2016-08-22 14:36:35 -0400

    Begin to clean up enable flags

--- CMake/external_fletch.cmake
+++ CMake/external_fletch.cmake
@@ -5,7 +5,11 @@
 #   VIAME_BUILD_INSTALL_PREFIX - directory install target
 #   VIAME_PACKAGES_DIR - location of git submodule packages
 #   VIAME_ARGS_COMMON -
-#
+##
+
+if( ${VIAME_ENABLE_CAFFE} OR ${VIAME_ENABLE_SCALLOP_TK} )
+  set( VIAME_ENABLE_OPENCV TRUE )
+endif()
 
 ExternalProject_Add(fletch
   PREFIX ${CMAKE_BINARY_DIR}/build
@@ -13,7 +17,17 @@ ExternalProject_Add(fletch
   CMAKE_GENERATOR ${gen}
   CMAKE_ARGS
     ${VIAME_ARGS_COMMON}
-    -DVIAME_ENABLE_DOCS:BOOL=${VIAME_ENABLE_DOCS}
+
+    # KWIVER Dependencies, Always on
+    -Dfletch_ENABLE_Boost:BOOL=TRUE
+    -Dfletch_ENABLE_Eigen:BOOL=TRUE
+
+    # Optional Dependencies
+    -Dfletch_ENABLE_VXL:BOOL=${VIAME_ENABLE_VXL}
+    -Dfletch_ENABLE_OpenCV:BOOL=${VIAME_ENABLE_OPENCV}
+    -Dfletch_ENABLE_Caffe:BOOL=${VIAME_ENABLE_CAFFE}
+    -DAUTO_ENABLE_CAFFE_DEPENDENCY:BOOL=${VIAME_ENABLE_CAFFE}
+
   INSTALL_DIR ${VIAME_BUILD_INSTALL_PREFIX}
   INSTALL_COMMAND ""
   )
@@ -28,5 +42,5 @@ ExternalProject_Add_Step(fletch forcebuild
   )
 
 set(VIAME_ARGS_fletch
-  -Dfletch_DIR:PATH=${VIAME_BUILD_INSTALL_PREFIX}/lib/cmake
+  -Dfletch_DIR:PATH=${VIAME_BUILD_PREFIX}/src/fletch-build
   )
