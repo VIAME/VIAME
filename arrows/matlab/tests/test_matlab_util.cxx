9255257085a1ab2e10e6b7843f97bd496ce39b74
Linus Sherrill <linus.sherrill@kitware.com>
2016-08-31 12:51:09 -0400

    Fix matlab image converter test

--- arrows/matlab/tests/test_matlab_util.cxx
+++ arrows/matlab/tests/test_matlab_util.cxx
@@ -42,6 +42,8 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
+#define DEBUG 0
+
 #define TEST_ARGS ( kwiver::vital::path_t const &data_dir )
 
 DECLARE_TEST_MAP();
@@ -73,15 +75,27 @@ IMPLEMENT_TEST(image_conversion)
 
   auto ic_sptr = std::make_shared< kwiver::arrows::ocv::image_container >( ocv_image );
 
+#if DEBUG
   cv::namedWindow( "input OCV image", cv::WINDOW_AUTOSIZE ); // Create a window for display.
   cv::imshow( "input OCV image", ocv_image ); // Show our image inside it.
   cv::waitKey( 000 ); // pause for keystroke
+#endif
 
   kwiver::arrows::matlab::MxArraySptr mx_image = kwiver::arrows::matlab::convert_mx_image( ic_sptr );
 
   auto ocv_ic = kwiver::arrows::matlab::convert_mx_image( mx_image );
+  cv::Mat ocv_ic_mat = kwiver::arrows::ocv::image_container::vital_to_ocv( ocv_ic->get_image() );
 
+#if DEBUG
   cv::namedWindow( "output OCV image", cv::WINDOW_AUTOSIZE ); // Create a window for display.
-  cv::imshow( "output OCV image", ocv_image ); // Show our image inside it.
+  cv::imshow( "output OCV image", ocv_ic_mat ); // Show our image inside it.
   cv::waitKey( 000 ); // pause for keystroke
+#endif
+
+  // Test to see if the images are the same
+  bool isEqual = (cv::sum(ocv_image != ocv_ic_mat) == cv::Scalar(0,0,0,0));
+  if ( ! isEqual )
+  {
+    TEST_ERROR( "Images fail comparison." );
+  }
 }
