ea88c836927d64ccaed4e9133c5f9f1168dc711a
Matt Dawkins <matt.dawkins@kitware.com>
2016-09-06 17:09:02 -0400

    Add new flags

--- CMake/add_project_fletch.cmake
+++ CMake/add_project_fletch.cmake
@@ -41,6 +41,8 @@ ExternalProject_Add(fletch
     -Dfletch_ENABLE_Caffe:BOOL=${VIAME_ENABLE_CAFFE}
     -DAUTO_ENABLE_CAFFE_DEPENDENCY:BOOL=${VIAME_ENABLE_CAFFE}
 
+    -DFLETCH_BUILD_WITH_PYTHON:BOOL=${VIAME_ENABLE_PYTHON}
+
     # Set fletch install path to be viame install path
     -Dfletch_BUILD_INSTALL_PREFIX:PATH=${VIAME_BUILD_INSTALL_PREFIX}
 
