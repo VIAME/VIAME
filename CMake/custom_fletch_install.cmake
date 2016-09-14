7282d1d4d36e8847599eae6d2aef4ff54ef18ee4
Matt Dawkins <matt.dawkins@kitware.com>
2016-09-14 16:50:13 -0400

    Update config with custom fletch install

---
+++ CMake/custom_fletch_install.cmake
@@ -0,0 +1,17 @@
+message("Running fletch install")
+
+include( ${VIAME_CMAKE_DIR}/common_macros.cmake )
+
+if( WIN32 )
+
+  if( MSVC AND MSVC_VERSION EQUAL 1900 )
+    RenameSubstr( ${VIAME_BUILD_INSTALL_PREFIX}/lib vc120 vc140 )
+  endif()
+
+  if( VIAME_ENABLE_OPENCV )
+    CopyFiles( ${VIAME_BUILD_INSTALL_PREFIX}/x64/*/bin/*.dll ${VIAME_BUILD_INSTALL_PREFIX}/bin )
+  endif()
+
+endif()
+
+message("Done")
