f604caa7ec1424668a2edf7e3934b96056edf0af
Matt Dawkins <matt.dawkins@kitware.com>
2016-09-14 20:16:24 -0400

    Adjust patch

--- CMake/windows-ksys-hack.cmake
+++ CMake/windows-ksys-hack.cmake
@@ -1,3 +1,7 @@
 if( WIN32 AND MSVC )
-  CopyFiles( ${CURRENT_BINARY_DIR}/../kwiver-build/bin/*/kwiversys.dll ${VIAME_BUILD_INSTALL_PREFIX}/bin )
+  CopyFiles( ${CMAKE_BINARY_DIR}/../kwiver-build/bin/*/kwiversys.dll ${CMAKE_BINARY_DIR}/bin )
+
+  if( EXISTS ${CMAKE_BINARY_DIR}/bin/kwiversys.dll )
+    install( FILES ${CMAKE_BINARY_DIR}/bin/kwiversys.dll DESTINATION bin )
+  endif()
 endif()
