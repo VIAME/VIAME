5975cf3ea48ee3459ed633e13fa17b92d40b7548
Matt Dawkins <matt.dawkins@kitware.com>
2016-09-18 09:29:41 -0400

    Add faster rcnn to build

---
+++ CMake/add_project_faster_rcnn.cmake
@@ -0,0 +1,22 @@
+# scallop_tk External Project
+#
+# Required symbols are:
+#   VIAME_BUILD_PREFIX - where packages are built
+#   VIAME_BUILD_INSTALL_PREFIX - directory install target
+#   VIAME_PACKAGES_DIR - location of git submodule packages
+#   VIAME_ARGS_COMMON -
+##
+
+# Configure faster-rcnn repository in packages to point to internal caffe
+if( NOT WIN32 )
+  ExternalProject_Add( py-faster-rcnn
+    DEPENDS fletch
+    PREFIX ${VIAME_BUILD_PREFIX}
+    SOURCE_DIR ${VIAME_PACKAGES_DIR}/py-faster-rcnn/lib
+    CONFIGURE_COMMAND ${CMAKE_COMMAND} -E create_symlink
+      ${VIAME_BUILD_PREFIX}/src/fletch-build/build/src/Caffe-build
+      ${VIAME_PACKAGES_DIR}/py-faster-rcnn/caffe-fast-rcnn
+    BUILD_COMMAND cd ${VIAME_PACKAGES_DIR}/py-faster-rcnn/lib && make
+    INSTALL_COMMAND ""
+    )
+endif()
