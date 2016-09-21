9084ab3ff565311156a6ad3f7c6bde53630b52bf
Matt Dawkins <matt.dawkins@kitware.com>
2016-09-20 22:36:31 -0400

    Remove unused code

--- CMake/custom_fletch_install.cmake
+++ CMake/custom_fletch_install.cmake
@@ -2,13 +2,6 @@ message("Running fletch install")
 
 include( ${VIAME_CMAKE_DIR}/common_macros.cmake )
 
-if( VIAME_ENABLE_PYTHON AND EXISTS ${VIAME_BUILD_PREFIX}/src/fletch-build/build/src/Boost/boost/python/raw_function.hpp )
-
-  file( COPY ${VIAME_BUILD_PREFIX}/src/fletch-build/build/src/Boost/boost/python/raw_function.hpp
-        DESTINATION ${VIAME_BUILD_INSTALL_PREFIX}/include/boost/python/ )
-
-endif()
-
 if( WIN32 )
 
   if( MSVC AND MSVC_VERSION EQUAL 1900 )
