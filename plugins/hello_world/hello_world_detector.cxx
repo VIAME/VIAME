94547b5c36275c4cf93a7e0d655b62d773e4b344
Matt Dawkins <matt.dawkins@kitware.com>
2016-09-21 21:15:11 -0400

    Check in example code

---
+++ plugins/hello_world/hello_world_detector.cxx
@@ -0,0 +1,124 @@
+/*ckwg +29
+ * Copyright 2016 by Kitware, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  * Neither name of Kitware, Inc. nor the names of any contributors may be used
+ *    to endorse or promote products derived from this software without specific
+ *    prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "hello_world_detector.h"
+
+#include <arrows/ocv/image_container.h>
+
+#include <cmath>
+
+namespace viame {
+
+// -----------------------------------------------------------------------------------------------
+/**
+ * @brief Storage class for private member variables
+ */
+class hello_world_detector::priv
+{
+public:
+
+  priv() {}
+  ~priv() {}
+
+  std::string m_text;
+}; // end class hello_world_detector::priv
+
+// =================================================================================================
+
+hello_world_detector::
+hello_world_detector()
+  : d( new priv )
+{}
+
+
+hello_world_detector::
+  hello_world_detector( const hello_world_detector& other )
+  : d( new priv( *other.d ) )
+{}
+
+
+hello_world_detector::
+  ~hello_world_detector()
+{}
+
+
+// -------------------------------------------------------------------------------------------------
+kwiver::vital::config_block_sptr
+hello_world_detector::
+get_configuration() const
+{
+  // Get base config from base class
+  kwiver::vital::config_block_sptr config = kwiver::vital::algorithm::get_configuration();
+
+  config->set_value( "text", "Hello World!", "Text to display to user." );
+
+  return config;
+}
+
+
+// -------------------------------------------------------------------------------------------------
+void
+hello_world_detector::
+set_configuration( kwiver::vital::config_block_sptr config )
+{
+  d->m_text = config->get_value< std::string >( "text" );
+}
+
+
+// -------------------------------------------------------------------------------------------------
+bool
+hello_world_detector::
+check_configuration( kwiver::vital::config_block_sptr config ) const
+{
+  if( d->m_text.empty() )
+  {
+    return false;
+  }
+
+  return true;
+}
+
+
+// -------------------------------------------------------------------------------------------------
+kwiver::vital::detected_object_set_sptr
+hello_world_detector::
+detect( kwiver::vital::image_container_sptr image_data ) const
+{
+  auto detected_set = std::make_shared< kwiver::vital::detected_object_set >();
+
+  // cv::Mat src = kwiver::arrows::ocv::image_container::vital_to_ocv( image_data->get_image() );
+
+  LOG_INFO( m_logger, "Text: " << d->m_text );
+
+  return detected_set;
+}
+
+
+} // end namespace
