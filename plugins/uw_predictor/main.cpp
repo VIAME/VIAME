cab67205539d4fe24ef99769f96c2e738715edfd
Matt Dawkins <matt.dawkins@kitware.com>
2016-09-22 12:26:56 -0400

    Remove links to cv

--- plugins/uw_predictor/main.cpp
+++ plugins/uw_predictor/main.cpp
@@ -1,7 +1,7 @@
 #include "mex.h"
 #include "matrix.h"
 #include <iostream>
-#include "cv.h"
+//#include "cv.h"
 #include "math_common.hpp"
 //need cstdint for uint8_t
 #include <cstdint>
@@ -19,30 +19,30 @@ using namespace std;
 int main(cv::Mat img, cv:: Mat img2, int & pred, double & prob, Mat &fgRect)
 {
     FishSpeciesID fishSpeciesID;
-    
+
     //cv::vector<Mat> images;
     //images.push_back(img);
-    //Display size of image, which appears to be opposite that of a 2D 
+    //Display size of image, which appears to be opposite that of a 2D
     //image in matlab- this doesn't work for opencv 2.3.1
     //cv::Size ss = img.size();
     //cout << ss;
-    
+
     //Display image
     cv::namedWindow( "Display window", CV_WINDOW_AUTOSIZE);// Create a window for display.
     cv::imshow( "Display window", img);
-	
+
     //cout << "Reading the classifier model ... ";
 	string xmlFilename = "Model_SVM.xml";//"hierPartClassifier.xml";
 	fishSpeciesID.loadModel(xmlFilename.c_str());
 	//cout << "done" << endl;
-    
+
     //cout << "Predicting data ... " << endl;
     vector<int> predictions;
     vector<double> probabilities;
-    
+
 	//waitKey(0);
     bool isPartial = fishSpeciesID.predict(img, img2, predictions, probabilities, fgRect);
-    
+
     pred=predictions.back();
     prob=probabilities.back();
     //cout << predictions.size();
