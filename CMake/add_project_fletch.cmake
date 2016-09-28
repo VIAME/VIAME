95b429c501884d58b7396dd8a0fda0b7f60f0e0b
Linus Sherrill <linus.sherrill@kitware.com>
2016-09-28 10:14:58 -0400

    Force fletch to always build in release mode.

--- CMake/add_project_fletch.cmake
+++ CMake/add_project_fletch.cmake
@@ -51,6 +51,7 @@ ExternalProject_Add(fletch
     ${VIAME_ARGS_COMMON}
     ${VIAME_PYTHON_FLAGS}
 
+    -DCMAKE_BUILD_TYPE:STRING=RELEASE
     -DBUILD_SHARED_LIBS:BOOL=ON
 
     # KWIVER Dependencies, Always On
