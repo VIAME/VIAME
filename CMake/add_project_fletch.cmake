5d24947ae6d1f843aa7a388d71bba9252a4d56ac
Matt Dawkins <matt.dawkins@kitware.com>
2017-01-21 19:13:59 -0500

    Add burn-out module, switch vivia to use it

--- CMake/add_project_fletch.cmake
+++ CMake/add_project_fletch.cmake
@@ -101,6 +101,13 @@ set(VIAME_ARGS_fletch
   -Dfletch_DIR:PATH=${VIAME_BUILD_PREFIX}/src/fletch-build
   )
 
+if( VIAME_ENABLE_VXL )
+  set(VIAME_ARGS_VXL
+    ${VIAME_ARGS_VXL}
+    -DVXL_DIR:PATH=${VIAME_BUILD_PREFIX}/src/fletch-build/build/src/VXL-build
+    )
+endif()
+
 if( VIAME_ENABLE_OPENCV )
   set(VIAME_ARGS_fletch
     ${VIAME_ARGS_fletch}
