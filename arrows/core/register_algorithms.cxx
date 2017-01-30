464cb1790477edb83f433ef7d609e85a66cb5394
Matt Leotta <matt.leotta@kitware.com>
2017-01-30 15:21:54 -0500

    register the feature_descriptor_io algorithm

--- arrows/core/register_algorithms.cxx
+++ arrows/core/register_algorithms.cxx
@@ -43,6 +43,7 @@
 #include <arrows/core/compute_ref_homography_core.h>
 #include <arrows/core/convert_image_bypass.h>
 #include <arrows/core/estimate_canonical_transform.h>
+#include <arrows/core/feature_descriptor_io.h>
 #include <arrows/core/filter_features_magnitude.h>
 #include <arrows/core/filter_tracks.h>
 #include <arrows/core/hierarchical_bundle_adjust.h>
@@ -72,6 +73,7 @@ int register_algorithms(vital::registrar &reg)
     REGISTER_TYPE( core::compute_ref_homography_core );
     REGISTER_TYPE( core::convert_image_bypass );
     REGISTER_TYPE( core::estimate_canonical_transform );
+    REGISTER_TYPE( core::feature_descriptor_io );
     REGISTER_TYPE( core::filter_features_magnitude );
     REGISTER_TYPE( core::filter_tracks );
     REGISTER_TYPE( core::hierarchical_bundle_adjust );
