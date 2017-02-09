956fda0827334987c5a4ccb7e870dec85690d448
Linus Sherrill <linus.sherrill@kitware.com>
2017-02-09 09:26:24 -0500

    Add missing algorithm

--- arrows/ceres/register_algorithms.cxx
+++ arrows/ceres/register_algorithms.cxx
@@ -64,6 +64,16 @@ register_factories( kwiver::vital::plugin_loader& vpm )
     .add_attribute( kwiver::vital::plugin_factory::PLUGIN_ORGANIZATION, "Kitware Inc." )
     ;
 
+
+  fact = vpm.ADD_ALGORITHM( "ceres", kwiver::arrows::ceres::optimize_camera );
+  fact->add_attribute( kwiver::vital::plugin_factory::PLUGIN_DESCRIPTION,
+                       "Uses Ceres Solver to optimize camera parameters" );
+    .add_attribute( kwiver::vital::plugin_factory::PLUGIN_MODULE_NAME, module_name )
+    .add_attribute( kwiver::vital::plugin_factory::PLUGIN_VERSION, "1.0" )
+    .add_attribute( kwiver::vital::plugin_factory::PLUGIN_ORGANIZATION, "Kitware Inc." )
+    ;
+
+
   vpm.mark_module_as_loaded( module_name );
 }
 
