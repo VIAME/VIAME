785bd7fc6d424bab8525eb5ec833fdbd2f246194
Matt Dawkins <matt.dawkins@kitware.com>
2017-02-23 06:51:35 -0500

    Finish adding darknet option

--- CMake/add_project_darknet.cmake
+++ CMake/add_project_darknet.cmake
@@ -9,6 +9,18 @@
 
 set( VIAME_PROJECT_LIST ${VIAME_PROJECT_LIST} darknet )
 
+if( ${VIAME_DISABLE_GPU_SUPPORT} )
+  set( DARKNET_USE_GPU OFF )
+else()
+  set( DARKNET_USE_GPU ON )
+endif()
+
+if( ${VIAME_DISABLE_CUDNN_SUPPORT} )
+  set( DARKNET_USE_CUDNN OFF )
+else()
+  set( DARKNET_USE_CUDNN OFF )
+endif()
+
 ExternalProject_Add(darknet
   DEPENDS fletch
   PREFIX ${VIAME_BUILD_PREFIX}
@@ -17,12 +29,10 @@ ExternalProject_Add(darknet
   CMAKE_ARGS
     ${VIAME_ARGS_COMMON}
     ${VIAME_ARGS_fletch}
-    ${ScallopTK_BUILD_SHARED}
-    -DVC_TOOLNAMES:BOOL=ON
-    -DBUILD_TOOLS:BOOL=ON
-    -DBUILD_TESTS:BOOL=OFF
-    -DENABLE_CAFFE:BOOL=${VIAME_ENABLE_CAFFE}
-    -DCAFFE_CPU_ONLY:BOOL=${VIAME_DISABLE_GPU_SUPPORT}
+    -DINSTALL_HEADER_FILES:BOOL=ON
+    -DUSE_GPU:BOOL=${DARKNET_USE_GPU}
+    -DUSE_CUDNN:BOOL=${DARKNET_USE_CUDNN}
+    -DUSE_OPENCV:BOOL=${VIAME_ENABLE_OPENCV}
   INSTALL_DIR ${VIAME_BUILD_INSTALL_PREFIX}
   )
 
@@ -33,4 +43,8 @@ ExternalProject_Add_Step(darknet forcebuild
   DEPENDEES configure
   DEPENDERS build
   ALWAYS 1
+  )
+
+set(VIAME_ARGS_darknet
+  -Ddarknet_DIR:PATH=${VIAME_BUILD_PREFIX}/src/darknet-build
   )
