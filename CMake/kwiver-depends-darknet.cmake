193c641d3ad309970dce50cf23121002d3dfef8e
Matt Dawkins <matt.dawkins@kitware.com>
2017-03-10 21:40:14 -0500

    Temporary hack

--- CMake/kwiver-depends-darknet.cmake
+++ CMake/kwiver-depends-darknet.cmake
@@ -8,5 +8,8 @@ option( KWIVER_ENABLE_DARKNET
 if (KWIVER_ENABLE_DARKNET)
   find_package(darknet)
   include_directories( SYSTEM ${darknet_INCLUDE_DIR})
+  if (WIN32)
+    include_directories( SYSTEM ${darknet_INCLUDE_DIR}/../3rdparty/include)
+  endif()
   link_directories( ${darknet_LIBRARY_DIR} )
 endif()
