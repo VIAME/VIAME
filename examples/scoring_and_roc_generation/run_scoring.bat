65b632622db068cb0a7c6dcb2a0e4b44833a2289
Matt Dawkins <matt.dawkins@kitware.com>
2017-03-12 00:07:23 -0500

    Update bat scripts

--- examples/scoring_and_roc_generation/run_scoring.bat
+++ examples/scoring_and_roc_generation/run_scoring.bat
@@ -1,13 +1,17 @@
 @echo off
 
-score_events \
---computed-tracks detections.kw18 \
---truth-tracks fish_gt.kw18 --fn2ts --a PersonDigging \
---kw19-hack --act-ext-ct all --gt-prefiltered --roc-dump roc.plot
+SET VIAME_INSTALL=%~dp0\..\..\
 
-score_tracks \
-  --hadwav \
-  --computed-tracks detections.kw18 \
+%VIAME_INSTALL%\bin\score_events.exe      ^
+ --computed-tracks detections.kw18        ^
+ --truth-tracks fish_gt.kw18 --fn2ts      ^
+ --a PersonDigging --kw19-hack            ^
+ --act-ext-ct all --gt-prefiltered        ^
+ --roc-dump roc.plot
+
+%VIAME_INSTALL%\bin\score_tracks.exe      ^
+  --hadwav                                ^
+  --computed-tracks detections.kw18       ^
   --truth-tracks groundtruth.kw18 --fn2ts
 
 pause
