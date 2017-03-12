ec688188eeedf1773ac50391b427dc9472752265
Matt Dawkins <matt.dawkins@kitware.com>
2017-03-11 23:02:27 -0500

    Add files

---
+++ examples/visualizing_detections_in_gui/run_vpview.bat
@@ -0,0 +1,13 @@
+@echo off
+
+# Setup VIAME Paths (no need to set if installed to registry or already set up)
+
+SET VIAME_INSTALL=%~dp0\..\..\
+
+SET KWIVER_PLUGIN_PATH=%KWIVER_PLUGIN_PATH%;%VIAME_INSTALL%\lib\modules
+SET VIAME_PLUGIN_PATH=%VIAME_PLUGIN_PATH%;%VIAME_INSTALL%\lib\modules
+
+# Run Pipeline
+
+%VIAME_INSTALL%\bin\vpView.exe
+
---
+++ examples/visualizing_detections_in_gui/run_vpview.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Setup VIAME Paths (no need to run multiple times if you already ran it)
+
+source ../../setup_viame.sh 
+
+# Run vpView
+
+vpView
