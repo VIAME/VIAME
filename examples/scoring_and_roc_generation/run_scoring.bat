88dada06714f94513ed30f793932fb2d193c10c8
Matt Dawkins <matt.dawkins@kitware.com>
2017-03-11 23:00:06 -0500

    Update file headers

--- examples/scoring_and_roc_generation/run_scoring.bat
+++ examples/scoring_and_roc_generation/run_scoring.bat
@@ -1,6 +1,6 @@
+@echo off
 
-
-/home/matt/Dev/vidtk/build/bin/score_events \
+score_events \
 --computed-tracks yolo_v2_detections.kw18 \
 --truth-tracks fish_gt.kw18 --fn2ts --a PersonDigging \
 --kw19-hack --act-ext-ct all --gt-prefiltered --roc-dump roc.plot
