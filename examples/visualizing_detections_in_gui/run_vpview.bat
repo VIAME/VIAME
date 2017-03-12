cb9faa5e5937b8e5e6946a9194ef2ae37b9c6711
Matt Dawkins <matt.dawkins@kitware.com>
2017-03-12 12:43:46 -0400

    Simplify examples

--- examples/visualizing_detections_in_gui/run_vpview.bat
+++ examples/visualizing_detections_in_gui/run_vpview.bat
@@ -4,9 +4,6 @@ REM Setup VIAME Paths (no need to set if installed to registry or already set up
 
 SET VIAME_INSTALL=%~dp0\..\..\
 
-SET KWIVER_PLUGIN_PATH=%KWIVER_PLUGIN_PATH%;%VIAME_INSTALL%\lib\modules
-SET KWIVER_PLUGIN_PATH=%KWIVER_PLUGIN_PATH%;%VIAME_INSTALL%\lib\sprokit
-
 REM Run Pipeline
 
 %VIAME_INSTALL%\bin\vpView.exe
