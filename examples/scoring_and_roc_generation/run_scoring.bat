516d7ec901f061d1a1d53bc9424b171e6367a9c4
Matt Dawkins <matt.dawkins@kitware.com>
2017-03-11 23:04:42 -0500

    More example updates

--- examples/scoring_and_roc_generation/run_scoring.bat
+++ examples/scoring_and_roc_generation/run_scoring.bat
@@ -1,11 +1,11 @@
 @echo off
 
 score_events \
---computed-tracks yolo_v2_detections.kw18 \
+--computed-tracks detections.kw18 \
 --truth-tracks fish_gt.kw18 --fn2ts --a PersonDigging \
 --kw19-hack --act-ext-ct all --gt-prefiltered --roc-dump roc.plot
 
 score_tracks \
   --hadwav \
-  --computed-tracks yolo_v2_detections.kw18 \
-  --truth-tracks fish_gt.kw18 --fn2ts
+  --computed-tracks detections.kw18 \
+  --truth-tracks groundtruth.kw18 --fn2ts
