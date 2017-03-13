d3ebc73bb75f90a3b2a8750af8a779fe5499c850
Matt Dawkins <matt.dawkins@kitware.com>
2017-03-12 23:31:50 -0400

    Update scoring example

--- examples/scoring_and_roc_generation/run_scoring.sh
+++ examples/scoring_and_roc_generation/run_scoring.sh
@@ -1,11 +1,26 @@
 
+#!/bin/sh
 
-score_events \
---computed-tracks detections.kw18 \
---truth-tracks fish_gt.kw18 --fn2ts --a PersonDigging \
---kw19-hack --act-ext-ct all --gt-prefiltered --roc-dump roc.plot
+# Setup VIAME Paths (no need to run multiple times if you already ran it)
+
+source ../../setup_viame.sh
+
+# Run score tracks on data for singular metrics
 
 score_tracks \
-  --hadwav \
+  --hadwav --fn2ts \       # Scoring options, use frames not timestamps
+  --computed-tracks detections.kw18 \  # Computed tracks
+  --truth-tracks groundtruth.kw18 \  # Groundtruth (annotated) tracks
+  > score_tracks_output.txt   # Output file
+
+# Generate ROC
+
+score_events \
   --computed-tracks detections.kw18 \
-  --truth-tracks groundtruth.kw18 --fn2ts
+  --truth-tracks groundtruth.kw18 \
+  --fn2ts --kw19-hack --gt-prefiltered --ct-prefiltered \
+  --roc-dump roc.plot
+
+# Plot ROC
+
+python plotroc.py roc.plot
