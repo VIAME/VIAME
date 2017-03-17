5313358b0bc4325678f42957f05bd165137cf4e1
Linus Sherrill <linus.sherrill@kitware.com>
2017-03-17 11:18:33 -0400

    Add missing methods

--- vital/bindings/c/types/detected_object.h
+++ vital/bindings/c/types/detected_object.h
@@ -1,5 +1,5 @@
 /*ckwg +29
- * Copyright 2016 by Kitware, Inc.
+ * Copyright 2016-2017 by Kitware, Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -39,7 +39,9 @@
 #include <vital/bindings/c/vital_c_export.h>
 #include <vital/bindings/c/types/bounding_box.h>
 #include <vital/bindings/c/types/detected_object_type.h>
+#include <vital/bindings/c/types/image.h>
 
+#include <stdint.h>
 #include <stddef.h>
 
 #ifdef __cplusplus
@@ -51,31 +53,56 @@ extern "C"
 typedef struct vital_detected_object_s vital_detected_object_t;
 
 VITAL_C_EXPORT
-void vital_detected_object_destroy();
+void vital_detected_object_destroy(vital_detected_object_t * obj);
 
 VITAL_C_EXPORT
 vital_detected_object_t* vital_detected_object_new_with_bbox(
+  vital_detected_object_t * obj,
   vital_bounding_box_t* bbox,
   double confidence,
   vital_detected_object_type_t* dot); // optional, could be NULL
 
 VITAL_C_EXPORT
-vital_bounding_box_t* vital_detected_object_bounding_box();
+vital_bounding_box_t* vital_detected_object_bounding_box(vital_detected_object_t * obj);
 
 VITAL_C_EXPORT
-void vital_detected_object_set_bounding_box( vital_bounding_box_t* bbox );
+void vital_detected_object_set_bounding_box( vital_detected_object_t * obj,
+                                             vital_bounding_box_t* bbox );
 
 VITAL_C_EXPORT
-double vital_detected_object_confidence();
+double vital_detected_object_confidence(vital_detected_object_t * obj);
 
 VITAL_C_EXPORT
-void vital_detected_object_set_confidence( double conf );
+void vital_detected_object_set_confidence( vital_detected_object_t * obj,
+                                           double conf );
 
 VITAL_C_EXPORT
-vital_detected_object_type_t* vital_detected_object_get_type();
+vital_detected_object_type_t* vital_detected_object_get_type(vital_detected_object_t * obj);
 
 VITAL_C_EXPORT
-void vital_detected_object_set_type( vital_detected_object_type_t*);
+void vital_detected_object_set_type( vital_detected_object_t * obj,
+                                     vital_detected_object_type_t* ob_type);
+
+VITAL_C_EXPORT
+int64_t vital_detected_object_index(vital_detected_object_t * obj);
+
+VITAL_C_EXPORT
+void vital_detected_object_set_index(vital_detected_object_t * obj,
+                                     int64_t idx);
+
+VITAL_C_EXPORT
+char* vital_detected_object_detector_name(vital_detected_object_t * obj);
+
+VITAL_C_EXPORT
+void vital_detected_object_detector_setname(vital_detected_object_t * obj,
+                                            char* name );
+
+VITAL_C_EXPORT
+vital_image_t* vital_detected_object_mask(vital_detected_object_t * obj);
+
+VITAL_C_EXPORT
+void vital_detected_object_set_mask(vital_detected_object_t * obj,
+                                    vital_image_t* mask);
 
 #ifdef __cplusplus
 }
