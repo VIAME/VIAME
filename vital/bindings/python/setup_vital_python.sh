f451f84eb391569df85eb3f7ccae7db265666419
Matt Brown <matt.brown@kitware.com>
2017-03-23 13:14:36 -0400

    Update copyright

--- vital/bindings/python/setup_vital_python.sh
+++ vital/bindings/python/setup_vital_python.sh
@@ -1,6 +1,5 @@
 #
 # Setting up build environment python bindings
 #
-#SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)"
-#export PYTHONPATH=${SCRIPT_DIR}:$PYTHONPATH
-export PYTHONPATH=/home/matt/libraries/KWIVER/vital/bindings/python:$PYTHONPATH
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)"
+export PYTHONPATH=${SCRIPT_DIR}:$PYTHONPATH
