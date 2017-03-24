b8ab8a5030771adae82132772f76f332280a1232
Matt Dawkins <matt.dawkins@kitware.com>
2017-03-24 02:23:14 -0400

    Begin to improve installer routine

--- CMake/build_package.cmake
+++ CMake/build_package.cmake
@@ -2,26 +2,53 @@
 # top level build file for NOAA VIAME Package Util
 ##
 
-file( GLOB VIAME_BIN_FILES "${CMAKE_INSTALL_PREFIX}/bin/*"  )
-list( APPEND FIXUP_DIRS ${CMAKE_INSTALL_PREFIX}/bin )
+set( CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/CMake ${CMAKE_MODULE_PATH} )
 
-file( GLOB VIAME_LIB_FILES "${CMAKE_INSTALL_PREFIX}/lib/*"  )
-list( APPEND FIXUP_DIRS ${CMAKE_INSTALL_PREFIX}/lib )
+if( NOT DEFINED VIAME_INSTALL_DIR )
+  set( VIAME_INSTALL_DIR "${VIAME_BUILD_INSTALL_PREFIX}")
+endif()
 
-file( GLOB VIAME_INCLUDE_FILES "${CMAKE_INSTALL_PREFIX}/include/*"  )
-list( APPEND FIXUP_DIRS ${CMAKE_INSTALL_PREFIX}/include )
+set( FIXUP_LIBS )
+set( FIXUP_DIRS )
 
-file( GLOB VIAME_GUI_PLUGINS_FILES "${CMAKE_INSTALL_PREFIX}/plugins/*"  )
-list( APPEND FIXUP_DIRS ${CMAKE_INSTALL_PREFIX}/plugins )
+macro( add_to_fixup_libs dir_path )
+  if( WIN32 )
+    file( GLOB FILES_TO_ADD "${dir_path}/*.dll" )
+    set( FIXUP_LIBS ${FIXUP_LIBS} ${FILES_TO_ADD} )
+  else()
+    file( GLOB FILES_TO_ADD "${dir_path}/*.so" )
+    set( FIXUP_LIBS ${FIXUP_LIBS} ${FILES_TO_ADD} )
+  endif()
+endmacro()
 
-file( GLOB VIAME_SPRKIT_PLUGINS_FILES "${CMAKE_INSTALL_PREFIX}/lib/sprokit/*" )
-list( APPEND FIXUP_DIRS ${CMAKE_INSTALL_PREFIX}/lib/sprokit )
+file( GLOB VIAME_BIN_FILES "${VIAME_INSTALL_DIR}/bin/*"  )
+list( APPEND FIXUP_DIRS ${VIAME_INSTALL_DIR}/bin )
 
-file( GLOB VIAME_VIAME_PLUGINS_FILES "${CMAKE_INSTALL_PREFIX}/lib/modules/*" )
-list( APPEND FIXUP_DIRS ${CMAKE_INSTALL_PREFIX}/lib/modules )
+file( GLOB VIAME_LIB_FILES "${VIAME_INSTALL_DIR}/lib/*"  )
+list( APPEND FIXUP_DIRS ${VIAME_INSTALL_DIR}/lib )
 
-file( GLOB VIAME_PLUGIN_PLUGINS_FILES "${CMAKE_INSTALL_PREFIX}/lib/modules/plugin_explorer/*" )
-list( APPEND FIXUP_DIRS ${CMAKE_INSTALL_PREFIX}/lib/modules/plugin_explorer/ )
+file( GLOB VIAME_INCLUDE_FILES "${VIAME_INSTALL_DIR}/include/*"  )
+list( APPEND FIXUP_DIRS ${VIAME_INSTALL_DIR}/include )
+
+file( GLOB VIAME_GUI_PLUGINS_FILES "${VIAME_INSTALL_DIR}/plugins/*"  )
+list( APPEND FIXUP_DIRS ${VIAME_INSTALL_DIR}/plugins )
+
+file( GLOB VIAME_SPRKIT_PLUGINS_FILES "${VIAME_INSTALL_DIR}/lib/sprokit/*" )
+list( APPEND FIXUP_DIRS ${VIAME_INSTALL_DIR}/lib/sprokit )
+
+file( GLOB VIAME_VIAME_PLUGINS_FILES "${VIAME_INSTALL_DIR}/lib/modules/*" )
+list( APPEND FIXUP_DIRS ${VIAME_INSTALL_DIR}/lib/modules )
+add_to_fixup_libs( ${VIAME_INSTALL_DIR}/lib/modules )
+
+file( GLOB VIAME_PLUGIN_PLUGINS_FILES "${VIAME_INSTALL_DIR}/lib/modules/plugin_explorer/*" )
+list( APPEND FIXUP_DIRS ${VIAME_INSTALL_DIR}/lib/modules/plugin_explorer )
+
+if( VIAME_ENABLE_MATLAB )
+  get_filename_component( MATLAB_LIB_PATH ${Matlab_MEX_LIBRARY} DIRECTORY )
+  set( MATLAB_DLL_PATH ${Matlab_ROOT_DIR}/bin/win64 )
+  list( APPEND FIXUP_DIRS ${MATLAB_LIB_PATH} )
+  list( APPEND FIXUP_DIRS ${MATLAB_DLL_PATH} )
+endif()
 
 set( CPACK_PACKAGE_DESCRIPTION_SUMMARY "VIAME" )
 set( CPACK_PACKAGE_VENDOR              "Kitware, NOAA, and Friends" )
@@ -35,31 +62,38 @@ set( CPACK_PACKAGE_INSTALL_DIRECTORY   "VIAME-${CMake_VERSION_MAJOR}.${CMake_VER
 include( BundleUtilities )
 include( InstallRequiredSystemLibraries )
 
-set( FIXUP_LIBS )
 
-foreach( path_id ${FIXUP_DIRS} )
-  if( WIN32 )
-    file( GLOB FILES_TO_ADD "${path_id}/*.dll" )
-    set( FIXUP_LIBS ${FIXUP_LIBS} ${FILES_TO_ADD} )
-  else()
-    file( GLOB FILES_TO_ADD "${path_id}/*.so" )
-    set( FIXUP_LIBS ${FIXUP_LIBS} ${FILES_TO_ADD} )
-  endif()
-endforeach()
+#foreach( path_id ${FIXUP_DIRS} )
+#  if( WIN32 )
+#    file( GLOB FILES_TO_ADD "${path_id}/*.dll" )
+#    set( FIXUP_LIBS ${FIXUP_LIBS} ${FILES_TO_ADD} )
+#  else()
+#    file( GLOB FILES_TO_ADD "${path_id}/*.so" )
+#    set( FIXUP_LIBS ${FIXUP_LIBS} ${FILES_TO_ADD} )
+#  endif()
+#endforeach()
 
 if( WIN32 )
-  set( pipeline_runner_app "${CMAKE_INSTALL_PREFIX}/bin/pipeline_runner.exe" )
+  set( PIPELINE_RUNNER_APP "${VIAME_INSTALL_DIR}/bin/pipeline_runner.exe" )
 else()
-  set( pipeline_runner_app "${CMAKE_INSTALL_PREFIX}/bin/pipeline_runner" )
+  set( PIPELINE_RUNNER_APP "${VIAME_INSTALL_DIR}/bin/pipeline_runner" )
 endif()
 
-if( WIN32 )
-  fixup_bundle(\"${pipeline_runner_app}\" \"${FIXUP_LIBS}\" \"${FIXUP_DIRS}\")
+fixup_bundle( "${PIPELINE_RUNNER_APP}" "${FIXUP_LIBS}" "${FIXUP_DIRS}" )
+
+if( VIAME_ENABLE_YOLO )
+  if( WIN32 )
+    set( DARKNET_APP "${VIAME_INSTALL_DIR}/bin/darknet.exe" )
+  else()
+    set( DARKNET_APP "${VIAME_INSTALL_DIR}/bin/darknet" )
+  endif()
+
+  fixup_bundle( "${DARKNET_APP}" "" "${FIXUP_DIRS}" )
 endif()
 
 if( CMAKE_INSTALL_SYSTEM_RUNTIME_LIBS )
   install( PROGRAMS ${CMAKE_INSTALL_SYSTEM_RUNTIME_LIBS} DESTINATION bin COMPONENT System )
-endif( CMAKE_INSTALL_SYSTEM_RUNTIME_LIBS )
+endif()
 
 #    if( WIN32 AND NOT UNIX )
 #      set( CPACK_PACKAGE_ICON "${CMake_SOURCE_DIR}/Utilities/Release\\\\InstallIcon.bmp")
