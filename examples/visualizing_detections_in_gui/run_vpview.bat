5f95cf741069c397e1b7e43f2183536c26e3372f
Matt Dawkins <matt.dawkins@kitware.com>
2017-03-27 03:59:08 -0400

    Update pipeline

--- examples/visualizing_detections_in_gui/run_vpview.bat
+++ examples/visualizing_detections_in_gui/run_vpview.bat
@@ -3,6 +3,7 @@
 REM Setup VIAME Paths (no need to set if installed to registry or already set up)
 
 SET VIAME_INSTALL=%~dp0\..\..\
+set VIDTK_MODULE_PATH=%VIAME_INSTALL%\lib\modules
 
 REM Run Pipeline
 
