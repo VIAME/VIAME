eca8130ba5da5922e6a813b98c187a71fd50c134
Matt Dawkins <matt.dawkins@kitware.com>
2017-04-11 09:48:42 -0400

    Add start of darknet implementation

---
+++ arrows/darknet/darknet_trainer.cxx
@@ -0,0 +1,140 @@
+/*ckwg +29
+ * Copyright 2017 by Kitware, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  * Neither name of Kitware, Inc. nor the names of any contributors may be used
+ *    to endorse or promote products derived from this software without specific
+ *    prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "darknet_trainer.h"
+
+#include <vital/logger/logger.h>
+#include <vital/util/cpu_timer.h>
+
+#include <arrows/ocv/image_container.h>
+#include <kwiversys/SystemTools.hxx>
+
+#include <opencv2/core/core.hpp>
+
+#include <string>
+#include <sstream>
+
+namespace kwiver {
+namespace arrows {
+namespace darknet {
+
+// ==================================================================
+class darknet_trainer::priv
+{
+public:
+  priv()
+    : m_net_config( "" )
+    , m_weight_file( "" )
+  {}
+
+  ~priv()
+  {
+  }
+
+  // Items from the config
+  std::string m_net_config;
+  std::string m_weight_file;
+};
+
+
+// ==================================================================
+darknet_trainer::
+darknet_trainer()
+  : d( new priv() )
+{
+}
+
+darknet_trainer::
+~darknet_trainer()
+{
+}
+
+
+// --------------------------------------------------------------------
+vital::config_block_sptr
+darknet_trainer::
+get_configuration() const
+{
+  // Get base config from base class
+  vital::config_block_sptr config = vital::algorithm::get_configuration();
+
+  config->set_value( "net_config", d->m_net_config, "Name of network config file." );
+  config->set_value( "weight_file", d->m_weight_file, "Name of optional weight file." );
+
+  return config;
+}
+
+
+// --------------------------------------------------------------------
+void
+darknet_trainer::
+set_configuration( vital::config_block_sptr config_in )
+{
+  // Starting with our generated config_block to ensure that assumed values are present
+  // An alternative is to check for key presence before performing a get_value() call.
+  vital::config_block_sptr config = this->get_configuration();
+
+  config->merge_config( config_in );
+
+  this->d->m_net_config  = config->get_value< std::string >( "net_config" );
+  this->d->m_weight_file = config->get_value< std::string >( "weight_file" );
+}
+
+
+// --------------------------------------------------------------------
+bool
+darknet_trainer::
+check_configuration( vital::config_block_sptr config ) const
+{
+  std::string net_config = config->get_value< std::string >( "net_config" );
+  std::string class_file = config->get_value< std::string >( "class_names" );
+
+  if( net_config.empty() || !kwiversys::SystemTools::FileExists( net_config ) )
+  {
+    LOG_ERROR( logger(), "net config file \"" << net_config << "\" not found." );
+    return false;
+  }
+
+  return true;
+}
+
+
+// --------------------------------------------------------------------
+void
+darknet_trainer::
+train_from_disk(std::vector< std::string > train_image_names,
+  std::vector< kwiver::vital::detected_object_set_sptr > train_groundtruth,
+  std::vector< std::string > test_image_names,
+  std::vector< kwiver::vital::detected_object_set_sptr > test_groundtruth)
+{
+}
+
+
+} } } // end namespace
