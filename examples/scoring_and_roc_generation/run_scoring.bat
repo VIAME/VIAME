689eb41bb3302594575bfc4be133233ec3e023a5
Matt Dawkins <matt.dawkins@kitware.com>
2017-04-23 16:38:20 -0700

    Modify scripts to use new setup script

--- examples/scoring_and_roc_generation/run_scoring.bat
+++ examples/scoring_and_roc_generation/run_scoring.bat
@@ -1,17 +1,19 @@
 @echo off
 
-SET VIAME_INSTALL=%~dp0\..\..\
+REM Setup VIAME Paths (no need to set if installed to registry or already set up)
+
+CALL .\..\..\setup_viame.bat
 
 REM Run score tracks on data for singular metrics
 
-%VIAME_INSTALL%\bin\score_tracks.exe      ^
+score_tracks.exe                          ^
   --hadwav                                ^
   --computed-tracks detections.kw18       ^
   --truth-tracks groundtruth.kw18 --fn2ts
 
 REM Generate ROC
 
-%VIAME_INSTALL%\bin\score_events.exe      ^
+score_events.exe                          ^
  --computed-tracks detections.kw18        ^
  --truth-tracks groundtruth.kw18          ^
  --fn2ts --kw19-hack --gt-prefiltered     ^
