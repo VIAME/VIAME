de0c7a7d3320a110cf2378de595f219b59bbfa3a
Linus Sherrill <linus.sherrill@kitware.com>
2017-04-24 17:09:01 -0400

    Move files from tools to pipeline_utils

--- sprokit/src/sprokit/pipeline_util/pipeline_builder.cxx
+++ sprokit/src/sprokit/pipeline_util/pipeline_builder.cxx
@@ -1,5 +1,5 @@
 /*ckwg +29
- * Copyright 2011-2012 by Kitware, Inc.
+ * Copyright 2011-2017 by Kitware, Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -30,25 +30,15 @@
 
 #include "pipeline_builder.h"
 
-#include "tool_io.h"
-#include "tool_usage.h"
-
 #include <sprokit/pipeline_util/load_pipe.h>
-#include <sprokit/pipeline_util/path.h>
 #include <sprokit/pipeline_util/pipe_bakery.h>
 #include <sprokit/pipeline_util/pipe_declaration_types.h>
+#include <sprokit/pipeline/pipeline.h>
 
 #include <vital/config/config_block.h>
-#include <sprokit/pipeline/pipeline.h>
-#include <sprokit/pipeline/scheduler_factory.h>
+#include <vital/util/tokenize.h>
 
-#include <boost/algorithm/string/classification.hpp>
-#include <boost/algorithm/string/split.hpp>
 #include <boost/filesystem/operations.hpp>
-#include <boost/program_options/options_description.hpp>
-#include <boost/program_options/value_semantic.hpp>
-#include <boost/program_options/variables_map.hpp>
-#include <boost/bind.hpp>
 
 #include <algorithm>
 #include <stdexcept>
@@ -64,32 +54,6 @@ namespace
 static std::string const split_str = "=";
 
 }
-
-pipeline_builder
-::pipeline_builder(boost::program_options::variables_map const& vm,
-                   boost::program_options::options_description const& desc)
-  : m_blocks()
-{
-  if (!vm.count("pipeline"))
-  {
-    std::cerr << "Error: pipeline option not set" << std::endl;
-
-    tool_usage(EXIT_FAILURE, desc);
-  }
-
-  {
-    sprokit::path_t const ipath = vm["pipeline"].as<sprokit::path_t>();
-
-    istream_t const istr = open_istream(ipath);
-
-    /// \todo Include paths?
-
-    load_pipeline(*istr);
-  }
-
-  load_from_options(vm);
-}
-
 
 // ==================================================================
 pipeline_builder
@@ -116,30 +80,7 @@ pipeline_builder
 // ------------------------------------------------------------------
 void
 pipeline_builder
-::load_from_options(boost::program_options::variables_map const& vm)
-{
-  // Load supplemental configuration files.
-  if (vm.count("config"))
-  {
-    sprokit::paths_t const configs = vm["config"].as<sprokit::paths_t>();
-
-    std::for_each(configs.begin(), configs.end(), boost::bind(&pipeline_builder::load_supplement, this, _1));
-  }
-
-  // Insert lone setting variables from the command line.
-  if (vm.count("setting"))
-  {
-    std::vector<std::string> const settings = vm["setting"].as<std::vector<std::string> >();
-
-    std::for_each(settings.begin(), settings.end(), boost::bind(&pipeline_builder::add_setting, this, _1));
-  }
-}
-
-
-// ------------------------------------------------------------------
-void
-pipeline_builder
-::load_supplement(sprokit::path_t const& path)
+::load_supplement( kwiver::vital::path_t const& path)
 {
   sprokit::pipe_blocks const supplement = sprokit::load_pipe_blocks_from_file(path);
 
@@ -173,24 +114,14 @@ pipeline_builder
 
   kwiver::vital::config_block_keys_t keys;
 
-  if (kwiver::vital::config_block::block_sep.size() != 1)
-  {
-    static std::string const reason = "Error: The block separator is longer than "
-                                      "one character and does not work here (this "
-                                      "is a sprokit limitation)";
-
-    throw std::runtime_error(reason);
-  }
-
-  /// \bug Does not work if (kwiver::vital::config_block::block_sep.size() != 1).
-  boost::split(keys, setting_key, boost::is_any_of(kwiver::vital::config_block::block_sep));
+  kwiver::vital::tokenize( setting_key, keys, kwiver::vital::config_block::block_sep, true );
 
   if (keys.size() < 2)
   {
-    std::string const reason = "Error: The key in the command line setting "
+    std::string const reason = "Error: The key portion of setting "
                                "\'" + setting + "\' does not contain "
                                "at least two keys in its keypath which is "
-                               "invalid";
+                               "invalid. (e.g. must be at least a:b)";
 
     throw std::runtime_error(reason);
   }
@@ -231,66 +162,6 @@ pipeline_builder
 ::blocks() const
 {
   return m_blocks;
-}
-
-
-// ------------------------------------------------------------------
-boost::program_options::options_description
-pipeline_common_options()
-{
-  boost::program_options::options_description desc("Common options");
-
-  desc.add_options()
-    ("config,c", boost::program_options::value<sprokit::paths_t>()->value_name("FILE"), "File containing supplemental configuration entries. ")
-    ("setting,s", boost::program_options::value<std::vector<std::string> >()->value_name("VAR=VALUE"), "additional configuration")
-    ("include,I", boost::program_options::value<sprokit::paths_t>()->value_name("DIR"),
-     "a directory to be added to configuration include path")
-  ;
-
-  return desc;
-}
-
-
-// ------------------------------------------------------------------
-boost::program_options::options_description
-pipeline_input_options()
-{
-  boost::program_options::options_description desc("Input options");
-
-  desc.add_options()
-    ("pipeline,p", boost::program_options::value<sprokit::path_t>()->value_name("FILE"),
-     "Main input pipeline description file")
-  ;
-
-  return desc;
-}
-
-
-// ------------------------------------------------------------------
-boost::program_options::options_description
-pipeline_output_options()
-{
-  boost::program_options::options_description desc("Output options");
-
-  desc.add_options()
-    ("output,o", boost::program_options::value<sprokit::path_t>()->value_name("FILE")->default_value("-"), "output path")
-  ;
-
-  return desc;
-}
-
-
-// ------------------------------------------------------------------
-boost::program_options::options_description
-pipeline_run_options()
-{
-  boost::program_options::options_description desc("Run options");
-
-  desc.add_options()
-    ("scheduler,S", boost::program_options::value<sprokit::scheduler::type_t>()->value_name("TYPE"), "scheduler type")
-  ;
-
-  return desc;
 }
 
 }
