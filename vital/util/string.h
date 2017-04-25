d206a930380cdb9f4fb5ff74caae20833c73e905
Linus Sherrill <linus.sherrill@kitware.com>
2017-04-25 10:23:44 -0400

    Move string code from header to source file.

--- vital/util/string.h
+++ vital/util/string.h
@@ -31,13 +31,11 @@
 #ifndef KWIVER_VITAL_UTIL_STRING_FORMAT_H
 #define KWIVER_VITAL_UTIL_STRING_FORMAT_H
 
+#include <vital/util/vital_util_export.h>
+
 #include <stdarg.h>  // For va_start, etc.
-#include <memory>    // For std::unique_ptr
 #include <string>
-#include <cstring>
-#include <sstream>
-#include <algorithm>
-#include <iterator>
+#include <vector>
 
 namespace kwiver {
 namespace vital {
@@ -52,34 +50,8 @@ namespace vital {
  *
  * @return Formatted string.
  */
-inline std::string
-string_format( const std::string fmt_str, ... )
-{
-  int final_n, n = ( (int)fmt_str.size() ) * 2; /* Reserve two times as much as the length of the fmt_str */
-  std::string str;
-  std::unique_ptr< char[] > formatted;
-  va_list ap;
-
-  while ( 1 )
-  {
-    formatted.reset( new char[n] );   /* Wrap the plain char array into the unique_ptr */
-    strcpy( &formatted[0], fmt_str.c_str() );
-    va_start( ap, fmt_str );
-    final_n = vsnprintf( &formatted[0], n, fmt_str.c_str(), ap );
-    va_end( ap );
-    if ( ( final_n < 0 ) || ( final_n >= n ) )
-    {
-      n += abs( final_n - n + 1 );
-    }
-    else
-    {
-      break;
-    }
-  }
-
-  return std::string( formatted.get() );
-}
-
+VITAL_UTIL_EXPORT std::string
+string_format( const std::string fmt_str, ... );
 
 /**
  * @brief Does string start with pattern
@@ -98,7 +70,6 @@ starts_with( const std::string& input, const std::string& pattern)
   return (0 == input.compare( 0, pattern.size(), pattern ) );
 }
 
-
 /**
  * @brief Join a set of strings with specified separator.
  *
@@ -111,28 +82,8 @@ starts_with( const std::string& input, const std::string& pattern)
  *
  * @return Single string with all elements joined with separator.
  */
-inline std::string
-join( const std::vector<std::string>& elements, const std::string& str_separator)
-{
-  const char* const separator = str_separator.c_str();
-  switch (elements.size())
-  {
-  case 0:
-    return "";
-
-  case 1:
-    return elements[0];
-
-  default:
-  {
-    std::ostringstream os;
-    std::copy(elements.begin(), elements.end()-1,
-              std::ostream_iterator<std::string>(os, separator));
-    os << *elements.rbegin();
-    return os.str();
-  }
-  } // end switch
-}
+VITAL_UTIL_EXPORT std::string
+join( const std::vector<std::string>& elements, const std::string& str_separator);
 
 } } // end namespace
 
