5123ea9e2ca2fcb27a11a44f510c64b7221ee2e1
Matt Dawkins <matt.dawkins@kitware.com>
2017-05-07 11:03:52 -0400

    Update trainer

--- arrows/darknet/darknet_trainer.cxx
+++ arrows/darknet/darknet_trainer.cxx
@@ -29,6 +29,7 @@
  */
 
 #include "darknet_trainer.h"
+#include "darknet_custom_resize.h"
 
 #include <vital/logger/logger.h>
 #include <vital/util/cpu_timer.h>
@@ -50,17 +51,35 @@ class darknet_trainer::priv
 {
 public:
   priv()
-    : m_net_config( "" )
-    , m_weight_file( "" )
+    : m_skip_format( false )
+    , m_gpu_index( -1 )
+    , m_resize_option( "maintain_ar" )
+    , m_scale( 1.0 )
+    , m_resize_i( 0 )
+    , m_resize_j( 0 )
+    , m_chip_step( 100 )
   {}
 
   ~priv()
-  {
-  }
+  {}
 
   // Items from the config
   std::string m_net_config;
-  std::string m_weight_file;
+  std::string m_output_weights;
+  std::string m_tmp_directory;
+  bool m_skip_format;
+  int m_gpu_index;
+  std::string m_resize_option;
+  double m_scale;
+  int m_resize_i;
+  int m_resize_j;
+  int m_chip_step;
+
+  // Helper functions
+  double format_image( const cv::Mat& src, cv::Mat& dst );
+  double scale_image_maintaining_ar( const cv::Mat& src, cv::Mat& dst );
+
+  kwiver::vital::logger_handle_t m_logger;
 };
 
 
@@ -85,8 +104,28 @@ get_configuration() const
   // Get base config from base class
   vital::config_block_sptr config = vital::algorithm::get_configuration();
 
-  config->set_value( "net_config", d->m_net_config, "Name of network config file." );
-  config->set_value( "weight_file", d->m_weight_file, "Name of optional weight file." );
+  config->set_value( "net_config", d->m_net_config,
+    "Name of network config file." );
+  config->set_value( "output_weights", d->m_output_weights,
+    "Output weights file." );
+  config->set_value( "tmp_directory", d->m_tmp_directory,
+    "Temp directory for all files used in training." );
+  config->set_value( "skip_format", d->m_skip_format,
+    "Skip file formatting, assume that the tmp_directory is pre-populated "
+    "with all files required for model training." );
+  config->set_value( "gpu_index", d->m_gpu_index,
+    "GPU index. Only used when darknet is compiled with GPU support." );
+  config->set_value( "resize_option", d->m_resize_option,
+    "Pre-processing resize option, can be: disabled, maintain_ar, scale, "
+    "chip, or chip_and_original." );
+  config->set_value( "scale", d->m_scale,
+    "Image scaling factor used when resize_option is scale or chip." );
+  config->set_value( "resize_ni", d->m_resize_i,
+    "Width resolution after resizing" );
+  config->set_value( "resize_nj", d->m_resize_j,
+    "Height resolution after resizing" );
+  config->set_value( "chip_step", d->m_chip_step,
+    "When in chip mode, the chip step size between chips." );
 
   return config;
 }
@@ -104,7 +143,15 @@ set_configuration( vital::config_block_sptr config_in )
   config->merge_config( config_in );
 
   this->d->m_net_config  = config->get_value< std::string >( "net_config" );
-  this->d->m_weight_file = config->get_value< std::string >( "weight_file" );
+  this->d->m_output_weights = config->get_value< std::string >( "output_weights" );
+  this->d->m_tmp_directory = config->get_value< std::string >( "tmp_directory" );
+  this->d->m_skip_format = config->get_value< bool >( "skip_format" );
+  this->d->m_gpu_index   = config->get_value< int >( "gpu_index" );
+  this->d->m_resize_option = config->get_value< std::string >( "resize_option" );
+  this->d->m_scale       = config->get_value< double >( "scale" );
+  this->d->m_resize_i    = config->get_value< int >( "resize_i" );
+  this->d->m_resize_j    = config->get_value< int >( "resize_j" );
+  this->d->m_chip_step   = config->get_value< int >( "chip_step" );
 }
 
 
@@ -133,6 +180,7 @@ train_from_disk(std::vector< std::string > train_image_names,
   std::vector< std::string > test_image_names,
   std::vector< kwiver::vital::detected_object_set_sptr > test_groundtruth)
 {
+
 }
 
 
