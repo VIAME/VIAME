608e4fe8eb90ebba0945ed81d89c4c2ff14259f9
Matt Dawkins <matt.dawkins@kitware.com>
2017-05-24 06:54:52 -0400

    Add example base classes

--- vital/algo/formulate_query.cxx
+++ vital/algo/formulate_query.cxx
@@ -1,5 +1,5 @@
 /*ckwg +29
- * Copyright 2017 by Kitware, Inc.
+ * Copyright 2014-2015 by Kitware, Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -28,56 +28,38 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-/**
- * \file
- * \brief compute_track_descriptors algorithm definition
- */
+#include <vital/algo/algorithm.txx>
 
-#ifndef VITAL_ALGO_COMPUTE_TRACK_DESCRIPTORS_H_
-#define VITAL_ALGO_COMPUTE_TRACK_DESCRIPTORS_H_
+#include "formulate_query.h"
 
-#include <vital/vital_config.h>
-
-#include <vital/algo/algorithm.h>
-#include <vital/types/image_container.h>
-#include <vital/types/descriptor_set.h>
-#include <vital/types/track_set.h>
+INSTANTIATE_ALGORITHM_DEF( kwiver::vital::algo::formulate_query );
 
 namespace kwiver {
 namespace vital {
 namespace algo {
 
-/// An abstract base class for extracting feature descriptors
-class VITAL_ALGO_EXPORT compute_track_descriptors
-  : public kwiver::vital::algorithm_def<compute_track_descriptors>
+formulate_query
+::formulate_query()
 {
-public:
-  /// Return the name of this algorithm
-  static std::string static_type_name() { return "compute_track_descriptors"; }
-
-  /// Extract from the image a descriptor corresoponding to each feature
-  /**
-   * \param image_data contains the image data to process
-   * \param features the feature locations at which descriptors are extracted
-   * \param image_mask Mask image of the same dimensions as \p image_data where
-   *                   positive values indicate regions of \p image_data to
-   *                   consider.
-   * \returns a set of feature descriptors
-   */
-  virtual kwiver::vital::descriptor_set_sptr
-  extract( kwiver::vital::image_container_sptr image_data,
-           kwiver::vital::track_set_sptr tracks ) const = 0;
-
-protected:
-  compute_track_descriptors();
-
-};
+  attach_logger( "formulate_query" );
+}
 
 
-/// Shared pointer for base compute_track_descriptors algorithm definition class
-typedef std::shared_ptr<compute_track_descriptors> compute_track_descriptors_sptr;
+/// Set this algorithm's properties via a config block
+void
+formulate_query
+::set_configuration( kwiver::vital::config_block_sptr config )
+{
+  (void) config;
+}
 
+/// Check that the algorithm's current configuration is valid
+bool
+formulate_query
+::check_configuration( kwiver::vital::config_block_sptr config ) const
+{
+  (void) config;
+  return true;
+}
 
 } } } // end namespace
-
-#endif // VITAL_ALGO_COMPUTE_TRACK_DESCRIPTORS_H_
--- vital/algo/formulate_query.h
+++ vital/algo/formulate_query.h
@@ -1,5 +1,5 @@
 /*ckwg +29
- * Copyright 2017 by Kitware, Inc.
+ * Copyright 2014-2015 by Kitware, Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -28,56 +28,47 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-/**
- * \file
- * \brief compute_track_descriptors algorithm definition
- */
-
-#ifndef VITAL_ALGO_COMPUTE_TRACK_DESCRIPTORS_H_
-#define VITAL_ALGO_COMPUTE_TRACK_DESCRIPTORS_H_
+#ifndef VITAL_ALGO_FORMULATE_QUERY_H_
+#define VITAL_ALGO_FORMULATE_QUERY_H_
 
 #include <vital/vital_config.h>
+
+#include <string>
+#include <memory>
 
 #include <vital/algo/algorithm.h>
 #include <vital/types/image_container.h>
 #include <vital/types/descriptor_set.h>
-#include <vital/types/track_set.h>
 
 namespace kwiver {
 namespace vital {
 namespace algo {
 
-/// An abstract base class for extracting feature descriptors
-class VITAL_ALGO_EXPORT compute_track_descriptors
-  : public kwiver::vital::algorithm_def<compute_track_descriptors>
+/// An abstract base class for converting base image type
+class VITAL_ALGO_EXPORT formulate_query
+  : public kwiver::vital::algorithm_def<formulate_query>
 {
 public:
   /// Return the name of this algorithm
-  static std::string static_type_name() { return "compute_track_descriptors"; }
+  static std::string static_type_name() { return "formulate_query"; }
 
-  /// Extract from the image a descriptor corresoponding to each feature
-  /**
-   * \param image_data contains the image data to process
-   * \param features the feature locations at which descriptors are extracted
-   * \param image_mask Mask image of the same dimensions as \p image_data where
-   *                   positive values indicate regions of \p image_data to
-   *                   consider.
-   * \returns a set of feature descriptors
-   */
-  virtual kwiver::vital::descriptor_set_sptr
-  extract( kwiver::vital::image_container_sptr image_data,
-           kwiver::vital::track_set_sptr tracks ) const = 0;
+  /// Set this algorithm's properties via a config block
+  virtual void set_configuration( kwiver::vital::config_block_sptr config );
+  /// Check that the algorithm's currently configuration is valid
+  virtual bool check_configuration( kwiver::vital::config_block_sptr config ) const;
+
+  /// Formulate query
+  virtual kwiver::vital::descriptor_set_sptr formulate(
+    int request,
+    std::vector< kwiver::vital::image_container_sptr > images ) const = 0;
 
 protected:
-  compute_track_descriptors();
+  formulate_query();
 
 };
 
-
-/// Shared pointer for base compute_track_descriptors algorithm definition class
-typedef std::shared_ptr<compute_track_descriptors> compute_track_descriptors_sptr;
-
+typedef std::shared_ptr<formulate_query> formulate_query_sptr;
 
 } } } // end namespace
 
-#endif // VITAL_ALGO_COMPUTE_TRACK_DESCRIPTORS_H_
+#endif // VITAL_ALGO_CONVERT_IMAGE_H_
