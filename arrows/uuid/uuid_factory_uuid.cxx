09bde3c109e5efcd5934a309ece5662f12d1322d
Linus Sherrill <linus.sherrill@kitware.com>
2017-06-01 13:21:32 -0400

    Add initial support for windows

--- arrows/uuid/uuid_factory_uuid.cxx
+++ arrows/uuid/uuid_factory_uuid.cxx
@@ -30,13 +30,16 @@
 
 /**
  * \file
- * \brief Implementation of detected object set csv outputuuid factory
+ * \brief Implementation of uuid factory
  */
 
 #include "uuid_factory_uuid.h"
 
+#if defined WIN32
+#include <windows.h>
+#else
 #include <uuid/uuid.h>
-
+#endif
 
 namespace kwiver {
 namespace arrows {
@@ -47,10 +50,7 @@ class uuid_factory_uuid::priv
 {
 public:
 
-
-
 };
-
 
 
 // ==================================================================
@@ -60,6 +60,7 @@ uuid_factory_uuid()
 {
 
 }
+
 
 uuid_factory_uuid::
 ~uuid_factory_uuid()
@@ -88,12 +89,20 @@ kwiver::vital::uid
 uuid_factory_uuid::
 create_uuid()
 {
+#if defined WIN32
+  UUID new_uuid;
+
+  UuidCreate( &new_uuid );
+
+  const char* cc = (const char*) &new_uuid.Data4[0];
+#else
   // This may need work to be more system independent.
   uuid_t new_uuid;
 
   // from libuuid
   uuid_generate( new_uuid );
-  const char* cc = (const char *)&new_uuid[0];
+  const char* cc = (const char *) &new_uuid[0];
+#endif
 
   return kwiver::vital::uid( cc, sizeof( new_uuid ));
 }
