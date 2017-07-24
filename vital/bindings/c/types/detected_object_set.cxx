548cff738c1e141da3daa3d167d451a77160a27d
Matt Dawkins <matt.dawkins@kitware.com>
2017-07-24 14:08:53 -0400

    Normalize names with other functions

--- vital/bindings/c/types/detected_object_set.cxx
+++ vital/bindings/c/types/detected_object_set.cxx
@@ -70,7 +70,7 @@ SharedPointerCache< kwiver::vital::detected_object_set, vital_detected_object_se
 vital_detected_object_set_t* vital_detected_object_set_from_sptr( kwiver::vital::detected_object_set_sptr sptr )
 {
   STANDARD_CATCH(
-    "C::detected_object_set::from_sptr", 0,
+    "vital_detected_object_set_from_sptr", 0,
 
     kwiver::vital_c::DOBJ_SET_SPTR_CACHE.store( sptr );
     return reinterpret_cast<vital_detected_object_set_t*>( sptr.get() );
@@ -82,7 +82,7 @@ vital_detected_object_set_t* vital_detected_object_set_from_sptr( kwiver::vital:
 vital_detected_object_set_t* vital_detected_object_set_from_c_pointer( kwiver::vital::detected_object_set* ptr )
 {
   STANDARD_CATCH(
-    "C::detected_object_set::from_c_ptr", 0,
+    "vital_detected_object_set_from_c_pointer", 0,
     kwiver::vital::detected_object_set_sptr sptr(ptr);
     kwiver::vital_c::DOBJ_SET_SPTR_CACHE.store( sptr );
     return reinterpret_cast<vital_detected_object_set_t*>( ptr );
@@ -94,7 +94,7 @@ vital_detected_object_set_t* vital_detected_object_set_from_c_pointer( kwiver::v
 kwiver::vital::detected_object_set_sptr vital_detected_object_set_to_sptr( vital_detected_object_set_t* handle )
 {
   STANDARD_CATCH(
-    "C::detected_object_set::to_sptr", 0,
+    "vital_detected_object_set_to_sptr", 0,
 
     return kwiver::vital_c::DOBJ_SET_SPTR_CACHE.get( handle );
     );
@@ -106,7 +106,7 @@ kwiver::vital::detected_object_set_sptr vital_detected_object_set_to_sptr( vital
 vital_detected_object_set_t* vital_detected_object_set_new()
 {
   STANDARD_CATCH(
-    "C::detected_object_set:new", 0,
+    "vital_detected_object_set_new", 0,
 
     auto dot_sptr = std::make_shared< kwiver::vital::detected_object_set> ();
 
@@ -124,7 +124,7 @@ vital_detected_object_set_new_from_list( vital_detected_object_t**  dobj,
                                          size_t                     n )
 {
   STANDARD_CATCH(
-    "C::detected_object_set:new_from_list", 0,
+    "vital_detected_object_set_new_from_list", 0,
 
     kwiver::vital::detected_object::vector_t input( n );
     for ( size_t i = 0; i < n; ++i )
@@ -145,7 +145,7 @@ vital_detected_object_set_new_from_list( vital_detected_object_t**  dobj,
 void vital_detected_object_set_destroy( vital_detected_object_set_t* obj)
 {
   STANDARD_CATCH(
-    "C::detected_object_set::destroy", 0,
+    "vital_detected_object_set_destroy", 0,
 
     kwiver::vital_c::DOBJ_SET_SPTR_CACHE.erase( obj );
   );
@@ -158,7 +158,7 @@ void vital_detected_object_set_add( vital_detected_object_set_t* set,
                                     vital_detected_object_t* obj )
 {
   STANDARD_CATCH(
-    "C::detected_object_set::add", 0,
+    "vital_detected_object_set_add", 0,
 
     kwiver::vital_c::DOBJ_SET_SPTR_CACHE.get( set )->add( kwiver::vital_c::DOBJ_SPTR_CACHE.get( obj ) );
     );
@@ -169,7 +169,7 @@ void vital_detected_object_set_add( vital_detected_object_set_t* set,
 size_t vital_detected_object_set_size( vital_detected_object_set_t* obj)
 {
   STANDARD_CATCH(
-    "C::detected_object_set::size", 0,
+    "vital_detected_object_set_size", 0,
 
     return kwiver::vital_c::DOBJ_SET_SPTR_CACHE.get( obj )->size();
     );
@@ -183,7 +183,7 @@ vital_detected_object_t** vital_detected_object_set_select_threshold( vital_dete
                                                                       size_t* length )
 {
   STANDARD_CATCH(
-    "C::detected_object_set::select_threshold", 0,
+    "vital_detected_object_set_select_threshold", 0,
 
     auto sel_set = kwiver::vital_c::DOBJ_SET_SPTR_CACHE.get( obj )->select( thresh );
 
@@ -210,7 +210,7 @@ vital_detected_object_t** vital_detected_object_set_select_class_threshold( vita
                                                                             size_t* length )
 {
   STANDARD_CATCH(
-    "C::detected_object_set::select_class_threshold", 0,
+    "vital_detected_object_set_select_class_threshold", 0,
 
     auto sel_set = kwiver::vital_c::DOBJ_SET_SPTR_CACHE.get( obj )->select( std::string (class_name), thresh );
 
