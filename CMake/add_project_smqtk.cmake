60aad12e4ab300484edc1589bf09d07027538723
Matt Dawkins <matt.dawkins@kitware.com>
2017-08-20 08:30:44 -0400

    Install SMQTK python code'

--- CMake/add_project_smqtk.cmake
+++ CMake/add_project_smqtk.cmake
@@ -9,6 +9,18 @@
 
 set( VIAME_PROJECT_LIST ${VIAME_PROJECT_LIST} smqtk )
 
+if( WIN32 )
+  message( FATAL_ERROR "SMQTK not yet supported on WIN32" )
+else()
+  set( PYTHON_BASEPATH
+    ${VIAME_BUILD_INSTALL_PREFIX}/lib/python${PYTHON_VERSION}${PYTHON_ABIFLAGS} )
+  set( CUSTOM_PYTHONPATH
+    ${PYTHON_BASEPATH}/site-packages:${PYTHON_BASEPATH}/dist-packages:$ENV{PYTHONPATH} )
+  set( SMQTK_PYTHON_INSTALL
+    ${CMAKE_COMMAND} -E env PYTHONPATH=${CUSTOM_PYTHONPATH}
+      ${PYTHON_EXECUTABLE} setup.py install --prefix=${VIAME_BUILD_INSTALL_PREFIX} )
+endif()
+
 ExternalProject_Add( smqtk
   DEPENDS fletch
   PREFIX ${VIAME_BUILD_PREFIX}
@@ -16,12 +28,19 @@ ExternalProject_Add( smqtk
   CMAKE_GENERATOR ${gen}
   CMAKE_ARGS
     -DSMQTK_BUILD_EXAMPLES:BOOL=OFF
-    -DSMQTK_BUILD_LIBSVM:BOOL=OFF
+    -DSMQTK_BUILD_LIBSVM:BOOL=ON
     -DSMQTK_INSTALL_SETUP_SCRIPT:BOOL=OFF
     ${VIAME_ARGS_COMMON}
     ${VIAME_ARGS_fletch}
 
   INSTALL_DIR ${VIAME_BUILD_INSTALL_PREFIX}
+  )
+
+ExternalProject_Add_Step(smqtk installpy
+  WORKING_DIRECTORY ${VIAME_PACKAGES_DIR}/smqtk
+  COMMAND ${SMQTK_PYTHON_INSTALL}
+  COMMENT "Installing SMQTK python files."
+  DEPENDEES build
   )
 
 ExternalProject_Add_Step(smqtk forcebuild
