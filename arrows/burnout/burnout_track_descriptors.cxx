753859035692302013a7bc894638b0051d85b8b6
Matt Dawkins <matt.dawkins@kitware.com>
2017-08-29 14:44:58 -0400

    Fix segfault when no tracks input

--- arrows/burnout/burnout_track_descriptors.cxx
+++ arrows/burnout/burnout_track_descriptors.cxx
@@ -210,24 +210,27 @@ burnout_track_descriptors
   vil_image_view< vxl_byte > input_image;
   std::vector< vidtk::track_sptr > input_tracks;
 
-  VITAL_FOREACH( auto vital_t, tracks->tracks() )
+  if( tracks )
   {
-    vidtk::track_sptr vidtk_t( new vidtk::track() );
-
-    vidtk_t->set_id( vital_t->id() );
-
-    VITAL_FOREACH( auto vital_ts, *vital_t )
+    VITAL_FOREACH( auto vital_t, tracks->tracks() )
     {
-      vital::object_track_state* ots =
-        dynamic_cast< vital::object_track_state* >( vital_ts.get() );
+      vidtk::track_sptr vidtk_t( new vidtk::track() );
 
-      if( ots )
+      vidtk_t->set_id( vital_t->id() );
+
+      VITAL_FOREACH( auto vital_ts, *vital_t )
       {
-        vidtk_t->add_state( vital_to_vidtk( ots ) );
-      }
-    }
+        vital::object_track_state* ots =
+          dynamic_cast< vital::object_track_state* >( vital_ts.get() );
 
-    input_tracks.push_back( vidtk_t );
+        if( ots )
+        {
+          vidtk_t->add_state( vital_to_vidtk( ots ) );
+        }
+      }
+
+      input_tracks.push_back( vidtk_t );
+    }
   }
 
   if( image_data )
