7938034877292307466b40ace4b6ae1470e4702d
Matt Dawkins <matt.dawkins@kitware.com>
2017-09-04 18:22:54 -0400

    Update example

--- examples/image_and_video_search/video_ingest/launch_gui.sh
+++ examples/image_and_video_search/video_ingest/launch_gui.sh
@@ -9,4 +9,4 @@ mkdir -p database/Queries
 
 # Run GUI
 this_dir=$(readlink -f $(dirname $BASH_SOURCE[0]))
-viqui --add-layer ${this_dir}/configs/context-bluemarble-low-res.kst
+viqui --add-layer ${this_dir}/configs/context_bluemarble_low_res.kst
