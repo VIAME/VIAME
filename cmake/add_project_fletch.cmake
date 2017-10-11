8688580252b1a3f8c5e5e881a7db208cd061956c
Matt Dawkins <matt.dawkins@kitware.com>
2017-10-11 16:02:23 -0400

    Enable gtests for testing

--- cmake/add_project_fletch.cmake
+++ cmake/add_project_fletch.cmake
@@ -147,6 +147,9 @@ ExternalProject_Add(fletch
     -Dfletch_ENABLE_PyBind11:BOOL=${VIAME_ENABLE_PYTHON}
 
     # Set fletch install path to be viame install path
+    -Dfletch_ENABLE_GTest:PATH=${VIAME_BUILD_TESTS}
+
+    # Set fletch install path to be viame install path
     -Dfletch_BUILD_INSTALL_PREFIX:PATH=${VIAME_BUILD_INSTALL_PREFIX}
 
     # Disable optional OpenCV build flags, these oft don't build
