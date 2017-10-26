dc85bf024225c68f81afedfde5c13b4fa6696eb9
Linus Sherrill <linus.sherrill@kitware.com>
2017-10-26 15:33:04 -0400

    Modify RST output class to generate config and ports in tables.

--- sprokit/processes/process_explorer_plugin.cxx
+++ sprokit/processes/process_explorer_plugin.cxx
@@ -39,6 +39,9 @@
 
 #include <sstream>
 #include <iterator>
+#include <fstream>
+#include <string>
+#include <regex>
 
 namespace kwiver {
 namespace vital {
@@ -280,11 +283,15 @@ public:
   virtual bool initialize( explorer_context* context );
   virtual void explore( const kwiver::vital::plugin_factory_handle_t fact );
 
-  std::ostream& out_stream() { return m_context->output_stream(); }
+  std::ostream& out_stream();
+  std::string wrap_rst_text( const std::string& txt );
 
   // instance data
   explorer_context* m_context;
   kwiver::vital::wrap_text_block m_wtb;
+  kwiver::vital::wrap_text_block m_comment_wtb;
+  bool opt_files;
+  std::ofstream m_out_stream;
 
 }; // end class process_explorer_rst
 
@@ -292,8 +299,10 @@ public:
 // ==================================================================
 process_explorer_rst::
 process_explorer_rst()
+  : opt_files( false )
 {
   m_wtb.set_indent_string( "" );
+  m_comment_wtb.set_indent_string( " # " );
 }
 
 
@@ -303,11 +312,51 @@ process_explorer_rst::
 
 
 // ------------------------------------------------------------------
+std::ostream&
+process_explorer_rst::
+out_stream()
+{
+  if (opt_files)
+  {
+    return m_out_stream;
+  }
+
+  return m_context->output_stream();
+}
+
+
+// ------------------------------------------------------------------
+std::string
+process_explorer_rst::
+wrap_rst_text( const std::string& txt )
+{
+  std::string wtxt = m_wtb.wrap_text( txt );
+
+  // trim trailing whitesapce new line
+  wtxt.erase( wtxt.find_last_not_of( " \t\n\r\f\v" ) + 1 );
+
+  return std::regex_replace( wtxt, std::regex("\n"), " |br|\\ " );
+}
+
+
+// ------------------------------------------------------------------
 bool
 process_explorer_rst::
 initialize( explorer_context* context )
 {
   m_context = context;
+
+  // Add plugin specific command line option.
+  auto cla = m_context->command_line_args();
+
+  static char flag_name[]("--sep-proc-files");
+  static char help_text[]("Generate .rst output for processes in separate files." );
+
+  cla->AddArgument( flag_name,
+                    kwiversys::CommandLineArguments::NO_ARGUMENT,
+                    &this->opt_files,
+                    help_text );
+
   return true;
 }
 
@@ -318,12 +367,14 @@ process_explorer_rst::
 explore( const kwiver::vital::plugin_factory_handle_t fact )
 {
   std::string proc_type = "-- Not Set --";
-
-  fact->get_attribute( kwiver::vital::plugin_factory::PLUGIN_NAME, proc_type );
+  if ( fact->get_attribute( kwiver::vital::plugin_factory::PLUGIN_NAME, proc_type ) && opt_files )
+  {
+    m_out_stream.open( proc_type + "-config.rst", std::ios_base::trunc);
+  }
 
   std::string descrip = "-- Not_Set --";
   fact->get_attribute( kwiver::vital::plugin_factory::PLUGIN_DESCRIPTION, descrip );
-  descrip = m_wtb.wrap_text( descrip );
+  descrip = wrap_rst_text( descrip );
 
   std::string proc_class = "-- Not Set --";
   if ( fact->get_attribute( kwiver::vital::plugin_factory::CONCRETE_TYPE, proc_class ) )
@@ -331,14 +382,15 @@ explore( const kwiver::vital::plugin_factory_handle_t fact )
     proc_class = kwiver::vital::demangle( proc_class );
   }
 
-  out_stream() << std::endl
-               << underline( proc_type, '=' )
+  // output some magic to ehable maintaining newlines
+  out_stream() << "  .. |br| raw:: html" << std::endl
                << std::endl
+               << "   <br />" << std::endl
+               << std::endl;
+
+  out_stream() << underline( proc_type, '=' ) << std::endl
                << descrip << std::endl
-               << "..  doxygenclass:: " << proc_class << std::endl
-               << "    :project: kwiver" << std::endl
-               << std::endl
-    ;
+               << std::endl;
 
   sprokit::process_factory* pf = dynamic_cast< sprokit::process_factory* > ( fact.get() );
 
@@ -349,8 +401,153 @@ explore( const kwiver::vital::plugin_factory_handle_t fact )
 
   // -- config --
   kwiver::vital::config_block_keys_t const keys = proc->available_config();
-  out_stream() << underline( "Configuration", '-' );
+  out_stream() << underline( "Configuration", '-' ) << std::endl;
 
+  if ( keys.empty() )
+  {
+    out_stream() << "*There are no configuration items for this process.*" << std::endl
+                 << std::endl;
+  }
+  else
+  {
+    // generate header text
+    out_stream() << ".. csv-table::" << std::endl
+                 << "   :header: \"Variable\", \"Default\", \"Tunable\", \"Description\"" << std::endl
+                 << "   :align: left" << std::endl
+                 << "   :widths: auto" << std::endl
+                 << std::endl;
+
+    for( kwiver::vital::config_block_key_t const & key : keys )
+    {
+      if ( key.substr( 0, hidden_prefix.size() ) == hidden_prefix )
+      {
+        // skip hidden items
+        continue;
+      }
+
+      sprokit::process::conf_info_t const info = proc->config_info( key );
+
+      kwiver::vital::config_block_value_t def = info->def;
+      kwiver::vital::config_block_description_t const  conf_desc =  wrap_rst_text( info->description );
+      bool const& tunable = info->tunable;
+      char const* const tunable_str = tunable ? "YES" : "NO";
+
+      if ( def.empty() )
+      {
+        def = "(no default value)";
+      }
+
+      out_stream() << "   \"" << key << "\", \"" << def << "\", \"" << tunable_str << "\", \""
+                   << conf_desc << "\"" << std::endl;
+    }
+  }
+
+  // -- input ports --
+  sprokit::process::ports_t const iports = proc->input_ports();
+  out_stream() << underline( "Input Ports", '-' ) << std::endl;
+
+  if ( iports.empty() )
+  {
+    out_stream() << "There are no input ports for this process." << std::endl
+                 << std::endl;
+  }
+  else
+  {
+    out_stream() << ".. csv-table::" << std::endl
+                 << "   :header: \"Port name\", \"Data Type\", \"Flags\", \"Description\"" << std::endl
+                 << "   :align: left" << std::endl
+                 << "   :widths: auto" << std::endl
+                 << std::endl;
+
+    for( sprokit::process::port_t const & port : iports )
+    {
+      if ( port.substr( 0, hidden_prefix.size() ) == hidden_prefix )
+      {
+        // skip hidden item
+        continue;
+      }
+
+      sprokit::process::port_info_t const info = proc->input_port_info( port );
+
+      sprokit::process::port_type_t const& type = info->type;
+      sprokit::process::port_flags_t const& flags = info->flags;
+      sprokit::process::port_description_t const port_desc = wrap_rst_text( info->description );
+
+      std::string flags_str = join( flags, ", " );
+      if ( flags_str.empty() )
+      {
+        flags_str = "(none)";
+      }
+
+      out_stream() << "   \"" << port << "\", \"" << type << "\", \"" <<  flags_str
+                   << "\", \"" << port_desc << "\"" << std::endl;
+    }   // end foreach
+  }
+
+  // -- output ports --
+  sprokit::process::ports_t const oports = proc->output_ports();
+  out_stream() << underline( "Output Ports", '-' ) << std::endl;
+
+  if ( oports.empty() )
+  {
+    out_stream() << "There are no output ports for this process." << std::endl
+                 << std::endl;
+  }
+  else
+  {
+    out_stream() << ".. csv-table::" << std::endl
+                 << "   :header: \"Port name\", \"Data Type\", \"Flags\", \"Description\"" << std::endl
+                 << "   :align: left" << std::endl
+                 << "   :widths: auto" << std::endl
+                 << std::endl;
+
+    for( sprokit::process::port_t const & port : oports )
+    {
+      if ( port.substr( 0, hidden_prefix.size() ) == hidden_prefix )
+      {
+        continue;
+      }
+
+      sprokit::process::port_info_t const info = proc->output_port_info( port );
+
+      sprokit::process::port_type_t const& type = info->type;
+      sprokit::process::port_flags_t const& flags = info->flags;
+      sprokit::process::port_description_t const port_desc = wrap_rst_text( info->description );
+
+      std::string flags_str = join( flags, ", " );
+      if ( flags_str.empty() )
+      {
+        flags_str = "(none)";
+      }
+
+      out_stream() << "   \"" << port << "\", \"" << type << "\", \"" <<  flags_str
+                   << "\", \"" << port_desc << "\"" << std::endl;
+    }   // end foreach
+  }
+  out_stream() << std::endl;
+
+  // ==================================================================
+  // -- pipefile usage --
+
+  // Switch to other file
+  if ( opt_files )
+  {
+    m_out_stream.close();
+    m_out_stream.open( proc_type + "-pipe.rst", std::ios_base::trunc);
+  }
+
+  out_stream() << underline( "Pipefile Usage", '-' ) << std::endl
+               << "The following sections describe the blocks needed ot use this process in a pipe file." << std::endl
+               << std::endl
+               << underline( "Pipefile block", '-' ) << std::endl
+               << ".. code::" << std::endl
+               << std::endl
+
+               << " # ================================================================" << std::endl
+               << " process <this-name>" << std::endl
+               << "   :: " << proc_type << std::endl;
+
+  // loop over config
   for( kwiver::vital::config_block_key_t const & key : keys )
   {
     if ( key.substr( 0, hidden_prefix.size() ) == hidden_prefix )
@@ -362,96 +559,83 @@ explore( const kwiver::vital::plugin_factory_handle_t fact )
     sprokit::process::conf_info_t const info = proc->config_info( key );
 
     kwiver::vital::config_block_value_t def = info->def;
-    kwiver::vital::config_block_description_t const  conf_desc =  m_wtb.wrap_text( info->description );
-    bool const& tunable = info->tunable;
-    char const* const tunable_str = tunable ? "*Tunable*" : "*Not tunable*";
+    kwiver::vital::config_block_description_t const  conf_desc =  m_comment_wtb.wrap_text( info->description );
 
     if ( def.empty() )
     {
-      def = "(no default value)";
+      def = "<value>";
     }
 
-    out_stream() << "**" << key << "**" << " = " << def << "       " << tunable_str << std::endl
-                 << conf_desc << std::endl
-      ;
-  }
+    out_stream() << conf_desc
+                 << "   " << key << " = " << def << std::endl;
+  } // end for
 
-  // -- input ports --
-  sprokit::process::ports_t const iports = proc->input_ports();
-  out_stream() << underline( "Input Ports", '-' ) << std::endl;
+  out_stream() << " # ================================================================" << std::endl
+               << std::endl;
 
-  for( sprokit::process::port_t const & port : iports )
-  {
-    if ( port.substr( 0, hidden_prefix.size() ) == hidden_prefix )
-    {
-      // skip hidden item
-      continue;
-    }
+  out_stream() << underline( "Process connections", '~' )
+               << std::endl
+               << underline( "The following Input ports will need to be set" , '^')
+               << ".. code::" << std::endl
+               << std::endl;
 
-    sprokit::process::port_info_t const info = proc->input_port_info( port );
-
-    sprokit::process::port_type_t const& type = info->type;
-    sprokit::process::port_flags_t const& flags = info->flags;
-    sprokit::process::port_description_t const port_desc = m_wtb.wrap_text( info->description );
-
-    std::string flags_str = join( flags, ", " );
-    if ( flags_str.empty() )
-    {
-      flags_str = "(none)";
-    }
-
-    out_stream() << "**" << port << "**" << std::endl
-                 << port_desc << std::endl
-                 << "Data type  : " << type << std::endl
-                 << "Flags      : " << flags_str << std::endl
-                 << std::endl
-      ;
-
-  }   // end foreach
-
+  // loop over input ports
   if ( iports.empty() )
   {
-    out_stream() << "(none)" << std::endl
+    out_stream() << " # There are no input port's for this process" << std::endl
                  << std::endl;
   }
-
-  // -- output ports --
-  sprokit::process::ports_t const oports = proc->output_ports();
-  out_stream() << underline( "Output Ports", '-' ) << std::endl;
-
-  for( sprokit::process::port_t const & port : oports )
+  else
   {
-    if ( port.substr( 0, hidden_prefix.size() ) == hidden_prefix )
+    out_stream() << "# This process will consume the following input ports" << std::endl;
+
+    for( sprokit::process::port_t const & port : iports )
     {
-      continue;
-    }
+      if ( port.substr( 0, hidden_prefix.size() ) == hidden_prefix )
+      {
+        // skip hidden item
+        continue;
+      }
 
-    sprokit::process::port_info_t const info = proc->output_port_info( port );
+      out_stream() << " connect from <this-proc>." << port << std::endl
+                   <<"          to   <upstream-proc>." << port << std::endl;
+    }   // end for
+  }
+  out_stream() << std::endl;
 
-    sprokit::process::port_type_t const& type = info->type;
-    sprokit::process::port_flags_t const& flags = info->flags;
-    sprokit::process::port_description_t const port_desc = m_wtb.wrap_text( info->description );
 
-    std::string flags_str = join( flags, ", " );
-    if ( flags_str.empty() )
-    {
-      flags_str = "(none)";
-    }
+  // loop over output ports
+  out_stream() << underline( "The following Output ports will need to be set" , '^')
+               << ".. code::" << std::endl
+               << std::endl;
 
-    out_stream() << "**" << port << "**" << std::endl
-                 << port_desc << std::endl
-                 << "Data Type  : " << type << std::endl
-                 << "Flags      : " << flags_str << std::endl
-                 << std::endl
-      ;
-
-  }   // end foreach
-
-    if ( oports.empty() )
+  if ( oports.empty() )
   {
-    out_stream() << "(none)" << std::endl
+    out_stream() << " # There are no output port's for this process" << std::endl
                  << std::endl;
   }
+  else
+  {
+    for( sprokit::process::port_t const & port : oports )
+    {
+      if ( port.substr( 0, hidden_prefix.size() ) == hidden_prefix )
+      {
+        continue;
+      }
+
+      out_stream() << " connect from <this-proc>." << port << std::endl
+                   <<"          to   <downstream-proc>." << port << std::endl;
+    }   // end foreach
+  }
+
+  out_stream() << std::endl
+               << underline( "Class Description", '-' ) << std::endl
+               << ".. doxygenclass:: " << proc_class << std::endl
+               << "   :project: kwiver" << std::endl
+               << std::endl;
+
+  // close file just in case it was open
+  m_out_stream.close();
 
 } // process_explorer_rst::explore
 
