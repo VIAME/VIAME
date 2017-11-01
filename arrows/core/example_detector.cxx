8a70571c7edc92f1ca0b429b2c0c0e21fe12ee8b
matt <matt.phillips@kitware.com>
2017-11-01 12:24:22 -0400

    Added example detection for video

--- arrows/core/example_detector.cxx
+++ arrows/core/example_detector.cxx
@@ -49,6 +49,9 @@ public:
   , m_center_y(100.0)
   , m_height(200.0)
   , m_width(200.0)
+  , m_dx(0.0)
+  , m_dy(0.0)
+  , m_frame_ct(0)
   {}
 
   ~priv()
@@ -58,6 +61,9 @@ public:
   double m_center_y;
   double m_height;
   double m_width;
+  double m_dx;
+  double m_dy;
+  int m_frame_ct;
 }; // end class example_detector::priv
 
 
@@ -85,6 +91,8 @@ get_configuration() const
   config->set_value( "center_y", d->m_center_y, "Bounding box center y coordinate." );
   config->set_value( "height", d->m_height, "Bounding box height." );
   config->set_value( "width", d->m_width, "Bounding box width." );
+  config->set_value( "dx", d->m_dx, "Bounding box x translation per frame." );
+  config->set_value( "dy", d->m_dy, "Bounding box y translation per frame." );
 
   return config;
 }
@@ -97,8 +105,12 @@ set_configuration(vital::config_block_sptr config)
 {
   d->m_center_x     = config->get_value<double>( "center_x" );
   d->m_center_y     = config->get_value<double>( "center_y" );
-  d->m_height     = config->get_value<double>( "height" );
-  d->m_width     = config->get_value<double>( "width" );
+  d->m_height       = config->get_value<double>( "height" );
+  d->m_width        = config->get_value<double>( "width" );
+  if (config->has_value("dx"))
+    d->m_dx           = config->get_value<double>( "dx" );
+  if (config->has_value("dy"))
+    d->m_dy           = config->get_value<double>( "dy" );
 }
 
 
@@ -118,11 +130,15 @@ detect( vital::image_container_sptr image_data) const
 {
   auto detected_set = std::make_shared< kwiver::vital::detected_object_set>();
 
+  const double ct = (double)d->m_frame_ct;
+
   kwiver::vital::bounding_box_d bbox(
-          d->m_center_x - d->m_width/2.0,
-          d->m_center_y - d->m_height/2.0,
-          d->m_center_x + d->m_width/2.0,
-          d->m_center_y + d->m_height/2.0);
+          d->m_center_x + ct*d->m_dx - d->m_width/2.0,
+          d->m_center_y + ct*d->m_dy - d->m_height/2.0,
+          d->m_center_x + ct*d->m_dx + d->m_width/2.0,
+          d->m_center_y + ct*d->m_dy + d->m_height/2.0);
+
+  ++d->m_frame_ct;
 
   auto dot = std::make_shared< kwiver::vital::detected_object_type >();
   dot->set_score( "detection", 1.0 );
