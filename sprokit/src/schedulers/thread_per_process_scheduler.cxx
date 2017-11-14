7a9a520d65373366e53ced5dbf07d9bb4031d127
Linus Sherrill <linus.sherrill@kitware.com>
2017-11-14 13:34:13 -0500

    Add test for python processes in threaded scheduler.
    This scheduler can not run python processes. The test added throws
    an exception if a python process is found in the pipeline.

--- sprokit/src/schedulers/thread_per_process_scheduler.cxx
+++ sprokit/src/schedulers/thread_per_process_scheduler.cxx
@@ -72,10 +72,13 @@ class thread_per_process_scheduler::priv
 
 // ------------------------------------------------------------------
 thread_per_process_scheduler
-::thread_per_process_scheduler(pipeline_t const& pipe, kwiver::vital::config_block_sptr const& config)
+::thread_per_process_scheduler(pipeline_t const& pipe,
+                               kwiver::vital::config_block_sptr const& config)
   : scheduler(pipe, config)
   , d(new priv)
 {
+  m_logger = kwiver::vital::get_logger( "scheduler.thread_per_process" );
+
   pipeline_t const p = pipeline();
 
   process_t proc = p->get_python_process();
@@ -88,6 +91,8 @@ thread_per_process_scheduler
 
   process::names_t const names = p->process_names();
 
+  // Scan all processes in the pipeline to see if any are not
+  // compatible with this scheduler.
   for (process::name_t const& name : names)
   {
     proc = p->process_by_name(name);
@@ -95,13 +100,26 @@ thread_per_process_scheduler
 
     if (consts.count(process::property_no_threads))
     {
-      std::string const reason = "The process \'" + name + "\' does "
-                                 "not support being in its own thread";
+      std::string const reason =
+        "The process \'" + name + "\' does not support being in its own thread.";
+
+      throw incompatible_pipeline_exception(reason);
+    }
+
+    // Test for a python process in the pipeline. This scheduler does
+    // not support running python processes and bad things happen if
+    // you try.
+    if ( consts.count( process::property_python) )
+    {
+      std::string const reason = "The process \'" + name + "\' of type \'" + proc->type()
+        + "\' is "
+        "a python process and that type of process is not supported by this scheduler.";
 
       throw incompatible_pipeline_exception(reason);
     }
   }
 }
+
 
 // ------------------------------------------------------------------
 thread_per_process_scheduler
@@ -138,6 +156,7 @@ thread_per_process_scheduler
   d->process_threads->join_all();
 }
 
+
 // ------------------------------------------------------------------
 void
 thread_per_process_scheduler
@@ -145,6 +164,7 @@ thread_per_process_scheduler
 {
   d->m_pause_mutex.lock();
 }
+
 
 // ------------------------------------------------------------------
 void
@@ -154,6 +174,7 @@ thread_per_process_scheduler
   d->m_pause_mutex.unlock();
 }
 
+
 // ------------------------------------------------------------------
 void
 thread_per_process_scheduler
@@ -162,12 +183,15 @@ thread_per_process_scheduler
   d->process_threads->interrupt_all();
 }
 
+
+// ============================================================================
 thread_per_process_scheduler::priv
 ::priv()
   : process_threads()
   , m_pause_mutex()
 {
 }
+
 
 // ------------------------------------------------------------------
 thread_per_process_scheduler::priv
@@ -179,10 +203,15 @@ static kwiver::vital::config_block_sptr monitor_edge_config();
 
 
 // ------------------------------------------------------------------
+/*
+ * This is the thread that runs a process. It loops until the process
+ * is complete or fails.
+ */
 void
 thread_per_process_scheduler::priv
 ::run_process(process_t const& process)
 {
+  // Create the monitor edge. This is only needed for this type of scheduler.
   kwiver::vital::config_block_sptr const edge_conf = monitor_edge_config();
 
   name_thread(process->name());
@@ -221,6 +250,15 @@ thread_per_process_scheduler::priv
 
 
 // ------------------------------------------------------------------
+/**
+ * This function returns the config block for the "monitor_edge". The
+ * monitor_edge being the one where the process generates a heart beat datum.
+ *
+ * Currently there is no config for these edges.
+ *
+ * One possibility for supplying this config would be to have it be
+ * part of the scheduler config.
+ */
 kwiver::vital::config_block_sptr
 monitor_edge_config()
 {
