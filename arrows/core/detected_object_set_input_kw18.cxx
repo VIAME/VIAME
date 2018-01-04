a54312f58dc8a7cb5d2ff0cc22a82e8d6f7e7dd2
Matt Dawkins <matt.dawkins@kitware.com>
2018-01-03 19:57:23 -0500

    Fix bug in reader

--- arrows/core/detected_object_set_input_kw18.cxx
+++ arrows/core/detected_object_set_input_kw18.cxx
@@ -147,8 +147,8 @@ read_set( kwiver::vital::detected_object_set_sptr & set, std::string& image_name
   // we do not return image name
   image_name.clear();
 
-  // test for end of stream
-  if (this->at_eof())
+  // test for end of all loaded detections
+  if (d->m_current_idx > d->m_last_idx)
   {
     return false;
   }
