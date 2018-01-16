c8c7c12ff94b4d1181485aa9c86462c9d3ec4d9f
Matt Dawkins <matt.dawkins@kitware.com>
2018-01-16 10:07:52 -0500

    Style

--- arrows/darknet/darknet_detector.cxx
+++ arrows/darknet/darknet_detector.cxx
@@ -297,7 +297,8 @@ detect( vital::image_container_sptr image_data ) const
 {
   kwiver::vital::scoped_cpu_timer t( "Time to Detect Objects" );
 
-  cv::Mat cv_image = kwiver::arrows::ocv::image_container::vital_to_ocv( image_data->get_image(), kwiver::arrows::ocv::image_container::BGR );
+  cv::Mat cv_image = kwiver::arrows::ocv::image_container::vital_to_ocv(
+    image_data->get_image(), kwiver::arrows::ocv::image_container::BGR );
 
   cv::Mat cv_resized_image;
 
