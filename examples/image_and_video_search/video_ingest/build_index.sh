832900241c05498508e6424d407afc7c146bec96
Matt Dawkins <matt.dawkins@kitware.com>
2018-02-01 18:40:43 -0500

    Update bit size

--- examples/image_and_video_search/video_ingest/build_index.sh
+++ examples/image_and_video_search/video_ingest/build_index.sh
@@ -5,7 +5,6 @@ source ../../../setup_viame.sh
 mkdir -p database/ITQ
 
 SMQTK_ITQ_TRAIN_CONFIG="configs/smqtk_train_itq.json"
-SMQTK_ITQ_BIT_SIZE=256
 
 SMQTK_HCODE_CONFIG="configs/smqtk_compute_hashes.json"
 SMQTK_HCODE_PICKLE="database/ITQ/alexnet_fc7.itq_b256_i50_n2_r0.lsh_hash2uuids.pickle"
