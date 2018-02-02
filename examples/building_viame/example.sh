7ee897ebf6a17df13704fb9f8a4ee183b680a251
Matt Dawkins <matt.dawkins@kitware.com>
2018-02-02 09:01:15 -0500

    Move files into examples

---
+++ examples/building_viame/example.sh
@@ -0,0 +1,14 @@
+
+mkdir -p ~/Dev/viame
+
+git clone https://github.com/Kitware/VIAME.git ~/Dev/viame/src
+
+cd ~/Dev/viame/src
+git submodule update --init --recusrive
+
+cd ..
+mkdir build
+
+cmake ../src
+
+make -j10
