2d9b76937bf0390656abeac27e92be3841f6b3f3
Matt Dawkins <matt.dawkins@kitware.com>
2018-02-02 12:50:15 -0500

    Revert out change

--- examples/image_and_video_search/video_ingest/build_index.sh
+++ examples/image_and_video_search/video_ingest/build_index.sh
@@ -5,11 +5,14 @@ source ../../../setup_viame.sh
 mkdir -p database/ITQ
 
 SMQTK_ITQ_TRAIN_CONFIG="configs/smqtk_train_itq.json"
+SMQTK_ITQ_BIT_SIZE=256
 
 SMQTK_HCODE_CONFIG="configs/smqtk_compute_hashes.json"
 SMQTK_HCODE_PICKLE="database/ITQ/alexnet_fc7.itq_b256_i50_n2_r0.lsh_hash2uuids.pickle"
 
-SMQTK_BTREE_CONFIG="configs/smqtk_make_balltree.json"
+SMQTK_HCODE_BTREE_LEAFSIZE=40
+SMQTK_HCODE_BTREE_RAND=0
+SMQTK_HCODE_BTREE_OUTPUT="database/ITQ/alexnet_fc7.itq_b256_i50_n2_r0.hi_btree.npz"
 
 # Train ITQ models on ingested descriptors
 train_itq -vc "${SMQTK_ITQ_TRAIN_CONFIG}"
@@ -20,4 +23,6 @@ compute_hash_codes \
     --output-hash2uuids "${SMQTK_HCODE_PICKLE}"
 
 # Compute balltree hash index
-make_balltree -c "${SMQTK_BTREE_PICKLE}"
+make_balltree "${SMQTK_HCODE_PICKLE}" ${SMQTK_ITQ_BIT_SIZE} \
+    ${SMQTK_HCODE_BTREE_LEAFSIZE} ${SMQTK_HCODE_BTREE_RAND} \
+    ${SMQTK_HCODE_BTREE_OUTPUT}
