a190717ceee9598271067661a26831e6941c29ff
Matt Dawkins <matt.dawkins@kitware.com>
2018-02-06 15:53:13 -0500

    Extra loggin

--- examples/image_and_video_search/video_ingest/build_index.sh
+++ examples/image_and_video_search/video_ingest/build_index.sh
@@ -12,10 +12,13 @@ SMQTK_HCODE_PICKLE="database/ITQ/alexnet_fc7.itq_b256_i50_n2_r0.lsh_hash2uuids.p
 SMQTK_BTREE_CONFIG="configs/smqtk_make_balltree.json"
 
 # Train ITQ models on ingested descriptors
+echo "1. Training ITQ Model"
 train_itq -vc "${SMQTK_ITQ_TRAIN_CONFIG}"
 
 # Compute hash codes for descriptors
+echo "2. Computing Hash Codes"
 compute_hash_codes -vc "${SMQTK_HCODE_CONFIG}"
 
 # Compute balltree hash index
+echo "3. Generating Ball Tree"
 make_balltree -vc "${SMQTK_BTREE_PICKLE}"
