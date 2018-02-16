e194f4081e9b513e21b5338362d624b145000ef0
Matthew Woehlke <matthew.woehlke@kitware.com>
2018-02-16 12:44:47 -0500

    Register interpolate_track_spline

--- arrows/core/interpolate_track_spline.h
+++ arrows/core/interpolate_track_spline.h
@@ -56,6 +56,13 @@ class KWIVER_ALGO_CORE_EXPORT interpolate_track_spline
                                  vital::algo::interpolate_track>
 {
 public:
+  /// Name of the algorithm
+  static constexpr char const* name = "spline";
+
+  /// Description of the algorithm
+  static constexpr char const* description =
+    "Fill in missing object track intervals using spline-based interpolation.";
+
   /// Default Constructor
   interpolate_track_spline();
 
