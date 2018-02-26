ea4a20ac113c73299e7875d4f0a0b07671e3a904
Bo Dong <bo.dong@kitware.com>
2018-02-26 18:40:21 -0500

    update

--- arrows/pytorch/seg_utils.py
+++ arrows/pytorch/seg_utils.py
@@ -11,12 +11,6 @@ import numpy as np
 
 import torch
 
-try:
-    import cv2
-except ImportError:
-    cv2 = None
-
-
 def bitget(byteval, idx):
     return ((byteval & (1 << idx)) != 0)
 
@@ -38,75 +32,6 @@ def label_colormap(N=256):
     cmap = cmap.astype(np.float32) / 255
     cmap = (cmap * 255).astype(np.uint8)
     return cmap
-
-
-def label2rgb(lbl, img=None, label_names=None, n_labels=None,
-              alpha=0.3, thresh_suppress=0):
-    if label_names is None:
-        if n_labels is None:
-            n_labels = lbl.max() + 1  # +1 for bg_label 0
-    else:
-        if n_labels is None:
-            n_labels = len(label_names)
-        else:
-            assert n_labels == len(label_names)
-    cmap = label_colormap(n_labels)
-
-    lbl_viz = cmap[lbl]
-    lbl_viz[lbl == -1] = (0, 0, 0)  # unlabeled
-
-    if img is not None:
-        img_gray = skimage.color.rgb2gray(img)
-        img_gray = skimage.color.gray2rgb(img_gray)
-        img_gray *= 255
-        lbl_viz = alpha * lbl_viz + (1 - alpha) * img_gray
-        lbl_viz = lbl_viz.astype(np.uint8)
-
-    if label_names is None:
-        return lbl_viz
-
-    # cv2 is required only if label_names is not None
-    import cv2
-    if cv2 is None:
-        warnings.warn('label2rgb with label_names requires OpenCV (cv2), '
-                      'so ignoring label_names values.')
-        return lbl_viz
-
-    np.random.seed(1234)
-    for label in np.unique(lbl):
-        if label == -1:
-            continue  # unlabeled
-
-        mask = lbl.squeeze() == label
-        if 1. * mask.sum() / mask.size < thresh_suppress:
-            continue
-        mask = (mask * 255).astype(np.uint8)
-        y, x = scipy.ndimage.center_of_mass(mask)
-        y, x = map(int, [y, x])
-
-        if lbl.squeeze()[y, x] != label:
-            Y, X = np.where(mask)
-            point_index = np.random.randint(0, len(Y))
-            y, x = Y[point_index], X[point_index]
-
-        text = label_names[label]
-        font_face = cv2.FONT_HERSHEY_SIMPLEX
-        font_scale = 0.7
-        thickness = 2
-        text_size, baseline = cv2.getTextSize(
-            text, font_face, font_scale, thickness)
-
-        def get_text_color(color):
-            if color[0] * 0.299 + color[1] * 0.587 + color[2] * 0.114 > 170:
-                return (0, 0, 0)
-            return (255, 255, 255)
-
-        color = get_text_color(lbl_viz[0, 0, y, x])
-        cv2.putText(lbl_viz[0, 0, :, :], text,
-                    (x - text_size[0] // 2, y),
-                    font_face, font_scale, color, thickness)
-    return lbl_viz
-
 
 def transform(img):
     mean_bgr = np.array([104.00698793, 116.66876762, 122.67891434])
