d47f0a0cd7eb1fffb72d63376c693286923e652d
Matt Dawkins <matt.dawkins@kitware.com>
2018-04-20 15:58:15 -0400

    Update example

--- examples/measurement_using_stereo/run_measurer.sh
+++ examples/measurement_using_stereo/run_measurer.sh
@@ -4,7 +4,10 @@
 export VIAME_INSTALL=../..
 
 # Setup VIAME Paths (no need to run multiple times if you already ran it)
-source ${VIAME_INSTALL}/setup_viame.sh 
+source ${VIAME_INSTALL}/setup_viame.sh
+
+# Extra path setup, in a future iteration this line will be deprecated
+export SPROKIT_PYTHON_MODULES=kwiver.processes:viame.processes:camtrawl_processes
 
 # Run pipeline
 pipeline_runner -p measurement_example.pipe
