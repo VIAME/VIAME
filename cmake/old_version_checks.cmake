6c2433d1aa98770fa8491b80c4b1449832c8814d
Matt Dawkins <matt.dawkins@kitware.com>
2018-05-17 22:32:13 -0400

    Old version check

--- cmake/old_version_checks.cmake
+++ cmake/old_version_checks.cmake
@@ -2,6 +2,6 @@
 # Old Example Checks
 if( EXISTS ${VIAME_BUILD_INSTALL_PREFIX}/examples/detector_pipelines )
   message( FATAL_ERROR "Your examples directory is too old. As of VIAME 0.9.7, "
-    "the examples directory has been refactored. Delete [build-dir]/install/examples "
+    "the examples directory has been refactored. Delete [build-dir]/install/examples, "
     "or alternatively consider a fresh build." )  
 endif()
