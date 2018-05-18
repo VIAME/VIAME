19dba72238420555bd120c1eff7fd1808b9ee60c
Matt Dawkins <matt.dawkins@kitware.com>
2018-05-18 08:35:12 -0400

    Update train example

--- examples/object_detector_training/train_yolo_v2_habcam.sh
+++ examples/object_detector_training/train_yolo_v2_habcam.sh
@@ -1,3 +1,13 @@
-source ../../setup_viame.sh
+#!/bin/sh
 
-viame_train_detector -i training_data -c yolo_v2_high_res_habcam.conf
+# Setup VIAME Paths (no need to run multiple times if you already ran it)
+
+export VIAME_INSTALL=./../..
+
+source ${VIAME_INSTALL}/setup_viame.sh 
+
+# Run pipeline
+
+viame_train_detector \
+  -i training_data \
+  -c ${VIAME_INSTALL}/configs/pipelines/train_yolo_v2_high_res_habcam.conf
