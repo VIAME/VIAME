7d60e05783022677ae34170c575aba22ab099b23
Matt Dawkins <matt.dawkins@kitware.com>
2018-05-21 08:48:23 -0400

    Add standalone ex

---
+++ examples/measurement_using_stereo/standalone_with_disp.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# VIAME Installation Location
+export VIAME_INSTALL=../..
+
+# Setup VIAME Paths (no need to run multiple times if you already ran it)
+source ${VIAME_INSTALL}/setup_viame.sh
+
+# Extra path setup, in a future iteration this line will be deprecated
+export SPROKIT_PYTHON_MODULES=kwiver.processes:viame.processes:camtrawl_processes
+
+# Run standalone script
+python -m viame.processes.camtrawl.demo \
+       --left=../example_imagery/camtrawl_example_image_set1/left \
+       --right=../example_imagery/camtrawl_example_image_set1/right \
+       --cal=calibration_matrices.npz \
+       --out=out --draw -f
