a9d9684fd96fcfdf128da741a1836ca41e46ff0a
Matt Dawkins <matt.dawkins@kitware.com>
2018-05-21 09:54:57 -0400

    Update example

--- examples/search_and_rapid_model_generation/video_ingest/create_searchable_index.sh
+++ examples/search_and_rapid_model_generation/video_ingest/create_searchable_index.sh
@@ -6,4 +6,4 @@ export VIAME_SCRIPT_DIR=${VIAME_INSTALL_DIR}/configs
 source ${VIAME_INSTALL_DIR}/setup_viame.sh
 
 python ${VIAME_SCRIPT_DIR}/ingest_video.py --init -l input_list.txt \
-  --build-index --ball-tree -p pipelines/ingest_video.tut.pipe
+  --build-index --ball-tree -p pipelines/ingest_list.tut.pipe
