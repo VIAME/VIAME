97ad63e5a9aea37b36acad66dcbb7685a4afa613
Matt Dawkins <matt.dawkins@kitware.com>
2018-05-21 13:20:57 -0400

    Add missing files

---
+++ examples/annotation_and_visualization/run_pipe_display.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Setup VIAME Paths (no need to run multiple times if you already ran it)
+
+export VIAME_INSTALL=./../..
+
+source ${VIAME_INSTALL}/setup_viame.sh 
+
+# Run vpView
+
+pipeline_runner -p display_annotation.pipe
