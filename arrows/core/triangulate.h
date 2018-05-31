743c2ed99d255fd68875057f64d0a3b7f83aed47
Brian Clipp <brian.clipp@kitware.com>
2018-05-30 22:10:37 -0400

    Added necessary comments.

--- arrows/core/triangulate.h
+++ arrows/core/triangulate.h
@@ -63,7 +63,18 @@ Eigen::Matrix<T,3,1>
 triangulate_inhomog(const std::vector<vital::simple_camera_perspective >& cameras,
                     const std::vector<Eigen::Matrix<T,2,1> >& points);
 
-
+/// Triangulate a 3D point from a set of cameras and 2D image points
+/**
+* This function uses only the first two cameras and two points to triangulate.
+* It uses the method laid out in the paper "Triangulation Made Easy" Lindstrom
+* CVPR 2010.  This approach is efficient and offers an alternative approach that
+* may be numerically superior in some cases.  This approach does not work for
+* points at infinty.
+*
+*  \param cameras a vector of camera objects
+*  \param points a vector of image points corresponding to each camera
+*  \return a 3D triangulated point location
+*/
 template <typename T>
 KWIVER_ALGO_CORE_EXPORT
 Eigen::Matrix<T, 3, 1>
