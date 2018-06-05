7bee32741fe63856c81c4c67c3de51565aeaa3c0
Matthew Woehlke <matthew.woehlke@kitware.com>
2018-06-05 15:45:58 -0400

    Introduce indirect range adapter
    Create a range adapter to apply a level of indirection to a range during
    iteration. This effectively suppresses dereferencing of the iterator, so
    that the user sees the iterators rather than the values, while still
    being able to use the more terse and convenient syntax of a range-based
    for. Also, fix some minor lingering issues with creating a mutating
    range adapter.

--- vital/range/defs.h
+++ vital/range/defs.h
@@ -48,6 +48,25 @@ namespace range {
 #define KWIVER_UNPACK_TOKENS(...) __VA_ARGS__
 
 // ----------------------------------------------------------------------------
+#define KWIVER_MUTABLE_RANGE_ADAPTER( name ) \
+  struct name##_view_adapter_t \
+  { \
+    template < typename Range > \
+    static name##_view< Range > \
+    adapt( Range& range ) \
+    { return { range }; } \
+    \
+    template < typename Range > \
+    static name##_view< Range const > \
+    adapt( Range const& range ) \
+    { return { range }; } \
+  }; \
+  \
+  inline constexpr \
+  range_adapter_t< name##_view_adapter_t > \
+  name() { return {}; }
+
+// ----------------------------------------------------------------------------
 #define KWIVER_RANGE_ADAPTER_TEMPLATE( name, args, arg_names ) \
   template < KWIVER_UNPACK_TOKENS args > \
   struct name##_view_adapter_t \
@@ -115,7 +134,7 @@ operator|( Range const&, Adapter );
 template < typename Range, typename Adapter >
 auto
 operator|(
-  Range const& range,
+  Range& range,
   range_adapter_t< Adapter >(*)() )
 -> decltype( Adapter::adapt( range ) )
 {
@@ -126,7 +145,7 @@ operator|(
 template < typename Range, typename Adapter >
 auto
 operator|(
-  Range const& range,
+  Range& range,
   range_adapter_t< Adapter > )
 -> decltype( Adapter::adapt( range ) )
 {
@@ -171,8 +190,13 @@ namespace range_detail {
   using namespace std;
 
   template < typename Range >
-  struct range_helper
+  class range_helper
   {
+  protected:
+    static auto begin_helper( Range&& range )
+    -> decltype( begin( range ) );
+
+  public:
     static auto begin_helper( Range& range )
     -> decltype( begin( range ) )
     {
