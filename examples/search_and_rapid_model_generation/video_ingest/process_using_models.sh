f0daf2488a40239562d82209d8ceda8d9f53e25f
Matt Dawkins <matt.dawkins@kitware.com>
2018-06-14 05:31:23 -0400

    Update list

--- examples/search_and_rapid_model_generation/video_ingest/process_using_models.sh
+++ examples/search_and_rapid_model_generation/video_ingest/process_using_models.sh
@@ -5,6 +5,6 @@ export VIAME_SCRIPT_DIR=${VIAME_INSTALL_DIR}/configs
 
 source ${VIAME_INSTALL_DIR}/setup_viame.sh
 
-pipeline_runner -p ${VIAME_INSTALL}/configs/pipelines/detector_use_svm_models.pipe \
+pipeline_runner -p ${VIAME_INSTALL_DIR}/configs/pipelines/detector_use_svm_models.pipe \
                 -s input:image_list_file=input_list.txt
 
