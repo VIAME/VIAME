f14e76558aaed7533009aae735e2387c61a8aab1
Chet Nieter <chet.nieter@kitware.com>
2018-06-18 15:42:08 -0400

    Fixed GDAL version check to work on Windows.
    Fixed GDAL version check in cmake configuration so it does not
    depend on the gdal_config script which does not exist on Windows.

--- CMake/kwiver-depends-GDAL.cmake
+++ CMake/kwiver-depends-GDAL.cmake
@@ -8,24 +8,33 @@ option( KWIVER_ENABLE_GDAL
 if( KWIVER_ENABLE_GDAL )
   find_package( GDAL REQUIRED )
   if( GDAL_FOUND )
-    if( GDAL_CONFIG )
-      EXEC_PROGRAM(${GDAL_CONFIG}
-        ARGS --version
-        OUTPUT_VARIABLE GDAL_VERSION )
-      STRING(REGEX REPLACE "([0-9]+)\\.([0-9]+)\\.([0-9]+)" "\\1" GDAL_VERSION_MAJOR "${GDAL_VERSION}")
-      STRING(REGEX REPLACE "([0-9]+)\\.([0-9]+)\\.([0-9]+)" "\\2" GDAL_VERSION_MINOR "${GDAL_VERSION}")
+    # We need to build the file in a line-by-line fashon because of
+    # portability with end of line markers.
+    file( WRITE  ${CMAKE_BINARY_DIR}/test_gdal_version.cxx "#include <gdal_version.h>\n" )
+    file( APPEND ${CMAKE_BINARY_DIR}/test_gdal_version.cxx "#include <iostream>\n" )
+    file( APPEND ${CMAKE_BINARY_DIR}/test_gdal_version.cxx "int main() { std::cout")
+    file( APPEND ${CMAKE_BINARY_DIR}/test_gdal_version.cxx " << GDAL_VERSION_MAJOR << \".\"" )
+    file( APPEND ${CMAKE_BINARY_DIR}/test_gdal_version.cxx " << GDAL_VERSION_MINOR << \".\"" )
+    file( APPEND ${CMAKE_BINARY_DIR}/test_gdal_version.cxx " << GDAL_VERSION_REV; }\n" )
 
-      message(STATUS "Found GDAL ${GDAL_VERSION}")
+    TRY_RUN( GDAL_TEST_RUN
+             GDAL_TEST_COMPILE
+             ${CMAKE_BINARY_DIR}
+             ${CMAKE_BINARY_DIR}/test_gdal_version.cxx
+             COMPILE_DEFINITIONS "-I${GDAL_INCLUDE_DIR}"
+             RUN_OUTPUT_VARIABLE GDAL_VERSION )
 
-      if( GDAL_VERSION_MAJOR LESS 2 OR GDAL_VERSION_MINOR LESS 3 )
-        message( FATAL_ERROR "GDAL ${GDAL_VERSION} found, but version must be 2.3.0 or higher." )
-        unset(GDAL_INCLUDE_DIR)
-      else()
-        include_directories(SYSTEM ${GDAL_INCLUDE_DIR})
-      endif()
-    else()
-      message( FATAL_ERROR "gdal-config missing from GDAL installation. Can not test for GDAL version." )
+    if ( NOT GDAL_TEST_COMPILE )
+      message( FATAL_ERROR "Failed to build GDAL version test executable." )
+    endif()
+
+    if( GDAL_VERSION VERSION_LESS 2.3.0 )
+      message( FATAL_ERROR "GDAL ${GDAL_VERSION} found, but version must be 2.3.0 or higher." )
       unset(GDAL_INCLUDE_DIR)
+    else()
+      file( REMOVE ${CMAKE_BINARY_DIR}/test_gdal_version.cxx )
+      message(STATUS "Found GDAL ${GDAL_VERSION}")
+      include_directories(SYSTEM ${GDAL_INCLUDE_DIR})
     endif()
   endif()
 endif()
