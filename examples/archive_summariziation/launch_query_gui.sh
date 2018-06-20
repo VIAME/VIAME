78bbc75a48859d01828e85a24ab928dc6b8f01fd
Matt Dawkins <matt.dawkins@kitware.com>
2018-06-20 16:05:57 -0400

    Update example

--- examples/archive_summariziation/launch_query_gui.sh
+++ examples/archive_summariziation/launch_query_gui.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-export VIAME_INSTALL_DIR=~/Dev/viame/build/install
-export VIAME_SCRIPT_DIR=${VIAME_INSTALL_DIR}/configs
+export VIAME_INSTALL_DIR=./../..
 
 source ${VIAME_INSTALL_DIR}/setup_viame.sh
-python ${VIAME_SCRIPT_DIR}/launch_query_gui.py -qp pipelines/query_retrieval_and_iqr.res.pipe
+
+python ${VIAME_INSTALL_DIR}/configs/launch_query_gui.py \
+  -qp pipelines/query_retrieval_and_iqr.res.pipe
--- examples/archive_summariziation/launch_timeline_viewer.sh
+++ examples/archive_summariziation/launch_timeline_viewer.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 export VIAME_INSTALL_DIR=./../..
-export VIAME_SCRIPT_DIR=${VIAME_INSTALL_DIR}/configs
 
 source ${VIAME_INSTALL_DIR}/setup_viame.sh
 
