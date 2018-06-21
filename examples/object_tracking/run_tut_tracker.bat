84e0094822cdfc0628ed8839b9664b222edb09b4
Matt Dawkins <matt.dawkins@kitware.com>
2018-06-21 15:40:40 -0400

    Update examples

--- examples/object_tracking/run_tut_tracker.bat
+++ examples/object_tracking/run_tut_tracker.bat
@@ -2,10 +2,13 @@
 
 REM Setup VIAME Paths (no need to set if installed to registry or already set up)
 
-CALL .\..\..\setup_viame.bat
+SET VIAME_INSTALL=.\..\..
+
+CALL %VIAME_INSTALL%\setup_viame.bat
 
 REM Run Pipeline
 
-pipeline_runner.exe -p simple_tracker.pipe
+pipeline_runner.exe -p %VIAME_INSTALL%\configs\pipelines\detector_yolo_v2_default.pipe ^
+                    -s input:image_list_file=input_image_list_small_set.txt
 
 pause
