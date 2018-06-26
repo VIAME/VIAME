1071e60eb12259054f4fa83b3dcd72e856f53155
Matt Dawkins <matt.dawkins@kitware.com>
2018-06-26 00:40:28 -0400

    Update configs

---
+++ examples/annotation_and_visualization/run_chip_pipe.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Setup VIAME Paths (no need to run multiple times if you already ran it)
+
+export VIAME_INSTALL=./../..
+
+source ${VIAME_INSTALL}/setup_viame.sh 
+
+# Run vpView
+
+pipeline_runner -p ${VIAME_INSTALL}/configs/pipelines/extract_chips_from_detections.pipe
---
+++ examples/annotation_and_visualization/run_draw_pipe.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Setup VIAME Paths (no need to run multiple times if you already ran it)
+
+export VIAME_INSTALL=./../..
+
+source ${VIAME_INSTALL}/setup_viame.sh 
+
+# Run vpView
+
+pipeline_runner -p ${VIAME_INSTALL}/configs/pipelines/draw_detections_on_images.pipe
