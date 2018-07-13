3b19b52fa1adf4182cb1cd4af25040d6724dba2f
Matt Dawkins <matt.dawkins@kitware.com>
2018-07-13 18:20:58 +0100

    Update scripts

--- configs/prj-windows/train_yolo_v2_viame_csv.bat
+++ configs/prj-windows/train_yolo_v2_viame_csv.bat
@@ -4,13 +4,13 @@ REM Setup VIAME Paths (no need to set if installed to registry or already set up
 
 SET VIAME_INSTALL=C:\Program Files\VIAME
 
-CALL %VIAME_INSTALL%\setup_viame.bat
+CALL "%VIAME_INSTALL%\setup_viame.bat"
 
 REM Run pipeline
 
 viame_train_detector.exe ^
   -i training_data_habcam ^
-  -c ${VIAME_INSTALL}/configs/pipelines/train_yolo_v2_high_res_viame_csv.conf ^
+  -c "${VIAME_INSTALL}/configs/pipelines/train_yolo_v2_high_res_viame_csv.conf" ^
   --threshold 0.0
 
 pause
