42c7b696904ae9d607db6a556a4e3b3e934ac0be
Matt Dawkins <matt.dawkins@kitware.com>
2018-07-25 11:52:06 -0400

    Add new example for ingestion from detections and image lists

---
+++ examples/search_and_rapid_model_generation/viame_ingest/create_index.from_detections.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export VIAME_INSTALL=./../../..
+
+source ${VIAME_INSTALL}/setup_viame.sh
+
+python ${VIAME_INSTALL}/configs/ingest_video.py --init -l input_list.txt \
+  --build-index --ball-tree -p pipelines/ingest_list_and_detections.res.pipe \
+  -install ${VIAME_INSTALL}
