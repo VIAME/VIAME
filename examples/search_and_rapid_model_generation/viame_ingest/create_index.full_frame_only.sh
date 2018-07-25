297c0b22bf66006bce5693e996f84a992f8274c0
Matt Dawkins <matt.dawkins@kitware.com>
2018-07-25 14:00:53 -0400

    Update example

--- examples/search_and_rapid_model_generation/viame_ingest/create_index.full_frame_only.sh
+++ examples/search_and_rapid_model_generation/viame_ingest/create_index.full_frame_only.sh
@@ -6,4 +6,5 @@ source ${VIAME_INSTALL}/setup_viame.sh
 
 python ${VIAME_INSTALL}/configs/ingest_video.py --init -l input_list.txt \
   --build-index --ball-tree -p pipelines/ingest_list.res.full_frame.pipe \
+  -archive-width 1400 -archive-height 1000 \
   -install ${VIAME_INSTALL}
