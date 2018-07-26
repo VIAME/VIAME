27f46b0b0645e867bfcf6db4889b19841b16d36b
Matt Dawkins <matt.dawkins@kitware.com>
2018-07-26 17:37:08 -0400

    Update proposal

---
+++ examples/search_and_rapid_model_generation/viame_ingest/process_ff_using_models.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export VIAME_INSTALL=./../../..
+
+source ${VIAME_INSTALL}/setup_viame.sh
+
+pipeline_runner -p ${VIAME_INSTALL}/configs/pipelines/full_frame_classifier_svm.pipe \
+                -s input:image_list_file=input_list.txt
+
