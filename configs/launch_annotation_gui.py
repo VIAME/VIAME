f5ad46e98461a1c157cc607843a06b44bf5eeb5c
Matt Dawkins <matt.dawkins@kitware.com>
2018-07-27 16:58:20 -0400

    Update configs for embedded pipes

--- configs/launch_annotation_gui.py
+++ configs/launch_annotation_gui.py
@@ -4,11 +4,17 @@ import sys
 import os
 import glob
 import argparse
+import atexit
+import shutil
+import tempfile
 
 if os.name == 'nt':
   div = '\\'
 else:
   div = '/'
+
+temp_dir = tempfile.mkdtemp(prefix='vpview-tmp')
+atexit.register(lambda: shutil.rmtree(temp_dir))
 
 # Helper class to list files with a given extension in a directory
 def list_files_in_dir( folder ):
@@ -120,6 +126,28 @@ def process_dir( args ):
   cmd = get_gui_cmd() + " -p " + project_file
   os.system( cmd )
 
+def create_pipelines_list( glob_str ):
+  (fd, name) = tempfile.mkstemp(prefix='vpview-pipelines-',
+                                suffix='.ini',
+                                text=True, dir=temp_dir)
+
+  search_str = os.path.join( get_script_path(), glob_str )
+  pipeline_files = glob.glob( search_str )
+  total_entries = len( pipeline_files )
+
+  f = os.fdopen(fd, 'w')
+
+  f.write("[EmbeddedPipelines]\n")
+  f.write("size=" + str( total_entries ) + "\n")
+
+  for ind, full_path in enumerate( pipeline_files ):
+    name_id = os.path.splitext( os.path.basename( full_path ) )[0]
+    f.write("%s\\Name=\"%s\"\n" % (ind+1, name_id) )
+    f.write("%s\\Path=\"%s\"\n" % (ind+1, full_path) )
+
+  f.close()
+  return name
+
 # Main Function
 if __name__ == "__main__" :
 
@@ -139,6 +167,10 @@ if __name__ == "__main__" :
                       default="gui-params" + div + "dark_gui_settings.ini",
                       help="Predefined query directory, if present")
 
+  parser.add_argument("-pipelines", dest="pipelines",
+                      default="pipelines/embedded*.pipe",
+                      help="Glob pattern for runable processing pipelines")
+
   parser.add_argument("--debug", dest="debug", action="store_true",
                       help="Run with debugger attached to process")
                       
@@ -156,4 +188,6 @@ if __name__ == "__main__" :
     command = get_gui_cmd()
     if len( args.gui_theme ) > 0:
       command = command + " --theme \"" + find_file( args.gui_theme ) + "\" "
+    if len( args.pipelines ) > 0:
+      command = command + " --import-config \"" + create_pipelines_list( args.pipelines ) + "\" "
     execute_command( command )
