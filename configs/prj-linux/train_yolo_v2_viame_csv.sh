41774003837044663396d2f5baa3e0e7b7025926
Matt Dawkins <matt.dawkins@kitware.com>
2018-07-29 21:47:53 -0400

    Update proj files

--- configs/prj-linux/train_yolo_v2_viame_csv.sh
+++ configs/prj-linux/train_yolo_v2_viame_csv.sh
@@ -10,5 +10,5 @@ source ${VIAME_INSTALL}/setup_viame.sh
 
 viame_train_detector \
   -i training_data_habcam \
-  -c ${VIAME_INSTALL}/configs/pipelines/train_yolo_v2_high_res_viame_csv.conf \
+  -c ${VIAME_INSTALL}/configs/pipelines/train_yolo_v2_704_viame_csv.conf \
   --threshold 0.0
