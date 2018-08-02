db10db05ba0df18cc39da462b97e7c0b017b2fdc
Chet Nieter <chet.nieter@kitware.com>
2018-08-02 16:16:36 -0400

    Added noisy tests for rpc triangulation.
    Added tests that have artifical noise added to the input data. One
    that adds noise to the landmarks and one that adds noise to half of
    the tracks.

--- arrows/core/tests/test_triangulate_landmarks_rpc.cxx
+++ arrows/core/tests/test_triangulate_landmarks_rpc.cxx
@@ -28,6 +28,8 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <test_scene.h>
+
 #include <arrows/core/metrics.h>
 #include <arrows/core/projected_track_set.h>
 #include <arrows/core/triangulate_landmarks.h>
@@ -35,6 +37,8 @@
 #include <tests/test_gtest.h>
 
 kwiver::vital::path_t g_data_dir;
+
+using namespace kwiver::vital;
 
 // ----------------------------------------------------------------------------
 int main(int argc, char** argv)
@@ -48,41 +52,46 @@ int main(int argc, char** argv)
 class triangulate_landmarks_rpc : public ::testing::Test
 {
   TEST_ARG(data_dir);
+
+  triangulate_landmarks_rpc()
+  {
+    // Landmarks pulled from Google Maps
+    std::vector< vector_3d > lm_pos;
+    lm_pos.push_back( vector_3d( -117.237465, 32.881208, 110.0 ) );
+    lm_pos.push_back( vector_3d( -117.235309, 32.879108, 110.0 ) );
+    lm_pos.push_back( vector_3d( -117.239404, 32.877824, 110.0 ) );
+    lm_pos.push_back( vector_3d( -117.236088, 32.877091, 110.0 ) );
+    lm_pos.push_back( vector_3d( -117.240455, 32.876183, 110.0 ) );
+
+    for ( size_t i = 0; i < lm_pos.size(); ++i )
+    {
+      auto landmark_ptr = std::make_shared< landmark_< double > >( lm_pos[i] );
+      landmark_map.insert(
+        std::pair< landmark_id_t, landmark_sptr >(i, landmark_ptr ) );
+    }
+
+    for ( size_t i = 0; i < 8; ++i )
+    {
+      path_t filepath = data_dir + "/rpc_data" + std::to_string(i) + ".dat";
+      auto cam_ptr =
+        std::make_shared< simple_camera_rpc >( read_rpc( filepath ) );
+      camera_map.insert( std::pair< frame_id_t, camera_sptr >( i, cam_ptr ) );
+    }
+  }
+
+  landmark_map::map_landmark_t landmark_map;
+  camera_map::map_camera_t camera_map;
 };
 
 // ----------------------------------------------------------------------------
 TEST_F(triangulate_landmarks_rpc, from_data)
 {
-  using namespace kwiver::vital;
-
   kwiver::arrows::core::triangulate_landmarks tri_lm;
   config_block_sptr cfg = tri_lm.get_configuration();
 
-  // Landmarks pulled from Google Maps
-  landmark_map::map_landmark_t landmark_map;
-  std::vector< vector_3d > lm_pos;
-  lm_pos.push_back( vector_3d( -117.237465, 32.881208, 110.0 ) );
-  lm_pos.push_back( vector_3d( -117.235309, 32.879108, 110.0 ) );
-  lm_pos.push_back( vector_3d( -117.239404, 32.877824, 110.0 ) );
-  lm_pos.push_back( vector_3d( -117.236088, 32.877091, 110.0 ) );
-  lm_pos.push_back( vector_3d( -117.240455, 32.876183, 110.0 ) );
-
-  for ( size_t i = 0; i < lm_pos.size(); ++i )
-  {
-    auto landmark_ptr = std::make_shared< landmark_< double > >( lm_pos[i] );
-    landmark_map.insert(
-      std::pair< landmark_id_t, landmark_sptr >(i, landmark_ptr ) );
-  }
   landmark_map_sptr landmarks =
     std::make_shared< simple_landmark_map >( landmark_map );
 
-  camera_map::map_camera_t camera_map;
-  for ( size_t i = 0; i < 8; ++i )
-  {
-    path_t filepath = data_dir + "/rpc_data" + std::to_string(i) + ".dat";
-    auto cam_ptr = std::make_shared< simple_camera_rpc >( read_rpc( filepath ) );
-    camera_map.insert( std::pair< frame_id_t, camera_sptr >( i, cam_ptr ) );
-  }
   camera_map_sptr cameras = std::make_shared< simple_camera_map >( camera_map );
 
   auto tracks = kwiver::arrows::projected_tracks(landmarks, cameras);
@@ -100,4 +109,71 @@ TEST_F(triangulate_landmarks_rpc, from_data)
                                                        landmarks->landmarks(),
                                                        tracks->tracks() );
   EXPECT_NEAR(0.0, end_rmse, 0.05) << "RMSE after triangulation";
+}
+
+// ----------------------------------------------------------------------------
+TEST_F(triangulate_landmarks_rpc, noisy_landmarks)
+{
+  kwiver::arrows::core::triangulate_landmarks tri_lm;
+  config_block_sptr cfg = tri_lm.get_configuration();
+
+  landmark_map_sptr landmarks =
+    std::make_shared< simple_landmark_map >( landmark_map );
+
+  camera_map_sptr cameras = std::make_shared< simple_camera_map >( camera_map );
+
+  auto tracks = kwiver::arrows::projected_tracks(landmarks, cameras);
+
+  // add Gaussian noise to the landmark positions
+  landmark_map_sptr landmarks0 =
+    kwiver::testing::noisy_landmarks(landmarks, 0.01);
+
+  double init_rmse = kwiver::arrows::reprojection_rmse( cameras->cameras(),
+                                                        landmarks0->landmarks(),
+                                                        tracks->tracks() );
+  std::cout << "initial reprojection RMSE: " << init_rmse << std::endl;
+
+  EXPECT_LE(init_rmse, 5000.0)
+    << "Initial reprojection RMSE should be large before triangulation";;
+
+  tri_lm.triangulate(cameras, tracks, landmarks0);
+
+  double end_rmse = kwiver::arrows::reprojection_rmse( cameras->cameras(),
+                                                       landmarks0->landmarks(),
+                                                       tracks->tracks() );
+  EXPECT_NEAR(0.0, end_rmse, 0.05) << "RMSE after triangulation";
+}
+
+// ----------------------------------------------------------------------------
+TEST_F(triangulate_landmarks_rpc, noisy_tracks)
+{
+  kwiver::arrows::core::triangulate_landmarks tri_lm;
+  config_block_sptr cfg = tri_lm.get_configuration();
+
+  landmark_map_sptr landmarks =
+    std::make_shared< simple_landmark_map >( landmark_map );
+
+  camera_map_sptr cameras = std::make_shared< simple_camera_map >( camera_map );
+
+  auto tracks = kwiver::arrows::projected_tracks(landmarks, cameras);
+
+  // remove some tracks/track_states and add Gaussian noise
+  const double track_stdev = 1.0;
+  feature_track_set_sptr tracks0 = kwiver::testing::noisy_tracks(
+    kwiver::testing::subset_tracks(tracks, 0.5), track_stdev);
+
+  double init_rmse = kwiver::arrows::reprojection_rmse( cameras->cameras(),
+                                                        landmarks->landmarks(),
+                                                        tracks0->tracks() );
+  std::cout << "initial reprojection RMSE: " << init_rmse << std::endl;
+
+  EXPECT_LE(init_rmse, 10.0)
+    << "Initial reprojection RMSE should be large before triangulation";;
+
+  tri_lm.triangulate(cameras, tracks0, landmarks);
+
+  double end_rmse = kwiver::arrows::reprojection_rmse( cameras->cameras(),
+                                                       landmarks->landmarks(),
+                                                       tracks0->tracks() );
+  EXPECT_NEAR(0.0, end_rmse, 2.0*track_stdev) << "RMSE after triangulation";
 }
