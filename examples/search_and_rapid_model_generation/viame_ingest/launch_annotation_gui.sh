3a499ecf395c569f82ac1720cfc6fa689cd4f00f
Matt Dawkins <matt.dawkins@kitware.com>
2018-08-03 12:35:46 -0400

    Restructure config examples

---
+++ examples/search_and_rapid_model_generation/viame_ingest/launch_annotation_gui.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Setup VIAME Paths (no need to run multiple times if you already ran it)
+
+export VIAME_INSTALL=./../../..
+
+source ${VIAME_INSTALL}/setup_viame.sh 
+
+# Run vpView annotation GUI
+
+python ${VIAME_INSTALL}/configs/launch_annotation_gui.py
