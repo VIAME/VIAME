c41f8a722aceaa0187813fbbdc2ce455e0f4e854
Matt Dawkins <matt.dawkins@kitware.com>
2018-08-07 19:48:16 -0400

    Adjust style

--- cmake/add_project_vivia.cmake
+++ cmake/add_project_vivia.cmake
@@ -63,15 +63,15 @@ ExternalProject_Add(vivia
   INSTALL_DIR ${VIAME_BUILD_INSTALL_PREFIX}
   )
 
-if ( VIAME_FORCEBUILD )
-ExternalProject_Add_Step(vivia forcebuild
-  COMMAND ${CMAKE_COMMAND}
-    -E remove ${VIAME_BUILD_PREFIX}/src/vivia-stamp/vivia-build
-  COMMENT "Removing build stamp file for build update (forcebuild)."
-  DEPENDEES configure
-  DEPENDERS build
-  ALWAYS 1
-  )
+if( VIAME_FORCEBUILD )
+  ExternalProject_Add_Step(vivia forcebuild
+    COMMAND ${CMAKE_COMMAND}
+      -E remove ${VIAME_BUILD_PREFIX}/src/vivia-stamp/vivia-build
+    COMMENT "Removing build stamp file for build update (forcebuild)."
+    DEPENDEES configure
+    DEPENDERS build
+    ALWAYS 1
+    )
 endif()
 
 set(VIAME_ARGS_vivia
