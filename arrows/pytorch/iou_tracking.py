f06b7534373d2663098b0f8e9e3361bd242d5a9e
Neal Siekierski <neal.siekierski@kitware.com>
2018-08-07 15:59:11 -0400

    Remove trailing whitespace from various files
    Also remove the two leading blank lines from iou_tracking.py

--- arrows/pytorch/iou_tracking.py
+++ arrows/pytorch/iou_tracking.py
@@ -1,10 +1,8 @@
-
-
 class IOU_tracker(object):
     def __init__(self, iou_accept_threshold, iou_reject_threshold):
         self._iou_accept_threshold = iou_accept_threshold
         self._iou_reject_threshold = iou_reject_threshold
-    
+
     def __call__(self, track_set, track_state_list):
         return self._track_iou(track_set, track_state_list)
 
@@ -25,7 +23,7 @@ class IOU_tracker(object):
                     if len(sorted_ts_list) >= 2 and self._iou_score(track_set[t_idx][-1].bbox, sorted_ts_list[1].bbox) < self._iou_reject_threshold:
                         track_set[t_idx].updated_flag = True
                         track_set.update_track(track_set[t_idx].id, best_match)
-    
+
                         # remove from best matching detection from detections
                         del track_state_list[track_state_list.index(best_match)]
 
@@ -34,15 +32,15 @@ class IOU_tracker(object):
     def _iou_score(self, bbox1, bbox2):
         """
         Calculates the intersection-over-union of two bounding boxes.
-        
+
         Args:
             bbox1 list of float: bounding box in format x1,y1,w,h
             bbox2 list of float: bounding box in format x1,y1,w,h.
-        
+
         Returns:
             int: intersection-over-onion of bbox1, bbox2
         """
-        
+
         (x1, y1, w1, h1) = bbox1
         (x2, y2, w2, h2) = bbox2
 
@@ -51,15 +49,15 @@ class IOU_tracker(object):
         overlap_y0 = max(y1, y2)
         overlap_x1 = min(x1 + w1, x2 + w2)
         overlap_y1 = min(y1 + h1, y2 + h2)
-        
+
         # check if there is an overlap
         if overlap_x1 - overlap_x0 <= 0 or overlap_y1 - overlap_y0 <= 0:
             return 0
-        
+
         # if yes, calculate the ratio of the overlap to each ROI size and the unified size
         size_1 = w1 * h1
         size_2 = w2 * h2
         size_intersection = (overlap_x1 - overlap_x0) * (overlap_y1 - overlap_y0)
         size_union = size_1 + size_2 - size_intersection
-        
+
         return float(size_intersection) / size_union
