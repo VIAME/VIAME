f0b680a45bc7a88fc8db6fd10f4f3bda65a913b6
Linus Sherrill <linus.sherrill@kitware.com>
2018-08-22 13:38:52 -0400

    Added includes to define missing functions on windows build
    - Needed definitions for std::isprint and std::min
    - Reformatted to be closer to style guidelines.

--- vital/util/hex_dump.h
+++ vital/util/hex_dump.h
@@ -46,24 +46,33 @@ namespace vital {
 /**
  * @brief Format a string as a traditional hex dump for debugging
  *
- * See this Stackexchange entry for further discussion: 
+ * See this Stackexchange entry for further discussion:
  * https://codereview.stackexchange.com/questions/165120/printing-hex-dumps-for-diagnostics
  *
  * @param[out] os Output stream for the formatted string
  * @param[in,out] buffer Input buffer to dump
  * @param[in] bufsize Length of input buffer or number of bytes to dump
- * @param[in] showPrintableCharacter  \b true will print printable characters along with hex
+ * @param[in] showPrintableCharacter \b true will print printable characters along with hex
  */
-VITAL_UTIL_EXPORT std::ostream& hex_dump(std::ostream& os, const void *buffer,
-                       std::size_t bufsize, bool showPrintableChars = true);
+VITAL_UTIL_EXPORT std::ostream& hex_dump( std::ostream& os,
+                                          const void*   buffer,
+                                          std::size_t   bufsize,
+                                          bool          showPrintableChars = true );
 
-struct VITAL_UTIL_EXPORT hexDump {
-    const void *buffer;
-    std::size_t bufsize;
-    hexDump(const void *buf, std::size_t bufsz) : buffer{buf}, bufsize{bufsz} {}
-    friend std::ostream &operator<<(std::ostream &out, const hexDump &hd) {
-        return hex_dump(out, hd.buffer, hd.bufsize, true);
-    }
+struct VITAL_UTIL_EXPORT hexDump
+{
+  const void* buffer;
+  std::size_t bufsize;
+
+  hexDump( const void* buf, std::size_t bufsz )
+    : buffer{ buf }
+    , bufsize{ bufsz }
+  {}
+
+  friend std::ostream& operator<<( std::ostream& out, const hexDump& hd )
+  {
+    return hex_dump( out, hd.buffer, hd.bufsize, true );
+  }
 };
 
 } // namespace vital
