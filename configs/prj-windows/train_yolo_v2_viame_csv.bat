a1df817698418227530fd44f9ec513b956af1d16
Matthew Dawkins <matt.dawkins@kitware.com>
2018-08-22 06:08:39 -0700

    Update configs

--- configs/prj-windows/train_yolo_v2_viame_csv.bat
+++ configs/prj-windows/train_yolo_v2_viame_csv.bat
@@ -9,7 +9,7 @@ CALL "%VIAME_INSTALL%\setup_viame.bat"
 REM Run pipeline
 
 viame_train_detector.exe ^
-  -i training_data_habcam ^
+  -i training_data ^
   -c "${VIAME_INSTALL}/configs/pipelines/train_yolo_v2_704_viame_csv.conf" ^
   --threshold 0.0
 
