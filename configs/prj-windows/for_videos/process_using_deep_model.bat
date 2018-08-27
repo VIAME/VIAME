9d3b811072f7c01b1ecc528442d0081fcef07fc7
Matthew Dawkins <matt.dawkins@kitware.com>
2018-08-27 08:23:30 -0500

    Update configs

--- configs/prj-windows/for_videos/process_using_deep_model.bat
+++ configs/prj-windows/for_videos/process_using_deep_model.bat
@@ -1,5 +1,18 @@
 @echo off
 
-echo Script not yet implemented on Windows (but will be shortly)
+REM Setup VIAME Paths (no need to set if installed to registry or already set up)
+
+SET VIAME_INSTALL=C:\Program Files\VIAME
+
+CALL "%VIAME_INSTALL%\setup_viame.bat"
+
+REM Run Pipeline
+
+pipeline_runner.exe -p "%VIAME_INSTALL%\configs\pipelines\detector_yolo_v2_default.pipe" ^
+                    -s input:image_list_file=input_list.txt ^
+                    -s detector:detector:darknet:net_config=deep_training\yolo_v2.cfg ^
+                    -s detector:detector:darknet:weight_file=deep_training\models\yolo_v2.backup ^
+                    -s detector:detector:darknet:class_names=deep_training\yolo_v2.lbl ^
+                    -s detector_csv_writer:file_name=deep_detections.csv
 
 pause
