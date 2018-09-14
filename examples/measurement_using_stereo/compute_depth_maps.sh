0504520375caa52020ffa9c995cd54f7aa0086a1
Matt Dawkins <matt.dawkins@kitware.com>
2018-09-14 14:32:41 -0400

    Add more to script

---
+++ examples/measurement_using_stereo/compute_depth_maps.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# VIAME Installation Location
+export VIAME_INSTALL=../..
+
+# Setup VIAME Paths (no need to run multiple times if you already ran it)
+source ${VIAME_INSTALL}/setup_viame.sh
+
+# Run pipeline
+pipeline_runner -p ${VIAME_INSTALL}/configs/pipelines/measurement_depth_map.pipe \
+                -s input:image_list_file=input_list.txt
