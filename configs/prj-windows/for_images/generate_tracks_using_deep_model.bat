6043f9706e968a1614b247647bad9361a3438872
Matt Dawkins <matt.dawkins@kitware.com>
2018-09-27 18:13:15 -0400

    Norm scale

--- configs/prj-windows/for_images/generate_tracks_using_deep_model.bat
+++ configs/prj-windows/for_images/generate_tracks_using_deep_model.bat
@@ -13,6 +13,7 @@ pipeline_runner.exe -p "%VIAME_INSTALL%\configs\pipelines\tracker_default.tut.pi
                     -s detector:detector:darknet:net_config=deep_training\yolo_v2.cfg ^
                     -s detector:detector:darknet:weight_file=deep_training\models\yolo_v2.backup ^
                     -s detector:detector:darknet:class_names=deep_training\yolo_v2.lbl ^
+                    -s detector:detector:darknet:scale=1.4 ^
                     -s detector_writer:file_name=deep_detections.csv ^
                     -s track_writer:file_name=deep_tracks.csv
 
