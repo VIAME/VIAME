e995bbd70285412fa5c36abafa76d555e9f71e5f
Kyle Edwards <kyle.edwards@kitware.com>
2018-10-01 15:01:46 -0400

    Remove timestamp hack now that kwiver is writing correct timestamp

--- configs/prj-linux/for_videos/generate_detection_plots.sh
+++ configs/prj-linux/for_videos/generate_detection_plots.sh
@@ -12,8 +12,3 @@ python ${VIAME_INSTALL}/configs/ingest_video.py --init -d ${VIDEO_DIRECTORY} \
   -species ${SPECIES_LIST} \
   -threshold 0.25 -frate 2 -smooth 2 \
   -p ${VIAME_INSTALL}/configs/pipelines/index_mouss.no_desc.pipe
-
-# Timestamp adjustment hack (will be removed in future iterations), converts milliseconds to seconds
-for f in database/*.kw18; do
-    awk '{print $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18 / 1e6, $19, $20}' < ${f}.bckup > ${f}
-done
--- examples/archive_summarization/summarize_videos.sh
+++ examples/archive_summarization/summarize_videos.sh
@@ -9,8 +9,3 @@ python ${VIAME_INSTALL}/configs/ingest_video.py --init -d INPUT_DIRECTORY \
   -species pristipomoides_auricilla,pristipomoides_zonatus,pristipomoides_sieboldii,etelis_carbunculus,etelis_coruscans,naso,aphareus_rutilans,seriola,hyporthodus_quernus,caranx_melampygus \
   -threshold 0.25 -frate 2 -smooth 2 \
   -p pipelines/index_mouss.no_desc.pipe
-
-# Timestamp adjustment hack (will be removed in future iterations), converts milliseconds to seconds
-for f in database/*.kw18; do
-    awk '{print $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18 / 1e6, $19, $20}' < ${f}.bckup > ${f}
-done
