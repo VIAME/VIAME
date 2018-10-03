f4cfdfafd5a27db103c7480a87fa4a1ba8c15155
Matt Dawkins <matt.dawkins@kitware.com>
2018-10-03 15:28:22 -0400

    Revert "Throw exception if database fails to start"
    This reverts commit b0fc89752918746bc66cd5028979d017beafb61c.

--- configs/database_tool.py
+++ configs/database_tool.py
@@ -3,7 +3,6 @@
 import sys
 import os
 import shutil
-import subprocess
 
 if os.name == 'nt':
   div = '\\'
@@ -84,7 +83,7 @@ def sequence_cmd( prefix, cmd, args  ):
   return prefix + " && " + format_cmd( cmd ) + " " + args
 
 def execute_cmd( cmd, args  ):
-  subprocess.check_output( format_cmd( cmd ) + " " + args, shell=True )
+  os.system( format_cmd( cmd ) + " " + args )
 
 def execute_pycmd( install_dir, cmd, args  ):
   os.system( format_pycmd( install_dir, cmd ) + " " + args )
