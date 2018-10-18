d11ba7e560456f777deb979d11a6191dfe8f739e
Matt Dawkins <matt.dawkins@kitware.com>
2018-10-18 17:54:33 -0400

    Rename more scripts

--- configs/prj-linux/for_videos/launch_search_interface.sh
+++ configs/prj-linux/for_videos/launch_search_interface.sh
@@ -6,5 +6,5 @@ export VIAME_INSTALL=/opt/noaa/viame
 # Setup paths and run command
 source ${VIAME_INSTALL}/setup_viame.sh
 
-python ${VIAME_INSTALL}/configs/launch_query_gui.py \
+python ${VIAME_INSTALL}/configs/launch_search_interface.py \
   -qp pipelines/query_retrieval_and_iqr.res.pipe
--- configs/prj-windows/for_videos/launch_search_interface.bat
+++ configs/prj-windows/for_videos/launch_search_interface.bat
@@ -6,5 +6,5 @@ SET VIAME_INSTALL=C:\Program Files\VIAME
 REM Setup paths and run command
 CALL "%VIAME_INSTALL%\setup_viame.bat"
 
-python.exe "%VIAME_INSTALL%\configs\launch_query_gui.py" ^
+python.exe "%VIAME_INSTALL%\configs\launch_search_interface.py" ^
   -qp pipelines\query_retrieval_and_iqr.res.pipe
