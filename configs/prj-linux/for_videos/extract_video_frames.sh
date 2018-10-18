68ed27ad21515990505836c49c7c3bcecf355012
Matt Dawkins <matt.dawkins@kitware.com>
2018-10-18 19:31:18 -0400

    Update scripts

--- configs/prj-linux/for_videos/extract_video_frames.sh
+++ configs/prj-linux/for_videos/extract_video_frames.sh
@@ -7,20 +7,12 @@ export VIAME_INSTALL=/opt/noaa/viame
 export INPUT_DIRECTORY=videos
 export FRAME_RATE=5
 export START_TIME=00:00:00.00
+export DURATION=99:99:99.99
 export OUTPUT_DIR=frames
 
 # Setup paths and run command
 source ${VIAME_INSTALL}/setup_viame.sh
 
-mkdir -p ${OUTPUT_DIR}
-
-for file in ${INPUT_DIRECTORY}/*
-do
-  file_no_path=${file##*/}
-  output_folder=${OUTPUT_DIR}/${file_no_path}
-  mkdir -p ${output_folder}
-
-  ffmpeg -i ${file} -r ${FRAME_RATE} \
-    -ss ${START_TIME} \
-    ${output_folder}/frame%06d.png
-done
+python ${VIAME_INSTALL}/configs/extract_video_frames.py \
+  -d ${INPUT_DIRECTORY} -o ${OUTPUT_DIR} \
+  -r ${FRAME_RATE} -s ${START_TIME} -t ${DURATION}
--- configs/prj-windows/for_videos/extract_video_frames.bat
+++ configs/prj-windows/for_videos/extract_video_frames.bat
@@ -7,15 +7,14 @@ REM Processing options
 SET INPUT_DIRECTORY=videos
 SET FRAME_RATE=5
 SET START_TIME=00:00:00.00
+SET DURATION=99:99:99.99
 SET OUTPUT_DIR=frames
 
 REM Setup paths and run command
 CALL "%VIAME_INSTALL%\setup_viame.bat"
 
-if not exist "%OUTPUT_DIR%" mkdir "%OUTPUT_DIR%"
-
-for /r %%FILE in (%INPUT_DIRECTORY%\*) do ^
-  if not exist "%OUTPUT_DIR%\%FILE%" mkdir "%OUTPUT_DIR%\%FILE%" && ^
-  ffmpeg.exe -i "%FILE%" -r %FRAME_RATE% -ss %START_TIME% "%OUTPUT_DIR%\%i%\frame%%06d.png"
+python.exe "%VIAME_INSTALL%\configs\extract_video_frames.py" ^
+  -d "%INPUT_DIRECTORY%" -o %OUTPUT_DIR% ^
+  -r %FRAME_RATE% -s %START_TIME% -t %DURATION%
 
 pause
