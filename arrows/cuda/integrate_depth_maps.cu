83bf560a74196ee3845023a3d92296efb77313c7
Matt Leotta <matt.leotta@kitware.com>
2018-11-28 18:01:09 -0500

    update copyright

--- arrows/cuda/integrate_depth_maps.cu
+++ arrows/cuda/integrate_depth_maps.cu
@@ -1,5 +1,5 @@
 /*ckwg +29
-* Copyright 2018 by Kitware, Inc.
+* Copyright 2016 by Kitware SAS, 2018 Kitware, Inc.
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
