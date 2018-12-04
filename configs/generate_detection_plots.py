5df7eb3995ee5be68e7bf3c990635af9d15516b5
Matt Dawkins <matt.dawkins@kitware.com>
2018-12-04 17:01:55 -0500

    Restructure plotting tool

--- configs/generate_detection_plots.py
+++ configs/generate_detection_plots.py
@@ -15,35 +15,45 @@ def create_dir( dirname ):
   if not os.path.exists( dirname ):
     os.makedirs( dirname )
 
-def detection_plot( input_directory, output_directory, objects, threshold, frame_rate,
-                    smooth=1, ext=".csv", net_category="all_fish", top_category_only=False ):
+unordered_ext = ".time_ordered.csv"
+ranked_ext = ".max_ordered.csv"
 
-  def format_x( x, pos ):
+def detection_plot( input_directory, output_directory, objects, threshold, frame_rate,
+                    smooth=1, ext=".csv", net_name="all_fish", top_category_only=False ):
+
+  def format_x( x, pos=0 ):
     t = datetime.timedelta( seconds = x )
     return str( t )
 
   warnings.filterwarnings( "ignore" )
 
+  if net_name in objects:
+    print( "Plotting error, net category id can't be in object list" )
+    sys.exit(0)
+  else:
+    objects = [ net_name ] + objects
+
+  if input_directory == output_directory:
+    print( "Plotting error, input and output directories must be different" )
+    sys.exit(0)
+
   videos = dict()
-  video_plots = dict()
+
+  create_dir( output_directory )
 
   for filename in os.listdir( input_directory ):
 
-    if not filename.endswith( ext ) or filename.endswith( ".output.csv" ):
+    if not filename.endswith( ext ):
       continue
 
-    fig, ax = video_plots[filename] = plt.subplots()
+    filebase = filename.replace( ext, "" )
 
-    plot_title = "Aggregate - " + filename
-
-    ax.xaxis.set_major_formatter( matplotlib.ticker.FuncFormatter( format_x ) )
-    ax.xaxis.set_major_locator( matplotlib.ticker.MaxNLocator( integer=True ) )
-    ax.set( xlabel="Time", ylabel="Object Count", title=plot_title )
-    ax.grid()
-
+    # Parse input computed detections file
     video_objects = videos[filename] = dict()
+
     for obj in objects:
       video_objects[obj] = dict()
+
     with open( os.path.join( input_directory, filename ), "r" ) as f:
       for line in f:
         line = line.rstrip()
@@ -56,16 +66,21 @@ def detection_plot( input_directory, output_directory, objects, threshold, frame
 
           detection_columns = columns[9:11] if top_category_only else columns[9:]
           name = None
+          is_first = True
           for column in detection_columns:
             if name is not None:
               if name in objects:
                 value = float(column)
                 if value >= threshold:
                   video_objects[name][frame_id] += 1
+                  if is_first:
+                    video_objects[net_name][frame_id] += 1
+                    is_first = False
               name = None
             else:
               name = column
 
+    # Apply smoothing factor
     for obj in objects:
       smoothed_video_frames = dict()
       for frame_id in sorted( video_objects[obj] ):
@@ -85,31 +100,47 @@ def detection_plot( input_directory, output_directory, objects, threshold, frame
 
       video_objects[obj] = smoothed_video_frames
 
-  for obj in objects:
-    sorted_frames = list()
-    with open( os.path.join( output_directory, obj + ".output.csv" ), "w" ) as outfile:
-      outfile.write( "#video_id,frame_id,detection_count\n" )
-      for filename in sorted(videos):
-        video_objects = videos[filename]
+    # Write out video specific items alongside aggregrate plot
+    video_subdir = os.path.join( output_directory, filebase )
+    create_dir( video_subdir )
+
+    agr_fig, agr_ax = plt.subplots()
+    agr_plot_title = "Aggregate - " + filename
+    agr_ax.xaxis.set_major_formatter( matplotlib.ticker.FuncFormatter( format_x ) )
+    agr_ax.xaxis.set_major_locator( matplotlib.ticker.MaxNLocator( integer=True ) )
+    agr_ax.set( xlabel="Time", ylabel="Object Count", title=agr_plot_title )
+    agr_ax.grid()
+
+    for obj in objects:
+      sorted_frames = list()
+      with open( os.path.join( video_subdir, obj + unordered_ext ), "w" ) as outfile:
+        outfile.write( "# video_id, time_id, frame_id, detection_count\n" )
+
         times = list()
         object_counts = list()
-        for frame_id in sorted(video_objects[obj]):
-          times.append(frame_id / frame_rate)
-          object_counts.append(video_objects[obj][frame_id])
-          outfile.write(filename + "," + str(frame_id) + "," + str(video_objects[obj][frame_id]) + "\n")
 
-          sorted_frames.append((filename, frame_id, video_objects[obj][frame_id]))
+        for frame_id in sorted( video_objects[obj] ):
+          frame_time = frame_id / frame_rate
+          times.append( frame_time )
+          object_counts.append( video_objects[obj][frame_id] )
 
-        x = np.array(times)
-        y = np.array(object_counts)
+          outfile.write( filename + "," )
+          outfile.write( format_x(frame_time) + "," )
+          outfile.write( str(frame_id) + "," )
+          outfile.write( str(video_objects[obj][frame_id]) + "\n" )
+
+          sorted_frames.append( (filename, frame_id, video_objects[obj][frame_id]) )
+
+        x = np.array( times )
+        y = np.array( object_counts )
 
         plot_title = obj + " - " + filename
 
         fig, ax = plt.subplots()
-        ax.xaxis.set_major_formatter(matplotlib.ticker.FuncFormatter(format_x))
-        ax.xaxis.set_major_locator(matplotlib.ticker.MaxNLocator(integer=True))
-        plt.locator_params(axis='x', nbins=6)
-        ax.set(xlabel="Time", ylabel="Object Count", title=plot_title)
+        ax.xaxis.set_major_formatter( matplotlib.ticker.FuncFormatter(format_x) )
+        ax.xaxis.set_major_locator( matplotlib.ticker.MaxNLocator(integer=True) )
+        plt.locator_params( axis='x', nbins=6 )
+        ax.set( xlabel="Time", ylabel="Object Count", title=plot_title )
         ax.grid()
 
         ax.plot( x, y )
@@ -118,30 +149,32 @@ def detection_plot( input_directory, output_directory, objects, threshold, frame
         if np.size( y ) > 0 and np.max( y ) < 5:
           ax.set_ylim( ymax = 5 )
         ax.locator_params( axis='x', nbins = 7 )
-        fig.savefig( os.path.join( output_directory, filename + "." + obj + ".png" ) )
+        fig.savefig( os.path.join( video_subdir, filename + "." + obj + ".png" ) )
 
-        fig, ax = video_plots[filename]
-        ax.plot(x, y, label=obj)
+        agr_ax.plot( x, y, label=obj )
 
-    sorted_frames.sort(key=lambda line: line[2], reverse=True)
-    with open( os.path.join( output_directory, obj + ".sorted.output.csv" ), "w" ) as outfile:
-      outfile.write("#video_id,frame_id,detection_count\n")
-      for filename, frame_id, count in sorted_frames:
-        outfile.write(filename + "," + str(frame_id) + "," + str(count) + "\n")
+        sorted_frames.sort( key=lambda line: line[2], reverse=True )
+        with open( os.path.join( video_subdir, obj + ranked_ext ), "w" ) as outfile:
+          outfile.write( "# video_id, time_id, frame_id, detection_count\n" )
+          for filename, frame_id, count in sorted_frames:
+            frame_time = frame_id / frame_rate
+            outfile.write( filename + "," + format_x(frame_time) + ",")
+            outfile.write( str(frame_id) + "," + str(count) + "\n" )
 
-  for filename in video_plots:
-    fig, ax = video_plots[filename]
-    ax.set_ylim( ymin = 0 )
-    ax.set_xlim( xmin = 0 )
-    ax.locator_params( axis='x', nbins = 7 )
-    ax.legend()
-    fig.savefig( os.path.join( output_directory, filename + ".png" ) )
+    agr_ax.set_ylim( ymin = 0 )
+    agr_ax.set_xlim( xmin = 0 )
+    agr_ax.locator_params( axis='x', nbins = 7 )
+    agr_ax.legend()
+    agr_fig.savefig( os.path.join( output_directory, filename + ".png" ) )
+
+  # Write out aggregate information across all videos
+  
 
 if __name__ == "__main__":
   try:
     smooth = int( sys.argv[4] )
   except IndexError:
     smooth = 1
-  detection_plot( ".", sys.argv[1].split(","),
+  detection_plot( ".", "plots", sys.argv[1].split(","),
                   float( sys.argv[2] ), float( sys.argv[3] ),
                   smooth )
