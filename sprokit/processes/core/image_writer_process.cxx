58db0a9d1c076beecf4cd0a87efa9cfa33c35a5d
Matt Dawkins <matt.dawkins@kitware.com>
2019-01-01 01:06:39 -0500

    Style and add optional input

--- sprokit/processes/core/image_writer_process.cxx
+++ sprokit/processes/core/image_writer_process.cxx
@@ -58,20 +58,23 @@ using namespace cv;
 
 namespace algo = kwiver::vital::algo;
 
-namespace kwiver {
+namespace kwiver
+{
 
 // (config-key, value-type, default-value, description )
 create_config_trait( file_name_template, std::string, "image%04d.png",
-                     "Template for generating output file names. The template is interpreted as a printf format with one "
-                     "format specifier to convert an integer increasing image number. "
-                     "The image file type is determined by the file extension and the concrete writer selected." );
+  "Template for generating output file names. The template is interpreted "
+  "as a printf format with one format specifier to convert an integer "
+  "increasing image number. The image file type is determined by the file "
+  "extension and the concrete writer selected." );
 
 // This is more for documentation
-create_config_trait( image_writer, std::string , "", "Config block name to configure algorithm. "
-                       "The algorithm type is selected with \"image_writer:type\". Specific writer parameters "
-                       "depend on writer type selected.");
+create_config_trait( image_writer, std::string , "", "Config block name "
+  "to configure algorithm. The algorithm type is selected with "
+  "\"image_writer:type\". Specific writer parameters depend on writer type "
+  "selected.");
 
-//----------------------------------------------------------------
+// -----------------------------------------------------------------------------
 // Private implementation class
 class image_writer_process::priv
 {
@@ -85,13 +88,16 @@ public:
   // Number for current image.
   kwiver::vital::frame_id_t m_frame_number;
 
+  // Optional pipeline input parameter
+  std::string m_filename_override;
+
   // processing classes
   algo::image_io_sptr m_image_writer;
 
 }; // end priv class
 
 
-// ================================================================
+// =============================================================================
 
 image_writer_process
 ::image_writer_process( kwiver::vital::config_block_sptr const& config )
@@ -109,7 +115,7 @@ image_writer_process
 }
 
 
-// ----------------------------------------------------------------
+// -----------------------------------------------------------------------------
 void image_writer_process
 ::_configure()
 {
@@ -135,20 +141,21 @@ void image_writer_process
 }
 
 
-// ----------------------------------------------------------------
+// -----------------------------------------------------------------------------
 void image_writer_process
 ::_step()
 {
-  if ( has_input_port_edge_using_trait( timestamp ) )
+  if( has_input_port_edge_using_trait( timestamp ) )
   {
     kwiver::vital::timestamp frame_time;
     frame_time = grab_from_port_using_trait( timestamp );
-    if (frame_time.has_valid_frame() )
+
+    if( frame_time.has_valid_frame() )
     {
       kwiver::vital::frame_id_t next_frame;
       next_frame = frame_time.get_frame();
 
-      if ( next_frame <= d->m_frame_number )
+      if( next_frame <= d->m_frame_number )
       {
         ++d->m_frame_number;
         LOG_WARN( logger(), "Frame number from input timestamp ("
@@ -173,18 +180,27 @@ void image_writer_process
 
   std::string a_file;
 
+  if( has_input_port_edge_using_trait( image_file_name ) )
   {
-    scoped_step_instrumentation();
-
+    a_file = grab_from_port_using_trait( image_file_name );
+  }
+  else
+  {
     a_file = kwiver::vital::string_format( d->m_file_template, d->m_frame_number );
-    LOG_DEBUG( logger(), "Writing image to file \"" << a_file << "\"" );
   }
 
-  d->m_image_writer->save( a_file, input );
+  {
+    scoped_step_instrumentation();
+    LOG_DEBUG( logger(), "Writing image to file \"" << a_file << "\"" );
+
+    d->m_image_writer->save( a_file, input );
+  }
+
+  push_to_port_using_trait( success_flag, ( input && input->size() > 0 ) );
 }
 
 
-// ----------------------------------------------------------------
+// -----------------------------------------------------------------------------
 void image_writer_process
 ::make_ports()
 {
@@ -194,13 +210,22 @@ void image_writer_process
   required.insert( flag_required );
 
   declare_input_port_using_trait( image, required );
+
+  declare_input_port_using_trait( image_file_name, optional,
+    "Name of the image file to write. If not specified, the pattern config "
+    "parameter will be used instead." );
+
   declare_input_port_using_trait( timestamp, optional,
-    "Image timestamp, optional. The frame number from this timestamp is used to number the output files. "
-    "If the timestamp is not connected or not valid, the output files are sequentially numbered from 1." );
+    "Image timestamp, optional. The frame number from this timestamp is used to "
+    "number the output files. If the timestamp is not connected or not valid, "
+    "the output files are sequentially numbered from 1." );
+
+  declare_output_port_using_trait( success_flag, optional,
+    "Flag indicating the image write was successful" );
 }
 
 
-// ----------------------------------------------------------------
+// -----------------------------------------------------------------------------
 void image_writer_process
 ::make_config()
 {
@@ -209,7 +234,7 @@ void image_writer_process
 }
 
 
-// ================================================================
+// =============================================================================
 image_writer_process::priv
 ::priv()
   : m_frame_number(0)
