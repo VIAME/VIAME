2f8f91b710df917982cb80b3d28b0d3e0f3a8350
Matt Leotta <matt.leotta@kitware.com>
2019-01-11 08:32:10 -0500

    Use WRITE instead of TOUCH to support older CMake versions.
    Also, write to a separate temp file instead of touching the source
    and add this temp files a target dependency.  This way we don't need
    to modify the source files.

--- CMake/utils/kwiver-utils-configuration.cmake
+++ CMake/utils/kwiver-utils-configuration.cmake
@@ -52,10 +52,12 @@ function(kwiver_configure_file name source dest)
   set(KWIVER_CONFIG_HELPER "kwiver-configure-helper.cmake")
   if(kwiver_configure_with_git)
     set(KWIVER_CONFIG_HELPER "kwiver-configure-git-helper.cmake")
-    # touch the source file to force this configuration to always run
+    # touch this status file to force the configuration to always run
     # this is needed so that Git will run and detect repository state
     # changes
-    file(TOUCH ${source})
+    set(stat_file "${CMAKE_CURRENT_BINARY_DIR}/configure.${name}.stat")
+    file(WRITE "${stat_file}"
+         "This file is touched to force ${name} to configure.")
   endif()
 
   add_custom_command(
@@ -67,7 +69,7 @@ function(kwiver_configure_file name source dest)
             "-D__OUTPUT_PATH__:PATH=${dest}"
             -P "${KWIVER_CMAKE_ROOT}/tools/${KWIVER_CONFIG_HELPER}"
     DEPENDS
-            "${source}" ${mcf_DEPENDS}
+            "${source}" ${mcf_DEPENDS} ${stat_file}
     WORKING_DIRECTORY
             "${CMAKE_CURRENT_BINARY_DIR}"
     COMMENT "Configuring ${name} file \"${source}\" -> \"${dest}\""
