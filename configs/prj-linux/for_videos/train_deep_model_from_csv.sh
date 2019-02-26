881b139e333c425afd7f15a3b05b950d5f91a393
Matt Dawkins <matt.dawkins@kitware.com>
2019-02-25 20:58:56 -0500

    Update config file

--- configs/prj-linux/for_videos/train_deep_model_from_csv.sh
+++ configs/prj-linux/for_videos/train_deep_model_from_csv.sh
@@ -9,5 +9,5 @@ source ${VIAME_INSTALL}/setup_viame.sh
 # Setup paths and run command
 viame_train_detector \
   -i training_data \
-  -c ${VIAME_INSTALL}/configs/pipelines/train_yolo_704.viame_csv.conf \
+  -c ${VIAME_INSTALL}/configs/pipelines/train_yolo_544.viame_csv.conf \
   --threshold 0.0
--- configs/prj-windows/for_videos/train_deep_model_from_csv.bat
+++ configs/prj-windows/for_videos/train_deep_model_from_csv.bat
@@ -11,7 +11,7 @@ CALL "%VIAME_INSTALL%\setup_viame.bat"
 
 viame_train_detector.exe ^
   -i training_data ^
-  -c "%VIAME_INSTALL%\configs\pipelines\train_yolo_704.viame_csv.conf" ^
+  -c "%VIAME_INSTALL%\configs\pipelines\train_yolo_544.viame_csv.conf" ^
   --threshold 0.0
 
 pause
