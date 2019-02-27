16f4e00cc855704f170b0a16d0f3b5723518dc51
Matt Dawkins <matt.dawkins@kitware.com>
2019-02-27 02:56:13 -0500

    Add buffering system

--- plugins/itk/RegistrationProcess.cxx
+++ plugins/itk/RegistrationProcess.cxx
@@ -34,6 +34,9 @@
  */
 
 #include "RegistrationProcess.h"
+#include "RegisterOpticalAndThermal.h"
+
+#include <itkOpenCVImageBridge.h>
 
 #include <vital/vital_types.h>
 #include <vital/types/timestamp.h>
@@ -48,6 +51,8 @@
 #include <sstream>
 #include <iostream>
 #include <list>
+#include <limits>
+#include <cmath>
 
 
 namespace viame
@@ -59,7 +64,7 @@ namespace itk
 create_config_trait( output_frames_without_match, bool, "true",
   "Output frames without any valid matches" );
 create_config_trait( max_time_offset, double, "0.5",
-  "The maximum time difference under whitch two frames can be tested" );
+  "The maximum time difference (s) under whitch two frames can be tested" );
 
 create_port_trait( optical_image, image, "Optical image" );
 create_port_trait( optical_timestamp, timestamp, "Optical timestamp" );
@@ -79,10 +84,11 @@ create_port_trait( thermal_to_optical_homog, homography, "Homography" );
 class itk_eo_ir_registration_process::priv
 {
 public:
-  priv();
+  priv( itk_eo_ir_registration_process* parent );
   ~priv();
 
   // Configuration values
+  itk_eo_ir_registration_process* m_parent;
   bool m_output_frames_without_match;
   double m_max_time_offset;
 
@@ -91,15 +97,20 @@ public:
   {
     entry( kwiver::vital::image_container_sptr i,
            kwiver::vital::timestamp t,
-           std::string name )
+           std::string n )
      : image( i ),
-       time( t ),
+       ts( t ),
        name( n )
     {}
 
     kwiver::vital::image_container_sptr image;
-    kwiver::vital::timestamp time;
+    kwiver::vital::timestamp ts;
     std::string name;
+
+    double time()
+    {
+      return ts.get_time_usec();
+    }
   };
 
   std::list< entry > m_optical_frames;
@@ -109,10 +120,10 @@ public:
   bool m_thermal_finished;
 
   // Helper functions
-  bool attempt_registration(
-    kwiver::vital::image_container_sptr optical,
-    kwiver::vital::image_container_sptr thermal,
-    kwiver::vital::homography& output );
+  void attempt_registration( const entry& optical, const entry& thermal );
+
+  void output_no_match_optical( const entry& optical );
+  void output_no_match_thermal( const entry& thermal );
 };
 
 // =============================================================================
@@ -120,7 +131,7 @@ public:
 itk_eo_ir_registration_process
 ::itk_eo_ir_registration_process( kwiver::vital::config_block_sptr const& config )
   : process( config ),
-    d( new itk_eo_ir_registration_process::priv )
+    d( new itk_eo_ir_registration_process::priv( this ) )
 {
   set_data_checking_level( check_none );
 
@@ -143,7 +154,7 @@ itk_eo_ir_registration_process
   d->m_output_frames_without_match =
     config_value_using_trait( output_frames_without_match );
   d->m_max_time_offset =
-    config_value_using_trait( max_time_offset );
+    config_value_using_trait( max_time_offset ) * 1e6;
 }
 
 
@@ -160,36 +171,86 @@ itk_eo_ir_registration_process
   kwiver::vital::image_container_sptr thermal_image;
   std::string thermal_file_name;
 
-  if( has_input_port_edge_using_trait( optical_timestamp ) )
+  // Check for completion of optical frame stream
+  if( !d->m_optical_finished )
+  {
+    auto port_info = peek_at_port_using_trait( optical_image );
+
+    if( port_info.datum->type() == sprokit::datum::complete )
+    {
+      d->m_optical_finished = true;
+      grab_edge_datum_using_trait( optical_image );
+
+      if( has_input_port_edge_using_trait( optical_timestamp ) )
+      {
+        grab_edge_datum_using_trait( optical_timestamp );
+      }
+      if( has_input_port_edge_using_trait( optical_file_name ) )
+      {
+        grab_edge_datum_using_trait( optical_file_name );
+      }
+    }
+  }
+
+  // Read optical frame
+  if( !d->m_optical_finished &&
+      has_input_port_edge_using_trait( optical_image ) )
+  {
+    //optical_image = grab_input_using_trait( optical_image );
+  }
+
+  if( !d->m_optical_finished &&
+      has_input_port_edge_using_trait( optical_timestamp ) )
   {
     optical_time = grab_input_using_trait( optical_timestamp );
 
     LOG_DEBUG( logger(), "Received optical frame " << optical_time );
   }
 
-  if( has_input_port_edge_using_trait( optical_image ) )
-  {
-    //optical_image = grab_input_using_trait( optical_image ); FIXME
-  }
-
-  if( has_input_port_edge_using_trait( optical_file_name ) )
+  if( !d->m_optical_finished &&
+      has_input_port_edge_using_trait( optical_file_name ) )
   {
     optical_file_name = grab_input_using_trait( optical_file_name );
   }
 
-  if( has_input_port_edge_using_trait( thermal_timestamp ) )
+  // Check for completion of thermal frame stream
+  if( !d->m_thermal_finished )
+  {
+    auto port_info = peek_at_port_using_trait( thermal_image );
+
+    if( port_info.datum->type() == sprokit::datum::complete )
+    {
+      d->m_thermal_finished = true;
+      grab_edge_datum_using_trait( thermal_image );
+
+      if( has_input_port_edge_using_trait( thermal_timestamp ) )
+      {
+        grab_edge_datum_using_trait( thermal_timestamp );
+      }
+      if( has_input_port_edge_using_trait( thermal_file_name ) )
+      {
+        grab_edge_datum_using_trait( thermal_file_name );
+      }
+    }
+  }
+
+  // Read thermal frame
+  if( !d->m_thermal_finished &&
+      has_input_port_edge_using_trait( thermal_image ) )
+  {
+    //thermal_image = grab_input_using_trait( thermal_image );
+  }
+
+  if( !d->m_thermal_finished &&
+      has_input_port_edge_using_trait( thermal_timestamp ) )
   {
     thermal_time = grab_input_using_trait( thermal_timestamp );
 
     LOG_DEBUG( logger(), "Received thermal frame " << thermal_time );
   }
 
-  if( has_input_port_edge_using_trait( thermal_image ) )
-  {
-    //thermal_image = grab_input_using_trait( thermal_image ); FIXME
-  }
-
-  if( has_input_port_edge_using_trait( thermal_file_name ) )
+  if( !d->m_thermal_finished &&
+      has_input_port_edge_using_trait( thermal_file_name ) )
   {
     thermal_file_name = grab_input_using_trait( thermal_file_name );
   }
@@ -197,29 +258,138 @@ itk_eo_ir_registration_process
   // Add images to buffer
   if( optical_image )
   {
-    optical_frames.push_back(
+    d->m_optical_frames.push_back(
       priv::entry( optical_image, optical_time, optical_file_name ) );
   }
 
   if( thermal_image )
   {
-    thermal_frames.push_back(
+    d->m_thermal_frames.push_back(
       priv::entry( thermal_image, thermal_time, thermal_file_name ) );
   }
 
-  // Determine if any images need to be tested
+  // Determine dominant type
   bool optical_dominant = true;
 
-  if( has_output_port_edge_using_trait( warped_optical_image ) )
+  if( count_output_port_edges_using_trait( warped_optical_image ) > 0 )
   {
     optical_dominant = false;
 
-    if( has_output_port_edge_using_trait( warped_optical_image ) )
+    if( count_output_port_edges_using_trait( warped_thermal_image ) > 0 )
     {
       throw std::runtime_error( "Cannot connect both warp image ports" );
     }
   }
 
+  // Determine if any images need to be tested
+  std::list< priv::entry >* dom =
+    ( optical_dominant ? &d->m_optical_frames : &d->m_thermal_frames );
+  std::list< priv::entry >* sub =
+    ( optical_dominant ? &d->m_thermal_frames : &d->m_optical_frames );
+
+  const bool this_is_the_end = ( d->m_optical_finished && d->m_thermal_finished );
+
+  if( !dom->empty() )
+  {
+    for( auto dom_entry = dom->begin(); dom_entry != dom->end(); )
+    {
+      bool found_in_range = false; // Crit1: A sub-frame in search interval found
+      bool minimum_found = false; // Crit2: Found guaranteed local minimum diff between times
+      bool bound_exceeded = false; // Crit3: Upper search bound exceeded
+
+      const double lower_time = dom_entry->time() - d->m_max_time_offset;
+      const double upper_time = dom_entry->time() + d->m_max_time_offset;
+
+      double min_dist = std::numeric_limits< double >::max();
+      priv::entry* closest_frame = NULL;
+
+      // Iterate over sub
+      for( auto sub_entry = sub->begin(); sub_entry != sub->end(); )
+      {
+        if( sub_entry->time() < lower_time )
+        {
+          sub_entry = sub->erase( sub_entry );
+        }
+        else if( sub_entry->time() > upper_time )
+        {
+          minimum_found = found_in_range;
+          bound_exceeded = true;
+          break;
+        }
+        else
+        {
+          // This frame is in the desired time range
+          found_in_range = true;
+
+          double abs_diff = std::fabs( dom_entry->time() - sub_entry->time() );
+
+          if( abs_diff < min_dist )
+          {
+            min_dist = abs_diff;
+            closest_frame = &(*sub_entry);
+          }
+          else
+          {
+            minimum_found = true;
+            break;
+          }
+
+          sub_entry++;
+        }
+      }
+
+      // Definite match 
+      if( found_in_range && ( minimum_found || this_is_the_end ) )
+      {
+        if( optical_dominant )
+        {
+          d->attempt_registration( *dom_entry, *closest_frame );
+        }
+        else
+        {
+          d->attempt_registration( *closest_frame, *dom_entry );
+        }
+        dom_entry = dom->erase( dom_entry );
+      }
+      // No match for this frame ever
+      if( ( bound_exceeded && !found_in_range ) || this_is_the_end )
+      {
+        if( optical_dominant )
+        {
+          d->output_no_match_optical( *dom_entry );
+        }
+        else
+        {
+          d->output_no_match_thermal( *dom_entry );
+        }
+        dom_entry = dom->erase( dom_entry );
+      }
+      // No match found yet...  wait until we receive more frames or EOV
+      else
+      {
+        break;
+      }
+    }
+  }
+
+  if( this_is_the_end )
+  {
+    // Send complete messages, shut down
+    mark_process_as_complete();
+
+    const sprokit::datum_t dat = sprokit::datum::complete_datum();
+
+    push_datum_to_port_using_trait( optical_image, dat );
+    push_datum_to_port_using_trait( optical_file_name, dat );
+    push_datum_to_port_using_trait( thermal_image, dat );
+    push_datum_to_port_using_trait( thermal_file_name, dat );
+    push_datum_to_port_using_trait( timestamp, dat );
+    push_datum_to_port_using_trait( warped_optical_image, dat );
+    push_datum_to_port_using_trait( warped_thermal_image, dat );
+    push_datum_to_port_using_trait( optical_to_thermal_homog, dat );
+    push_datum_to_port_using_trait( thermal_to_optical_homog, dat );
+    push_datum_to_port_using_trait( success_flag, dat );
+  }
 }
 
 
@@ -268,7 +438,10 @@ itk_eo_ir_registration_process
 
 // =============================================================================
 itk_eo_ir_registration_process::priv
-::priv()
+::priv( itk_eo_ir_registration_process* parent )
+  : m_parent( parent )
+  , m_optical_finished( false )
+  , m_thermal_finished( false )
 {
 }
 
@@ -277,6 +450,55 @@ itk_eo_ir_registration_process::priv
 ::~priv()
 {
 }
+
+
+void
+itk_eo_ir_registration_process::priv
+::attempt_registration( const entry& optical, const entry& thermal )
+{
+  viame::itk::AffineTransformType::Pointer output_matrix;
+
+  if( PerformRegistration(
+    *::itk::OpenCVImageBridge::CVMatToITKImage< viame::itk::OpticalImageType >(
+      kwiver::arrows::ocv::image_container::vital_to_ocv(
+        optical.image->get_image(),
+        kwiver::arrows::ocv::image_container::BGR_COLOR ) ),
+    *::itk::OpenCVImageBridge::CVMatToITKImage< viame::itk::ThermalImageType >(
+      kwiver::arrows::ocv::image_container::vital_to_ocv(
+        thermal.image->get_image(),
+        kwiver::arrows::ocv::image_container::BGR_COLOR ) ),
+    output_matrix ) )
+  {
+    
+  }
+  else
+  {
+    
+  }
+}
+
+
+void
+itk_eo_ir_registration_process::priv
+::output_no_match_optical( const entry& optical )
+{
+  if( !m_output_frames_without_match )
+  {
+    return;
+  }
+}
+
+
+void
+itk_eo_ir_registration_process::priv
+::output_no_match_thermal( const entry& thermal )
+{
+  if( !m_output_frames_without_match )
+  {
+    return;
+  }
+}
+
 
 } // end namespace itk
 
