9e99a5195ac928cfdd57bd69fa58d72798727057
Matt Dawkins <matt.dawkins@kitware.com>
2019-03-03 14:02:59 -0500

    Add python placeholder

---
+++ plugins/opencv/ocv_multimodal_registration.py
@@ -0,0 +1,74 @@
+#ckwg +28
+# Copyright 2019 by Kitware, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#  * Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+#
+#  * Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+#  * Neither name of Kitware, Inc. nor the names of any contributors may be used
+#    to endorse or promote products derived from this software without specific
+#    prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from PIL import Image as pil_image
+
+from kwiver.kwiver_process import KwiverProcess
+from sprokit.pipeline import process
+
+from vital.types import Image
+from vital.types import ImageContainer
+
+from vital.util.VitalPIL import get_pil_image, from_pil
+
+class register_frames_process( KwiverProcess ):
+    """
+    This process blanks out images which don't have detections on them.
+    """
+    # -------------------------------------------------------------------------
+    def __init__( self, conf ):
+        KwiverProcess.__init__( self, conf )
+
+        # set up required flags
+        optional = process.PortFlags()
+        required = process.PortFlags()
+        required.add( self.flag_required )
+
+        #  declare our ports (port-name, flags)
+        self.declare_input_port_using_trait( 'image', required )
+
+        self.declare_output_port_using_trait( 'image', optional )
+
+    # -------------------------------------------------------------------------
+    def _configure( self ):
+        self._base_configure()
+
+    # -------------------------------------------------------------------------
+    def _step( self ):
+        # grab image container from port using traits
+        in_img_c = self.grab_input_using_trait( 'image' )
+        tracks = self.grab_input_using_trait( 'object_track_set' )
+
+        # Get python image from conatiner (just for show)
+        in_img = get_pil_image( in_img_c.image() ).convert( 'RGB' )
+
+        # push dummy image object (same as input) to output port
+        self.push_to_port_using_trait( 'image', ImageContainer( from_pil( in_img ) ) )
+
+        self._base_step()
