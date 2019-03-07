5d291f31568956bdab198bbca1683897954b89d7
Roddy Collins <roddy.collins@kitware.com>
2019-03-06 21:54:51 -0500

    Add utility to compute the MD5 of a file.
    This commit adds a utility routine which takes a filename and computes
    the md5 of the file.

---
+++ vital/util/file_md5.h
@@ -0,0 +1,57 @@
+/*ckwg +29
+ * Copyright 2019 by Kitware, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  * Neither name of Kitware, Inc. nor the names of any contributors may be used
+ *    to endorse or promote products derived from this software without specific
+ *    prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef KWIVER_VITAL_UTIL_FILE_MD5_H
+#define KWIVER_VITAL_UTIL_FILE_MD5_H
+
+#include <vital/util/vital_util_export.h>
+
+#include <string>
+
+namespace kwiver {
+namespace vital {
+
+/**
+ * @brief Compute the md5 of a file
+ *
+ * Compute the md5 sum of the named file
+ *
+ * @param fn Path to the file
+ *
+ * @return MD5 sum as a string; empty if an error ocurred.
+ */
+
+VITAL_UTIL_EXPORT std::string
+file_md5( const std::string& fn );
+
+} // ...vital
+} // ...kwiver
+
+#endif
