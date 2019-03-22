49da4e8f2cd8d6da0b954463f236d03558ab4549
Matt Dawkins <matt.dawkins@kitware.com>
2019-03-22 00:39:04 -0400

    Fix example paths

--- examples/object_detection/run_tf_arctic_seal_rgb.sh
+++ examples/object_detection/run_tf_arctic_seal_rgb.sh
@@ -2,10 +2,10 @@
 
 # Setup VIAME Paths (no need to run multiple times if you already ran it)
 
-export VIAME_INSTALL=../../../build/install
+export VIAME_INSTALL=./../..
 
 source ${VIAME_INSTALL}/setup_viame.sh 
 
 # Run pipeline
 
-pipeline_runner -p seal_rgb_tf_detector.pipe
+pipeline_runner -p ${VIAME_INSTALL}/configs/pipelines/detector_arctic_seal_rgb_tf.pipe
