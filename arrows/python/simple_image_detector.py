446c29bf13345b93f54ffc2ddd80987fef7e098e
Ameya Shringi <ameya.shringi@kitware.com>
2019-03-26 10:15:19 -0400

    Documented simple_image_detector and replaced dummy configuration check

--- arrows/python/simple_image_detector.py
+++ arrows/python/simple_image_detector.py
@@ -1,10 +1,75 @@
+# ckwg +29
+# Copyright 2019 by Kitware, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#  * Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+#
+#  * Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+#  * Neither name of Kitware, Inc. nor the names of any contributors may be used
+#    to endorse or promote products derived from this software without specific
+#    prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 from __future__ import print_function
 
 from vital.algo import ImageObjectDetector
 from vital.types import DetectedObjectSet, DetectedObject, BoundingBox
-from sprokit.pipeline import config
 
 class SimpleImageObjectDetector(ImageObjectDetector):
+    """
+    Implementation of ImageObjectDetector that creates a bounding box on the
+    coordinates specified by the user using configuration
+
+    Examples:
+        With default value of center(200, 200) and bbox dimension (100, 200)
+
+        >>> from vital.modules import modules
+        >>> modules.load_known_modules()
+        >>> from vital.algo import ImageObjectDetector
+        >>> image_detector = ImageObjectDetector.create("SimpleImageObjectDetector")
+        >>> from vital.types import Image, ImageContainer
+        >>> image = ImageContainer(Image())
+        >>> detection = image_detector.detect(image)
+        >>> print(str(detection[0].bounding_box()))
+        <BoundingBox(150.0, 100.0, 250.0, 300.0)>
+
+        With configuration that changes the center value
+
+        >>> modules.load_known_modules()
+        >>> from vital.algo import ImageObjectDetector
+        >>> image_detector = ImageObjectDetector.create("SimpleImageObjectDetector")
+        >>> from vital.types import Image, ImageContainer
+        >>> image = ImageContainer(Image())
+        from sprokit.pipeline import config
+        >>> tc = config.empty_config()
+        >>> tc.set_value("center_x", "200")
+        >>> tc.set_value("center_y", "100")
+        >>> image_detector.check_configuration(tc)
+        False
+        >>> image_detector.set_configuration(tc)
+        >>> detection = image_detector.detect(image)
+        >>> print(detection[0].bounding_box())
+        <BoundingBox(150.0, 0.0, 250.0, 200.0)>
+
+        Using pipeline_runner from build/install directory
+        $ pipeline_runner -p examples/pipelines/example_pydetector_on_image.pipe
+    """
     def __init__(self):
         ImageObjectDetector.__init__(self)
         self.m_center_x = 200.0
@@ -29,15 +94,27 @@ class SimpleImageObjectDetector(ImageObjectDetector):
     def set_configuration( self, cfg_in ):
         cfg = self.get_configuration()
         cfg.merge_config(cfg_in)
-        self.m_center_x     = float(cfg.get_value( "center_x" ));
-        self.m_center_y     = float(cfg.get_value( "center_y" ));
-        self.m_height       = float(cfg.get_value( "height" ));
-        self.m_width        = float(cfg.get_value( "width" ));
-        self.m_dx           = int(cfg.get_value( "dx" ));
-        self.m_dy           = int(cfg.get_value( "dy" ));
+        self.m_center_x     = float(cfg.get_value( "center_x" ))
+        self.m_center_y     = float(cfg.get_value( "center_y" ))
+        self.m_height       = float(cfg.get_value( "height" ))
+        self.m_width        = float(cfg.get_value( "width" ))
+        self.m_dx           = int(float(cfg.get_value( "dx" )))
+        self.m_dy           = int(float(cfg.get_value( "dy" )))
 
     def check_configuration( self, cfg):
-        return True
+        if cfg.has_value("center_x") and not float(cfg.get_value( "center_x" ))==self.m_center_x:
+            return False
+        if cfg.has_value("center_y") and not float(cfg.get_value( "center_y" ))==self.m_center_y:
+            return False
+        if cfg.has_value("height") and not float(cfg.get_value( "height" ))==self.m_height:
+            return False
+        if cfg.has_value("width") and not float(cfg.get_value( "width" ))==self.m_width:
+            return False
+        if cfg.has_value("dx") and not float(cfg.get_value( "dx" ))==self.m_dx:
+            return False
+        if cfg.has_value("dy") and not float(cfg.get_value( "dy" ))==self.m_dy:
+            return False
+        return True 
 
     def detect(self, image_data):
         dot = DetectedObjectSet([DetectedObject(
