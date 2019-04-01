95e78a5e1f3416c21b43c65193f37eddb4cafffb
Matt Dawkins <matt.dawkins@kitware.com>
2019-04-01 11:29:34 -0400

    Normalize option
    Conflicts:
    	configs/prj-windows/for_images/train_deep_model_from_csv.bat
    	configs/prj-windows/for_videos/train_deep_model_from_csv.bat

--- configs/prj-windows/for_videos/train_deep_model_from_csv.bat
+++ configs/prj-windows/for_videos/train_deep_model_from_csv.bat
@@ -3,11 +3,14 @@
 REM Path to VIAME installation
 SET VIAME_INSTALL=C:\Program Files\VIAME
 
+REM Processing options
+SET INPUT_DIRECTORY=training_data
+
 REM Setup paths and run command
 CALL "%VIAME_INSTALL%\setup_viame.bat"
 
 viame_train_detector.exe ^
-  -i training_data ^
+  -i "%INPUT_DIRECTORY%" ^
   -c "%VIAME_INSTALL%\configs\pipelines\train_yolo_704.viame_csv.conf" ^
   --threshold 0.0
 
