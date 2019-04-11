3ab61d71686a9478950e88b3b2fcf61924a7c4b5
Matt Dawkins <matt.dawkins@kitware.com>
2019-04-11 10:28:20 -0400

    Add new pipes

---
+++ examples/object_detector_training/train_cfrnn_viame_csv.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Setup VIAME Paths (no need to run multiple times if you already ran it)
+
+export VIAME_INSTALL=./../..
+
+source ${VIAME_INSTALL}/setup_viame.sh 
+
+# Run pipeline
+
+viame_train_detector \
+  -i training_data_mouss \
+  -c ${VIAME_INSTALL}/configs/pipelines/train_yolo_704.viame_csv.conf \
+  --threshold 0.0
