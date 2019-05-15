c4dda8fdf8f72f8c0fcfd2cb452a6970de95e57c
Matt Dawkins <matt.dawkins@kitware.com>
2019-05-15 14:05:13 -0400

    Update example

---
+++ examples/object_detector_training/train_cfrnn_viame_csv.habcam.bat
@@ -0,0 +1,16 @@
+@echo off
+
+REM Setup VIAME Paths (no need to set if installed to registry or already set up)
+
+SET VIAME_INSTALL=.\..\..
+
+CALL "%VIAME_INSTALL%\setup_viame.bat"
+
+REM Run pipeline
+
+viame_train_detector.exe ^
+  -i training_data_habcam ^
+  -c "%VIAME_INSTALL%\configs\pipelines\train_yolo_704.habcam.conf" ^
+  --threshold 0.0
+
+pause
---
+++ examples/object_detector_training/train_cfrnn_viame_csv.habcam.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Setup VIAME Paths (no need to run multiple times if you already ran it)
+
+export VIAME_INSTALL=./../..
+
+source ${VIAME_INSTALL}/setup_viame.sh 
+
+# Run pipeline
+
+viame_train_detector \
+  -i training_data_habcam \
+  -c ${VIAME_INSTALL}/configs/pipelines/train_mmdet_cascade.habcam.conf \
+  --threshold 0.0
