57c572d19c6ab29c56829c94896ee56a5763b9f7
Matt Dawkins <matt.dawkins@kitware.com>
2019-05-19 15:43:35 -0400

    Up interval count to prevent excessive hard drive usage

--- configs/pipelines/models/cfrnn_train.py
+++ configs/pipelines/models/cfrnn_train.py
@@ -193,7 +193,7 @@ lr_config = dict(
     warmup_iters=500,
     warmup_ratio=1.0 / 3,
     step=[26, 38])
-checkpoint_config = dict(interval=1)
+checkpoint_config = dict(interval=5)
 # yapf:disable
 log_config = dict(
     interval=50,
