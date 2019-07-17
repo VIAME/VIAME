05a9ffae4c2c7591ac8899fc071908fbf8304425
zwhile <zackwhile@outlook.com>
2019-07-17 17:17:56 -0400

    Initial push for pysot tracking library.

---
+++ examples/object_tracking/run_pysot_tracker.bat
@@ -0,0 +1,14 @@
+@echo off
+
+REM Setup VIAME Paths (no need to set if installed to registry or already set up)
+
+SET VIAME_INSTALL=.\..\..
+
+CALL "%VIAME_INSTALL%\setup_viame.bat"
+
+REM Run Pipeline
+
+pipeline_runner.exe -p "%VIAME_INSTALL%\configs\pipelines\pysot_tracker_default.pipe" ^
+                    -s input:video_filename=input_list.txt
+
+pause
