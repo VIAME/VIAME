a2509f01b25b852f1c1a533610d2f2044ead870a
Matt Dawkins <matt.dawkins@kitware.com>
2019-07-23 00:33:19 -0400

    Update ubuntu binaries

--- cmake/build_server_ubuntu.sh
+++ cmake/build_server_ubuntu.sh
@@ -26,8 +26,7 @@ zlib1g-dev
 wget https://repo.continuum.io/archive/Anaconda3-5.2.0-Linux-x86_64.sh
 bash Anaconda3-5.2.0-Linux-x86_64.sh -b
 export PATH=$PATH:/root/anaconda3/bin
-conda create -q -y --name viame python=3.6
-source activate viame
+source /root/anaconda3/bin/activate
 rm -rf Anaconda3-5.2.0-Linux-x86_64.sh
 
 # Install CMAKE
@@ -51,7 +50,8 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/viame/build/install/lib
 # Configure VIAME
 cmake ../ -DCMAKE_BUILD_TYPE:STRING=Release \
 -DVIAME_BUILD_DEPENDENCIES:BOOL=ON \
--DVIAME_ENABLE_BURNOUT:BOOL=ON \
+-DVIAME_CREATE_PACKAGE:BOOL=ON \
+-DVIAME_ENABLE_BURNOUT:BOOL=OFF \
 -DVIAME_ENABLE_CAFFE:BOOL=OFF \
 -DVIAME_ENABLE_CAMTRAWL:BOOL=ON \
 -DVIAME_ENABLE_CUDA:BOOL=ON \
@@ -77,5 +77,31 @@ cmake ../ -DCMAKE_BUILD_TYPE:STRING=Release \
 -DVIAME_ENABLE_VXL:BOOL=ON \
 -DVIAME_ENABLE_YOLO:BOOL=ON 
 
-# Build VIAME
+# Build VIAME first attempt
+make -j$(nproc) -k || true
+
+# HACK: Python3.6 copy so that create_package succeeds
+# Should be removed when this issue is fixed
+# (V) (°,,,°) (V)   (V) (°,,,°) (V)   (V) (°,,,°) (V)
+mkdir -p install/lib
+cp /root/anaconda3/lib/libpython3.6m.* install/lib
+
+# HACK: Double tap the build tree after adding in python
+# Should be removed when this issue is fixed
 make -j$(nproc)
+
+# HACK: Remove libpython.so files necessary for create_package
+# Should be removed when this issue is fixed
+rm -r install/lib/libpython*
+
+# HACK: Copy setup_viame.sh.install over setup_viame.sh
+# Should be removed when this issue is fixed
+cp ../viame/cmake/setup_viame.sh.install install/setup_viame.sh
+
+# HACK: Copy in CUDA dlls missed by create_package
+# Should be removed when this issue is fixed
+cp -P /usr/local/cuda/lib64/libcudart.so* lib
+cp -P /usr/local/cuda/lib64/libcusparse.so* lib
+cp -P /usr/local/cuda/lib64/libcufft.so* lib
+cp -P /usr/local/cuda/lib64/libcusolver.so* lib
+
