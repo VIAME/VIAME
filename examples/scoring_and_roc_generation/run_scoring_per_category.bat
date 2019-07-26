c03a7439456d4393a8a61de90e93893ef251016e
Matt Dawkins <matt.dawkins@kitware.com>
2019-07-25 20:21:48 -0400

    Add in extra quotes in case copied elsewhere

--- examples/scoring_and_roc_generation/run_scoring_per_category.bat
+++ examples/scoring_and_roc_generation/run_scoring_per_category.bat
@@ -8,14 +8,14 @@ CALL "%VIAME_INSTALL%\setup_viame.bat"
 
 REM Run score tracks on data for singular metrics
 
-python %VIAME_INSTALL%\configs\score_results.py ^
+python "%VIAME_INSTALL%\configs\score_results.py" ^
  -computed detections.csv -truth groundtruth.csv ^
  -threshold 0.05 -stats output_score_tracks.txt ^
  --per-category
 
 REM Generate ROC
 
-python %VIAME_INSTALL%\configs\score_results.py ^
+python "%VIAME_INSTALL%\configs\score_results.py" ^
  -computed detections.csv -truth groundtruth.csv ^
  -roc output_roc.png --per-category
 
