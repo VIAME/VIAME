60f52fe86ed140ccf71c396f7b00b8d603443371
Matt Dawkins <matt.dawkins@kitware.com>
2019-08-12 17:48:25 -0400

    Set build dir override

--- cmake/build_server_windows.bat
+++ cmake/build_server_windows.bat
@@ -1,3 +1,8 @@
 IF EXIST build rmdir /s /q build
+
+IF NOT EXIST C:\tmp mkdir C:\tmp
+IF EXIST C:\tmp\kv1 rmdir /s /q C:\tmp\kv1
+
 git submodule update --init --recursive
+
 "C:\Program Files\CMake\bin\ctest.exe" -S jenkins_dashboard.cmake -VV
