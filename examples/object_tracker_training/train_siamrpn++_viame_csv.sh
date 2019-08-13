caf4c08c3d47d1f7bdd91cb2c068616da6b2f3a5
zwhile <zackwhile@outlook.com>
2019-08-13 17:05:17 -0400

    Added pysot (siamrp++) trainer.

---
+++ examples/object_tracker_training/train_siamrpn++_viame_csv.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Setup VIAME Paths (no need to run multiple times if you already ran it)
+
+export VIAME_INSTALL=./../..
+export DATA_FOLDER=data_folder/
+export MODEL_FOLDER=siamrpn++_model/
+export NUM_PROC=1
+
+source ${VIAME_INSTALL}/setup_viame.sh
+
+# Run pipeline
+
+python -m torch.distributed.launch \
+        --nproc_per_node=${NUM_PROC} \
+        --master_port=2335 \
+    ${VIAME_INSTALL}/lib/python3.6/site-packages/pysot/viame/viame_train_tracker.py \
+        -i ${VIAME_INSTALL}/examples/object_tracker_training/${DATA_FOLDER} \
+        -s ${VIAME_INSTALL}/examples/object_tracker_training/${MODEL_FOLDER} \
+        -c ${VIAME_INSTALL}/configs/pipelines/models/pysot_training_config.yaml \
+        --threshold 0.0 # --skip-crop
