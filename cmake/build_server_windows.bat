bc42a00cd8af5d7b6db55685ab2e934bfb31dd92
Matt Dawkins <matt.dawkins@kitware.com>
2019-08-12 22:32:23 -0400

    Update folder

--- cmake/build_server_windows.bat
+++ cmake/build_server_windows.bat
@@ -2,6 +2,7 @@ IF EXIST build rmdir /s /q build
 
 IF NOT EXIST C:\tmp mkdir C:\tmp
 IF EXIST C:\tmp\kv1 rmdir /s /q C:\tmp\kv1
+IF EXIST C:\tmp\kv1 rmdir /s /q C:\tmp\vm1
 
 git submodule update --init --recursive
 
--- cmake/build_server_windows_cpu.bat
+++ cmake/build_server_windows_cpu.bat
@@ -2,6 +2,7 @@ IF EXIST build rmdir /s /q build
 
 IF NOT EXIST C:\tmp mkdir C:\tmp
 IF EXIST C:\tmp\kv2 rmdir /s /q C:\tmp\kv2
+IF EXIST C:\tmp\kv2 rmdir /s /q C:\tmp\vm2
 
 git submodule update --init --recursive
 
