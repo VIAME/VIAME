2e9789d11550afc94c6962107a4afd3dfae20afd
Matt Dawkins <matt.dawkins@kitware.com>
2019-09-08 01:02:53 -0400

    Add new files for revised example

---
+++ examples/image_registration/launch_point_alignment_interface.bat
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Setup VIAME Paths (no need to run multiple times if you already ran it)
+
+export VIAME_INSTALL=./../..
+
+source ${VIAME_INSTALL}/setup_viame.sh
+
+# Run pipeline
+
+pipeline_runner -p ${VIAME_INSTALL}/configs/pipelines/register_multimodal_unsync_itk.pipe
