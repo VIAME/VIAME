f3153a06666b3a39054166f1f5a11c925a940885
Matt Dawkins <matt.dawkins@kitware.com>
2019-09-10 00:57:08 -0400

    Add registration example scripts

--- examples/image_registration/generate_transform_from_points.sh
+++ examples/image_registration/generate_transform_from_points.sh
@@ -1,1 +1,12 @@
+#!/bin/sh
+
+# Setup VIAME Paths (no need to run multiple times if you already ran it)
+
+export VIAME_INSTALL=./../..
+
+source ${VIAME_INSTALL}/setup_viame.sh
+
+# Run pipeline
+
+itk_point_set_to_transform annotated_points.txt output_transform.h5
 
