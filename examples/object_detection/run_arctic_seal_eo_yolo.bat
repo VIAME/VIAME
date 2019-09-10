8e7f3f71bb6c8fdc5af0b722453f97202c9c7471
Matt Dawkins <matt.dawkins@kitware.com>
2019-09-10 00:40:11 -0400

    Fill in example run scripts

--- examples/object_detection/run_arctic_seal_eo_yolo.bat
+++ examples/object_detection/run_arctic_seal_eo_yolo.bat
@@ -1,1 +1,15 @@
  
+@echo off
+
+REM Setup VIAME Paths (no need to set if installed to registry or already set up)
+
+SET VIAME_INSTALL=.\..\..
+
+CALL "%VIAME_INSTALL%\setup_viame.bat"
+
+REM Run Pipeline
+
+pipeline_runner.exe -p "%VIAME_INSTALL%/configs/pipelines/detector_arctic_seal_eo_yolo.pipe" ^
+                    -s input:video_filename=input_image_list_seal_eo.txt
+
+pause
--- examples/object_detection/run_arctic_seal_eo_yolo.sh
+++ examples/object_detection/run_arctic_seal_eo_yolo.sh
@@ -1,1 +1,12 @@
- 
+#!/bin/sh
+
+# Setup VIAME Paths (no need to run multiple times if you already ran it)
+
+export VIAME_INSTALL=./../..
+
+source ${VIAME_INSTALL}/setup_viame.sh 
+
+# Run pipeline
+
+pipeline_runner -p ${VIAME_INSTALL}/configs/pipelines/detector_arctic_seal_eo_yolo.pipe \
+                -s input:video_filename=input_image_list_seal_eo.txt
