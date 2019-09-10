dc5dc3ef2ac8980d0d5e37c2b34621e84eedda71
Matt Dawkins <matt.dawkins@kitware.com>
2019-09-09 23:58:00 -0400

    Fix run script param

--- cmake/launch_seal_interface.sh
+++ cmake/launch_seal_interface.sh
@@ -7,5 +7,5 @@ export VIAME_INSTALL=.
 source ${VIAME_INSTALL}/setup_viame.sh
 
 # Launch the GUI
-sealtk --pipeline-directory $this_dir/configs/pipelines/embedded_dual_stream
+sealtk --pipeline-directory ${VIAME_INSTALL}/configs/pipelines/embedded_dual_stream
 
