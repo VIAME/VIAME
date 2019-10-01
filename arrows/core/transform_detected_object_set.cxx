05784285895f71563546bc4e3c46e7c949160b4a
David Joy <david.joy@krs-3004.khq.kitware.com>
2019-10-01 13:26:53 -0400

    Code review style fixes

--- arrows/core/transform_detected_object_set.cxx
+++ arrows/core/transform_detected_object_set.cxx
@@ -43,7 +43,7 @@ namespace kwiver {
 namespace arrows {
 namespace core {
 
-// ------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 transform_detected_object_set::
 transform_detected_object_set()
   : src_camera_krtd_file_name( "" )
@@ -51,18 +51,19 @@ transform_detected_object_set()
 {
 }
 
-// ------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 transform_detected_object_set::
 transform_detected_object_set(kwiver::vital::camera_perspective_sptr src_cam,
-			      kwiver::vital::camera_perspective_sptr dest_cam)
+                              kwiver::vital::camera_perspective_sptr dest_cam)
   : src_camera( src_cam )
   , dest_camera( dest_cam )
 {
 }
 
-// ------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 vital::config_block_sptr
-transform_detected_object_set::get_configuration() const
+transform_detected_object_set::
+get_configuration() const
 {
   // Get base config from base class
   vital::config_block_sptr config = vital::algorithm::get_configuration();
@@ -77,7 +78,7 @@ transform_detected_object_set::get_configuration() const
 }
 
 
-// ------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 void
 transform_detected_object_set::
 set_configuration( vital::config_block_sptr config_in )
@@ -85,19 +86,19 @@ set_configuration( vital::config_block_sptr config_in )
   vital::config_block_sptr config = this->get_configuration();
 
   config->merge_config( config_in );
-  this->src_camera_krtd_file_name =\
+  this->src_camera_krtd_file_name =
     config->get_value< std::string > ( "src_camera_krtd_file_name" );
-  this->dest_camera_krtd_file_name =\
+  this->dest_camera_krtd_file_name =
     config->get_value< std::string > ( "dest_camera_krtd_file_name" );
 
-  this->src_camera =\
+  this->src_camera =
     kwiver::vital::read_krtd_file( this->src_camera_krtd_file_name );
-  this->dest_camera =\
+  this->dest_camera =
     kwiver::vital::read_krtd_file( this->dest_camera_krtd_file_name );
 }
 
 
-// ------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 bool
 transform_detected_object_set::
 check_configuration( vital::config_block_sptr config ) const
@@ -108,30 +109,30 @@ check_configuration( vital::config_block_sptr config ) const
   if ( ! key_list.empty() )
   {
     LOG_WARN( logger(), "Additional parameters found in config block that are "
-	      "not required or desired: "
-              << kwiver::vital::join( key_list, ", " ) );
+                        "not required or desired: "
+                        << kwiver::vital::join( key_list, ", " ) );
   }
 
   return true;
 }
 
 
-// ------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 vector_3d
 transform_detected_object_set::
 backproject_to_ground( const kwiver::vital::camera_perspective_sptr camera,
-		       const vector_2d img_pt ) const
+                       const vector_2d img_pt ) const
 {
   auto ground_plane = vector_4d(0, 0, 1, 0);
   return this->backproject_to_plane( camera, img_pt, ground_plane );
 }
 
-// ------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 vector_3d
 transform_detected_object_set::
 backproject_to_plane( const kwiver::vital::camera_perspective_sptr camera,
-		      const vector_2d img_pt,
-		      const vector_4d plane ) const
+                      const vector_2d img_pt,
+                      const vector_4d plane ) const
 {
   // Back an image point to a specified world plane
   vector_2d npt_ = camera->intrinsics()->unmap(img_pt);
@@ -148,11 +149,11 @@ backproject_to_plane( const kwiver::vital::camera_perspective_sptr camera,
   return Mp * (n.dot( Mt ) - d) / n.dot( Mp ) - Mt;
 }
 
-// ------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 Eigen::Matrix<double, 8, 3>
 transform_detected_object_set::
 backproject_bbox( const kwiver::vital::camera_perspective_sptr camera,
-		  const vector_4d box ) const
+                  const vector_4d box ) const
 {
   // project base of the box to the ground
   auto base_pt = vector_2d((box(0) + box(2)) / 2, box(3));
@@ -161,9 +162,9 @@ backproject_bbox( const kwiver::vital::camera_perspective_sptr camera,
   ray(2) = 0.0;
   ray.normalize();
 
-  vector_3d p1 =\
+  vector_3d p1 =
     this->backproject_to_ground( camera, vector_2d(box(0), box(3)) );
-  vector_3d p2 =\
+  vector_3d p2 =
     this->backproject_to_ground( camera, vector_2d(box(2), box(3)) );
 
   vector_3d vh = p2 - p1;
@@ -181,28 +182,28 @@ backproject_bbox( const kwiver::vital::camera_perspective_sptr camera,
 
   vector_4d back_plane;
   back_plane << n, d;
-  vector_3d p5 =\
+  vector_3d p5 =
     this->backproject_to_plane( camera, vector_2d(box(0), box(1)), back_plane );
   double height = p5(2);
 
   Eigen::Matrix<double, 8, 3> box3d;
   box3d << p1.transpose(),
-    p2.transpose(),
-    p3.transpose(),
-    p4.transpose(),
-    p1(0), p1(1), height,
-    p2(0), p2(1), height,
-    p3(0), p3(1), height,
-    p4(0), p4(1), height;
+           p2.transpose(),
+           p3.transpose(),
+           p4.transpose(),
+           p1(0), p1(1), height,
+           p2(0), p2(1), height,
+           p3(0), p3(1), height,
+           p4(0), p4(1), height;
 
   return box3d;
 }
 
-// ------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 vector_4d
 transform_detected_object_set::
 box_around_box3d( const kwiver::vital::camera_perspective_sptr camera,
-		  const Eigen::Matrix<double, 8, 3> box3d ) const
+                  const Eigen::Matrix<double, 8, 3> box3d ) const
 {
   Eigen::Matrix<double, 8, 2> projected_points;
 
@@ -214,45 +215,45 @@ box_around_box3d( const kwiver::vital::camera_perspective_sptr camera,
 
   vector_4d out_box;
   out_box << (vector_2d)projected_points.colwise().minCoeff(),
-    (vector_2d)projected_points.colwise().maxCoeff();
+             (vector_2d)projected_points.colwise().maxCoeff();
 
   return out_box;
 }
 
-// ------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 vector_4d
 transform_detected_object_set::
 view_to_view( const kwiver::vital::camera_perspective_sptr src_camera,
-	      const kwiver::vital::camera_perspective_sptr dest_camera,
-	      const vector_4d bounds ) const
+              const kwiver::vital::camera_perspective_sptr dest_camera,
+              const vector_4d bounds ) const
 {
-  Eigen::Matrix<double, 8, 3> box3d =\
+  Eigen::Matrix<double, 8, 3> box3d =
     this->backproject_bbox( src_camera, bounds );
   vector_4d tgt_box = this->box_around_box3d( dest_camera, box3d );
 
   return tgt_box;
 }
 
-// ------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 vital::bounding_box<double>
 transform_detected_object_set::
 transform_bounding_box( vital::bounding_box<double>& bbox ) const
 {
   vector_4d bbox_bounds;
   bbox_bounds << bbox.min_x(), bbox.min_y(),
-    bbox.max_x(), bbox.max_y();
+                 bbox.max_x(), bbox.max_y();
 
   vector_4d out_bounds = this->view_to_view( this->src_camera,
-					     this->dest_camera,
-					     bbox_bounds );
+                                             this->dest_camera,
+                                             bbox_bounds );
 
   return vital::bounding_box<double>(out_bounds(0),
-				     out_bounds(1),
-				     out_bounds(2),
-				     out_bounds(3));
+                                     out_bounds(1),
+                                     out_bounds(2),
+                                     out_bounds(3));
 }
 
-// ------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 vital::detected_object_set_sptr
 transform_detected_object_set::
 filter( const vital::detected_object_set_sptr input_set ) const
--- arrows/core/transform_detected_object_set.h
+++ arrows/core/transform_detected_object_set.h
@@ -42,32 +42,36 @@ namespace kwiver {
 namespace arrows {
 namespace core {
 
-// ----------------------------------------------------------------
-/**
- * @brief Transforms detections based on source and destination cameras.
- *
- */
-
+/// @brief Transforms detections based on source and destination cameras.
 class KWIVER_ALGO_CORE_EXPORT transform_detected_object_set
   : public vital::algorithm_impl<transform_detected_object_set,
-				 vital::algo::detected_object_filter>
+                                 vital::algo::detected_object_filter>
 {
 public:
   PLUGIN_INFO( "transform_detected_object_set",
                "Transforms a detected object set based on source and "
-	       "destination cameras.\n\n" )
+               "destination cameras.\n\n" )
 
+  /// @brief Default constructor
   transform_detected_object_set();
 
+  /// @brief Constructor taking source and destination cameras directly
   transform_detected_object_set(kwiver::vital::camera_perspective_sptr src_cam,
-				kwiver::vital::camera_perspective_sptr dest_cam);
+                                kwiver::vital::camera_perspective_sptr dest_cam);
 
+  /// @brief Default destructor
   virtual ~transform_detected_object_set() = default;
 
+  /// @brief Get this algorithm's configuration block
   virtual vital::config_block_sptr get_configuration() const;
+
+  /// @brief Set this algorithm's properties via a config block
   virtual void set_configuration(vital::config_block_sptr config);
+
+  /// @brief Check that the algorithm's currently configuration is valid
   virtual bool check_configuration(vital::config_block_sptr config) const;
 
+  /// @brief Apply the transformation
   virtual vital::detected_object_set_sptr
     filter( const vital::detected_object_set_sptr input_set) const;
 
@@ -83,25 +87,25 @@ private:
 
   virtual vector_3d
     backproject_to_ground(const kwiver::vital::camera_perspective_sptr camera,
-			  const vector_2d img_pt) const;
+                          const vector_2d img_pt) const;
 
   virtual vector_3d
     backproject_to_plane(const kwiver::vital::camera_perspective_sptr camera,
-			 const vector_2d img_pt,
-			 const vector_4d plane) const;
+                         const vector_2d img_pt,
+                         const vector_4d plane) const;
 
   virtual Eigen::Matrix<double, 8, 3>
     backproject_bbox(const kwiver::vital::camera_perspective_sptr camera,
-		     const vector_4d box) const;
+                     const vector_4d box) const;
 
   virtual vector_4d
     box_around_box3d(const kwiver::vital::camera_perspective_sptr camera,
-		     const Eigen::Matrix<double, 8, 3> box3d) const;
+                     const Eigen::Matrix<double, 8, 3> box3d) const;
 
   virtual vector_4d
     view_to_view(const kwiver::vital::camera_perspective_sptr src_camera,
-		 const kwiver::vital::camera_perspective_sptr dest_camera,
-		 const vector_4d bounds) const;
+                 const kwiver::vital::camera_perspective_sptr dest_camera,
+                 const vector_4d bounds) const;
 };
 
 }}} //End namespace
