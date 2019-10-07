6dc695bb3051e0849e74e827de2f9cf618267ee3
Matt Leotta <matt.leotta@kitware.com>
2019-10-06 23:17:42 -0400

    refactored necker reverse function

--- arrows/core/transform.cxx
+++ arrows/core/transform.cxx
@@ -218,76 +218,121 @@ vital::landmark_map_sptr transform(vital::landmark_map_sptr landmarks,
 }
 
 
+/// Compute a plane passing through the landmarks
+vital::vector_4d
+landmark_plane(const vital::landmark_map::map_landmark_t& landmarks)
+{
+  using namespace kwiver::vital;
+  // compute the landmark location mean and covariance
+  vector_3d lc(0.0, 0.0, 0.0);
+  matrix_3x3d covar = matrix_3x3d::Zero();
+  for (auto const& p : landmarks)
+  {
+    vector_3d pt = p.second->loc();
+    lc += pt;
+    covar += pt * pt.transpose();
+  }
+  const double num_lm = static_cast<double>(landmarks.size());
+  lc /= num_lm;
+  covar /= num_lm;
+  covar -= lc * lc.transpose();
+
+  // the plane will pass through the landmark centeroid (lc)
+  // and have a normal vector aligned with the smallest eigenvector of covar
+  Eigen::JacobiSVD<matrix_3x3d> svd(covar, Eigen::ComputeFullV);
+  vector_3d axis = svd.matrixV().col(2);
+  return vector_4d(axis.x(), axis.y(), axis.z(), -(lc.dot(axis)));
+}
+
+
+/// Mirror landmarks about the specified plane
+vital::landmark_map_sptr
+mirror_landmarks(vital::landmark_map const& landmarks,
+                 vital::vector_4d const& plane)
+{
+  using namespace kwiver::vital;
+  landmark_map::map_landmark_t new_lms;
+  const vector_3d axis(plane.x(), plane.y(), plane.z());
+  const double d = plane[3];
+  // mirror landmark locations about the mirroring plane
+  for (auto const& p : landmarks.landmarks())
+  {
+    vector_3d v = p.second->loc();
+    v -= 2.0 * (v.dot(axis) + d) * axis;
+    auto new_lm = std::make_shared<vital::landmark_d>(*p.second);
+    new_lm->set_loc(v);
+    new_lms[p.first] = new_lm;
+  }
+  return std::make_shared<simple_landmark_map>(new_lms);
+}
+
+
+/// Compute the Necker reversal of a camera in place
+void
+necker_reverse_inplace(vital::simple_camera_perspective& camera,
+                       vital::vector_4d const& plane)
+{
+  using namespace kwiver::vital;
+  const vector_3d axis(plane.x(), plane.y(), plane.z());
+  const double d = plane[3];
+  const rotation_d Ra180(vector_4d(axis.x(), axis.y(), axis.z(), 0.0));
+  static const rotation_d Rz180(vector_4d(0.0, 0.0, 1.0, 0.0));
+
+  // extract the camera center
+  const vital::vector_3d cc = camera.center();
+  // extract the camera principal axis
+  const vital::vector_3d pa = camera.rotation().matrix().row(2);
+  // compute the distance from cc along pa until intersection with
+  // the mirroring plane of the points
+  const double dist = -(cc.dot(axis) + d) / pa.dot(axis);
+  // compute the ground point where the principal axis
+  // intersects the mirroring plane
+  const vital::vector_3d gp = cc + dist * pa;
+  // rotate the camera center 180 degrees about the mirroring plane normal
+  // axis centered at gp, also rotate the camera 180 about its principal axis
+  camera.set_center(Ra180 * (cc - gp) + gp);
+  camera.set_rotation(Rz180 * camera.rotation() * Ra180);
+}
+
+
+/// Compute the Necker reversal of the cameras
+vital::camera_map_sptr
+necker_reverse(vital::camera_map const& cameras,
+               vital::vector_4d const& plane)
+{
+  using namespace kwiver::vital;
+  camera_map::map_camera_t cams;
+  // flip cameras around
+  for (auto & p : cameras.cameras())
+  {
+    // make a clone of this camera as a simple_camera_perspective
+    auto flipped = std::make_shared<vital::simple_camera_perspective>(
+      dynamic_cast<vital::simple_camera_perspective&>(*p.second));
+
+    necker_reverse_inplace(*flipped, plane);
+    cams[p.first] = flipped;
+  }
+  return std::make_shared<simple_camera_map>(cams);
+}
+
+
 /// Compute an approximate Necker reversal of cameras and landmarks
 void
 necker_reverse(vital::camera_map_sptr& cameras,
                vital::landmark_map_sptr& landmarks,
                bool reverse_landmarks)
 {
-  typedef vital::landmark_map::map_landmark_t lm_map_t;
-  typedef vital::camera_map::map_camera_t cam_map_t;
+  using namespace kwiver::vital;
+  const vector_4d plane = landmark_plane(landmarks->landmarks());
 
-  cam_map_t cams = cameras->cameras();
-  lm_map_t lms = landmarks->landmarks();
-
-  // compute the landmark location mean and covariance
-  vital::vector_3d lc(0.0, 0.0, 0.0);
-  vital::matrix_3x3d covar = vital::matrix_3x3d::Zero();
-  for (const lm_map_t::value_type& p : lms)
-  {
-    vital::vector_3d pt = p.second->loc();
-    lc += pt;
-    covar += pt * pt.transpose();
-  }
-  const double num_lm = static_cast<double>(lms.size());
-  lc /= num_lm;
-  covar /= num_lm;
-  covar -= lc * lc.transpose();
-
-  // the mirroring plane will pass through the landmark centeroid (lc)
-  // and have a normal vector aligned with the smallest eigenvector of covar
-  Eigen::JacobiSVD<vital::matrix_3x3d> svd(covar, Eigen::ComputeFullV);
-  vital::vector_3d axis = svd.matrixV().col(2);
-
-  // flip cameras around
-  vital::rotation_d Ra180(vital::vector_4d(axis.x(), axis.y(), axis.z(), 0.0));
-  vital::rotation_d Rz180(vital::vector_4d(0.0, 0.0, 1.0, 0.0));
-  for (cam_map_t::value_type& p : cams)
-  {
-    auto flipped = std::make_shared<vital::simple_camera_perspective>(
-      dynamic_cast<vital::simple_camera_perspective&>(*p.second));
-    // extract the camera center
-    const vital::vector_3d cc = flipped->center();
-    // extract the camera principal axis
-    vital::vector_3d pa = flipped->rotation().matrix().row(2);
-    // compute the distance from cc along pa until intersection with
-    // the mirroring plane of the points
-    const double dist = (lc - cc).dot(axis) / pa.dot(axis);
-    // compute the ground point where the principal axis
-    // intersects the mirroring plane
-    vital::vector_3d gp = cc + dist * pa;
-    // rotate the camera center 180 degrees about the mirroring plane normal
-    // axis centered at gp, also rotate the camera 180 about its principal axis
-    flipped->set_center(Ra180 * (flipped->center() - gp) + gp);
-    flipped->set_rotation(Rz180 * flipped->rotation() * Ra180);
-    p.second = vital::camera_perspective_sptr(flipped);
-  }
+  // reverse the cameras
+  cameras = necker_reverse(*cameras, plane);
 
   if (reverse_landmarks)
   {
-    // mirror landmark locations about the mirroring plane
-    for (lm_map_t::value_type& p : lms)
-    {
-      vital::vector_3d v = p.second->loc();
-      v -= 2.0 * (v - lc).dot(axis) * axis;
-      auto new_lm = std::make_shared<vital::landmark_d>(*p.second);
-      new_lm->set_loc(v);
-      p.second = new_lm;
-    }
+    // mirror the landmarks
+    landmarks = mirror_landmarks(*landmarks, plane);
   }
-
-  cameras = vital::camera_map_sptr(new vital::simple_camera_map(cams));
-  landmarks = vital::landmark_map_sptr(new vital::simple_landmark_map(lms));
 }
 
 
--- arrows/core/transform.h
+++ arrows/core/transform.h
@@ -132,6 +132,54 @@ vital::landmark_map_sptr transform(vital::landmark_map_sptr landmarks,
                                    const vital::similarity_d& xform);
 
 
+/// Compute a plane passing through the landmarks
+/**
+ * Returns the parameters of the best fit plan passing through
+ * the centroid of the landmarks.  The plane is [nx, ny, nz, d] where
+ * [nx, ny, nz] is a unit normal and d is the offset.
+ */
+KWIVER_ALGO_CORE_EXPORT
+vital::vector_4d
+landmark_plane(const vital::landmark_map::map_landmark_t& landmarks);
+
+
+/// Mirror landmarks about the specified plane
+KWIVER_ALGO_CORE_EXPORT
+vital::landmark_map_sptr
+mirror_landmarks(vital::landmark_map const& landmarks,
+                 vital::vector_4d const& plane);
+
+
+/// Compute the Necker reversal of a camera in place
+/**
+ * Using the specified landmark plane, apply a Necker reversal
+ * transformation to the camera in place.  The camera rotates 180 degrees
+ * about the normal vector of the plane at the point where the principal
+ * ray intersects the plane.  The camera also rotates 180 degrees about its
+ * principal.  The resulting camera provides a similar projection of points
+ * near the specified plane, espcially for very long focal lengths.
+ */
+KWIVER_ALGO_CORE_EXPORT
+void
+necker_reverse_inplace(vital::simple_camera_perspective& camera,
+                       vital::vector_4d const& plane);
+
+
+/// Compute the Necker reversal of the cameras
+/**
+* Using the specified landmark plane, apply a Necker reversal
+* transformation to each camera.  The camera rotates 180 degrees
+* about the normal vector of the plane at the point where the principal
+* ray intersects the plane.  The camera also rotates 180 degrees about its
+* principal.  The resulting camera provides a similar projection of points
+* near the specified plane, espcially for very long focal lengths.
+*/
+KWIVER_ALGO_CORE_EXPORT
+vital::camera_map_sptr
+necker_reverse(vital::camera_map const& cameras,
+               vital::vector_4d const& plane);
+
+
 /// Compute an approximate Necker reversal of cameras and landmarks
 /**
  * This operation help restart bundle adjustment after falling into
