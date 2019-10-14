f4a8b655a68d755b3d74fd2df575af32fbcb05c8
Matt Dawkins <matt.dawkins@kitware.com>
2019-10-14 01:32:13 -0400

    First pass svm trainer support

--- examples/object_detector_training/train_svm_viame_csv.sh
+++ examples/object_detector_training/train_svm_viame_csv.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Input locations and types
+
+export INPUT_DIRECTORY=training_data
+export ANNOTATION_TYPE=habcam
+
 # Setup VIAME Paths (no need to run multiple times if you already ran it)
 
 export VIAME_INSTALL=./../..
@@ -8,8 +13,8 @@ source ${VIAME_INSTALL}/setup_viame.sh
 
 # Perform indexing operation required for SVM model train
 
-python ${VIAME_INSTALL}/configs/process_video.py --init -l input_list.txt \
-  --build-index --ball-tree -p pipelines/index_default.svm.pipe \
-  -install ${VIAME_INSTALL}
+python ${VIAME_INSTALL}/configs/process_video.py --init -d ${INPUT_DIRECTORY} \
+  -p pipelines/index_default.pipe --build-index --ball-tree \
+  -auto-detect-gt ${ANNOTATION_TYPE} -install ${VIAME_INSTALL}
 
 # Perform actual SVM model generation
