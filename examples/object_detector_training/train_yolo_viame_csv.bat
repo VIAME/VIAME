a845a42b269d41059b02599611ac40e711a168d7
Matt Dawkins <matt.dawkins@kitware.com>
2019-10-22 22:38:33 -0400

    Fix incorrectly set script

--- examples/object_detector_training/train_yolo_viame_csv.bat
+++ examples/object_detector_training/train_yolo_viame_csv.bat
@@ -3,7 +3,6 @@
 REM Input locations and types
 
 SET INPUT_DIRECTORY=training_data
-SET ANNOTATION_TYPE=viame_csv
 
 REM Setup VIAME Paths (no need to set if installed to registry or already set up)
 
@@ -13,14 +12,10 @@ CALL "%VIAME_INSTALL%\setup_viame.bat"
 
 REM Run Pipeline
 
-python.exe "%VIAME_INSTALL%\configs\process_video.py" --init -d %INPUT_DIRECTORY% -p pipelines\index_default.svm.pipe --build-index --ball-tree -auto-detect-gt %ANNOTATION_TYPE% -install "%VIAME_INSTALL%"
-
-REM Perform actual SVM model generation
-
-SET SVM_TRAIN_IMPORT="import viame.arrows.smqtk.smqtk_train_svm_models as trainer"
-SET SVM_TRAIN_START="trainer.generate_svm_models()"
-
-python.exe -c "%SVM_TRAIN_IMPORT%;%SVM_TRAIN_START%"
+viame_train_detector.exe ^
+  -i "%INPUT_DIRECTORY%" ^
+  -c "%VIAME_INSTALL%\configs\pipelines\train_yolo_704.viame_csv.conf" ^
+  --threshold 0.0
 
 pause
 
