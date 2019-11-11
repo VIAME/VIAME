cecd51b1786855762c18fde3c7b17ba4182c1de5
Matt Dawkins <matt.dawkins@kitware.com>
2019-11-11 02:41:04 -0500

    Generalize cross system

--- cmake/setup_viame_paths.cmake
+++ cmake/setup_viame_paths.cmake
@@ -26,3 +26,5 @@ if( VIAME_ENABLE_PYTHON )
   set( SETUP_PYTHON_LOCATION "export PYTHON_INSTALL_DIR=${PYTHON_ROOT_DIR}" )
   set( SETUP_PYTHON_LIBRARY_PATH "export PYTHON_LIBRARY=${PYTHON_LIBRARY}" )
 endif()
+
+set( SETUP_CUSTOM_TERMINAL "export PS1=\"\${PS1//\"(viame) \"/}\"\nexport PS1=\"(viame) \${PS1//\"(base) \"/}\"" )
