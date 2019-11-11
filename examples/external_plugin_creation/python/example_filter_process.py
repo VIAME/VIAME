e0469bd44e1fac78cbf38fdd64ff7e22e86c0009
Matt Dawkins <matt.dawkins@kitware.com>
2019-11-11 16:10:59 -0500

    Cleanup comments

--- examples/external_plugin_creation/python/example_filter_process.py
+++ examples/external_plugin_creation/python/example_filter_process.py
@@ -43,39 +43,40 @@ class example_filter_process( KwiverProcess ):
     def __init__( self, conf ):
         KwiverProcess.__init__( self, conf )
 
-        self.add_config_trait( "text", "text", 'Hello World',
-          'Text to display to user.' )
+        self.add_config_trait( "text",    # Config name
+          "text",                         # Config type
+          'Hello World',                  # Default value
+          'Text to display to user.' )    # Config
 
-        self.declare_config_using_trait( 'text' )
+        self.declare_config_using_trait( 'text' )  # Register config name
 
         # Would be used if declaring a new output field 'out_image' instead
         # of the default 'image' output port name defined in KwiverProcess
         #self.add_port_trait( 'out_image', 'image', 'Processed image' )
 
-        # set up required flags
+        # set up required port flags
         optional = process.PortFlags()
         required = process.PortFlags()
         required.add( self.flag_required )
 
-        #  declare our input port ( port-name,flags)
+        # declare our input and output ports ( port-name, flags )
         self.declare_input_port_using_trait( 'image', required )
         self.declare_output_port_using_trait( 'image', optional )
 
     # --------------------------------------------------------------------------
     def _configure( self ):
-        self.text = self.config_value( 'text' )
-
+        self.text = self.config_value( 'text' ) # Read config file from file
         self._base_configure()
 
     # --------------------------------------------------------------------------
     def _step( self ):
-        # grab image container from port using traits
+        # Get c image pointer from incoming pipeline
         in_img_c = self.grab_input_using_trait( 'image' )
 
-        # Get python image from conatiner (just for show)
+        # Get python image from conatiner (not used here, just for show)
         in_img = in_img_c.image()
 
-        # Print out text to screen
+        # Print out text to screen, just so we're doing something here
         print( "Text: " + str( self.text ) )
 
         # push dummy detections object to output port
