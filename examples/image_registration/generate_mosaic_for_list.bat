461c262ef6f111b82b093b8dec74053f77b20489
Matt Dawkins <matt.dawkins@kitware.com>
2019-11-24 23:28:21 -0500

    First pass

--- examples/image_registration/generate_mosaic_for_list.bat
+++ examples/image_registration/generate_mosaic_for_list.bat
@@ -1,1 +1,15 @@
- 
+@echo off
+
+REM Setup VIAME Paths (no need to set if installed to registry or already set up)
+
+SET VIAME_INSTALL=.\..\..
+SET INPUT_IMAGE_PATTERN=/home/matt/Desktop/track_experiment/images/*.png
+
+CALL "%VIAME_INSTALL%\setup_viame.bat"
+
+REM Run Pipeline
+
+kwiver.exe runner "${VIAME_INSTALL}/configs/pipelines/register_using_homographies.pipe" ^
+                  -s input:video_filename=input_list.txt
+
+python.exe create_mosaic.py first_mosaic.png homog.txt %INPUT_IMAGE_PATTERN%
--- examples/image_registration/generate_mosaic_for_list.sh
+++ examples/image_registration/generate_mosaic_for_list.sh
@@ -1,9 +1,17 @@
+#!/bin/sh
 
-export VIAME_INSTALL=../..
-export INPUT_IMAGE_PATTERN=*.png
+# Setup VIAME Paths (no need to run multiple times if you already ran it)
+
+export VIAME_INSTALL=./../..
+export INPUT_IMAGE_PATTERN=/home/matt/Desktop/track_experiment/images/*.png
+
+source ${VIAME_INSTALL}/setup_viame.sh 
+
+# Run pipelines
 
 ls ${INPUT_IMAGE_GLOB} > input_list.txt
 
-kwiver runner 
+kwiver runner ${VIAME_INSTALL}/configs/pipelines/register_using_homographies.pipe \
+              -s input:video_filename=input_list.txt
 
-python create_mosaic.py 
+python create_mosaic.py first_mosaic.png homog.txt ${INPUT_IMAGE_PATTERN}
