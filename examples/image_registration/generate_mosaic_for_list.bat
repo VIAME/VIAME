2b97cf1f15389f58e6754aa9e1e4fb2952431eff
Matt Dawkins <matt.dawkins@kitware.com>
2019-11-25 13:57:40 -0500

    Update path

--- examples/image_registration/generate_mosaic_for_list.bat
+++ examples/image_registration/generate_mosaic_for_list.bat
@@ -11,7 +11,7 @@ REM Run Pipeline
 
 dir /s/b "%INPUT_IMAGE_PATTERN%" > input_list.txt
 
-kwiver.exe runner "${VIAME_INSTALL}/configs/pipelines/register_using_homographies.pipe" ^
+kwiver.exe runner "%VIAME_INSTALL%/configs/pipelines/register_using_homographies.pipe" ^
                   -s input:video_filename=input_list.txt
 
-python.exe create_mosaic.py first_mosaic.png homog.txt "%INPUT_IMAGE_PATTERN%" --step 1
+python.exe create_mosaic.py first_mosaic.png output-homog.txt "%INPUT_IMAGE_PATTERN%" --step 1
