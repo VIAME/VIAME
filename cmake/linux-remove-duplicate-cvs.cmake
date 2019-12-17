0ae7080f7910c38f38a2926d77cc7e2ca488e7b6
Matt Dawkins <matt.dawkins@kitware.com>
2019-12-17 10:39:53 -0500

    Update script

--- cmake/linux-remove-duplicate-cvs.cmake
+++ cmake/linux-remove-duplicate-cvs.cmake
@@ -6,7 +6,7 @@ if( NOT WIN32 AND VIAME_ENABLE_OPENCV AND VIAME_ENABLE_PYTHON )
 
   set( PIP_CV2_LIB ${kwiver_python_output_path}/site-packages/cv2 )
 
-  if( EXISTS ${BUILT_CV2} AND EXISTS ${PIP_CV2_LIB} AND EXISTS ${PIP_CV2_HDR} )
+  if( EXISTS "${BUILT_CV2}" AND EXISTS "${PIP_CV2_LIB}" AND EXISTS "${PIP_CV2_HDR}" )
     file( REMOVE_RECURSE ${PIP_CV2_LIB} )
     file( REMOVE_RECURSE ${PIP_CV2_HDR} )
   endif()
