d07b3801f7ca384e23a06b4a13fb62c1462cf6c5
Matt Dawkins <matt.dawkins@kitware.com>
2019-12-17 10:29:03 -0500

    Add cv2 duplicate hash for some linux

---
+++ cmake/linux-remove-duplicate-cvs.cmake
@@ -0,0 +1,13 @@
+
+if( NOT WIN32 AND VIAME_ENABLE_OPENCV AND VIAME_ENABLE_PYTHON )
+
+  file( GLOB BUILT_CV2 ${kwiver_python_output_path}/site-packages/cv2*.so )
+
+  set( PIP_CV2_LIB ${kwiver_python_output_path}/site-packages/cv2 )
+  set( PIP_CV2_HDR ${kwiver_python_output_path}/site-packages/cv2 )
+
+  if( EXISTS ${BUILT_CV2} AND EXISTS ${PIP_CV2_LIB} AND EXISTS ${PIP_CV2_HDR} )
+    file( REMOVE_RECURSE ${PIP_CV2_LIB} )
+    file( REMOVE_RECURSE ${PIP_CV2_HDR} )
+  endif()
+endif()
