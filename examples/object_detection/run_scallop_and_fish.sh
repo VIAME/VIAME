177862c20a7181109468175b32c3d83d8c5d0cf6
Matt Dawkins <matt.dawkins@kitware.com>
2019-12-18 23:54:27 -0500

    Add new examples

---
+++ examples/object_detection/run_scallop_and_fish.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Setup VIAME Paths (no need to run multiple times if you already ran it)
+
+export VIAME_INSTALL=./../..
+
+source ${VIAME_INSTALL}/setup_viame.sh 
+
+# Run pipeline
+
+kwiver runner ${VIAME_INSTALL}/configs/pipelines/detector_scallop_and_fish.pipe \
+              -s input:video_filename=input_image_list_habcam.txt
