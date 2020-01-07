63770b004873846c44681b63b4902101162a51c8
Neal Siekierski <neal.siekierski@kitware.com>
2020-01-07 16:24:11 -0500

    [clean] Extract SRNNTracker._step_unwrapped with no exception wrapping
    This removes an entire level of indentation, and the remaining code in
    ._step is structured to make it easier to attempt recovery in the future
    if so desired, most basically by replacing "raise" with:
    
        ots, det_obj_set = ObjectTrackSet(), DetectedObjectSet()

--- python/kwiver/sprokit/processes/pytorch/srnn_tracker.py
+++ python/kwiver/sprokit/processes/pytorch/srnn_tracker.py
@@ -338,180 +338,189 @@ class SRNNTracker(KwiverProcess):
     # ----------------------------------------------
     def _step(self):
         try:
-            def timing(desc, f):
-                """Return f(), printing a message about how long it took"""
-                start = timer()
-                result = f()
-                end = timer()
-                print('%%%', desc, ' elapsed time: ', end - start, sep='')
-                return result
-
-            print('step', self._step_id)
-
-            # grab image container from port using traits
-            in_img_c = self.grab_input_using_trait('image')
-            timestamp = self.grab_input_using_trait('timestamp')
-            dos_ptr = self.grab_input_using_trait('detected_object_set')
-            if self.has_input_port_edge('homography_src_to_ref'):
-                homog_f2f = self.grab_input_using_trait('homography_src_to_ref')
-            else:
-                homog_f2f = None
-            print('timestamp =', repr(timestamp))
-
-            # Get current frame
-            im = get_pil_image(in_img_c.image()).convert('RGB')
-
-            # Get detection bbox
-            if self._gtbbox_flag:
-                dos = self._m_bbox[self._step_id]
-                bbox_num = len(dos)
-            else:
-                dos = dos_ptr.select(self._select_threshold)
-                bbox_num = dos.size()
-            #print('bbox list len is', dos.size())
-
-            # Update homography
-            if homog_f2f is not None:
-                homog_f2f_arr, homog_f2f_from, homog_f2f_to = from_homog_f2f(homog_f2f)
-            if homog_f2f is None or homog_f2f_to != self._homog_ref_id:
-                # We have a new reference frame
-                # Update self._homog_ref_to_base (assume curr->prev is identity)
-                if self._homog_src_to_ref is not None:
-                    self._homog_ref_to_base = np.matmul(self._homog_ref_to_base, self._homog_src_to_ref)
-                    self._homog_src_to_ref = None
-                # Update self._homog_ref_id
-                if homog_f2f is None:
-                    self._homog_ref_id = None
-                else:
-                    assert homog_f2f_from == homog_f2f_to, "After break homog should map to self"
-                    self._homog_ref_id = homog_f2f_to
-                # This is a reference frame, so src->base is just ref->base
-                homog_src_to_base = self._homog_ref_to_base
-            else:
-                # We use the same reference frame
-                self._homog_src_to_ref = homog_f2f_arr
-                homog_src_to_base = np.matmul(self._homog_ref_to_base, self._homog_src_to_ref)
-
-            det_obj_set = DetectedObjectSet()
-            if bbox_num == 0:
-                print('!!! No bbox is provided on this frame.  Skipping this frame !!!')
-            else:
-                # interaction features
-                grid_feature_list = timing('grid feature', lambda: (
-                    self._grid(im.size, dos, self._gtbbox_flag)))
-
-                # appearance features (format: pytorch tensor)
-                pt_app_features = timing('app feature', lambda: (
-                    self._app_feature_extractor(im, dos, self._gtbbox_flag)))
-
-                track_state_list = []
-                next_track_id = int(self._track_set.get_max_track_id()) + 1
-
-                # get new track state from new frame and detections
-                for idx, item in enumerate(dos):
-                    if self._gtbbox_flag:
-                        bbox = item
-                        fid = self._step_id
-                        ts = self._step_id
-                        d_obj = DetectedObject(bbox=item, confidence=1.0)
-                    else:
-                        bbox = item.bounding_box()
-                        fid = timestamp.get_frame()
-                        ts = timestamp.get_time_usec()
-                        d_obj = item
-
-                    if self._add_features_to_detections:
-                        # store app feature to detected_object
-                        app_f = new_descriptor(g_config.A_F_num)
-                        app_f[:] = pt_app_features[idx].numpy()
-                        d_obj.set_descriptor(app_f)
-                    det_obj_set.add(d_obj)
-
-                    # build track state for current bbox for matching
-                    bbox_as_list = [bbox.min_x(), bbox.min_y(), bbox.width(), bbox.height()]
-                    cur_ts = track_state(frame_id=self._step_id,
-                                        bbox_center=bbox.center(),
-                                        ref_point=transform_homog(homog_src_to_base, bbox.center()),
-                                        interaction_feature=grid_feature_list[idx],
-                                        app_feature=pt_app_features[idx],
-                                        bbox=[int(x) for x in bbox_as_list],
-                                        ref_bbox=transform_homog_bbox(homog_src_to_base, bbox_as_list),
-                                        detected_object=d_obj,
-                                        sys_frame_id=fid, sys_frame_time=ts)
-                    track_state_list.append(cur_ts)
-
-                # if there are no tracks, generate new tracks from the track_state_list
-                if not self._track_flag:
-                    next_track_id = self._track_set.add_new_track_state_list(next_track_id,
-                                    track_state_list, self._track_initialization_threshold)
-                    self._track_flag = True
-                else:
-                    # check whether we need to terminate a track
-                    for track in list(self._track_set.iter_active()):
-                        # terminating a track based on readin_frame_id or original_frame_id gap
-                        if (self._step_id - track[-1].frame_id > self._terminate_track_threshold
-                            or fid - track[-1].sys_frame_id > self._sys_terminate_track_threshold):
-                            self._track_set.deactivate_track(track)
-
-
-                    # call IOU tracker
-                    if self._IOU_flag:
-                        self._track_set, track_state_list = timing('IOU tracking', lambda: (
-                            self._iou_tracker(self._track_set, track_state_list)
-                        ))
-
-                    #print('***track_set len', len(self._track_set))
-                    #print('***track_state_list len', len(track_state_list))
-
-                    # estimate similarity matrix
-                    similarity_mat, track_idx_list = timing('SRNN association', lambda: (
-                        self._srnn_matching(self._track_set, track_state_list, self._ts_threshold)
-                    ))
-
-                    # reset update_flag
-                    self._track_set.reset_updated_flag()
-
-                    # Hungarian algorithm
-                    row_idx_list, col_idx_list = timing('Hungarian algorithm', lambda: (
-                        sp.optimize.linear_sum_assignment(similarity_mat)
-                    ))
-
-                    # Contains the row associated with each column, or None
-                    hung_idx_list = [None] * len(track_state_list)
-                    for r, c in zip(row_idx_list, col_idx_list):
-                        hung_idx_list[c] = r
-
-                    for c, r in enumerate(hung_idx_list):
-                        if r is None or -similarity_mat[r, c] < self._similarity_threshold:
-                            # Conditionally initialize a new track
-                            if (track_state_list[c].detected_object.confidence()
-                                   >= self._track_initialization_threshold):
-                                self._track_set.add_new_track_state(next_track_id,
-                                        track_state_list[c])
-                                next_track_id += 1
-                        else:
-                            # add to existing track
-                            self._track_set.update_track(track_idx_list[r], track_state_list[c])
-
-                print('total tracks', len(self._track_set))
-
-            # push track set to output port
-            ot_list = ts2ot_list(self._track_set)
-            ots = ObjectTrackSet(ot_list)
-
-            self.push_to_port_using_trait('object_track_set', ots)
-            self.push_to_port_using_trait('detected_object_set', det_obj_set)
-
-            self._step_id += 1
-
-            self._base_step()
-
+            ots, det_obj_set = self._step_unwrapped()
         except BaseException as e:
             print( repr( e ) )
             import traceback
             print( traceback.format_exc() )
             sys.stdout.flush()
             raise
+
+        self.push_to_port_using_trait('object_track_set', ots)
+        self.push_to_port_using_trait('detected_object_set', det_obj_set)
+
+        self._step_id += 1
+        self._base_step()
+
+
+    def _step_unwrapped(self):
+        """Perform _step, but don't handle errors or increment self._step_id
+        and return the output ObjectTrackSet and DetectedObjectSet.
+        Mutates this object.
+
+        """
+        def timing(desc, f):
+            """Return f(), printing a message about how long it took"""
+            start = timer()
+            result = f()
+            end = timer()
+            print('%%%', desc, ' elapsed time: ', end - start, sep='')
+            return result
+
+        print('step', self._step_id)
+
+        # grab image container from port using traits
+        in_img_c = self.grab_input_using_trait('image')
+        timestamp = self.grab_input_using_trait('timestamp')
+        dos_ptr = self.grab_input_using_trait('detected_object_set')
+        if self.has_input_port_edge('homography_src_to_ref'):
+            homog_f2f = self.grab_input_using_trait('homography_src_to_ref')
+        else:
+            homog_f2f = None
+        print('timestamp =', repr(timestamp))
+
+        # Get current frame
+        im = get_pil_image(in_img_c.image()).convert('RGB')
+
+        # Get detection bbox
+        if self._gtbbox_flag:
+            dos = self._m_bbox[self._step_id]
+            bbox_num = len(dos)
+        else:
+            dos = dos_ptr.select(self._select_threshold)
+            bbox_num = dos.size()
+        #print('bbox list len is', dos.size())
+
+        # Update homography
+        if homog_f2f is not None:
+            homog_f2f_arr, homog_f2f_from, homog_f2f_to = from_homog_f2f(homog_f2f)
+        if homog_f2f is None or homog_f2f_to != self._homog_ref_id:
+            # We have a new reference frame
+            # Update self._homog_ref_to_base (assume curr->prev is identity)
+            if self._homog_src_to_ref is not None:
+                self._homog_ref_to_base = np.matmul(self._homog_ref_to_base, self._homog_src_to_ref)
+                self._homog_src_to_ref = None
+            # Update self._homog_ref_id
+            if homog_f2f is None:
+                self._homog_ref_id = None
+            else:
+                assert homog_f2f_from == homog_f2f_to, "After break homog should map to self"
+                self._homog_ref_id = homog_f2f_to
+            # This is a reference frame, so src->base is just ref->base
+            homog_src_to_base = self._homog_ref_to_base
+        else:
+            # We use the same reference frame
+            self._homog_src_to_ref = homog_f2f_arr
+            homog_src_to_base = np.matmul(self._homog_ref_to_base, self._homog_src_to_ref)
+
+        det_obj_set = DetectedObjectSet()
+        if bbox_num == 0:
+            print('!!! No bbox is provided on this frame.  Skipping this frame !!!')
+        else:
+            # interaction features
+            grid_feature_list = timing('grid feature', lambda: (
+                self._grid(im.size, dos, self._gtbbox_flag)))
+
+            # appearance features (format: pytorch tensor)
+            pt_app_features = timing('app feature', lambda: (
+                self._app_feature_extractor(im, dos, self._gtbbox_flag)))
+
+            track_state_list = []
+            next_track_id = int(self._track_set.get_max_track_id()) + 1
+
+            # get new track state from new frame and detections
+            for idx, item in enumerate(dos):
+                if self._gtbbox_flag:
+                    bbox = item
+                    fid = self._step_id
+                    ts = self._step_id
+                    d_obj = DetectedObject(bbox=item, confidence=1.0)
+                else:
+                    bbox = item.bounding_box()
+                    fid = timestamp.get_frame()
+                    ts = timestamp.get_time_usec()
+                    d_obj = item
+
+                if self._add_features_to_detections:
+                    # store app feature to detected_object
+                    app_f = new_descriptor(g_config.A_F_num)
+                    app_f[:] = pt_app_features[idx].numpy()
+                    d_obj.set_descriptor(app_f)
+                det_obj_set.add(d_obj)
+
+                # build track state for current bbox for matching
+                bbox_as_list = [bbox.min_x(), bbox.min_y(), bbox.width(), bbox.height()]
+                cur_ts = track_state(frame_id=self._step_id,
+                                    bbox_center=bbox.center(),
+                                    ref_point=transform_homog(homog_src_to_base, bbox.center()),
+                                    interaction_feature=grid_feature_list[idx],
+                                    app_feature=pt_app_features[idx],
+                                    bbox=[int(x) for x in bbox_as_list],
+                                    ref_bbox=transform_homog_bbox(homog_src_to_base, bbox_as_list),
+                                    detected_object=d_obj,
+                                    sys_frame_id=fid, sys_frame_time=ts)
+                track_state_list.append(cur_ts)
+
+            # if there are no tracks, generate new tracks from the track_state_list
+            if not self._track_flag:
+                next_track_id = self._track_set.add_new_track_state_list(next_track_id,
+                                track_state_list, self._track_initialization_threshold)
+                self._track_flag = True
+            else:
+                # check whether we need to terminate a track
+                for track in list(self._track_set.iter_active()):
+                    # terminating a track based on readin_frame_id or original_frame_id gap
+                    if (self._step_id - track[-1].frame_id > self._terminate_track_threshold
+                        or fid - track[-1].sys_frame_id > self._sys_terminate_track_threshold):
+                        self._track_set.deactivate_track(track)
+
+
+                # call IOU tracker
+                if self._IOU_flag:
+                    self._track_set, track_state_list = timing('IOU tracking', lambda: (
+                        self._iou_tracker(self._track_set, track_state_list)
+                    ))
+
+                #print('***track_set len', len(self._track_set))
+                #print('***track_state_list len', len(track_state_list))
+
+                # estimate similarity matrix
+                similarity_mat, track_idx_list = timing('SRNN association', lambda: (
+                    self._srnn_matching(self._track_set, track_state_list, self._ts_threshold)
+                ))
+
+                # reset update_flag
+                self._track_set.reset_updated_flag()
+
+                # Hungarian algorithm
+                row_idx_list, col_idx_list = timing('Hungarian algorithm', lambda: (
+                    sp.optimize.linear_sum_assignment(similarity_mat)
+                ))
+
+                # Contains the row associated with each column, or None
+                hung_idx_list = [None] * len(track_state_list)
+                for r, c in zip(row_idx_list, col_idx_list):
+                    hung_idx_list[c] = r
+
+                for c, r in enumerate(hung_idx_list):
+                    if r is None or -similarity_mat[r, c] < self._similarity_threshold:
+                        # Conditionally initialize a new track
+                        if (track_state_list[c].detected_object.confidence()
+                               >= self._track_initialization_threshold):
+                            self._track_set.add_new_track_state(next_track_id,
+                                    track_state_list[c])
+                            next_track_id += 1
+                    else:
+                        # add to existing track
+                        self._track_set.update_track(track_idx_list[r], track_state_list[c])
+
+            print('total tracks', len(self._track_set))
+
+        # push track set to output port
+        ot_list = ts2ot_list(self._track_set)
+        ots = ObjectTrackSet(ot_list)
+
+        return ots, det_obj_set
 
 
 # ==================================================================
