27ee24ac857f6a15e32f1ec7e944dbf0f1482d87
Matt Dawkins <matt.dawkins@kitware.com>
2020-02-17 14:37:34 -0500

    Only manually build netharn on windows

--- cmake/add_project_pytorch.cmake
+++ cmake/add_project_pytorch.cmake
@@ -36,7 +36,11 @@ if( VIAME_ENABLE_PYTORCH-MDNET )
 endif()
 
 if( VIAME_ENABLE_PYTORCH-NETHARN )
-  set( PYTORCH_LIBS_TO_BUILD ${PYTORCH_LIBS_TO_BUILD} netharn bioharn )
+  if( WIN32 )
+    set( PYTORCH_LIBS_TO_BUILD ${PYTORCH_LIBS_TO_BUILD} netharn bioharn )
+  else()
+    set( PYTORCH_LIBS_TO_BUILD ${PYTORCH_LIBS_TO_BUILD} bioharn )
+  endif()
 endif()
 
 if( VIAME_ENABLE_PYTORCH-MMDET )
@@ -184,7 +188,7 @@ foreach( LIB ${PYTORCH_LIBS_TO_BUILD} )
                             "${CUDNN_ENV}"
       ${LIBRARY_PIP_INSTALL_CMD} )
 
-  if( "${LIB}" STREQUAL "bioharn" )
+  if( "${LIB}" STREQUAL "bioharn" AND WIN32 )
     set( PROJECT_DEPS ${COMMON_PYTORCH_PROJECT_DEP} netharn )
   elseif( "${LIB}" STREQUAL "netharn" )
     set( PROJECT_DEPS ${COMMON_PYTORCH_PROJECT_DEP} mmdetection )
