894a1e584f333bdd1f255b6eac95ee8f8b322891
Matt Dawkins <matt.dawkins@kitware.com>
2020-02-26 17:45:57 -0500

    Update project files

--- configs/prj-windows/for_images/train_deep_cfrnn_wtf_model_from_csv.bat
+++ configs/prj-windows/for_images/train_deep_cfrnn_wtf_model_from_csv.bat
@@ -11,7 +11,7 @@ CALL "%VIAME_INSTALL%\setup_viame.bat"
 
 viame_train_detector.exe ^
   -i "%INPUT_DIRECTORY%" ^
-  -c "%VIAME_INSTALL%\configs\pipelines\train_netharn_cascade_ihm.viame_csv.conf" ^
+  -c "%VIAME_INSTALL%\configs\pipelines\train_netharn_cascade_wtf.viame_csv.conf" ^
   --threshold 0.0
 
 pause
