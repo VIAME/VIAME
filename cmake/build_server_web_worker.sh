9ee740b2b15476e10ab7cae2e91a101794dbd97a
Matt Dawkins <matt.dawkins@kitware.com>
2020-03-17 01:10:25 -0400

    Update ver

--- cmake/build_server_web_worker.sh
+++ cmake/build_server_web_worker.sh
@@ -1,4 +1,114 @@
 #! /bin/bash
 
-find / -name "*python*"
-ls /usr/local/include/pycon* 
+# debugging flag
+set -x
+
+# Fletch, VIAME, CMAKE system deps
+apt-get update 
+apt-get install -y zip \
+git \
+wget \
+curl \
+libcurl4-openssl-dev \
+libgl1-mesa-dev \
+libexpat1-dev \
+libgtk2.0-dev \
+libxt-dev \
+libxml2-dev \
+liblapack-dev \
+openssl \
+libssl-dev \
+g++ \
+zlib1g-dev \
+bzip2 \
+libbz2-dev
+
+
+# Install CMAKE
+wget https://cmake.org/files/v3.14/cmake-3.14.0.tar.gz
+tar zxvf cmake-3.*
+cd cmake-3.14.0
+./bootstrap --prefix=/usr/local --system-curl
+make -j$(nproc)
+make install
+cd /
+rm -rf cmake-3.14.0.tar.gz
+
+# Update VIAME sub git deps
+cd /viame/
+git submodule update --init --recursive
+mkdir build
+cd build
+
+# Configure Paths [should be removed when no longer necessary by fletch]
+export PATH=$PATH:/viame/build/install/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/viame/build/install/lib:/viame/build/install/lib/python3.6
+export C_INCLUDE_PATH=$C_INCLUDE_PATH:/viame/build/install/include/python3.6m
+export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:/viame/build/install/include/python3.6m
+
+# Configure VIAME
+cmake ../ -DCMAKE_BUILD_TYPE:STRING=Release \
+-DVIAME_BUILD_DEPENDENCIES:BOOL=ON \
+-DVIAME_CREATE_PACKAGE:BOOL=OFF \
+-DVIAME_ENABLE_BURNOUT:BOOL=OFF \
+-DVIAME_ENABLE_CAFFE:BOOL=OFF \
+-DVIAME_ENABLE_CAMTRAWL:BOOL=ON \
+-DVIAME_ENABLE_CUDA:BOOL=ON \
+-DVIAME_ENABLE_CUDNN:BOOL=ON \
+-DVIAME_ENABLE_DOCS:BOOL=OFF \
+-DVIAME_ENABLE_FFMPEG:BOOL=ON \
+-DVIAME_ENABLE_FASTER_RCNN:BOOL=OFF \
+-DVIAME_ENABLE_GDAL:BOOL=OFF \
+-DVIAME_ENABLE_FLASK:BOOL=OFF \
+-DVIAME_ENABLE_ITK:BOOL=OFF \
+-DVIAME_ENABLE_KWANT:BOOL=OFF \
+-DVIAME_ENABLE_KWIVER:BOOL=ON \
+-DVIAME_ENABLE_MATLAB:BOOL=OFF \
+-DVIAME_ENABLE_OPENCV:BOOL=ON \
+-DVIAME_ENABLE_PYTHON:BOOL=ON \
+-DVIAME_ENABLE_PYTHON-INTERNAL:BOOL=OFF \
+-DVIAME_ENABLE_PYTORCH:BOOL=ON \
+-DVIAME_ENABLE_PYTORCH-INTERNAL:BOOL=ON \
+-DVIAME_ENABLE_PYTORCH-MMDET:BOOL=ON \
+-DVIAME_ENABLE_PYTORCH-NETHARN:BOOL=ON \
+-DVIAME_ENABLE_PYTORCH-PYSOT:BOOL=ON \
+-DVIAME_ENABLE_SCALLOP_TK:BOOL=OFF \
+-DVIAME_ENABLE_SEAL_TK:BOOL=OFF \
+-DVIAME_ENABLE_SMQTK:BOOL=ON \
+-DVIAME_ENABLE_TENSORFLOW:BOOL=ON \
+-DVIAME_ENABLE_UW_PREDICTOR:BOOL=OFF \
+-DVIAME_ENABLE_VIVIA:BOOL=OFF \
+-DVIAME_ENABLE_VXL:BOOL=ON \
+-DVIAME_ENABLE_YOLO:BOOL=ON \
+-DVIAME_DOWNLOAD_MODELS:BOOL=ON \
+-DVIAME_DOWNLOAD_MODELS-ARCTIC-SEAL:BOOL=ON \
+-DVIAME_DOWNLOAD_MODELS-HABCAM:BOOL=ON \
+-DVIAME_DOWNLOAD_MODELS-MOUSS:BOOL=ON \
+-DVIAME_DOWNLOAD_MODELS-PYTORCH:BOOL=ON \
+-DPYTHON_EXECUTABLE:PATH=/usr/local/bin/python3 \
+-DPYTHON_INCLUDE_DIR:PATH=/usr/include/python3.6 \
+-DPYTHON_LIBRARY:PATH=/usr/local/lib/libpython3.6.so \
+-DPYTHON_VERSION:PATH=3.6
+
+# Build VIAME first attempt
+make -j$(nproc) -k || true
+
+# Below be krakens
+# (V) (°,,,°) (V)   (V) (°,,,°) (V)   (V) (°,,,°) (V)
+
+# HACK: Double tap the build tree
+# Should be removed when non-determinism in kwiver python build fixed
+make -j$(nproc)
+
+# Remove all source files used for the build to save space
+cd /viame
+rm -rfv !("build")
+
+# Remove all build files used for the build to save space
+cd /viame/build
+rm -rfv !("install")
+
+# HACK: Ensure invalid libsvm symlink isn't created
+# Should be removed when this issue is fixed
+rm install/lib/libsvm.so
+cp install/lib/libsvm.so.2 install/lib/libsvm.so
