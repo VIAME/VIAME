739e7d9dcfcc551a0518c3d9304416af3335ba57
Matt Dawkins <matt.dawkins@kitware.com>
2020-04-17 00:45:43 -0400

    Update scripts

--- configs/prj-linux/for_videos/continue_training_deep_cfrnn_model.sh
+++ configs/prj-linux/for_videos/continue_training_deep_cfrnn_model.sh
@@ -10,8 +10,18 @@ export INITIAL_MODEL=category_models/trained_detector.zip
 # Setup paths and run command
 source ${VIAME_INSTALL}/setup_viame.sh
 
-viame_train_detector \
-  -i ${INPUT_DIRECTORY} \
-  -c ${VIAME_INSTALL}/configs/pipelines/train_netharn_cascade.viame_csv.conf \
-  -s detector_trainer:ocv_windowed:trainer:netharn:seed_model=${INITIAL_MODEL} \
-  --threshold 0.0
+if [ -f ${INITIAL_MODEL} ]; then
+  viame_train_detector \
+    -i ${INPUT_DIRECTORY} \
+    -c ${VIAME_INSTALL}/configs/pipelines/train_netharn_cascade.viame_csv.conf \
+    -s detector_trainer:ocv_windowed:trainer:netharn:seed_model=${INITIAL_MODEL} \
+    --threshold 0.0
+elif [ -d deep_training ]; then
+  viame_train_detector \
+    -i ${INPUT_DIRECTORY} \
+    -c ${VIAME_INSTALL}/configs/pipelines/train_netharn_cascade.viame_csv.conf \
+    -s detector_trainer:ocv_windowed:skip_format=true \
+    --threshold 0.0
+else
+  echo "Initial model or in progress training folder does not exist, exiting"
+fi
