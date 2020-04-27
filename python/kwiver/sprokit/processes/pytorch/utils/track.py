50c7d24e5f9fe8ecef66979946b25a1a39ff6429
Neal Siekierski <neal.siekierski@kitware.com>
2020-04-27 15:40:17 -0400

    [other_algo] Avoid zeroing motion features in track.duplicate_track_state
    This makes it match the training behavior.  The previous use of
    .append would set the motion feature associated with the final,
    duplicated track state to zero.  This is particularly bad since,
    because duplicate_track_state is called regularly, it means the motion
    features will be zeroed for the first several detections.

--- python/kwiver/sprokit/processes/pytorch/utils/track.py
+++ python/kwiver/sprokit/processes/pytorch/utils/track.py
@@ -91,11 +91,10 @@ class track(object):
 
     def duplicate_track_state(self, timestep_len = 6):
         du_track = track(self.track_id)
-        du_track.track_state_list = list(self.track_state_list)
+        tsl = self.track_state_list
+        tsl = tsl + [tsl[-1]] * (timestep_len - len(tsl))
+        du_track.track_state_list = tsl
         du_track.max_conf = self.max_conf
-
-        for _ in range(timestep_len - len(du_track)):
-            du_track.append(du_track[-1])
 
         return du_track
 
