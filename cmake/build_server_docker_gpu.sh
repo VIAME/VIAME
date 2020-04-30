eb13b40eb04efd4790b22305600cd3f3e6ddbd46
Matt Dawkins <matt.dawkins@kitware.com>
2020-04-29 23:26:35 -0400

    Set paths

--- cmake/build_server_docker_gpu.sh
+++ cmake/build_server_docker_gpu.sh
@@ -25,14 +25,14 @@ libbz2-dev
 
 
 # Install CMAKE
-wget https://cmake.org/files/v3.14/cmake-3.14.0.tar.gz
+wget https://cmake.org/files/v3.17/cmake-3.17.0.tar.gz
 tar zxvf cmake-3.*
-cd cmake-3.14.0
+cd cmake-3.17.0
 ./bootstrap --prefix=/usr/local --system-curl
 make -j$(nproc)
 make install
 cd /
-rm -rf cmake-3.14.0.tar.gz
+rm -rf cmake-3.17.0.tar.gz
 
 # Update VIAME sub git deps
 cd /viame/
@@ -103,10 +103,11 @@ make -j$(nproc)
 rm install/lib/libsvm.so
 cp install/lib/libsvm.so.2 install/lib/libsvm.so
 
-# Remove all source files used for the build to save space
-cd /viame
-find . -not -path "./build*" -not -path "." -not -path ".." -delete
-
-# Remove all build files used for the build to save space
+# Remove all source files used for the build to save space and mode
+# viame install to default linux install location
 cd /viame/build
-find . -not -path "./install*" -not -path "." -not -path ".." -delete
+mkdir /opt/noaa
+mv install viame
+mv viame /opt/noaa
+cd /
+rm -rf /viame
