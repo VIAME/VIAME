624d8d8393f5e8075779d6923961a771df752f0c
Matt Dawkins <matt.dawkins@kitware.com>
2020-05-23 00:07:31 -0400

    Update link

--- cmake/build_server_windows_cpu.bat
+++ cmake/build_server_windows_cpu.bat
@@ -13,6 +13,7 @@ IF EXIST C:\tmp\vm2 rmdir /s /q C:\tmp\vm2
 
 SET "PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.1\bin;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.1\libnvvp;C:\Python36;C:\Python36\Scripts;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\Program Files (x86)\NVIDIA Corporation\PhysX\Common;C:\Program Files\Git\cmd;C:\Program Files\CMake\bin;C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0\;C:\WINDOWS\System32\OpenSSH\;C:\Program Files\NVIDIA Corporation\NVIDIA NvDLISR;C:\Users\kitware\AppData\Local\Microsoft\WindowsApps"
 SET "PYTHONPATH=%VIAME_BUILD_DIR%\install\lib\python3.6;%VIAME_BUILD_DIR%\install\lib\python3.6\site-packages"
+SET "INCLUDE=C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\ucrt"
 
 git submodule update --init --recursive
 
