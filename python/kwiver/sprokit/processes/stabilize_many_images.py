abc1fe15058e03574b5863bed5e3eee848c8fa73
Neal Siekierski <neal.siekierski@kitware.com>
2020-06-26 15:18:31 -0400

    [stab] WIP Specify types, add parametrization, and implement some functions

--- python/kwiver/sprokit/processes/stabilize_many_images.py
+++ python/kwiver/sprokit/processes/stabilize_many_images.py
@@ -29,22 +29,32 @@
 
 import itertools
 
+import kwiver.vital.types as kvt
+
 # XXX This class should be defined somewhere else
 from .simple_homog_tracker import Transformer
 
 @Transformer.decorate
-def stabilize_many_images():
+def stabilize_many_images(
+        compute_features_and_descriptors,
+        estimate_single_homography,
+):
     """Create a Transformer that performs stabilization on multiple
-    images captured by a multi-camera system.  The .step call expects
-    one argument:
-    - a list of images (XXX what image type?)
+    images captured by a multi-camera system.  Arguments:
+    - compute_features_and_descriptors should be a
+      FeatureAndDescriptorComputer or similar callable
+    - estimate_single_homography should be a SingleHomographyEstimator
+      or similar callable
+
+    The .step call expects one argument:
+    - a list of kvt.BaseImageContainer objects
     and returns:
     - a list of homographies, one for each input image, that maps them
       and other processed images to a common coordinate system (XXX
       what homography type?)
 
     """
-    ris = register_image_set()
+    ris = register_image_set(estimate_single_homography)
     eh = estimate_homography()
     output = None
     while True:
@@ -63,13 +73,17 @@ def stabilize_many_images():
                   for sh in spatial_homogs]
 
 @Transformer.decorate
-def register_image_set():
+def register_image_set(estimate_single_homography):
     """Create a Transformer that registers sets of images (described
     by feature and descriptor sets) from a camera system together,
     maintaining information on previously estimated relative
-    positions.  The .step call expects one argument:
-    - a list of feature-set--descriptor-set pairs (adjacent images
-      should overlap) (XXX what feature and descriptor types?)
+    positions.  Arguments:
+    - estimate_single_homography should be a SingleHomographyEstimator
+      or similar callable
+
+    The .step call expects one argument:
+    - a list of kvt.FeatureSet--kvt.DescriptorSet pairs (adjacent
+      images should overlap)
     and returns:
     - a list of homographies, one for each input image, that map them
       to some common coordinate space (XXX what homography type?)
@@ -91,41 +105,63 @@ def register_image_set():
         r2c = itertools.accumulate(r2c, compose_homographies)
         output = [*l2c, get_identity_homography(), *r2c]
 
+@Transformer.decorate
 def estimate_homography():
     """Create a Transformer that estimates homographies using features and
     descriptors.  The .step call expects two arguments:
-    - a feature set
-    - the corresponding descriptor set
+    - a kvt.FeatureSet
+    - the corresponding kvt.DescriptorSet
     and returns:
     - a homography mapping the current coordinates to those in a
       reference frame
 
     """
-    raise NotImplementedError
+    output = None
+    while True:
+        features, descriptors = yield output
+        raise NotImplementedError
 
 def get_identity_homography():
     """Return a homography representing the identity transformation"""
-    raise NotImplementedError
+    return kvt.HomographyD()
 
-def estimate_single_homography(
-        source_features, source_descriptors,
-        target_features, target_descriptors,
-):
-    """Return a homography that converts coordinates of the source to
-    those of the target, estimated using the provided feature and
-    descriptor sets.
+class SingleHomographyEstimator:
+    __slots__ = '_feature_matcher', '_homography_estimator'
+    def __init__(self, feature_matcher, homography_estimator):
+        self._feature_matcher = feature_matcher
+        self._homography_estimator = homography_estimator
 
-    (XXX What happens when estimation fails?)
+    def __call__(self,
+                 source_features, source_descriptors,
+                 target_features, target_descriptors):
+        """Return a homography that converts coordinates of the source to
+        those of the target, estimated using the provided feature and
+        descriptor sets.
 
-    """
-    raise NotImplementedError
+        (XXX What happens when estimation fails?)
 
-def compute_features_and_descriptors(image):
-    """Return a pair containing the feature set and corresponding
-    descriptor set for the given image.
+        """
+        matches = self._feature_matcher.match(
+            source_features, source_descriptors,
+            target_features, target_descriptors,
+        )
+        return self._homography_estimator.estimate(
+            source_features, target_features, matches,
+        )[0]  # Only return the homography, not the inliers
 
-    """
-    raise NotImplementedError
+class FeatureAndDescriptorComputer:
+    __slots__ = '_feature_detector', '_descriptor_extractor'
+    def __init__(self, feature_detector, descriptor_extractor):
+        self._feature_detector = feature_detector
+        self._descriptor_extractor = descriptor_extractor
+
+    def __call__(self, image):
+        """Return a pair containing the kvt.FeatureSet and corresponding
+        kvt.DescriptorSet for the given kvt.BaseImageContainer
+
+        """
+        features = self._feature_detector.detect(image)
+        return self._descriptor_extractor.extract(image, features)[::-1]
 
 def warp_features_and_descriptors(homog, features, descriptors):
     """Return a pair of the given feature set and descriptor set, warped
@@ -152,5 +188,28 @@ def compose_homographies(second, first):
     """Return a homography corresponding to applying the first, then the
     second homography.
 
+    The arguments may be either kvt.BaseHomography or
+    kvt.F2FHomography objects.  The result is a kvt.BaseHomography if
+    both arguments are, and a kvt.F2FHomography if at least one
+    argument is.  In the latter case, if only one argument is a
+    kvt.F2FHomography, its frame numbers are preserved in the output,
+    while if both are, their frame numbers are combined in the
+    expected manner.
+
     """
-    raise NotImplementedError
+    # XXX This is broken or at least incredibly fragile because
+    # multiplication of kvt.BaseHomography objects only works if they
+    # have the same scalar data type.  (The C++ f2f_homography class
+    # works around this...)
+    if isinstance(second, kvt.F2FHomography):
+        if isinstance(first, kvt.F2FHomography):
+            return second * first
+        else:
+            return kvt.F2FHomography(second.homography * first,
+                                     second.from_id, second.to_id)
+    else:
+        if isinstance(first, kvt.F2FHomography):
+            return kvt.F2FHomography(second * first.homography,
+                                     first.from_id, first.to_id)
+        else:
+            return second * first
