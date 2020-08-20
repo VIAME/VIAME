d375fe5aa2e593cf87180c1ccd0d1631a9f2179e
Matt Dawkins <matt.dawkins@kitware.com>
2020-08-19 23:42:15 -0400

    Modify script names

---
+++ configs/prj-windows/for_images/train_svm_frame_classifier_from_csv.bat
@@ -0,0 +1,26 @@
+@echo off
+
+REM Input locations and types
+
+SET INPUT_DIRECTORY=training_data
+SET ANNOTATION_TYPE=viame_csv
+
+REM Setup VIAME Paths (no need to set if installed to registry or already set up)
+
+SET VIAME_INSTALL=C:\Program Files\VIAME
+
+CALL "%VIAME_INSTALL%\setup_viame.bat"
+
+REM Run Pipeline
+
+python.exe "%VIAME_INSTALL%\configs\process_video.py" --init -d %INPUT_DIRECTORY% -p pipelines\index_full_frame.svm.pipe -o database --build-index -auto-detect-gt %ANNOTATION_TYPE% -install "%VIAME_INSTALL%"
+
+REM Perform actual SVM model generation
+
+SET SVM_TRAIN_IMPORT=import viame.arrows.smqtk.smqtk_train_svm_models as trainer
+SET SVM_TRAIN_START=trainer.generate_svm_models()
+
+python.exe -c "%SVM_TRAIN_IMPORT%;%SVM_TRAIN_START%"
+
+pause
+
