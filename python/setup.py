0c7ee2ce08b97911127d568eee63088bf297d96f
Neal Siekierski <neal.siekierski@kitware.com>
2020-09-09 09:40:23 -0400

    Add scipy to setup.py
    It's required by simple_homog_tracker and some of the new
    PyTorch-using code.

--- python/setup.py
+++ python/setup.py
@@ -53,6 +53,7 @@ setup(
         install_requires=[
                           'numpy',
                           'pillow',
+                          'scipy',
                           'six',
                          ],
         tests_require=[
