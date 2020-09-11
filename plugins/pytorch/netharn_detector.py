802f7e5c9f956a57ac75ae8a9234d71137ae1c1e
Neal Siekierski <neal.siekierski@kitware.com>
2020-09-11 16:20:21 -0400

    Use property-style access to DetectedObject's attributes
    This will change as part of KWIVER's dev/finish_vital_type_bindings.

--- plugins/pytorch/netharn_detector.py
+++ plugins/pytorch/netharn_detector.py
@@ -261,13 +261,13 @@ def _kwiver_to_kwimage_detections(detected_objects):
     classes = []
     if len(detected_objects) > 0:
         obj = ub.peek(detected_objects)
-        classes = obj.type().all_class_names()
+        classes = obj.type.all_class_names()
 
     for obj in detected_objects:
-        box = obj.bounding_box()
+        box = obj.bounding_box
         tlbr = [box.min_x(), box.min_y(), box.max_x(), box.max_y()]
-        score = obj.confidence()
-        cname = obj.type().get_most_likely_class()
+        score = obj.confidence
+        cname = obj.type.get_most_likely_class()
         cidx = classes.index(cname)
         boxes.append(tlbr)
         scores.append(score)
