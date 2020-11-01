45fa90060158c32c9d7b991db9a3f43d2d1370de
Matt Dawkins <matt.dawkins@kitware.com>
2020-10-31 22:00:58 -0400

    Update script

--- cmake/download_all_viame_addons.sh
+++ cmake/download_all_viame_addons.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Configurable Input Paths
-export VIAME_LOCATION="$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)/.."
+export VIAME_INSTALL="$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)/.."
 export DOWNLOAD_LOCATION=~/VIAME-Addons
 
 # Ensure Download Location is Created
@@ -9,7 +9,7 @@ mkdir -p ${DOWNLOAD_LOCATION}
 
 # Download All Optional Packages
 wget -O ${DOWNLOAD_LOCATION}/download1.zip https://data.kitware.com/api/v1/item/5f6bb7e850a41e3d19a63047/download
-tar -xvf ${DOWNLOAD_LOCATION}/download1.zip -C ${VIAME_INSTALL}
+unzip -o ${DOWNLOAD_LOCATION}/download1.zip -d ${VIAME_INSTALL}
 
 # Ensure Download Location is Removed
 rm -rf ${DOWNLOAD_LOCATION}
