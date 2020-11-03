b99448903f228b92c401ddd178282d88ad186399
Matt Dawkins <matt.dawkins@kitware.com>
2020-11-02 21:44:48 -0500

    Update cmake

--- cmake/filter_non_web_pipelines.sh
+++ cmake/filter_non_web_pipelines.sh
@@ -6,5 +6,17 @@ export VIAME_INSTALL="$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)/.."
 # Other paths generated from root
 export VIAME_PIPELINES=${VIAME_INSTALL}/configs/pipelines/
 
-# Remove sea lion pipelines
-rm -rf ${VIAME_PIPELINES}/arctic*fusion*
+# Remove utilities pipes which hold no meaning in web
+rm -rf ${VIAME_PIPELINES}/*local*.pipe
+rm -rf ${VIAME_PIPELINES}/*hough*.pipe
+rm -rf ${VIAME_PIPELINES}/*_svm_models.pipe
+rm -rf ${VIAME_PIPELINES}/detector_extract_chips.pipe
+
+# Remove tracker pipelines which hold no meaning in web
+rm -rf ${VIAME_PIPELINES}/tracker_short_term.pipe
+rm -rf ${VIAME_PIPELINES}/tracker_stabilized_iou.pipe
+
+# Remove seal and sea lion specialized pipelines un-runnable in web
+rm -rf ${VIAME_PIPELINES}/arctic*fusion*.pipe
+rm -rf ${VIAME_PIPELINES}/*2-cam.pipe
+rm -rf ${VIAME_PIPELINES}/*3-cam.pipe
