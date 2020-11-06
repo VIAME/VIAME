802fbb441f03ab47f6120a72300ebbc41af981f0
Matt Dawkins <matt.dawkins@kitware.com>
2020-11-06 12:37:54 -0500

    Use hard paths for server

--- cmake/filter_non_web_pipelines.sh
+++ cmake/filter_non_web_pipelines.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Configurable Input Paths
-export VIAME_INSTALL="$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)/.."
+export VIAME_INSTALL=/opt/noaa/viame
 
 # Other paths generated from root
 export VIAME_PIPELINES=${VIAME_INSTALL}/configs/pipelines/
