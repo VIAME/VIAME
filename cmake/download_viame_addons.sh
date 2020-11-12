8b19b6a0d2639a37cf8bc3e3d019580f50d1bfa3
Matt Dawkins <matt.dawkins@kitware.com>
2020-11-12 14:19:27 -0500

    Update script

--- cmake/download_viame_addons.sh
+++ cmake/download_viame_addons.sh
@@ -11,5 +11,8 @@ mkdir -p ${DOWNLOAD_LOCATION}
 wget -O ${DOWNLOAD_LOCATION}/download1.zip https://data.kitware.com/api/v1/item/5f9e115f50a41e3d19253c84/download
 unzip -o ${DOWNLOAD_LOCATION}/download1.zip -d ${VIAME_INSTALL}
 
+wget -O ${DOWNLOAD_LOCATION}/download2.zip https://data.kitware.com/api/v1/item/5fad8a9750a41e3d194f4961/download
+unzip -o ${DOWNLOAD_LOCATION}/download2.zip -d ${VIAME_INSTALL}
+
 # Ensure Download Location is Removed
 rm -rf ${DOWNLOAD_LOCATION}
