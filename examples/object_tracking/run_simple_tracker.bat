ab2261fc3d4ed5add19eb1ef82bde0eb25286aaf
Matt Dawkins <matt.dawkins@kitware.com>
2020-11-15 16:56:49 -0500

    Update examples

--- examples/object_tracking/run_simple_tracker.bat
+++ examples/object_tracking/run_simple_tracker.bat
@@ -8,7 +8,7 @@ CALL "%VIAME_INSTALL%\setup_viame.bat"
 
 REM Run Pipeline
 
-kwiver.exe runner "%VIAME_INSTALL%\configs\pipelines\tracker_default.sfd.desc.pipe" ^
+kwiver.exe runner "%VIAME_INSTALL%\configs\pipelines\tracker_fish.sfd.pipe" ^
                   -s input:video_filename=input_list.txt
 
 pause
