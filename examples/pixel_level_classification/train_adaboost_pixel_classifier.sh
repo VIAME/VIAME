d9190639adba5c14d2cfe5f2c3f6a24048b52212
David Russell <david.russell@kitware.com>
2020-11-23 16:32:17 -0500

    Add pixel classification training example

---
+++ examples/pixel_level_classification/train_adaboost_pixel_classifier.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# TODO write a .bat file
+# Input locations and types
+export INPUT_DIRECTORY=training_data_adaboost
+
+# Path to VIAME installation
+export VIAME_INSTALL="$(cd "$(dirname $0)" && pwd)/../.."
+
+source ${VIAME_INSTALL}/setup_viame.sh
+
+# Run pipeline
+viame_train_detector \
+  -i ${INPUT_DIRECTORY} \
+  -c ${VIAME_INSTALL}/configs/pipelines/train_adaboost_pixel_classifier.viame_csv.conf \
+  --threshold 0.0 # I don't know if this is needed
