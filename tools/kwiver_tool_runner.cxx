73e8a672ecbf9efe23ca01c079b896433c84b500
Matthew Woehlke <matthew.woehlke@kitware.com>
2020-11-24 10:02:08 -0500

    Update copyright banner in tools, examples
    Update copyright banner and squash consecutive blank lines, as in the
    preceding commits.

--- tools/kwiver_tool_runner.cxx
+++ tools/kwiver_tool_runner.cxx
@@ -1,33 +1,6 @@
-/*ckwg +29
- * Copyright 2018-2019 by Kitware, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *  * Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- *  * Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- *  * Neither name of Kitware, Inc. nor the names of any contributors may be used
- *    to endorse or promote products derived from this software without specific
- *    prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
+// This file is part of KWIVER, and is distributed under the
+// OSI-approved BSD 3-Clause License. See top-level LICENSE file or
+// https://github.com/Kitware/kwiver/blob/master/LICENSE for details.
 
 #include <vital/applets/kwiver_applet.h>
 #include <vital/applets/applet_context.h>
@@ -104,7 +77,6 @@ public:
   std::string m_applet_name;
 };
 
-
 // ----------------------------------------------------------------------------
 /**
  * Generate list of all applets that have been discovered.
@@ -141,7 +113,6 @@ void tool_runner_usage( applet_context_t ctxt,
   }
 }
 
-
 // ----------------------------------------------------------------------------
 /**
  * This function handles the "help" operation. If there is an arg
@@ -175,7 +146,6 @@ void help_applet( const command_line_parser& options,
   // display help text
   std::cout << applet->m_cmd_options->help();
 }
-
 
 // ============================================================================
 int main(int argc, char *argv[])
