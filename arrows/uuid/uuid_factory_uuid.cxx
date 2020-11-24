f3bb7f71e38989723334b1e83a60313ca2ce10cf
Matthew Woehlke <matthew.woehlke@kitware.com>
2020-11-24 10:01:25 -0500

    Update copyright banner in arrows/uuid
    Update copyright banner and squash consecutive blank lines, as in the
    preceding commits.

--- arrows/uuid/uuid_factory_uuid.cxx
+++ arrows/uuid/uuid_factory_uuid.cxx
@@ -1,32 +1,6 @@
-/*ckwg +29
- * Copyright 2017 by Kitware, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *  * Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- *  * Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- *  * Neither name of Kitware, Inc. nor the names of any contributors may be used
- *    to endorse or promote products derived from this software without specific
- *    prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+// This file is part of KWIVER, and is distributed under the
+// OSI-approved BSD 3-Clause License. See top-level LICENSE file or
+// https://github.com/Kitware/kwiver/blob/master/LICENSE for details.
 
 /**
  * \file
@@ -52,7 +26,6 @@ public:
 
 };
 
-
 // ==================================================================
 uuid_factory_uuid::
 uuid_factory_uuid()
@@ -61,11 +34,9 @@ uuid_factory_uuid()
 
 }
 
-
 uuid_factory_uuid::
 ~uuid_factory_uuid()
 { }
-
 
 // ------------------------------------------------------------------
 void
@@ -74,7 +45,6 @@ set_configuration(vital::config_block_sptr config)
 {
 }
 
-
 // ------------------------------------------------------------------
 bool
 uuid_factory_uuid::
@@ -82,7 +52,6 @@ check_configuration(vital::config_block_sptr config) const
 {
   return true;
 }
-
 
 // ------------------------------------------------------------------
 kwiver::vital::uid
@@ -106,6 +75,5 @@ create_uuid()
 
   return kwiver::vital::uid( cc, sizeof( new_uuid ));
 }
-
 
 } } } // end namespace
