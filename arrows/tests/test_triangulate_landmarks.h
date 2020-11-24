281c125fd88822cccd88d3fd5bed327610fef277
Matthew Woehlke <matthew.woehlke@kitware.com>
2020-11-24 10:00:39 -0500

    Update copyright banner in arrows/tests
    Update copyright banner and squash consecutive blank lines, as in the
    preceding commits.

--- arrows/tests/test_triangulate_landmarks.h
+++ arrows/tests/test_triangulate_landmarks.h
@@ -1,32 +1,6 @@
-/*ckwg +29
- * Copyright 2014-2019 by Kitware, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *  * Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- *  * Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- *  * Neither name of Kitware, Inc. nor the names of any contributors may be used
- *    to endorse or promote products derived from this software without specific
- *    prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+// This file is part of KWIVER, and is distributed under the
+// OSI-approved BSD 3-Clause License. See top-level LICENSE file or
+// https://github.com/Kitware/kwiver/blob/master/LICENSE for details.
 
 /**
  * \file
@@ -102,7 +76,6 @@ void test_noisy_landmarks(kwiver::vital::algo::triangulate_landmarks& tri_lm)
   // add Gaussian noise to the landmark positions
   landmark_map_sptr landmarks0 = testing::noisy_landmarks(landmarks, 0.1);
 
-
   double init_rmse =
     kwiver::arrows::mvg::reprojection_rmse(cameras->cameras(),
                                            landmarks0->landmarks(),
@@ -140,7 +113,6 @@ void test_zero_landmarks(kwiver::vital::algo::triangulate_landmarks& tri_lm)
   // initialize all landmarks to the origin
   landmark_id_t num_landmarks = static_cast<landmark_id_t>(landmarks->size());
   landmark_map_sptr landmarks0 = testing::init_landmarks(num_landmarks);
-
 
   double init_rmse =
     kwiver::arrows::mvg::reprojection_rmse(cameras->cameras(),
@@ -191,7 +163,6 @@ void test_subset_cameras(kwiver::vital::algo::triangulate_landmarks& tri_lm)
     }
   }
   camera_map_sptr cameras0(new simple_camera_map(cam_map2));
-
 
   EXPECT_EQ(7, cameras0->size()) << "Reduced number of cameras";
 
@@ -326,7 +297,6 @@ void test_noisy_tracks(kwiver::vital::algo::triangulate_landmarks& tri_lm)
   feature_track_set_sptr tracks0 = testing::noisy_tracks(
                                testing::subset_tracks(tracks, 0.5),
                                track_stdev);
-
 
   double init_rmse =
     kwiver::arrows::mvg::reprojection_rmse(cameras->cameras(),
