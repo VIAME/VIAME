ddb77dda19f2e37bc038086a597929c6681202eb
Matthew Woehlke <matthew.woehlke@kitware.com>
2020-11-25 11:05:48 -0500

    Update copyright banner in arrows/matlab
    Update copyright banner and squash consecutive blank lines, as in the
    preceding commits.

--- arrows/matlab/matlab_detection_output.cxx
+++ arrows/matlab/matlab_detection_output.cxx
@@ -1,32 +1,6 @@
-/*ckwg +29
- * Copyright 2016-2020 by Kitware, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *  * Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- *  * Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- *  * Neither name of Kitware, Inc. nor the names of any contributors may be used
- *    to endorse or promote products derived from this software without specific
- *    prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+// This file is part of KWIVER, and is distributed under the
+// OSI-approved BSD 3-Clause License. See top-level LICENSE file or
+// https://github.com/Kitware/kwiver/blob/master/LICENSE for details.
 
 /**
  * \file
@@ -76,7 +50,6 @@ public:
     return m_matlab_engine.get();
   }
 
-
   // ------------------------------------------------------------------
   void check_result()
   {
@@ -87,7 +60,6 @@ public:
     }
   }
 
-
   // ------------------------------------------------------------------
   void eval( const std::string& expr )
   {
@@ -95,7 +67,6 @@ public:
     engine()->eval( expr );
     check_result();
   }
-
 
   // ------------------------------------------------------------------
   void initialize_once()
@@ -134,7 +105,6 @@ public:
     eval( "detector_initialize()" );
   }
 
-
   // --- instance data -----
   matlab_detection_output* m_parent;
   kwiver::vital::logger_handle_t m_logger;
@@ -150,7 +120,6 @@ private:
 
 };
 
-
 // ==================================================================
 matlab_detection_output::
 matlab_detection_output()
@@ -158,12 +127,10 @@ matlab_detection_output()
 {
 }
 
-
 matlab_detection_output::
 ~matlab_detection_output()
 {
 }
-
 
 // ------------------------------------------------------------------
 vital::config_block_sptr
@@ -179,7 +146,6 @@ get_configuration() const
   return config;
 }
 
-
 // ------------------------------------------------------------------
 void
 matlab_detection_output::
@@ -191,7 +157,6 @@ set_configuration( vital::config_block_sptr config )
   d->m_matlab_program = config->get_value<std::string>( "program_file" );
 }
 
-
 // ------------------------------------------------------------------
 bool
 matlab_detection_output::
@@ -199,7 +164,6 @@ check_configuration( vital::config_block_sptr config ) const
 {
   return true;
 }
-
 
 // ------------------------------------------------------------------
 void
--- arrows/matlab/matlab_engine.cxx
+++ arrows/matlab/matlab_engine.cxx
@@ -1,32 +1,6 @@
-/*ckwg +29
- * Copyright 2016-2018 by Kitware, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *  * Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- *  * Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- *  * Neither name of Kitware, Inc. nor the names of any contributors may be used
- *    to endorse or promote products derived from this software without specific
- *    prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+// This file is part of KWIVER, and is distributed under the
+// OSI-approved BSD 3-Clause License. See top-level LICENSE file or
+// https://github.com/Kitware/kwiver/blob/master/LICENSE for details.
 
 /**
  * \file
@@ -63,7 +37,6 @@ matlab_engine()
   }
 }
 
-
 // ------------------------------------------------------------------
 matlab_engine::
 ~matlab_engine()
@@ -80,7 +53,6 @@ matlab_engine::
   m_output_buffer = 0;
 }
 
-
 // ------------------------------------------------------------------
 void
 matlab_engine::
@@ -92,7 +64,6 @@ eval( const std::string& cmd )
     VITAL_THROW( matlab_exception, "Engine session no longer active" );
   }
 }
-
 
 // ------------------------------------------------------------------
 MxArraySptr
@@ -110,7 +81,6 @@ get_variable( const std::string& name )
   return MxArraySptr( new MxArray( var ) );
 }
 
-
 // ------------------------------------------------------------------
 void
 matlab_engine::
@@ -124,7 +94,6 @@ put_variable( const std::string& name, MxArraySptr val )
     VITAL_THROW( matlab_exception, str.str() );
   }
 }
-
 
 // ------------------------------------------------------------------
 bool
@@ -140,7 +109,6 @@ get_visible()
   return retval;
 }
 
-
 // ------------------------------------------------------------------
 void
 matlab_engine::
@@ -152,7 +120,6 @@ set_visible( bool vis )
     LOG_WARN( m_logger, "Error returned from engSetVisible()");
   }
 }
-
 
 // ------------------------------------------------------------------
 std::string
