73a4d371802d53cd57e461210576090cf730ac86
Matthew Woehlke <matthew.woehlke@kitware.com>
2020-11-30 09:48:16 -0500

    Update copyright banner in arrows/serialize
    Update copyright banner and squash consecutive blank lines, as in the
    preceding commit.

--- arrows/serialize/json/load_save.cxx
+++ arrows/serialize/json/load_save.cxx
@@ -1,32 +1,6 @@
-/*ckwg +29
- * Copyright 2018-2020 by Kitware, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *  * Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- *  * Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- *  * Neither name of Kitware, Inc. nor the names of any contributors may be used
- *    to endorse or promote products derived from this software without specific
- *    prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+// This file is part of KWIVER, and is distributed under the
+// OSI-approved BSD 3-Clause License. See top-level LICENSE file or
+// https://github.com/Kitware/kwiver/blob/master/LICENSE for details.
 
 #include <arrows/serialize/json/load_save.h>
 #include <arrows/serialize/json/load_save_point.h>
@@ -56,7 +30,6 @@
 
 #include <zlib.h>
 #include <iostream>
-
 
 namespace cereal {
 // ============================================================================
@@ -380,7 +353,6 @@ void load( ::cereal::JSONInputArchive& archive, ::kwiver::vital::image_container
            CEREAL_NVP( img_data )  // compressed image
     );
 
-
   auto img_mem = std::make_shared< ::kwiver::vital::image_memory >( img_size );
 
   const ::kwiver::vital::image_pixel_traits pix_trait(
@@ -471,7 +443,6 @@ void load( ::cereal::JSONInputArchive& archive, ::kwiver::vital::geo_polygon& po
   load( archive, raw_poly ); // load raw polygon
   poly.set_polygon( raw_poly, crs );
 }
-
 
 // ============================================================================
 void save( ::cereal::JSONOutputArchive& archive, const ::kwiver::vital::geo_point& point )
@@ -584,7 +555,6 @@ void load( ::cereal::JSONInputArchive& archive,
   load(archive, *detection);
   obj_trk_state.set_detection(detection);
 }
-
 
 // ============================================================================
 void save( ::cereal::JSONOutputArchive& archive, const ::kwiver::vital::track_set& trk_set )
