a2cb462fedf7618b9e657a8b04693432a7a7c660
Matthew Woehlke <matthew.woehlke@kitware.com>
2020-11-30 09:49:35 -0500

    Update copyright banner in extras
    Update copyright banner and squash consecutive blank lines, as in the
    preceding commits.

--- extras/process_instrumentation/process_instrumentation_plugin.cxx
+++ extras/process_instrumentation/process_instrumentation_plugin.cxx
@@ -1,32 +1,6 @@
-/*ckwg +29
- * Copyright 2017 by Kitware, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *  * Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- *  * Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- *  * Neither name of Kitware, Inc. nor the names of any contributors may be used
- *    to endorse or promote products derived from this software without specific
- *    prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+// This file is part of KWIVER, and is distributed under the
+// OSI-approved BSD 3-Clause License. See top-level LICENSE file or
+// https://github.com/Kitware/kwiver/blob/master/LICENSE for details.
 
 #include "process_instrumentation_plugin_export.h"
 
@@ -68,17 +42,14 @@ public:
   explorer_context* m_context;
 }; // end class process_instrumentation_explorer
 
-
 // ==================================================================
 process_instrumentation_explorer::
 process_instrumentation_explorer()
 { }
 
-
 process_instrumentation_explorer::
 ~process_instrumentation_explorer()
 { }
-
 
 // ------------------------------------------------------------------
 bool
@@ -89,7 +60,6 @@ initialize( explorer_context* context )
 
   return true;
 }
-
 
 // ------------------------------------------------------------------
 void
@@ -148,9 +118,7 @@ explore( const kwiver::vital::plugin_factory_handle_t fact )
 
 } // process_instrumentation_explorer::explore
 
-
 } }
-
 
 // ==================================================================
 extern "C"
--- extras/process_instrumentation/timing_process_instrumentation.cxx
+++ extras/process_instrumentation/timing_process_instrumentation.cxx
@@ -1,32 +1,6 @@
-/*ckwg +29
- * Copyright 2017-2019, 2020 by Kitware, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *  * Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- *  * Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- *  * Neither name of Kitware, Inc. nor the names of any contributors may be used
- *    to endorse or promote products derived from this software without specific
- *    prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+// This file is part of KWIVER, and is distributed under the
+// OSI-approved BSD 3-Clause License. See top-level LICENSE file or
+// https://github.com/Kitware/kwiver/blob/master/LICENSE for details.
 
 #include "timing_process_instrumentation.h"
 
@@ -60,7 +34,6 @@ timing_process_instrumentation()
   , m_logger( kwiver::vital::get_logger( "sprokit.timing_process_instrumentation" ) )
 { }
 
-
 timing_process_instrumentation::
 ~timing_process_instrumentation()
 {
@@ -76,7 +49,6 @@ timing_process_instrumentation::
   }
 }
 
-
 // ------------------------------------------------------------------
 void
 timing_process_instrumentation::
@@ -84,7 +56,6 @@ start_init_processing( VITAL_UNUSED std::string const& data )
 {
   m_timer->start();
 }
-
 
 // ------------------------------------------------------------------
 void
@@ -98,7 +69,6 @@ stop_init_processing()
   write_interval( "init", m_timer->elapsed() );
 }
 
-
 // ------------------------------------------------------------------
 void
 timing_process_instrumentation::
@@ -106,7 +76,6 @@ start_finalize_processing( VITAL_UNUSED std::string const& data )
 {
   m_timer->start();
 }
-
 
 // ------------------------------------------------------------------
 void
@@ -120,7 +89,6 @@ stop_finalize_processing()
   write_interval( "finalize", m_timer->elapsed() );
 }
 
-
 // ------------------------------------------------------------------
 void
 timing_process_instrumentation::
@@ -128,7 +96,6 @@ start_reset_processing( VITAL_UNUSED std::string const& data )
 {
   m_timer->start();
 }
-
 
 // ------------------------------------------------------------------
 void
@@ -139,7 +106,6 @@ stop_reset_processing()
   write_interval( "reset", m_timer->elapsed() );
 }
 
-
 // ------------------------------------------------------------------
 void
 timing_process_instrumentation::
@@ -147,7 +113,6 @@ start_flush_processing( VITAL_UNUSED std::string const& data )
 {
   m_timer->start();
 }
-
 
 // ------------------------------------------------------------------
 void
@@ -158,7 +123,6 @@ stop_flush_processing()
   write_interval( "flush", m_timer->elapsed() );
 }
 
-
 // ------------------------------------------------------------------
 void
 timing_process_instrumentation::
@@ -166,7 +130,6 @@ start_step_processing( VITAL_UNUSED std::string const& data )
 {
   m_timer->start();
 }
-
 
 // ------------------------------------------------------------------
 void
@@ -179,7 +142,6 @@ stop_step_processing()
   m_step_stats.add_datum( m_timer->elapsed() );
 }
 
-
 // ------------------------------------------------------------------
 void
 timing_process_instrumentation::
@@ -187,7 +149,6 @@ start_configure_processing( VITAL_UNUSED std::string const& data )
 {
   m_timer->start();
 }
-
 
 // ------------------------------------------------------------------
 void
@@ -198,7 +159,6 @@ stop_configure_processing()
   write_interval( "configure", m_timer->elapsed() );
 }
 
-
 // ------------------------------------------------------------------
 void
 timing_process_instrumentation::
@@ -206,7 +166,6 @@ start_reconfigure_processing( VITAL_UNUSED std::string const& data )
 {
   m_timer->start();
 }
-
 
 // ------------------------------------------------------------------
 void
@@ -216,7 +175,6 @@ stop_reconfigure_processing()
   m_timer->stop();
   write_interval( "reconfigure", m_timer->elapsed() );
 }
-
 
 // ----------------------------------------------------------------------------
 void timing_process_instrumentation::
@@ -263,7 +221,6 @@ configure( kwiver::vital::config_block_sptr const conf )
 
 }
 
-
 // ----------------------------------------------------------------------------
 kwiver::vital::config_block_sptr
 timing_process_instrumentation::
@@ -288,7 +245,6 @@ get_configuration() const
 
   return conf;
 }
-
 
 // ----------------------------------------------------------------------------
 void
