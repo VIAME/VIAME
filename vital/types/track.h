849e1f936c935ad5580112dc453f5f1baaa7461d
Matthew Woehlke <matthew.woehlke@kitware.com>
2020-12-08 13:16:11 -0500

    Update documentation style in track.{h,cxx}
    Update the documentation in track.h to conform to the new style being
    proposed. This includes removal of superfluous documentation,
    consistency improvements, and minor wording improvements. Also, remove
    repeated documentation in track.cxx and replace with header lines
    instead (also removing repeated blank lines in the process).

--- vital/types/track.h
+++ vital/types/track.h
@@ -2,10 +2,9 @@
 // OSI-approved BSD 3-Clause License. See top-level LICENSE file or
 // https://github.com/Kitware/kwiver/blob/master/LICENSE for details.
 
-/**
- * \file
- * \brief Header for \link kwiver::vital::track track \endlink objects
- */
+/// \file
+///
+/// Header for \link kwiver::vital::track track \endlink objects.
 
 #ifndef VITAL_TRACK_H_
 #define VITAL_TRACK_H_
@@ -47,38 +46,33 @@ public:
 };
 
 // ----------------------------------------------------------------------------
-/// Empty base class for data associated with a track state
+/// Empty base class for data associated with a track state.
 class VITAL_EXPORT track_state
 {
 public:
   friend class track;
 
-  track_state() = default;
-
-  /// Constructor
   track_state( frame_id_t frame )
     : frame_id_( frame )
   {}
 
-  /// Copy constructor
+  track_state() = default;
   track_state( track_state const& other ) = default;
-
-  /// Move constructor
   track_state( track_state&& other ) = default;
 
-  /// Clone the track state (polymorphic copy constructor)
+  /// Clone the track state (polymorphic copy constructor).
   virtual track_state_sptr clone( clone_type = clone_type::DEEP ) const
   {
     return std::make_shared<track_state>( *this );
   }
 
-  /// Access the frame identifier
+  /// Access the frame identifier.
   frame_id_t frame() const { return frame_id_; }
 
-  /// Access the track containing this state
+  /// Access the track containing this state.
   track_sptr track() const { return track_.lock(); }
 
-  /// Set the frame identifier
+  /// Set the frame identifier.
   void set_frame( frame_id_t frame_id ) { frame_id_ = frame_id; }
 
   virtual ~track_state() = default;
@@ -86,10 +80,10 @@ public:
   bool operator==( track_state other ) const { return frame_id_ == other.frame(); }
 
 private:
-  /// The frame identifier for this state
+  /// Frame identifier for this state.
   frame_id_t frame_id_ = 0;
 
-  /// A weak reference back to the parent track
+  /// Weak reference back to the parent track.
   track_ref track_;
 };
 
@@ -104,149 +98,143 @@ protected:
 typedef std::shared_ptr< track_data > track_data_sptr;
 
 // ----------------------------------------------------------------------------
-/// A special type of track data that redirects to another track
-/**
- * The primary use case for this class is to aid bookkeeping for track merging.
- * When you merge one track into another and transfer all of its states it
- * leaves behind an invalid track with no states.  A track may appear in
- * multiple matches and merging one match may invalidate another match if one
- * of the tracks is invalidated.  This class allows the merging function
- * to insert a redirect into an invalidated track such that later matches
- * can find the new version of the track.
- *
- * Example: assume a matcher returns matches (A,B) and (B,C).  The merge
- * function merges B into A producing a longer A and an empty B.  B uses this
- * class to redirect to A.  The merge function tries to merge C into B, but B
- * is now invalid.  However, B redirects to A, so the function tries to merge
- * C into A instead.
- */
+/// Special type of track data that redirects to another track.
+///
+/// The primary use case for this class is to aid bookkeeping for track
+/// merging. When you merge one track into another and transfer all of its
+/// states it leaves behind an invalid track with no states. A track may
+/// appear in multiple matches and merging one match may invalidate another
+/// match if one of the tracks is invalidated. This class allows the merging
+/// function to insert a redirect into an invalidated track such that later
+/// matches can find the new version of the track.
+///
+/// \par Example:
+///   Assume a matcher returns matches (A,B) and (B,C). The merge function
+///   merges B into A producing a longer A and an empty B. B uses this class to
+///   redirect to A. The merge function tries to merge C into B, but B is now
+///   invalid. However, B redirects to A, so the function tries to merge C
+///   into A instead.
 class VITAL_EXPORT track_data_redirect : public track_data
 {
 public:
-  // Constructor
   track_data_redirect( track_sptr t, track_data_sptr d )
     : redirect_track( t )
     , old_track_data( d )
   {
   }
 
-  // A redirect to another track
+  // Redirect to another track.
   track_sptr redirect_track;
-  // The track data that used to be associated with this track
+
+  // Track data that used to be associated with this track.
   track_data_sptr old_track_data;
 };
 
 // ----------------------------------------------------------------------------
-/// A representation of a track.
-/**
- * A track is a sequence of corresponding identifiers associated with each
- * other across time (i.e. frames indicies).  Each track consists of a
- * sequence of track states each with a frame id and optional data field.
- * Frame ids are in monotonically increasing order but need not be sequential.
- * The same track structure can be used to represent feature tracks for
- * image registration or moving object tracks.
- */
+/// Representation of a track.
+///
+/// A track is a sequence of corresponding identifiers associated with each
+/// other across time (i.e. frames indices). Each track consists of a sequence
+/// of track states each with a frame id and optional data field. Frame
+/// identifiers are in monotonically increasing order but need not be
+/// sequential. The same track structure can be used to represent feature
+/// tracks for image registration or moving object tracks.
 class VITAL_EXPORT track : public std::enable_shared_from_this<track>
 {
 public:
-  /// convenience type for the const iterator of the track state vector
+  /// Convenience type for the \c const iterator of the track state vector.
   typedef std::vector< track_state_sptr >::const_iterator history_const_itr;
 
-  /// Destructor
   ~track() = default;
 
-  /// Factory function
+  /// Factory function to create a new track state.
   static track_sptr create( track_data_sptr data = nullptr );
 
-  /// Clone
+  /// Clone the track (polymorphic copy constructor).
+  // TODO document difference between shallow and deep clones
   track_sptr clone( clone_type = clone_type::DEEP ) const;
 
-  /// Access the track identification number
+  /// Access the track identification number.
   track_id_t id() const { return id_; }
 
-  /// Access the track data
+  /// Access the track data.
   track_data_sptr data() const { return data_; }
 
-  /// Set the track identification number
+  /// Set the track identification number.
   void set_id( track_id_t id ) { id_ = id; }
 
-  /// Set the track data
+  /// Set the track data.
   void set_data( track_data_sptr d ) { data_ = d; }
 
-  /// Access the first frame number covered by this track
+  /// Access the first frame number covered by this track.
   frame_id_t first_frame() const;
 
-  /// Access the last frame number covered by this track
+  /// Access the last frame number covered by this track.
   frame_id_t last_frame() const;
 
   /// Append a track state.
-  /**
-   * The added track state must have a frame_id greater than
-   * the last frame in the history.
-   *
-   * \returns true if successful, false not correctly ordered
-   * \param state track state to add to this track.
-   */
+  ///
+  /// The added track state must have a \c frame_id greater than the last frame
+  /// in the history.
+  ///
+  /// \returns \c true if successful, \c false not correctly ordered.
+  /// \param state Track state to add to this track.
   bool append( track_state_sptr&& state );
 
   bool append( track_state_sptr const& state )
   { auto copy = state; return this->append( std::move( copy ) ); }
 
   /// Append the history contents of another track.
-  /**
-   * The first state of the input track must contain a frame number
-   * greater than the last state of this track.  Track states from
-   * \p to_append are reassigned to this track and removed from
-   * \p to_append.  The track data in \p to_append is modified to redirect
-   * to this track using \p track_data_redirect.
-   *
-   * \returns true if successful, false not correctly ordered
-   */
+  ///
+  /// The first state of the input track must contain a frame number greater
+  /// than the last state of this track. Track states from \p to_append are
+  /// reassigned to this track and removed from \p to_append. The track data in
+  /// \p to_append is modified to redirect to this track using
+  /// \p track_data_redirect.
+  ///
+  /// \returns \c true if successful, \c false not correctly ordered.
   bool append( track& to_append );
 
   /// Insert a track state.
-  /**
-   * The added track state must have a frame_id that is not already
-   * present in the track history.
-   *
-   * \returns true if successful, false if already a state on this frame
-   * \param state track state to add to this track.
-   */
+  ///
+  /// The added track state must have a frame_id that is not already
+  /// present in the track history.
+  ///
+  /// \returns
+  ///   \c true if successful, \c false if already a state on this frame.
+  /// \param state Track state to add to this track.
   bool insert( track_state_sptr&& state );
 
   bool insert( track_state_sptr const& state )
   { auto copy = state; return this->insert( std::move( copy ) ); }
 
-  /// Remove track state
-  /**
-   * Removes the track state
-   * Returns true if the state was found and removed
-  */
+  /// Remove track state.
+  ///
+  /// \returns \c true if the state was found and removed.
   bool remove( track_state_sptr const& state );
 
   /// Remove all track states.
   void clear();
 
-  /// Access a const iterator to the start of the history
+  /// Access a \c const iterator to the start of the history.
   history_const_itr begin() const { return history_.begin(); }
 
-  /// Access a const iterator to the end of the history
+  /// Access a \c const iterator to the end of the history.
   history_const_itr end() const { return history_.end(); }
 
-  /// Access the first entry of the history
+  /// Access the first entry of the history.
   track_state_sptr front() const { return history_.front(); }
 
-  /// Access the last entry of the history
+  /// Access the last entry of the history.
   track_state_sptr back() const { return history_.back(); }
 
-  /// Find the track state iterator matching \a frame
-  /**
-   *  \param [in] frame the frame number to access
-   *  \return an iterator at the frame if found, or end() if not
-   */
+  /// Find the track state iterator matching \a frame.
+  ///
+  /// \param frame The frame number to access.
+  /// \return An iterator at the frame if found, or end() if not.
   history_const_itr find( frame_id_t frame ) const;
 
-  /// Return the set of all frame IDs covered by this track
+  /// Return the set of all frame identifiers covered by this track.
   std::set< frame_id_t > all_frame_ids() const;
 
   /// Return the number of states in the track.
@@ -256,17 +244,16 @@ public:
   bool empty() const { return history_.empty(); }
 
 protected:
-  /// Default Constructor
   explicit track( track_data_sptr d = nullptr );
-
-  /// Copy Constructor
   track( const track& other );
 
-  /// The ordered array of track states
+  /// Ordered array of track states.
   std::vector< track_state_sptr > history_;
-  /// The unique track identification number
+
+  /// Unique track identification number.
   track_id_t id_;
-  /// The optional data structure associated with this track
+
+  /// Optional data structure associated with this track.
   track_data_sptr data_;
 };
 
