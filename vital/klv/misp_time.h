5ff2622641cfc9d9192d07028720935975b75d94
Matthew Woehlke <matthew.woehlke@kitware.com>
2020-12-15 14:06:00 -0500

    Update copyright banner in vital/klv
    Update copyright banner and squash consecutive blank lines, as in the
    preceding commits.

--- vital/klv/misp_time.h
+++ vital/klv/misp_time.h
@@ -1,32 +1,6 @@
-/*ckwg +29
- * Copyright 2016 by Kitware, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *  * Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- *  * Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- *  * Neither name of Kitware, Inc. nor the names of any contributors may be used
- *    to endorse or promote products derived from this software without specific
- *    prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+// This file is part of KWIVER, and is distributed under the
+// OSI-approved BSD 3-Clause License. See top-level LICENSE file or
+// https://github.com/Kitware/kwiver/blob/master/LICENSE for details.
 
 #ifndef VITAL_KLV_MISP_TIME_H
 #define VITAL_KLV_MISP_TIME_H
@@ -54,7 +28,6 @@ namespace vital {
 VITAL_KLV_EXPORT
 bool find_MISP_microsec_time(  std::vector< unsigned char > const& raw_data, std::int64_t& ts );
 
-
 /**
  * @brief Convert MISP time packet to uSec
  *
@@ -68,7 +41,6 @@ bool find_MISP_microsec_time(  std::vector< unsigned char > const& raw_data, std
  */
 VITAL_KLV_EXPORT
 bool convert_MISP_microsec_time( std::vector< unsigned char > const& buf, std::int64_t& ts );
-
 
 } } // end namespace
 
