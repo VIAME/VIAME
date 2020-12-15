36acd97b20cd209b8c2c718379ccb384bd801b5a
Matthew Woehlke <matthew.woehlke@kitware.com>
2020-12-15 15:37:01 -0500

    Update copyright banner in vital
    Update copyright banner and squash consecutive blank lines, as in the
    preceding commits. All of the vital headers in subdirectories are
    addressed by the previous commits or separate branches; this takes care
    of those headers which are not in subdirectories.

--- vital/algorithm_capabilities.h
+++ vital/algorithm_capabilities.h
@@ -1,32 +1,6 @@
-/*ckwg +29
- * Copyright 2016 by Kitware, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *  * Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- *  * Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- *  * Neither name of Kitware, Inc. nor the names of any contributors may be used
- *    to endorse or promote products derived from this software without specific
- *    prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+// This file is part of KWIVER, and is distributed under the
+// OSI-approved BSD 3-Clause License. See top-level LICENSE file or
+// https://github.com/Kitware/kwiver/blob/master/LICENSE for details.
 
 #ifndef VITAL_ALGORITHM_CAPABILITIES_H
 #define VITAL_ALGORITHM_CAPABILITIES_H
@@ -62,7 +36,6 @@ public:
   algorithm_capabilities( algorithm_capabilities const& other );
   ~algorithm_capabilities();
 
-
   /// Indicate if capability is supported.
   /**
    * This method reports if the specified capability is supported by the
@@ -76,7 +49,6 @@ public:
    */
   bool has_capability( capability_name_t const& name ) const;
 
-
   /// Get list of supported capabilities.
   /**
    * This method returns a vector of all capabilities supported by the
@@ -85,7 +57,6 @@ public:
    * @return Vector of supported capabilities.
    */
   capability_list_t capability_list() const;
-
 
   /// Return value of capability,
   /**
@@ -100,7 +71,6 @@ public:
    * @return Value of capability.
    */
   bool capability( capability_name_t const& name ) const;
-
 
   /// Set capability value.
   /**
