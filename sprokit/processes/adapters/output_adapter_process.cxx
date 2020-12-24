9bdabbf70a8bbe1f542b484665d034db60a39200
Matthew Woehlke <matthew.woehlke@kitware.com>
2020-12-24 09:12:43 -0500

    Update copyright banner in sprokit/processes/adapters
    Update copyright banner and squash consecutive blank lines, as in the
    preceding commits.

--- sprokit/processes/adapters/output_adapter_process.cxx
+++ sprokit/processes/adapters/output_adapter_process.cxx
@@ -1,32 +1,6 @@
-/*ckwg +29
- * Copyright 2016 by Kitware, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *  * Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- *  * Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- *  * Neither name of Kitware, Inc. nor the names of any contributors may be used
- *    to endorse or promote products derived from this software without specific
- *    prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+// This file is part of KWIVER, and is distributed under the
+// OSI-approved BSD 3-Clause License. See top-level LICENSE file or
+// https://github.com/Kitware/kwiver/blob/master/LICENSE for details.
 
 /**
  * \file
@@ -70,7 +44,6 @@ public:
 
 }; // end priv class
 
-
 // ------------------------------------------------------------------
 output_adapter_process
 ::output_adapter_process( kwiver::vital::config_block_sptr const& config )
@@ -80,11 +53,9 @@ output_adapter_process
   declare_config_using_trait( wait_on_queue_full );
 }
 
-
 output_adapter_process
 ::~output_adapter_process()
 { }
-
 
 // ------------------------------------------------------------------
 void
@@ -94,7 +65,6 @@ _configure()
   d->m_wait_on_queue_full = config_value_using_trait( wait_on_queue_full );
 
 }
-
 
 // ------------------------------------------------------------------
 kwiver::adapter::ports_info_t
@@ -112,7 +82,6 @@ output_adapter_process
 
   return p_info;
 }
-
 
 // ------------------------------------------------------------------
 void
@@ -138,7 +107,6 @@ output_adapter_process
     m_active_ports.insert( port );
   }
 }
-
 
 // ------------------------------------------------------------------
 void
@@ -167,7 +135,6 @@ output_adapter_process
   }
 }
 
-
 // ----------------------------------------------------------------
 void
 output_adapter_process
@@ -180,14 +147,12 @@ output_adapter_process
   this->get_interface_queue()->Send( ds );
 }
 
-
 // ================================================================
 output_adapter_process::priv
 ::priv()
   : m_wait_on_queue_full( true )
 {
 }
-
 
 output_adapter_process::priv
 ::~priv()
