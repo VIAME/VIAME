8665084d16f6e74417310b9733f4ea4d004dca4b
Matthew Woehlke <matthew.woehlke@kitware.com>
2020-12-24 09:10:12 -0500

    Update copyright banner in sprokit/processes/transport
    Update copyright banner and squash consecutive blank lines, as in the
    preceding commits.

--- sprokit/processes/transport/file_transport_send_process.cxx
+++ sprokit/processes/transport/file_transport_send_process.cxx
@@ -1,32 +1,6 @@
-/*ckwg +29
- * Copyright 2018 by Kitware, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *  * Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- *  * Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- *  * Neither name of Kitware, Inc. nor the names of any contributors may be used
- *    to endorse or promote products derived from this software without specific
- *    prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+// This file is part of KWIVER, and is distributed under the
+// OSI-approved BSD 3-Clause License. See top-level LICENSE file or
+// https://github.com/Kitware/kwiver/blob/master/LICENSE for details.
 
 #include "file_transport_send_process.h"
 
@@ -36,13 +10,11 @@
 
 #include <fstream>
 
-
 namespace kwiver {
 
 // (config-key, value-type, default-value, description )
 create_config_trait( file_name, std::string, "serialized_data.dat",
                      "Name of file where serialized messages are written. ");
-
 
 //----------------------------------------------------------------
 // Private implementation class
@@ -70,12 +42,10 @@ file_transport_send_process
   make_config();
 }
 
-
 file_transport_send_process
 ::~file_transport_send_process()
 {
 }
-
 
 // ----------------------------------------------------------------
 void file_transport_send_process
@@ -94,7 +64,6 @@ void file_transport_send_process
   }
 }
 
-
 // ----------------------------------------------------------------
 void file_transport_send_process
 ::_step()
@@ -107,7 +76,6 @@ void file_transport_send_process
   d->m_output_file << *mess;
 }
 
-
 // ----------------------------------------------------------------
 void file_transport_send_process
 ::make_ports()
@@ -119,14 +87,12 @@ void file_transport_send_process
   declare_input_port_using_trait( serialized_message, required );
 }
 
-
 // ----------------------------------------------------------------
 void file_transport_send_process
 ::make_config()
 {
   declare_config_using_trait( file_name );
 }
-
 
 // ================================================================
 file_transport_send_process::priv
@@ -135,14 +101,9 @@ file_transport_send_process::priv
 {
 }
 
-
 file_transport_send_process::priv
 ::~priv()
 {
 }
-
-
-
-
 
 } // end namespace
--- sprokit/processes/transport/zmq_transport_send_process.cxx
+++ sprokit/processes/transport/zmq_transport_send_process.cxx
@@ -1,32 +1,6 @@
-/*ckwg +29
- * Copyright 2018 by Kitware, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *  * Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- *  * Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- *  * Neither name of Kitware, Inc. nor the names of any contributors may be used
- *    to endorse or promote products derived from this software without specific
- *    prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+// This file is part of KWIVER, and is distributed under the
+// OSI-approved BSD 3-Clause License. See top-level LICENSE file or
+// https://github.com/Kitware/kwiver/blob/master/LICENSE for details.
 
 #include "zmq_transport_send_process.h"
 
@@ -115,12 +89,10 @@ zmq_transport_send_process
   d->m_logger = logger();
 }
 
-
 zmq_transport_send_process
 ::~zmq_transport_send_process()
 {
 }
-
 
 // ----------------------------------------------------------------
 void zmq_transport_send_process
@@ -160,7 +132,6 @@ void zmq_transport_send_process
   d->m_pub_socket.send(datagram);
 }
 
-
 // ----------------------------------------------------------------
 void zmq_transport_send_process
 ::make_ports()
@@ -172,7 +143,6 @@ void zmq_transport_send_process
   declare_input_port_using_trait( serialized_message, required );
 }
 
-
 // ----------------------------------------------------------------
 void zmq_transport_send_process
 ::make_config()
@@ -180,7 +150,6 @@ void zmq_transport_send_process
   declare_config_using_trait( port );
   declare_config_using_trait( expected_subscribers );
 }
-
 
 // ================================================================
 zmq_transport_send_process::priv
@@ -191,12 +160,10 @@ zmq_transport_send_process::priv
 {
 }
 
-
 zmq_transport_send_process::priv
 ::~priv()
 {
 }
-
 
 // ----------------------------------------------------------------------------
 void
