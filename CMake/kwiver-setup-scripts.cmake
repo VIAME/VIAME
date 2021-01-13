Kitware/kwiver

Squashed commit
Add python bindings for draw tracks algorithms

Add python bindings for estimate canonical transform algorithms

Fix typo and whitespace issue

Add python bindings for extract descriptors algorithms

Add python bindings for estimate essential matrix algorithm

Add python bindings for estimate fundamental matrix algorithm

Add python bindings for estimate homography algorithm

Add python bindings for estimate pnp algorithm

Fix whitespace

Add python bindings for estimate similarity transform algorithm

Add python bindings for feature descriptor io algorithm

Add python bindings for filter features algorithm

Add python bindings for filter tracks algorithm

Add python bindings for image io algorithm

Add python bindings for initialize cameras landmarks algorithm

Add python bindings for initialize object tracks algorithm

Add python bindings for integrate depth maps algorithm

Add python bindings for interpolate track algorithm

Add python bindings for keyframe selection algorithm

Add python bindings for match descriptor sets algorithm

Add python bindings for match features algorithm

Add python bindings for merge images algorithm

Add python bindings for optimize cameras algorithm

Make open and close virtual in read_object_track_set

Add python bindings for read object track set algorithm

Make open and close virtual in read_track_descriptor_set

Add python bindings for read track descriptor set algorithm

Add python bindings for refine detections algorithm

Add python bindings for split image algorithm

Add python bindings for track features algorithm

Add python bindings for train detector algorithm

Add python bindings for transform 2d io algorithm

Add python bindings for triangulate landmarks algorithm

Add python bindings for uuid factory algorithm

Add python bindings for uv unwrap mesh algorithm

Bind algorithm capabilities for image io

Fix typo

Move simple_image_object_detector from arrows to test

Modify tests for image_object_detector

CMake/kwiver-setup-scripts.cmake
@@ -90,7 +90,11 @@ if (KWIVER_ENABLE_PYTHON)
   file( APPEND "${KWIVER_SETUP_SCRIPT_FILE}" "export PYTHON_LIBRARY=\"${PYTHON_LIBRARY}\"\n" )
   file( APPEND "${KWIVER_SETUP_SCRIPT_FILE}" "export PYTHONPATH=$this_dir/${python_site_packages}:$PYTHONPATH\n" )
   file( APPEND "${KWIVER_SETUP_SCRIPT_FILE}" "\n# additional python mudules to load, separated by ':'\n" )
-  file( APPEND "${KWIVER_SETUP_SCRIPT_FILE}" "export SPROKIT_PYTHON_MODULES=kwiver.sprokit.processes:kwiver.sprokit.schedulers:kwiver.sprokit.tests.processes:kwiver.arrows.python\n" )
+  file( APPEND "${KWIVER_SETUP_SCRIPT_FILE}" "export SPROKIT_PYTHON_MODULES=")
+  file( APPEND "${KWIVER_SETUP_SCRIPT_FILE}" "kwiver.sprokit.processes:")
+  file( APPEND "${KWIVER_SETUP_SCRIPT_FILE}" "kwiver.sprokit.schedulers:")
+  file( APPEND "${KWIVER_SETUP_SCRIPT_FILE}" "kwiver.sprokit.tests.processes:")
+  file( APPEND "${KWIVER_SETUP_SCRIPT_FILE}" "kwiver.vital.tests.algorithms\n")
   file( APPEND "${KWIVER_SETUP_SCRIPT_FILE}" "\n# set to suppress loading python modules/processes\n" )
   file( APPEND "${KWIVER_SETUP_SCRIPT_FILE}" "# export SPROKIT_NO_PYTHON_MODULES\n\n" )
   file( APPEND "${KWIVER_SETUP_SCRIPT_FILE}" "export KWIVER_PYTHON_DEFAULT_LOG_LEVEL=WARN\n" )
@@ -99,7 +103,11 @@ if (KWIVER_ENABLE_PYTHON)
   file( APPEND "${KWIVER_SETUP_BATCH_FILE}" "set PYTHON_LIBRARY=${PYTHON_LIBRARY}\n" )
   file( APPEND "${KWIVER_SETUP_BATCH_FILE}" "set PYTHONPATH=%~dp0/lib/%config%/python2.7/site-packages\n" )
   file( APPEND "${KWIVER_SETUP_BATCH_FILE}" "\n:: additional python mudules to load, separated by ':'\n" )
-  file( APPEND "${KWIVER_SETUP_BATCH_FILE}" "set SPROKIT_PYTHON_MODULES=kwiver.sprokit.processes:kwiver.sprokit.schedulers:kwiver.sprokit.tests.processes:kwiver.arrows.python\n" )
+  file( APPEND "${KWIVER_SETUP_BATCH_FILE}" "set SPROKIT_PYTHON_MODULES=" )
+  file( APPEND "${KWIVER_SETUP_BATCH_FILE}" "kwiver.sprokit.processes:" )
+  file( APPEND "${KWIVER_SETUP_BATCH_FILE}" "kwiver.sprokit.schedulers:" )
+  file( APPEND "${KWIVER_SETUP_BATCH_FILE}" "kwiver.sprokit.tests.processes:")
+  file( APPEND "${KWIVER_SETUP_BATCH_FILE}" "kwiver.vital.tests.algorithms\n" )
   file( APPEND "${KWIVER_SETUP_BATCH_FILE}" "\n:: set to suppress loading python modules/processes\n" )
   file( APPEND "${KWIVER_SETUP_BATCH_FILE}" "::set SPROKIT_NO_PYTHON_MODULES=false\n\n" )
   file( APPEND "${KWIVER_SETUP_BATCH_FILE}" "set KWIVER_PYTHON_DEFAULT_LOG_LEVEL=WARN\n" )

VIAME/VIAME

CMake/kwiver-setup-scripts.cmake
@@ -90,7 +90,12 @@ if (KWIVER_ENABLE_PYTHON)
   file( APPEND "${KWIVER_SETUP_SCRIPT_FILE}" "export PYTHON_LIBRARY=\"${PYTHON_LIBRARY}\"\n" )
   file( APPEND "${KWIVER_SETUP_SCRIPT_FILE}" "export PYTHONPATH=$this_dir/${python_site_packages}:$PYTHONPATH\n" )
   file( APPEND "${KWIVER_SETUP_SCRIPT_FILE}" "\n# additional python mudules to load, separated by ':'\n" )
-  file( APPEND "${KWIVER_SETUP_SCRIPT_FILE}" "export SPROKIT_PYTHON_MODULES=kwiver.sprokit.processes:kwiver.sprokit.schedulers:kwiver.sprokit.tests.processes:kwiver.arrows.python:kwiver.sprokit.processes.pytorch\n" )
+  file( APPEND "${KWIVER_SETUP_SCRIPT_FILE}" "export SPROKIT_PYTHON_MODULES=")
+  file( APPEND "${KWIVER_SETUP_SCRIPT_FILE}" "kwiver.sprokit.processes:")
+  file( APPEND "${KWIVER_SETUP_SCRIPT_FILE}" "kwiver.sprokit.schedulers:")
+  file( APPEND "${KWIVER_SETUP_SCRIPT_FILE}" "kwiver.sprokit.tests.processes:")
+  file( APPEND "${KWIVER_SETUP_SCRIPT_FILE}" "kwiver.vital.tests.algorithms:")
+  file( APPEND "${KWIVER_SETUP_SCRIPT_FILE}" "kwiver.sprokit.processes.pytorch\n")
   file( APPEND "${KWIVER_SETUP_SCRIPT_FILE}" "\n# set to suppress loading python modules/processes\n" )
   file( APPEND "${KWIVER_SETUP_SCRIPT_FILE}" "# export SPROKIT_NO_PYTHON_MODULES\n\n" )
   file( APPEND "${KWIVER_SETUP_SCRIPT_FILE}" "export KWIVER_PYTHON_DEFAULT_LOG_LEVEL=WARN\n" )
@@ -99,7 +104,12 @@ if (KWIVER_ENABLE_PYTHON)
   file( APPEND "${KWIVER_SETUP_BATCH_FILE}" "set PYTHON_LIBRARY=${PYTHON_LIBRARY}\n" )
   file( APPEND "${KWIVER_SETUP_BATCH_FILE}" "set PYTHONPATH=%~dp0/lib/%config%/python2.7/site-packages\n" )
   file( APPEND "${KWIVER_SETUP_BATCH_FILE}" "\n:: additional python mudules to load, separated by ':'\n" )
-  file( APPEND "${KWIVER_SETUP_BATCH_FILE}" "set SPROKIT_PYTHON_MODULES=kwiver.sprokit.processes:kwiver.sprokit.schedulers:kwiver.sprokit.tests.processes:kwiver.arrows.python:kwiver.sprokit.processes.pytorch\n" )
+  file( APPEND "${KWIVER_SETUP_BATCH_FILE}" "set SPROKIT_PYTHON_MODULES=" )
+  file( APPEND "${KWIVER_SETUP_BATCH_FILE}" "kwiver.sprokit.processes:" )
+  file( APPEND "${KWIVER_SETUP_BATCH_FILE}" "kwiver.sprokit.schedulers:" )
+  file( APPEND "${KWIVER_SETUP_BATCH_FILE}" "kwiver.sprokit.tests.processes:")
+  file( APPEND "${KWIVER_SETUP_BATCH_FILE}" "kwiver.vital.tests.algorithms:" )
+  file( APPEND "${KWIVER_SETUP_BATCH_FILE}" "kwiver.sprokit.processes.pytorch\n")
   file( APPEND "${KWIVER_SETUP_BATCH_FILE}" "\n:: set to suppress loading python modules/processes\n" )
   file( APPEND "${KWIVER_SETUP_BATCH_FILE}" "::set SPROKIT_NO_PYTHON_MODULES=false\n\n" )
   file( APPEND "${KWIVER_SETUP_BATCH_FILE}" "set KWIVER_PYTHON_DEFAULT_LOG_LEVEL=WARN\n" )