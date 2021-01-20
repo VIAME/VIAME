9642dee897e65ef4763f8451f4baf93f408dde38
Matt Dawkins <matt.dawkins@kitware.com>
2021-01-20 12:45:33 -0500

    Add new script

---
+++ cmake/limit_train_time_for_viame_web.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Configurable Input Paths
+export VIAME_INSTALL="$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)/.."
+
+# Other paths generated from root
+export VIAME_PIPELINES=${VIAME_INSTALL}/configs/pipelines/
