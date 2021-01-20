c9fac0c05809db87f788cb8283f58c1a09d7d9b9
Matt Dawkins <matt.dawkins@kitware.com>
2021-01-20 00:17:42 -0500

    Update script name

--- examples/object_tracking/run_pysot_tracker.bat
+++ examples/object_tracking/run_pysot_tracker.bat
@@ -8,7 +8,7 @@ CALL "%VIAME_INSTALL%\setup_viame.bat"
 
 REM Run Pipeline
 
-pipeline_runner.exe -p "%VIAME_INSTALL%\configs\pipelines\tracker_pysot.pipe" ^
+pipeline_runner.exe -p "%VIAME_INSTALL%\configs\pipelines\tracker_short_term.pipe" ^
                     -s input:video_filename=input_list.txt
 
 pause
--- examples/object_tracking/run_pysot_tracker.sh
+++ examples/object_tracking/run_pysot_tracker.sh
@@ -8,5 +8,5 @@ source ${VIAME_INSTALL}/setup_viame.sh
 
 # Run pipeline
 
-pipeline_runner -p ${VIAME_INSTALL}/configs/pipelines/tracker_pysot.pipe \
+pipeline_runner -p ${VIAME_INSTALL}/configs/pipelines/tracker_short_term.pipe \
                 -s input:video_filename=input_list.txt
