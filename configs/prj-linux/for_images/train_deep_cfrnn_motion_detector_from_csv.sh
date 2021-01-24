38cc0e535eaf7a5d6e6b2771a731501b311e9109
Matt Dawkins <matt.dawkins@kitware.com>
2021-01-24 14:26:47 -0500

    Add new motion trainer

---
+++ configs/prj-linux/for_images/train_deep_cfrnn_motion_detector_from_csv.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Path to VIAME installation
+export VIAME_INSTALL=/opt/noaa/viame
+
+# Core processing options
+export INPUT_DIRECTORY=training_data
+
+# Setup paths and run command
+source ${VIAME_INSTALL}/setup_viame.sh
+
+# Adjust log level
+export KWIVER_DEFAULT_LOG_LEVEL=info
+
+viame_train_detector \
+  -i ${INPUT_DIRECTORY} \
+  -c ${VIAME_INSTALL}/configs/pipelines/train_netharn_cascade_motion.viame_csv.conf \
+  --threshold 0.0
