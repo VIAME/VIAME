e63d9ba06e34f78f7d83ecd4a365daa7b491231d
Matt Dawkins <matt.dawkins@kitware.com>
2021-01-25 22:23:24 -0500

    Add script

---
+++ tools/check_gpu_usability.py
@@ -0,0 +1,40 @@
+ #!/usr/bin/env python
+
+import sys
+import os
+import shutil
+import argparse
+import glob
+
+# Main Function
+if __name__ == "__main__" :
+
+    parser = argparse.ArgumentParser(description="Check GPU properties of the system",
+                       formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument("--use-pytorch", dest="use_pytorch", action="store_true",
+                        help="Use pytorch for checking gpu properties")
+
+    args = parser.parse_args()
+    use_pytorch = True
+
+    print( "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" )
+    print( "~~~~~~~~~~VIAME GPU CHECK UTILITY~~~~~~~~~~~~~" )
+    print( "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" )
+    print( "" )
+
+    if use_pytorch:
+
+        import torch
+        if not torch.cuda.is_available():
+            print( "Unable to access CUDA on this system" )
+            sys.exit( 0 )
+
+        gpu_count = torch.cuda.device_count()
+        print( "Usable devices: " + str( gpu_count ) + "\n" )
+        for i in range( self._gpu_count ):
+            gpu_mem = torch.cuda.get_device_properties( i ).total_memory
+            print( "Device #1, usable mem: " + str( gpu_mem ) )
+
+
+
