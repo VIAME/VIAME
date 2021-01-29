d855633d261fdd58a7fd9783bebf80f4da05a997
Neal Siekierski <neal.siekierski@kitware.com>
2021-01-29 14:22:17 -0500

    Teach write_detected_object_set to write masks as polygons

--- plugins/core/write_detected_object_set_viame_csv.cxx
+++ plugins/core/write_detected_object_set_viame_csv.cxx
@@ -34,6 +34,13 @@
 
 #include <vital/util/tokenize.h>
 
+#ifdef VIAME_ENABLE_OPENCV
+  #include <arrows/ocv/image_container.h>
+
+  #include <opencv2/core/core.hpp>
+  #include <opencv2/imgproc/imgproc.hpp>
+#endif
+
 #include <memory>
 #include <vector>
 #include <fstream>
@@ -61,6 +68,7 @@ public:
     , m_stream_identifier( "" )
     , m_model_identifier( "" )
     , m_version_identifier( "" )
+    , m_mask_to_poly_tol( -1 )
   {}
 
   ~priv() {}
@@ -72,6 +80,7 @@ public:
   std::string m_stream_identifier;
   std::string m_model_identifier;
   std::string m_version_identifier;
+  double m_mask_to_poly_tol;
 };
 
 
@@ -107,6 +116,15 @@ write_detected_object_set_viame_csv
     config->get_value< std::string >( "model_identifier" );
   d->m_version_identifier =
     config->get_value< std::string >( "version_identifier" );
+  d->m_mask_to_poly_tol =
+    config->get_value< double >( "mask_to_poly_tol" );
+
+#ifndef VIAME_ENABLE_OPENCV
+  if( d->m_mask_to_poly_tol >= 0 )
+  {
+    throw std::runtime_error( "Must have OpenCV enabled to use mask_to_poly_tol" );
+  }
+#endif
 }
 
 
@@ -128,6 +146,9 @@ write_detected_object_set_viame_csv
     "Model identifier string to write to the header or the csv." );
   config->set_value( "version_identifier", d->m_version_identifier,
     "Version identifier string to write to the header or the csv." );
+  config->set_value( "mask_to_poly_tol", d->m_mask_to_poly_tol,
+    "Write segmentation masks when available as polygons with the specified "
+    "relative tolerance for the conversion.  Set to a negative value to disable." );
 
   return config;
 }
@@ -241,6 +262,45 @@ write_detected_object_set_viame_csv
         stream() << "," << name << "," << dot->score( name );
       }
     }
+
+#ifdef VIAME_ENABLE_OPENCV
+    if( (*det)->mask() && d->m_mask_to_poly_tol >= 0 )
+    {
+      using ic = kwiver::arrows::ocv::image_container;
+      auto ref_x = static_cast< int >( bbox.min_x() );
+      auto ref_y = static_cast< int >( bbox.min_y() );
+      cv::Mat mask = ic::vital_to_ocv( (*det)->mask()->get_image(),
+                                       ic::OTHER_COLOR );
+      std::vector< std::vector< cv::Point > > contours;
+      std::vector< cv::Vec4i > hierarchy;
+      // Pre-3.2 OpenCV may modify the passed image, so we clone it.
+      cv::findContours( mask.clone(), contours, hierarchy,
+                        cv::RETR_CCOMP, cv::CHAIN_APPROX_SIMPLE );
+      for( size_t i = 0; i < contours.size(); ++i )
+      {
+        auto& contour = contours[i];
+        int x_min, x_max, y_min, y_max;
+        x_min = x_max = contour[0].x;
+        y_min = y_max = contour[0].y;
+        for( size_t j = 1; j < contour.size(); ++j )
+        {
+          x_min = std::min( x_min, contour[j].x );
+          x_max = std::max( x_max, contour[j].x );
+          y_min = std::min( y_min, contour[j].y );
+          y_max = std::max( y_max, contour[j].y );
+        }
+        double tol = d->m_mask_to_poly_tol * std::min( x_max - x_min + 1,
+                                                       y_max - y_min + 1 );
+        std::vector< cv::Point > simp_contour;
+        cv::approxPolyDP( contour, simp_contour, tol, /*closed:*/ true );
+        stream() << ( hierarchy[i][3] < 0 ? ",(poly)" : ",(hole)" );
+        for( auto&& p : simp_contour )
+        {
+          stream() << " " << p.x + ref_x << " " << p.y + ref_y;
+        }
+      }
+    }
+#endif
 
     if( !(*det)->keypoints().empty() )
     {
