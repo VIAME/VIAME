ac1a27493ccd50639005d28b0c7fb0bd8352f252
Matt Dawkins <matt.dawkins@kitware.com>
2021-01-29 01:36:34 -0500

    Update package

--- cmake/download_viame_addons.sh
+++ cmake/download_viame_addons.sh
@@ -14,7 +14,7 @@ wget -O ${DOWNLOAD_LOCATION}/download1.zip https://data.kitware.com/api/v1/item/
 unzip -o ${DOWNLOAD_LOCATION}/download1.zip -d ${VIAME_INSTALL}
 
 # SEFSC -
-wget -O ${DOWNLOAD_LOCATION}/download2.zip https://data.kitware.com/api/v1/item/5ff8b8d22fa25629b9d0e4ed/download
+wget -O ${DOWNLOAD_LOCATION}/download2.zip https://data.kitware.com/api/v1/item/6013acad2fa25629b9207b52/download
 unzip -o ${DOWNLOAD_LOCATION}/download2.zip -d ${VIAME_INSTALL}
 
 # PengHead -
