429bdceb2a6b16d64e34e7caf1ef5eb6883da3fe
Matt Dawkins <matt.dawkins@kitware.com>
2021-02-04 22:10:13 -0500

    Add run scripts

---
+++ examples/annotation_and_visualization/launch_dive_interface.bat
@@ -0,0 +1,12 @@
+@echo off
+
+REM Setup VIAME Paths (no need to set if installed to registry or already set up)
+
+SET VIAME_INSTALL=.\..\..
+
+CALL "%VIAME_INSTALL%\setup_viame.bat"
+
+REM Run Pipeline
+
+"%VIAME_INSTALL%\bin\dive\vue-media-annotator.exe"
+
---
+++ examples/annotation_and_visualization/launch_dive_interface.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Setup VIAME Paths (no need to run multiple times if you already ran it)
+
+export VIAME_INSTALL="$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)/../.."
+
+source ${VIAME_INSTALL}/setup_viame.sh 
+
+# Run vpView annotation GUI
+
+exec ${VIAME_INSTALL}/bin/dive/vue-media-annotator
