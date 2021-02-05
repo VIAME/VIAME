17c8d8ee32253c1c1d39510cb3d76dbaa3030942
Matt Dawkins <matt.dawkins@kitware.com>
2021-02-04 21:34:54 -0500

    Update gui pos

--- cmake/add_project_dive.cmake
+++ cmake/add_project_dive.cmake
@@ -8,12 +8,12 @@ if( WIN32 )
     6ea9b6c226575ef9dc6cbd0085e37908
     ${VIAME_DOWNLOAD_DIR}/dive_interface_binaries.zip
     ${VIAME_DIVE_BUILD_DIR}
-    ${VIAME_BUILD_INSTALL_PREFIX}/dive )
+    bin/dive )
 elseif( UNIX )
   DownloadExtractAndInstall(
     https://github.com/VIAME/VIAME-Web/releases/download/1.4.1/DIVE-Desktop-1.4.1.tar.gz
     5ed66a76cde3acb556f8b5a603da66cd
     ${VIAME_DOWNLOAD_DIR}/dive_interface_binaries.tar.gz
     ${VIAME_DIVE_BUILD_DIR}
-    ${VIAME_BUILD_INSTALL_PREFIX}/dive )
+    bin/dive )
 endif()
