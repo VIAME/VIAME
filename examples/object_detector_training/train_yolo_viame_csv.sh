b68e2179cfd7296ed6d2fb222a1587dc66b94ce6
Matt Dawkins <matt.dawkins@kitware.com>
2021-02-14 14:39:04 -0500

    Update run script

--- examples/object_detector_training/train_yolo_viame_csv.sh
+++ examples/object_detector_training/train_yolo_viame_csv.sh
@@ -14,5 +14,5 @@ export KWIVER_DEFAULT_LOG_LEVEL=info
 # Run pipeline
 viame_train_detector \
   -i ${INPUT_DIRECTORY} \
-  -c ${VIAME_INSTALL}/configs/pipelines/train_yolo_704.viame_csv.conf \
+  -c ${VIAME_INSTALL}/configs/pipelines/train_yolo_640.viame_csv.conf \
   --threshold 0.0
