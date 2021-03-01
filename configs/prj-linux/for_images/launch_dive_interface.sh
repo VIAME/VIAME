cb6e18c477158d7778dcc5ee7184b8e66ae2bbcb
Matt Dawkins <matt.dawkins@kitware.com>
2021-02-28 23:55:03 -0500

    Set VIAME install path for DIVE

--- configs/prj-linux/for_images/launch_dive_interface.sh
+++ configs/prj-linux/for_images/launch_dive_interface.sh
@@ -6,4 +6,6 @@ export VIAME_INSTALL=/opt/noaa/viame
 # Setup paths and run command
 source ${VIAME_INSTALL}/setup_viame.sh 
 
+export DIVE_VIAME_INSTALL_PATH="${VIAME_INSTALL}"
+
 exec ${VIAME_INSTALL}/dive/vue-media-annotator
--- configs/prj-windows/for_videos/launch_dive_interface.bat
+++ configs/prj-windows/for_videos/launch_dive_interface.bat
@@ -6,4 +6,6 @@ SET VIAME_INSTALL=C:\Program Files\VIAME
 REM Setup paths and run command
 CALL "%VIAME_INSTALL%\setup_viame.bat"
 
+SET DIVE_VIAME_INSTALL_PATH="%VIAME_INSTALL%"
+
 "%VIAME_INSTALL%\dive\DIVE-Desktop.exe"
