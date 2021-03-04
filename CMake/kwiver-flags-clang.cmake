66b74283cb7fc5e2148c66e1d7729f95151ad3e7
Matthew Woehlke <matthew.woehlke@kitware.com>
2021-03-04 13:59:36 -0500

    Improve CMake logic and remove some cruft
    Use CMAKE_CXX_VISIBILITY_PRESET rather than trying to specify visibility
    flags manually, as the old approach seems to not work consistently. Also
    remove no-longer-needed attempts to set C++ language support flags (not
    needed since we use CMAKE_CXX_STANDARD), and remove an unneeded call to
    cmake_minimum_required in a subfolder that was requesting a much older
    version (3.0 vs. 3.15) and was causing policy warnings.

--- CMake/kwiver-flags-clang.cmake
+++ CMake/kwiver-flags-clang.cmake
@@ -2,12 +2,10 @@
 # Compiler flags specific to use with clang++
 #
 
-kwiver_check_compiler_flag( -std=c++11 -std=c++0x )
 if(NOT APPLE)
   #MacOS produces warnings with this flag and doesn't seem to need it
   kwiver_check_compiler_flag( -pthread )
 endif()
-kwiver_check_compiler_flag( -fvisibility=hidden )
 kwiver_check_compiler_flag( -Wall )
 kwiver_check_compiler_flag( -Werror=return-type )
 kwiver_check_compiler_flag( -Werror=non-virtual-dtor )
