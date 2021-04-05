c3b9930a413cd690edbf091d943499b7e6b480dc
Eugene Borovikov <Eugene.Borovikov@kitware.com>
2021-04-05 09:19:01 -0400

    Completed the resection_camera API

---
+++ vital/algo/resection_camera.cxx
@@ -0,0 +1,79 @@
+// This file is part of KWIVER, and is distributed under the
+// OSI-approved BSD 3-Clause License. See top-level LICENSE file or
+// https://github.com/Kitware/kwiver/blob/master/LICENSE for details.
+
+/// \file
+/// \brief resection_camera instantiation
+
+#include <vital/algo/algorithm.txx>
+#include <vital/algo/resection_camera.h>
+
+/// \cond DoxygenSuppress
+INSTANTIATE_ALGORITHM_DEF( kwiver::vital::algo::resection_camera );
+/// \endcond
+
+using namespace std;
+
+namespace kwiver {
+
+namespace vital {
+
+namespace algo {
+
+// ----------------------------------------------------------------------------
+resection_camera
+::resection_camera()
+{
+  attach_logger( "algo.resection_camera" );
+}
+
+// ----------------------------------------------------------------------------
+camera_perspective_sptr
+resection_camera
+::resection( frame_id_t frame_id,
+             landmark_map_sptr landmarks,
+             feature_track_set_sptr tracks,
+             unsigned width, unsigned height ) const
+{
+  // Generate calibration guess from image dimensions.
+  auto const principal_point = vector_2d{ width * 0.5, height * 0.5 };
+  auto cal = std::make_shared< simple_camera_intrinsics >(
+    ( width + height ) * 0.5, principal_point, 1.0, 0.0,
+    Eigen::VectorXd(), width, height );
+
+  // Resection using guessed calibration.
+  return resection( frame_id, landmarks, tracks, cal );
+}
+
+// ----------------------------------------------------------------------------
+camera_perspective_sptr
+resection_camera
+::resection( frame_id_t frame_id,
+             landmark_map_sptr landmarks,
+             feature_track_set_sptr tracks,
+             kwiver::vital::camera_intrinsics_sptr cal ) const
+{
+  auto world_points = vector< vector_3d >{};
+  auto camera_points = vector< vector_2d >{};
+
+  auto const& real_landmarks = landmarks->landmarks();
+  for( auto const& fts : tracks->frame_feature_track_states(frame_id) )
+  {
+    auto lmi = real_landmarks.find( fts->track()->id() );
+    if( lmi != real_landmarks.end() )
+    {
+      world_points.emplace_back( lmi->second->loc() );
+      camera_points.emplace_back( fts->feature->loc() );
+    }
+  }
+
+  // Resection camera using point correspondences and initial calibration guess.
+  vector< bool > inliers;
+  return resection( camera_points, world_points, inliers, cal );
+}
+
+} // namespace algo
+
+} // namespace vital
+
+} // namespace kwiver
--- vital/algo/resection_camera.h
+++ vital/algo/resection_camera.h
@@ -2,73 +2,99 @@
 // OSI-approved BSD 3-Clause License. See top-level LICENSE file or
 // https://github.com/Kitware/kwiver/blob/master/LICENSE for details.
 
-/**
- * \file
- * \brief resection_camera algorithm definition
- */
+/// \file
+/// \brief resection_camera algorithm definition
 
 #ifndef VITAL_ALGO_RESECTION_CAMERA_H_
 #define VITAL_ALGO_RESECTION_CAMERA_H_
 
-#include <vital/vital_config.h>
+#include <vital/algo/algorithm.h>
+#include <vital/types/camera_perspective.h>
+#include <vital/types/feature_track_set.h>
+#include <vital/types/landmark_map.h>
 
 #include <vector>
-#include <memory>
-
-#include <vital/algo/algorithm.h>
-#include <vital/types/feature_set.h>
-#include <vital/types/match_set.h>
-#include <vital/types/camera_perspective.h>
 
 namespace kwiver {
+
 namespace vital {
+
 namespace algo {
 
-/// An abstract base class to resection a camera using 3D feature
-/// and point projection pairs.
-
+/// An abstract base class to resection a camera using 3D feature and point
+/// projection pairs.
 class VITAL_ALGO_EXPORT resection_camera
-  : public kwiver::vital::algorithm_def<resection_camera>
+  : public kwiver::vital::algorithm_def< resection_camera >
 {
 public:
-  /// Return the name of this algorithm
-  static std::string static_type_name() { return "resection_camera"; }
+  /// \return name of this algorithm
+  static std::string
+  static_type_name() { return "resection_camera"; }
 
-  /// Estimate camera parameters from 3D points and their corresponding projections
+  /// Estimate camera parameters from 3D points and their corresponding
+  /// projections.
   ///
-  /// \param [in]  pts2d 2d projections of pts3d in the same order as pts3d
-  /// \param [in]  pts3d 3d points in the same order as pts2d.  Both must be same size.
-  /// \param [in]  init_cal the initial guess intrinsic parameters of the camera
-  /// \param [out] inliers for each point, the value is true if
-  ///                      this pair is an inlier to the estimate
+  /// \param [in] pts2d 2d projections of pts3d points
+  /// \param [in] pts3d 3d points in a 1-1 correspondence with pts2d
+  /// \param [out] inliers inlier flags for the point pairs
+  /// \param [in] cal initial guess on intrinsic parameters of the camera
+  /// \return estimated camera parameters
   virtual
   kwiver::vital::camera_perspective_sptr
-  resection(std::vector<vector_2d> const& pts2d,
-            std::vector<vector_3d> const& pts3d,
-            kwiver::vital::camera_intrinsics_sptr init_cal = nullptr,
-            std::vector<bool>& inliers) const = 0;
+  resection(
+    std::vector< kwiver::vital::vector_2d > const& pts2d,
+    std::vector< kwiver::vital::vector_3d > const& pts3d,
+    std::vector< bool >& inliers,
+    kwiver::vital::camera_intrinsics_sptr cal ) const = 0;
 
-  /// Estimate camera parameters for a frame from landmarks and tracks
+  /// Estimate camera parameters for a frame from landmarks and tracks.
   ///
-  /// \param [in]  frame     frame number for which to estimate a camera
-  /// \param [in]  landmarks 3D landmarks locations to constrain camera
-  /// \param [in]  tracks    2D feature tracks in image coordinates
-  /// \param [in]  init_cal  the initial guess intrinsic parameters of the camera
+  /// This is a convenience function, callin internally
+  /// resection(pts2d, pts3d, ...) with the recoverd point correspondences.
+  ///
+  /// \param [in] frmID frame number for which to estimate a camera
+  /// \param [in] landmarks 3D landmark locations to constrain camera
+  /// \param [in] tracks 2D feature tracks in image coordinates
+  /// \param [in] width image size in the x dimension in pixels
+  /// \param [in] height image size in the y dimension in pixels
+  /// \return estimated camera parameters
   virtual
   kwiver::vital::camera_perspective_sptr
-  resection(kwiver::vital::frame_id_t const& frame,
-            kwiver::vital::landmark_map_sptr landmarks,
-            kwiver::vital::feature_track_set_sptr tracks,
-            kwiver::vital::camera_intrinsics_sptr init_cal = nullptr) const;
+  resection(
+    kwiver::vital::frame_id_t frmID,
+    kwiver::vital::landmark_map_sptr landmarks,
+    kwiver::vital::feature_track_set_sptr tracks,
+    unsigned width, unsigned height ) const;
+
+  /// Estimate camera parameters for a frame from landmarks and tracks.
+  ///
+  /// This is a convenience overload; the default implementation calls
+  /// resection(pts2d, pts3d, ...) with the recovered point correspondences.
+  ///
+  /// \param [in] frmID frame number for which to estimate a camera
+  /// \param [in] landmarks 3D landmarks locations to constrain camera
+  /// \param [in] tracks 2D feature tracks in image coordinates
+  /// \param [in] cal initial guess on intrinsic parameters of the camera
+  /// \return estimated camera parameters
+  virtual
+  kwiver::vital::camera_perspective_sptr
+  resection(
+    kwiver::vital::frame_id_t frmID,
+    kwiver::vital::landmark_map_sptr landmarks,
+    kwiver::vital::feature_track_set_sptr tracks,
+    kwiver::vital::camera_intrinsics_sptr cal ) const;
 
 protected:
   resection_camera();
-
 };
 
-/// Shared pointer type of base resection_camera algorithm definition class
-typedef std::shared_ptr<resection_camera> resection_camera_sptr;
+/// Shared pointer type of base resection_camera algorithm definition class.
+using resection_camera_sptr = std::shared_ptr< resection_camera >;
 
-} } } // end namespace
+} // namespace algo
 
-#endif // VITAL_ALGO_RESECTION_CAMERA_H_
+} // namespace vital
+
+} // namespace kwiver
+
+#endif
