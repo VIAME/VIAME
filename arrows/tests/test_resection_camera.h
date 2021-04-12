e4a51f417cacef976681674c55a999287783a27f
Matthew Woehlke <matthew.woehlke@kitware.com>
2021-04-12 16:02:52 -0400

    Overhaul resection_camera test
    Refactor the resection_camera test to ruthlessly eliminate duplicated
    code, and to use the existing helper function to subset the test track
    set.

--- arrows/tests/test_resection_camera.h
+++ arrows/tests/test_resection_camera.h
@@ -11,166 +11,135 @@
 
 using namespace kwiver::vital;
 using namespace kwiver::arrows::mvg;
-using namespace std;
 
 // ----------------------------------------------------------------------------
-// Test camera resection with ideal points.
+// Helper to test camera resection
+template < typename... Args >
+void
+test_resection_camera(
+  landmark_map_sptr const& landmarks, feature_track_set_sptr const& tracks,
+  frame_id_t test_frame, camera_perspective_sptr const& expected_camera,
+  double center_tolerance, double rotation_tolerance, Args... args )
+{
+  // Compute camera pose from the points and their projections
+  resection_camera algo;
+  vector< bool > inliers;
+  auto const estimated_camera =
+    algo.resection( test_frame, landmarks, tracks, args... );
+
+  ASSERT_NE( nullptr, estimated_camera );
+
+  // True and computed camera poses
+  auto const& expected_rotation = expected_camera->rotation();
+  auto const& estimated_rotation = estimated_camera->rotation();
+  std::cout
+    << "expected center:\n" << expected_camera->center().transpose() << '\n'
+    << "estimated center:\n" << estimated_camera->center().transpose() << '\n'
+    << "expected rotation:\n" << expected_rotation.matrix() << '\n'
+    << "estimated rotation:\n" << estimated_rotation.matrix() << '\n';
+
+  auto rotation_error = expected_rotation.inverse() * estimated_rotation;
+  std::cout << "rotation error = "
+            << rad_to_deg * rotation_error.angle() << " degrees" << std::endl;
+
+  EXPECT_LT( rotation_error.angle(), rotation_tolerance );
+  EXPECT_MATRIX_SIMILAR( expected_camera->center(),
+                         estimated_camera->center(), center_tolerance );
+}
+
+// ----------------------------------------------------------------------------
+template < typename Func >
+void
+test_resection_cameras(
+  Func const& func, camera_map_sptr const& camera_map )
+{
+  for( auto const& camera_iter : camera_map->cameras() )
+  {
+    auto const test_frame = camera_iter.first;
+    auto const& camera =
+      dynamic_pointer_cast< camera_perspective >( camera_iter.second );
+
+    SCOPED_TRACE( "At frame " + std::to_string( test_frame ) );
+    ASSERT_NE( nullptr, camera );
+
+    func( test_frame, camera );
+  }
+}
+
+// ----------------------------------------------------------------------------
+// Test camera resection with ideal points
 TEST ( resection_camera, ideal_points )
 {
   // Landmarks at random locations
-  landmark_map_sptr landmarks = kwiver::testing::init_landmarks( 128 );
+  auto landmarks = kwiver::testing::init_landmarks( 128 );
   landmarks = kwiver::testing::noisy_landmarks( landmarks, 1.0 );
 
-  // camera sequence (elliptical path)
-  camera_map_sptr cameras = kwiver::testing::camera_seq();
+  // Camera sequence (elliptical path)
+  auto camera_map = kwiver::testing::camera_seq();
 
-  // tracks from projections
-  track_set_sptr tracks = projected_tracks( landmarks, cameras );
+  // Tracks from projections
+  auto tracks = projected_tracks( landmarks, camera_map );
 
-  constexpr frame_id_t test_frame = 0;
+  // Do the test
+  test_resection_cameras(
+    [ & ]( frame_id_t test_frame, camera_perspective_sptr const& camera ){
+      auto inliers = std::unordered_set< landmark_id_t >{};
+      test_resection_camera( landmarks, tracks, test_frame, camera,
+                             ideal_center_tolerance, ideal_rotation_tolerance,
+                             camera->intrinsics(), &inliers );
 
-  // corresponding image points
-  vector< vector_2d > pts_projs;
-  vector< vector_3d > pts_3d;
-  for( auto const& track : tracks->tracks() )
-  {
-    auto lm_id = track->id();
-    auto lm_it = landmarks->landmarks().find( lm_id );
-    pts_3d.push_back( lm_it->second->loc() );
-
-    auto const fts =
-      dynamic_pointer_cast< feature_track_state >( *track->find( test_frame ) );
-    pts_projs.push_back( fts->feature->loc() );
-  }
-
-  // camera pose from the points and their projections
-  resection_camera res_cam;
-  auto cams = cameras->cameras();
-  auto cam = dynamic_pointer_cast< camera_perspective >( cams[ test_frame ] );
-  vector< bool > inliers;
-  auto est_cam = res_cam.resection( pts_projs, pts_3d, cam->intrinsics(),
-                                    &inliers );
-
-  // true and computed camera poses
-  auto const& camR = cam->rotation();
-  auto const& estR = est_cam->rotation();
-  cout << "cam R:\n" << camR.matrix() << endl <<
-    "est R:\n" << estR.matrix() << endl <<
-    "cam C = " << cam->center().transpose() << endl <<
-    "est C = " << est_cam->center().transpose() << endl;
-
-  auto R_err = camR.inverse() * estR;
-  cout << "rotation error = " << rad_to_deg * R_err.angle() << " degrees" <<
-    endl;
-
-  EXPECT_LT( R_err.angle(), ideal_rotation_tolerance );
-  EXPECT_MATRIX_SIMILAR( cam->center(),
-                         est_cam->center(), ideal_center_tolerance );
-
-  auto inlier_cnt = count( inliers.begin(), inliers.end(), true );
-  cout << "inlier count = " << inlier_cnt << endl;
-  EXPECT_EQ( pts_projs.size(), inlier_cnt ) << "all points should be inliers";
+      std::cout << "inlier count = " << inliers.size() << std::endl;
+      EXPECT_EQ( landmarks->size(), inliers.size() )
+        << "all points should be inliers";
+    }, camera_map );
 }
 
 // ----------------------------------------------------------------------------
-// Test camera resection with noisy points.
-TEST ( resection_camera, noisy_points )
+// Test camera resection with noisy points
+TEST ( resection_camera, noisy_points_with_image_size )
 {
-  // landmarks at random locations
-  landmark_map_sptr landmarks = kwiver::testing::init_landmarks( 128 );
+  // Landmarks at random locations
+  auto landmarks = kwiver::testing::init_landmarks( 128 );
   landmarks = kwiver::testing::noisy_landmarks( landmarks, 1.0 );
 
-  // camera sequence (elliptical path)
-  camera_map_sptr cameras = kwiver::testing::camera_seq();
+  // Camera sequence (elliptical path)
+  auto camera_map = kwiver::testing::camera_seq();
 
-  // tracks from the projections
-  auto tracks = projected_tracks( landmarks, cameras );
-
-  // add some random noise to the track image locations
+  // Tracks from the projections with some random noise added
+  auto tracks = projected_tracks( landmarks, camera_map );
   tracks = kwiver::testing::noisy_tracks( tracks, 0.5 );
 
-  const frame_id_t frmID = 1;
-
-  auto cams = cameras->cameras();
-  auto cam = dynamic_pointer_cast< camera_perspective >( cams[ frmID ] );
-  resection_camera res_cam;
-  auto est_cam = res_cam.resection( frmID, landmarks, tracks,
-                                    cam->image_width(), cam->image_height() );
-  EXPECT_NE( est_cam, nullptr ) << "noisy resection camera failed";
-
-  // true and computed camera poses
-  const auto
-  & camR = cam->rotation(),
-  & estR = est_cam->rotation();
-  cout << "cam R:\n" << camR.matrix() << endl <<
-    "est R:\n" << estR.matrix() << endl <<
-    "cam C = " << cam->center().transpose() << endl <<
-    "est C = " << est_cam->center().transpose() << endl;
-
-  auto R_err = camR.inverse() * estR;
-  cout << "rotation error = " << R_err.angle() * 180 / pi << " degrees" <<
-    endl;
-
-  EXPECT_LT( R_err.angle(), noisy_rotation_tolerance );
-  EXPECT_MATRIX_SIMILAR( cam->center(),
-                         est_cam->center(), noisy_center_tolerance );
+  // Do the test
+  test_resection_cameras(
+    [ & ]( frame_id_t test_frame, camera_perspective_sptr const& camera ){
+      test_resection_camera( landmarks, tracks, test_frame, camera,
+                             noisy_center_tolerance, noisy_rotation_tolerance,
+                             camera->image_width(), camera->image_height() );
+    }, camera_map );
 }
 
 // ----------------------------------------------------------------------------
-// Test camera resection with noisy points, missing tracks and initial
-// calibration guess.
-TEST ( resection_camera, noisy_points_cal )
+// Test camera resection with noisy points, missing tracks, and initial
+// calibration guess
+TEST ( resection_camera, noisy_points_with_initial_calibration )
 {
-  // landmarks at random locations
-  landmark_map_sptr landmarks = kwiver::testing::init_landmarks( 128 );
+  // Landmarks at random locations
+  auto landmarks = kwiver::testing::init_landmarks( 128 );
   landmarks = kwiver::testing::noisy_landmarks( landmarks, 1.0 );
 
-  // camera sequence (elliptical path)
-  camera_map_sptr cameras = kwiver::testing::camera_seq();
+  // Camera sequence (elliptical path)
+  auto camera_map = kwiver::testing::camera_seq();
 
-  // tracks from the projections
-  auto tracks = projected_tracks( landmarks, cameras );
+  // Sparse tracks from the projections with some random noise added
+  auto tracks = projected_tracks( landmarks, camera_map );
+  tracks = kwiver::testing::noisy_tracks( tracks, 0.5 );
+  tracks = kwiver::testing::subset_tracks( tracks, 0.2 );
 
-  // drop some tracks
-  const unsigned track_remove_n = 10;
-  unsigned ndx = 0;
-  feature_track_set_sptr trks( new feature_track_set() );
-  for( auto const& track : tracks->tracks() )
-  {
-    if( ndx++ % track_remove_n ) { continue; }
-    trks->insert( track );
-  }
-
-  // add random noise to track image locations
-  tracks = kwiver::testing::noisy_tracks( trks, 0.5 );
-
-  const frame_id_t frmID = 2;
-
-  // resection camera using 3D landmarks, 2D feature tracks and a camera
-  // intrinsics guess
-  resection_camera res_cam;
-  auto cams = cameras->cameras();
-  auto cam = dynamic_pointer_cast< camera_perspective >( cams[ frmID ] );
-  auto est_cam =
-    res_cam.resection( frmID, landmarks, tracks, cam->intrinsics() );
-  EXPECT_NE( est_cam,
-             nullptr ) <<
-    "null resection camera on noisy points, missing tracks with initial cal guess";
-
-  // true and computed camera poses
-  const auto
-  & camR = cam->rotation(),
-  & estR = est_cam->rotation();
-  cout << "cam R:\n" << camR.matrix() << endl <<
-    "est R:\n" << estR.matrix() << endl <<
-    "cam C = " << cam->center().transpose() << endl <<
-    "est C = " << est_cam->center().transpose() << endl;
-
-  auto R_err = camR.inverse() * estR;
-  cout << "rotation error = " << R_err.angle() * 180 / pi << " degrees" <<
-    endl;
-
-  EXPECT_LT( R_err.angle(), noisy_rotation_tolerance );
-  EXPECT_MATRIX_SIMILAR( cam->center(),
-                         est_cam->center(), noisy_center_tolerance );
+  // Do the test
+  test_resection_cameras(
+    [ & ]( frame_id_t test_frame, camera_perspective_sptr const& camera ){
+      test_resection_camera( landmarks, tracks, test_frame, camera,
+                             noisy_center_tolerance, noisy_rotation_tolerance,
+                             camera->intrinsics() );
+    }, camera_map );
 }
