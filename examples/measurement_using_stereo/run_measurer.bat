76fc37c9d6d0cad0dd610d5e391bba3d6d90299b
Matt Dawkins <matt.dawkins@kitware.com>
2021-05-23 21:11:30 -0400

    Update path

--- examples/measurement_using_stereo/run_measurer.bat
+++ examples/measurement_using_stereo/run_measurer.bat
@@ -12,6 +12,6 @@ SET SPROKIT_PYTHON_MODULES=kwiver.processes;viame.processes;camtrawl_processes
 
 REM Run Pipeline
 
-kwiver.exe runner "%VIAME_INSTALL%\configs\pipelines\measurement.gmm_only.pipe"
+kwiver.exe runner "%VIAME_INSTALL%\configs\pipelines\measurement_gmm_only.pipe"
 
 pause
