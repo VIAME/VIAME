eb4d447e5e763d560cdd0e65ca1102638179b2f1
Matt Dawkins <matt.dawkins@kitware.com>
2021-06-23 19:21:41 -0400

    Update link

--- cmake/download_viame_addons.sh
+++ cmake/download_viame_addons.sh
@@ -10,7 +10,7 @@ mkdir -p ${DOWNLOAD_LOCATION}
 # Download All Optional Packages
 
 # Habcam -
-wget -O ${DOWNLOAD_LOCATION}/download1.zip https://viame.kitware.com/api/v1/item/60c6d1b4c127e91034371726/download
+wget -O ${DOWNLOAD_LOCATION}/download1.zip https://viame.kitware.com/api/v1/item/60d3c198b91def413908961a/download
 unzip -o ${DOWNLOAD_LOCATION}/download1.zip -d ${VIAME_INSTALL}
 
 # SEFSC -
