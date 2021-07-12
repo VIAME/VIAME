bd56f5afc2bddfa08be5909dd3d05b6cf41cd496
Matt Dawkins <matt.dawkins@kitware.com>
2021-07-12 01:51:35 -0400

    Update hash

--- cmake/add_project_dive.cmake
+++ cmake/add_project_dive.cmake
@@ -6,14 +6,14 @@ file( MAKE_DIRECTORY ${VIAME_DIVE_BUILD_DIR} )
 
 if( WIN32 )
   DownloadAndExtract(
-    https://github.com/Kitware/dive/releases/download/1.4.7/DIVE-Desktop-1.4.7.zip
-    b0976ecaf63b601097b54912c2f6f7e3
+    https://github.com/Kitware/dive/releases/download/1.4.9/DIVE-Desktop-1.4.9.zip
+    e78a1dd85ffd2cde6b00b164e10e7779
     ${VIAME_DOWNLOAD_DIR}/dive_interface_binaries.zip
     ${VIAME_DIVE_BUILD_DIR} )
 elseif( UNIX )
   DownloadAndExtract(
-    https://github.com/Kitware/dive/releases/download/1.4.7/DIVE-Desktop-1.4.7.tar.gz
-    361f04d5f8d1a8da9c27ffbf97bb5be1
+    https://github.com/Kitware/dive/releases/download/1.4.9/DIVE-Desktop-1.4.9.tar.gz
+    4c3415b4e75f44093927fb777efd475e
     ${VIAME_DOWNLOAD_DIR}/dive_interface_binaries.tar.gz
     ${VIAME_DIVE_BUILD_DIR} )
 endif()
