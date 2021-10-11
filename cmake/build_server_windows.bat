67c51550b5659af6e923af863e2d45c8a5bb6bd3
Matt Dawkins <matt.dawkins@kitware.com>
2021-10-10 23:16:15 -0400

    Update script

--- cmake/build_server_windows.bat
+++ cmake/build_server_windows.bat
@@ -26,12 +26,11 @@ REM ---------------------------------------------------
 SET GPU_ADD_ON_PACKAGE=C:\tmp\VIAME-Windows-GPU.zip
 SET MISSING_SVM_DLL=%VIAME_SOURCE_DIR%\packages\smqtk\TPL\libsvm-3.1-custom\libsvm.dll
 
-move "%VIAME_BUILD_DIR%\install" "%VIAME_BUILD_DIR%\VIAME"
+MOVE "%VIAME_BUILD_DIR%\install" "%VIAME_BUILD_DIR%\VIAME"
 "C:\Program Files\7-Zip\7z.exe" x -aoa %GPU_ADD_ON_PACKAGE% -o%VIAME_BUILD_DIR%
-move %MISSING_SVM_DLL% %VIAME_BUILD_DIR%\VIAME\bin
+MOVE %MISSING_SVM_DLL% %VIAME_BUILD_DIR%\VIAME\bin
 
-copy /y "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Team Tools\Performance Tools\msvcr120.dll" %VIAME_BUILD_DIR%\VIAME\bin
-copy /y %VIAME_SOURCE_DIR%\cmake\setup_viame.bat.install %VIAME_BUILD_DIR%\VIAME\setup_viame.bat
+COPY /y %VIAME_SOURCE_DIR%\cmake\setup_viame.bat.install %VIAME_BUILD_DIR%\VIAME\setup_viame.bat
 
 REM ---------------------------------------------------
 REM COMPRESS FINAL PACKAGE
