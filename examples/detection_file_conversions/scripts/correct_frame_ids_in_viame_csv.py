f589a88d7791c393b1f5310209a00c67b7940f67
Yuval <yuval@yuvalboss.com>
2021-10-13 15:50:31 -0700

    Should just leave as was and increment

--- examples/detection_file_conversions/scripts/correct_frame_ids_in_viame_csv.py
+++ examples/detection_file_conversions/scripts/correct_frame_ids_in_viame_csv.py
@@ -48,7 +48,7 @@ def read_records(path):
 
                 classifiers = row[len(field_types):]
                 while len(classifiers) >= 2:
-                    record += classifiers[:2]
+                    record += [classifiers[0], float(classifiers[1])]
                     classifiers = classifiers[2:]
 
                 if len(classifiers):
