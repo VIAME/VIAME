2173dfc6a188eecd2bacfc2a31145bde4852e84e
JeromeDias <jerome.dias@kitware.com>
2022-03-17 14:14:33 +0100

    [feat] Add stereo depth map pipe and update example

---
+++ examples/object_tracking_using_stereo/compute_depth_maps.bat
@@ -0,0 +1,18 @@
+@echo off
+
+REM Setup VIAME Paths (no need to set if installed to registry or already set up)
+SET VIAME_INSTALL=.\..\..
+
+REM Processing options
+SET OUTPUT_DIRECTORY=output
+
+CALL "%VIAME_INSTALL%\setup_viame.bat"
+
+REM Run Pipeline
+
+kwiver.exe runner "%VIAME_INSTALL%\configs\pipelines\measurement_gmm_stereo_depth_map.pipe" ^
+                  -s input1:video_filename=left_images.txt ^
+                  -s input2:video_filename=right_images.txt ^
+                  -o %OUTPUT_DIRECTORY% --no-reset-prompt
+
+pause
