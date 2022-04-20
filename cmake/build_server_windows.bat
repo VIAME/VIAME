ddbf37437c76b005dd96d1171426bffa0d3b359b
Matt Dawkins <matt.dawkins@kitware.com>
2022-04-20 13:47:23 -0400

    More windows install rules

--- cmake/build_server_windows.bat
+++ cmake/build_server_windows.bat
@@ -38,6 +38,30 @@ MOVE %MISSING_DNET_EXE% %VIAME_BUILD_DIR%\VIAME\bin
 
 DEL "%VIAME_BUILD_DIR%\VIAME\lib\python3.8\site-packages\torch\lib\cu*"
 
+COPY "%CUDA_ROOT%\bin\cublas64_11.dll" %VIAME_BUILD_DIR%\VIAME\bin
+COPY "%CUDA_ROOT%\bin\cublasLt64_11.dll" %VIAME_BUILD_DIR%\VIAME\bin
+COPY "%CUDA_ROOT%\bin\cudart64_110.dll" %VIAME_BUILD_DIR%\VIAME\bin
+COPY "%CUDA_ROOT%\bin\cudnn_adv_infer64_8.dll" %VIAME_BUILD_DIR%\VIAME\bin
+COPY "%CUDA_ROOT%\bin\cudnn_adv_train64_8.dll" %VIAME_BUILD_DIR%\VIAME\bin
+COPY "%CUDA_ROOT%\bin\cudnn_cnn_infer64_8.dll" %VIAME_BUILD_DIR%\VIAME\bin
+COPY "%CUDA_ROOT%\bin\cudnn_cnn_train64_8.dll" %VIAME_BUILD_DIR%\VIAME\bin
+COPY "%CUDA_ROOT%\bin\cudnn_ops_infer64_8.dll" %VIAME_BUILD_DIR%\VIAME\bin
+COPY "%CUDA_ROOT%\bin\cudnn_ops_train64_8.dll" %VIAME_BUILD_DIR%\VIAME\bin
+COPY "%CUDA_ROOT%\bin\cudnn64_8.dll" %VIAME_BUILD_DIR%\VIAME\bin
+COPY "%CUDA_ROOT%\bin\cufft64_10.dll" %VIAME_BUILD_DIR%\VIAME\bin
+COPY "%CUDA_ROOT%\bin\cufftw64_10.dll" %VIAME_BUILD_DIR%\VIAME\bin
+COPY "%CUDA_ROOT%\bin\curand64_10.dll" %VIAME_BUILD_DIR%\VIAME\bin
+COPY "%CUDA_ROOT%\bin\cusolver64_11.dll" %VIAME_BUILD_DIR%\VIAME\bin
+COPY "%CUDA_ROOT%\bin\cusolverMg64_11.dll" %VIAME_BUILD_DIR%\VIAME\bin
+COPY "%CUDA_ROOT%\bin\cusparse64_11.dll" %VIAME_BUILD_DIR%\VIAME\bin
+
+COPY %WIN32_ROOT%\msvcr100.dll %VIAME_BUILD_DIR%\VIAME\bin
+COPY %WIN32_ROOT%\vcruntime140_1.dll %VIAME_BUILD_DIR%\VIAME\bin
+COPY C:\Windows\SysWOW64\msvcr120.dll %VIAME_BUILD_DIR%\VIAME\bin
+COPY %VIAME_SOURCE_DIR%\packages\darknet\3rdparty\pthreads\pthreadVC2.dll %VIAME_BUILD_DIR%\VIAME\bin
+
+XCOPY /E /I "%VIAME_SOURCE_DIR%\packages\patches\pycocotools %VIAME_BUILD_DIR%\VIAME\lib\python3.8\site-packages
+
 COPY /y %VIAME_SOURCE_DIR%\cmake\setup_viame.bat.install %VIAME_BUILD_DIR%\VIAME\setup_viame.bat
 
 REM ------------------------------------------------------------------------------------------------------------------------------------------------------------
