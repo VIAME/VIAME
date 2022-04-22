4438165dfc9d8e934472d68fd02bc19d952baea2
Matt Dawkins <matt.dawkins@kitware.com>
2022-04-22 10:14:37 -0400

    Update version

--- cmake/build_server_centos.sh
+++ cmake/build_server_centos.sh
@@ -44,14 +44,14 @@ yum -y install devtoolset-7
 source /opt/rh/devtoolset-7/enable
 
 # Install CMAKE
-wget https://cmake.org/files/v3.17/cmake-3.17.0.tar.gz
+wget https://cmake.org/files/v3.23/cmake-3.23.1.tar.gz
 tar zxvf cmake-3.*
-cd cmake-3.17.0
+cd cmake-3.23.1
 ./bootstrap --prefix=/usr/local --system-curl
 make -j$(nproc)
 make install
 cd /
-rm -rf cmake-3.17.0.tar.gz
+rm -rf cmake-3.23.1.tar.gz
 
 # Update VIAME sub git sources
 cd /viame/
