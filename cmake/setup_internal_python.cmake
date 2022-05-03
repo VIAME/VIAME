e61a8643c53c9de2b162c201c80b09c6fa79ef32
Matt Dawkins <matt.dawkins@kitware.com>
2022-05-03 09:50:19 -0400

    Add switch for internal python

--- cmake/setup_internal_python.cmake
+++ cmake/setup_internal_python.cmake
@@ -1,10 +1,16 @@
 
-set( Python_FOUND TRUE CACHE INTERNAL "Forced" FORCE )
+set( VIAME_PYTHON_VERSION 3.6.15 CACHE STRING "Select the version of Python to build." )
+set_property( CACHE VIAME_PYTHON_VERSION PROPERTY STRINGS "3.6.15" "3.8.13" "3.10.4" )
+mark_as_advanced( VIAME_PYTHON_VERSION )
 
-set( Python_VERSION_MAJOR "3"  CACHE INTERNAL "Forced" FORCE )
-set( Python_VERSION_MINOR "6"  CACHE INTERNAL "Forced" FORCE )
-set( Python_VERSION_PATCH "15" CACHE INTERNAL "Forced" FORCE )
-set( Python_SOABI         ""   CACHE INTERNAL "Forced" FORCE )
+set( Python_VERSION ${VIAME_PYTHON_VERSION} CACHE INTERNAL "Forced" FORCE )
+string( REPLACE "." ";" Python_VERSION_LIST ${Python_VERSION} )
+list( GET Python_VERSION_LIST 0 Python_VERSION_MAJOR )
+list( GET Python_VERSION_LIST 1 Python_VERSION_MINOR )
+list( GET Python_VERSION_LIST 2 Python_VERSION_PATCH )
+
+set( Python_FOUND TRUE CACHE INTERNAL "Forced" FORCE )
+set( Python_SOABI ""   CACHE INTERNAL "Forced" FORCE )
 
 set( Python_INCLUDE_DIR ${VIAME_INSTALL_PREFIX}/include CACHE PATH "Forced" FORCE )
 
