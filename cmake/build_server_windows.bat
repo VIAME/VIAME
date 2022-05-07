32c466a511867988434990652d5118dbabb8b232
Matthew Dawkins <matt.dawkins@kitware.com>
2022-05-07 08:30:21 -0400

    Update build_server_windows.bat

--- cmake/build_server_windows.bat
+++ cmake/build_server_windows.bat
@@ -13,11 +13,11 @@ IF EXIST C:\tmp\vm1 rmdir /s /q C:\tmp\vm1
 
 SET "CUDA_ROOT=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.3"
 SET "WIN32_ROOT=C:\Windows\System32"
+Set "PYTHON_SUBDIR=lib\python3.6"
 SET "PATH=%WIN32_ROOT%;C:\Windows;%WIN32_ROOT%\Wbem;%WIN32_ROOT%\WindowsPowerShell\v1.0;%WIN32_ROOT%\OpenSSH"
 SET "PATH=%CUDA_ROOT%\bin;%CUDA_ROOT%\libnvvp;C:\Program Files (x86)\NVIDIA Corporation\PhysX\Common;C:\Program Files\NVIDIA Corporation\NVIDIA NvDLISR;%PATH%"
 SET "PATH=C:\Program Files\Git\cmd;C:\Program Files\CMake\bin;%PATH%"
-
-SET "PYTHONPATH=%VIAME_BUILD_DIR%\install\lib\python3.6;%VIAME_BUILD_DIR%\install\lib\python3.6\site-packages"
+SET "PYTHONPATH=%VIAME_BUILD_DIR%\install\%PYTHON_SUBDIR;%VIAME_BUILD_DIR%\install\%PYTHON_SUBDIR%\site-packages"
 
 git submodule update --init --recursive
 
@@ -42,7 +42,7 @@ COPY %WIN32_ROOT%\vcruntime140_1.dll %VIAME_BUILD_DIR%\VIAME\bin
 COPY C:\Windows\SysWOW64\msvcr120.dll %VIAME_BUILD_DIR%\VIAME\bin
 COPY %VIAME_SOURCE_DIR%\packages\darknet\3rdparty\pthreads\bin\pthreadVC2.dll %VIAME_BUILD_DIR%\VIAME\bin
 
-DEL "%VIAME_BUILD_DIR%\VIAME\lib\python3.8\site-packages\torch\lib\cu*"
+DEL "%VIAME_BUILD_DIR%\VIAME\%PYTHON_SUBDIR%\site-packages\torch\lib\cu*"
 
 COPY "%CUDA_ROOT%\bin\cublas64_11.dll" %VIAME_BUILD_DIR%\VIAME\bin
 COPY "%CUDA_ROOT%\bin\cublasLt64_11.dll" %VIAME_BUILD_DIR%\VIAME\bin
