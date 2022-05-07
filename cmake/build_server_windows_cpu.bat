f2dcbd7ae439605eb93571afc1564031fc816609
Matthew Dawkins <matt.dawkins@kitware.com>
2022-05-07 08:37:02 -0400

    Update build_server_windows_cpu.bat

--- cmake/build_server_windows_cpu.bat
+++ cmake/build_server_windows_cpu.bat
@@ -4,6 +4,7 @@ REM ----------------------------------------------------------------------------
 
 SET VIAME_SOURCE_DIR=C:\workspace\VIAME-Windows-CPU-Release
 SET VIAME_BUILD_DIR=%VIAME_SOURCE_DIR%\build
+SET VIAME_INSTALL_DIR=%VIAME_BUILD_DIR%\install
 
 IF EXIST build rmdir /s /q build
 
@@ -16,7 +17,7 @@ SET "PYTHON_SUBDIR=lib\python3.6"
 SET "PATH=%WIN32_ROOT%;C:\Windows;%WIN32_ROOT%\Wbem;%WIN32_ROOT%\WindowsPowerShell\v1.0;%WIN32_ROOT%\OpenSSH"
 SET "PATH=C:\Program Files\Git\cmd;C:\Program Files\CMake\bin;%PATH%"
 
-SET "PYTHONPATH=%VIAME_BUILD_DIR%\install\%PYTHON_SUBDIR%;%VIAME_BUILD_DIR%\install\%PYTHON_SUBDIR%\site-packages"
+SET "PYTHONPATH=%VIAME_INSTALL_DIR%\%PYTHON_SUBDIR%;%VIAME_INSTALL_DIR%\%PYTHON_SUBDIR%\site-packages"
 
 git submodule update --init --recursive
 
@@ -26,24 +27,24 @@ REM ----------------------------------------------------------------------------
 REM HACKS UNTIL THESE THINGS ARE BETTER HANDLED IN CODE
 REM -------------------------------------------------------------------------------------------------------
 
-MOVE "%VIAME_BUILD_DIR%\install" "%VIAME_BUILD_DIR%\VIAME"
-
 SET MISSING_SVM_DLL=%VIAME_SOURCE_DIR%\packages\smqtk\TPL\libsvm-3.1-custom\libsvm.dll
 SET MISSING_DNET_EXE=%VIAME_BUILD_DIR%\build\src\darknet-build\Release\darknet.exe
 
-MOVE %MISSING_SVM_DLL% %VIAME_BUILD_DIR%\VIAME\bin
-MOVE %MISSING_DNET_EXE% %VIAME_BUILD_DIR%\VIAME\bin
+MOVE %MISSING_SVM_DLL% %VIAME_INSTALL_DIR%\bin
+MOVE %MISSING_DNET_EXE% %VIAME_INSTALL_DIR%\bin
 
-COPY /y "%VIAME_SOURCE_DIR%\cmake\setup_viame.bat.install" %VIAME_BUILD_DIR%\VIAME\setup_viame.bat
+COPY /y "%VIAME_SOURCE_DIR%\cmake\setup_viame.bat.install" %VIAME_INSTALL_DIR%\setup_viame.bat
 
-COPY %WIN32_ROOT%\msvcr100.dll %VIAME_BUILD_DIR%\VIAME\bin
-COPY %WIN32_ROOT%\vcruntime140_1.dll %VIAME_BUILD_DIR%\VIAME\bin
-COPY C:\Windows\SysWOW64\msvcr120.dll %VIAME_BUILD_DIR%\VIAME\bin
-COPY %VIAME_SOURCE_DIR%\packages\darknet\3rdparty\pthreads\bin\pthreadVC2.dll %VIAME_BUILD_DIR%\VIAME\bin
+COPY %WIN32_ROOT%\msvcr100.dll %VIAME_INSTALL_DIR%\bin
+COPY %WIN32_ROOT%\vcruntime140_1.dll %VIAME_INSTALL_DIR%\bin
+COPY C:\Windows\SysWOW64\msvcr120.dll %VIAME_INSTALL_DIR%\bin
+COPY %VIAME_SOURCE_DIR%\packages\darknet\3rdparty\pthreads\bin\pthreadVC2.dll %VIAME_INSTALL_DIR%\bin
 
 REM -------------------------------------------------------------------------------------------------------
 REM COMPRESS FINAL PACKAGE
 REM -------------------------------------------------------------------------------------------------------
+
+MOVE "%VIAME_BUILD_DIR%\install" "%VIAME_BUILD_DIR%\VIAME"
 
 "C:\Program Files\7-Zip\7z.exe" a "%VIAME_BUILD_DIR%/VIAME-v1.0.0-Windows-64Bit-CPU.zip" "%VIAME_BUILD_DIR%/VIAME
 
