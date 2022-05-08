dc1fc75b1115ad723d3a1942c7ec4da619e81c2b
Matthew Dawkins <matt.dawkins@kitware.com>
2022-05-08 14:17:29 -0400

    Update build_server_centos.sh

--- cmake/build_server_centos.sh
+++ cmake/build_server_centos.sh
@@ -62,8 +62,8 @@ cd build
 # Configure Paths [should be removed when no longer necessary by fletch]
 export PATH=$PATH:/viame/build/install/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/viame/build/install/lib:/viame/build/install/lib/python3.6
-export C_INCLUDE_PATH=$C_INCLUDE_PATH:/viame/build/install/include/python3.6
-export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:/viame/build/install/include/python3.6
+export C_INCLUDE_PATH=$C_INCLUDE_PATH:/viame/build/install/include/python3.6m
+export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:/viame/build/install/include/python3.6m
 
 # Configure VIAME
 cmake ../ -DCMAKE_BUILD_TYPE:STRING=Release \
