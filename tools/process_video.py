af15662b45afb51e3bf346730a22dad0905d5e68
Matt Dawkins <matt.dawkins@kitware.com>
2022-05-23 22:47:21 -0400

    Fix string

--- tools/process_video.py
+++ tools/process_video.py
@@ -815,7 +815,7 @@ if __name__ == "__main__" :
       if os.path.isfile( args.input ):
         textchars = bytearray( {7,8,9,10,12,13,27} | set( range(0x20, 0x100) ) - {0x7f} )
         is_binary_string = lambda bytes: bool( bytes.translate( None, textchars ) )
-        if is_binary_string( open( args.input, 'rb' ).read( 1024 ) )
+        if is_binary_string( open( args.input, 'rb' ).read( 1024 ) ):
           args.input_video = args.input
         else:
           args.input_list = args.input
