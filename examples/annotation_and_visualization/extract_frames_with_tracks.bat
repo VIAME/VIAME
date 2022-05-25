583a444ba43f22d79a54f94c746dc71e1da6e1d8
Matt Dawkins <matt.dawkins@kitware.com>
2022-05-25 11:05:45 -0400

    Clarify setting

--- examples/annotation_and_visualization/extract_frames_with_tracks.bat
+++ examples/annotation_and_visualization/extract_frames_with_tracks.bat
@@ -3,16 +3,19 @@
 REM Path to VIAME installation
 SET VIAME_INSTALL=C:\Program Files\VIAME
 
-REM Processing options
+REM Input and output folders
 SET INPUT=videos
 SET OUTPUT=frames
-SET FRAME_RATE=5
+
+REM The default frame rate is only used when the csvs alongside videos
+REM do not contain frame rates, otherwise the CSV frame rate is used.
+SET DEFAULT_FRAME_RATE=5
 
 REM Setup paths and run command
 CALL "%VIAME_INSTALL%\setup_viame.bat"
 
 python.exe "%VIAME_INSTALL%\configs\process_video.py" ^
-  -i "%INPUT%" -o %OUTPUT% -frate %FRAME_RATE% ^
+  -i "%INPUT%" -o %OUTPUT% -frate %DEFAULT_FRAME_RATE% ^
   -p "pipelines/filter_tracks_only.pipe" ^
   -auto-detect-gt viame_csv
 
--- examples/annotation_and_visualization/extract_frames_with_tracks.sh
+++ examples/annotation_and_visualization/extract_frames_with_tracks.sh
@@ -3,15 +3,18 @@
 # Path to VIAME installation
 export VIAME_INSTALL=/home/matt/Dev/viame/build/install
 
-# Core processing options
+# Input and output folders
 export INPUT=videos
 export OUTPUT=frames
-export FRAME_RATE=5
+
+# The default frame rate is only used when the csvs alongside videos
+# do not contain frame rates, otherwise the CSV frame rate is used.
+export DEFAULT_FRAME_RATE=5
 
 # Setup paths and run command
 source ${VIAME_INSTALL}/setup_viame.sh
 
 python ${VIAME_INSTALL}/configs/process_video.py \
-  -i ${INPUT} -o ${OUTPUT} -frate ${FRAME_RATE} \
+  -i ${INPUT} -o ${OUTPUT} -frate ${DEFAULT_FRAME_RATE} \
   -p "pipelines/filter_tracks_only.pipe" \
   -auto-detect-gt viame_csv
