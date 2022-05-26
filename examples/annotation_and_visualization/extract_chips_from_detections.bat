c86fbff314ac83da3c677d47a8ed0ac2b1418bbe
Matt Dawkins <matt.dawkins@kitware.com>
2022-05-25 20:53:23 -0400

    Adjust script

--- examples/annotation_and_visualization/extract_chips_from_detections.bat
+++ examples/annotation_and_visualization/extract_chips_from_detections.bat
@@ -1,11 +1,22 @@
 @echo off
 
-REM Setup VIAME Paths (no need to set if installed to registry or already set up)
-
+REM Location of VIAME install
 SET VIAME_INSTALL=.\..\..
 
+REM Processing options
+SET INPUT=videos
+SET OUTPUT=output
+SET PIPELINE=pipelines\utility_extract_chips.pipe
+
+REM The default frame rate is only used when the csvs alongside videos
+REM do not contain frame rates, otherwise the CSV frame rate is used.
+SET DEFAULT_FRAME_RATE=5
+
+REM Setup paths and run command
 CALL "%VIAME_INSTALL%\setup_viame.bat"
 
-REM Run Pipeline
+python.exe "%VIAME_INSTALL%\configs\process_video.py" ^
+  -i "%INPUT%" -o %OUTPUT% -frate %DEFAULT_FRAME_RATE% ^
+  -p %PIPELINE% -auto-detect-gt viame_csv
 
-kwiver.exe runner "%VIAME_INSTALL%\configs\pipelines\utility_extract_chips.pipe"
+pause
--- examples/annotation_and_visualization/extract_chips_from_detections.sh
+++ examples/annotation_and_visualization/extract_chips_from_detections.sh
@@ -1,11 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
-# Setup VIAME Paths (no need to run multiple times if you already ran it)
-
+# Path to VIAME installation
 export VIAME_INSTALL="$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)/../.."
 
-source ${VIAME_INSTALL}/setup_viame.sh 
+# Input and output folders
+export INPUT=videos
+export OUTPUT=frames
+export PIPELINE=pipelines/utility_extract_chips.pipe
 
-# Run chip pipeline
+# The default frame rate is only used when the csvs alongside videos
+# do not contain frame rates, otherwise the CSV frame rate is used.
+export DEFAULT_FRAME_RATE=5
 
-kwiver runner ${VIAME_INSTALL}/configs/pipelines/utility_extract_chips.pipe
+# Setup paths and run command
+source ${VIAME_INSTALL}/setup_viame.sh
+
+python ${VIAME_INSTALL}/configs/process_video.py \
+  -i ${INPUT} -o ${OUTPUT} -frate ${DEFAULT_FRAME_RATE} \
+  -p ${PIPELINE} -auto-detect-gt viame_csv
