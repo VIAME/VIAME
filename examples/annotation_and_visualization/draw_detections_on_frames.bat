56f00e0087382483831922bea1f7e8dc822cb8c0
Matt Dawkins <matt.dawkins@kitware.com>
2022-05-29 20:46:38 -0400

    Style

--- examples/annotation_and_visualization/draw_detections_on_frames.bat
+++ examples/annotation_and_visualization/draw_detections_on_frames.bat
@@ -1,11 +1,9 @@
 @echo off
 
 REM Setup VIAME Paths (no need to set if installed to registry or already set up)
-
 SET VIAME_INSTALL=.\..\..
 
 CALL "%VIAME_INSTALL%\setup_viame.bat"
 
 REM Run Pipeline
-
 kwiver.exe runner "%VIAME_INSTALL%\configs\pipelines\filter_draw_dets.pipe
--- examples/annotation_and_visualization/draw_detections_on_frames.sh
+++ examples/annotation_and_visualization/draw_detections_on_frames.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
 # Setup VIAME Paths (no need to run multiple times if you already ran it)
-
 export VIAME_INSTALL="$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)/../.."
 
 source ${VIAME_INSTALL}/setup_viame.sh 
 
 # Run draw pipeline
-
 kwiver runner ${VIAME_INSTALL}/configs/pipelines/filter_draw_dets.pipe
