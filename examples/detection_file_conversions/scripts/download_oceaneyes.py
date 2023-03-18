60a8ebb0efa896573591762d61a4ad07c0aaa630
Matt Dawkins <matt.dawkins@kitware.com>
2023-03-17 21:31:55 -0400

    Update script

--- examples/detection_file_conversions/scripts/download_oceaneyes.py
+++ examples/detection_file_conversions/scripts/download_oceaneyes.py
@@ -8,14 +8,15 @@ last_fname = ''
 
 with open( input_csv, newline='' ) as fin:
   spamreader = csv.reader( fin )
+  all_files = {}
   for row in spamreader:
     if len( row ) == 0 or row[0][0:5] == "photo":
       continue
     url = row[0]
     fname = row[1]
-    if fname == last_fname:
+    if fname in all_files:
       continue
     print( "Downloading " + fname )
     r = requests.get( url, allow_redirects=True )
     open( fname, 'wb' ).write( r.content )
-    last_fname = fname
+    all_files.add( fname )
