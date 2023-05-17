9e2fba2772cf6fff2ba284a9060cd6c4e264e0dc
ajlynch <alexander.lynch@kitware.com>
2023-05-17 14:37:02 -0400

    initial commit from hannah's cutler_trainer branch

--- cmake/add_project_learn.cmake
+++ cmake/add_project_learn.cmake
@@ -10,6 +10,29 @@ set( PYTHON_LEARN_DEP_BUILD
 
 # Install learn
 set( LEARN_DIR ${VIAME_SOURCE_DIR}/packages/learn )
+ExternalProject_Add( learn_requirements
+    DEPENDS ${PROJECT_DEPS}
+    PREFIX ${VIAME_BUILD_PREFIX}
+    SOURCE_DIR ${LEARN_DIR}
+    BUILD_IN_SOURCE 1
+    PATCH_COMMAND ${LIBRARY_PATCH_COMMAND}
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ${PYTHON_LEARN_DEP_BUILD} -r ${LEARN_DIR}/requirements.txt
+    INSTALL_COMMAND ${LIBRARY_PYTHON_INSTALL}
+    LIST_SEPARATOR "----"
+    )
+
+ExternalProject_Add( learn
+    DEPENDS ${PROJECT_DEPS}
+    PREFIX ${VIAME_BUILD_PREFIX}
+    SOURCE_DIR ${LEARN_DIR}
+    BUILD_IN_SOURCE 1
+    PATCH_COMMAND ${LIBRARY_PATCH_COMMAND}
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ${PYTHON_LEARN_DEP_BUILD} -e ${LEARN_DIR}
+    INSTALL_COMMAND ${LIBRARY_PYTHON_INSTALL}
+    LIST_SEPARATOR "----"
+    )
 execute_process(
     COMMAND
         ${PYTHON_LEARN_DEP_BUILD} 
@@ -36,10 +59,22 @@ if( VIAME_ENABLE_PYTORCH-CUTLER )
             "git+https://github.com/cocodataset/panopticapi.git"
             "git+https://github.com/mcordts/cityscapesScripts.git"
     )
-    # git repos
     execute_process(
         COMMAND
             ${PYTHON_LEARN_DEP_BUILD} 
             --upgrade --force-reinstall Pillow
+    )
+    # mmdet for training
+    execute_process(
+        COMMAND
+            MMCV_WITH_OPS=1 FORCE_CUDA=1 
+            ${PYTHON_LEARN_DEP_BUILD} 
+            mmcv-full==1.5.3 -f https://download.openmmlab.com/mmcv/dist/cu111/torch1.10.0/index.html
+            
+    )
+    execute_process(
+        COMMAND
+            ${PYTHON_LEARN_DEP_BUILD} 
+            mmdet==2.25.0
     )
 endif()
--- plugins/pytorch/mmdet_detector.py
+++ plugins/pytorch/mmdet_detector.py
@@ -14,6 +14,12 @@ from kwiver.vital.types import (
 import mmcv
 import numpy as np
 
+import learn.algorithms.MMDET.register_modules
+import torch
+import torch.nn.functional as F
+from mmdet.models.builder import LOSSES
+from functools import partial
+
 
 _Option = namedtuple('_Option', ['attr', 'config', 'default', 'parse'])
 
@@ -31,10 +37,11 @@ class MMDetDetector(ImageObjectDetector):
         _Option('_class_names', 'class_names', '', str),
         _Option('_thresh', 'thresh', 0.01, float),
         _Option('_gpu_index', 'gpu_index', "0", str),
+        _Option('_num_classes', 'num_classes', 1, int),
         _Option('_display_detections', 'display_detections', False, strtobool),
         _Option('_template', 'template', "", str),
         _Option('_auto_update_model', 'auto_update_model', True, strtobool),
-        _Option('_rgb_to_bgr', 'rgb_to_bgr', True, strtobool),
+        _Option('_rgb_to_bgr', 'rgb_to_bgr', True, strtobool)
     ]
 
     def __init__(self):
@@ -48,8 +55,193 @@ class MMDetDetector(ImageObjectDetector):
         for opt in self._options:
             cfg.set_value(opt.config, str(getattr(self, opt.attr)))
         return cfg
+    def register_new_losses(self):
+        LOSSES._module_dict.pop('ReducedFocalLoss', None)
+        LOSSES._module_dict.pop('EQLv2', None)
+
+        # https://github.com/facebookresearch/fvcore/blob/main/fvcore/nn/focal_loss.py
+        def reduced_focal_loss(inputs, targets, threshold=0.5, alpha=0.25, gamma=2.0):
+            
+            targets = F.one_hot(targets, num_classes=inputs.shape[-1]).type_as(inputs)
+            p = torch.sigmoid(inputs)
+            ce_loss = F.binary_cross_entropy_with_logits(
+                inputs, targets, reduction="none"
+            )
+            p_t = p * targets + (1 - p) * (1 - targets)
+            ones_mask = p_t < threshold
+            other_mask = p_t >= threshold
+            other_mask = other_mask * (((1 - p_t) ** gamma) / threshold ** gamma)
+            f_x = ones_mask + other_mask
+            
+            loss = ce_loss * f_x
+
+            if alpha >= 0:
+                alpha_t = alpha * targets + (1 - alpha) * (1 - targets)
+                loss = alpha_t * loss
+
+            return loss
+
+        @LOSSES.register_module()
+        class ReducedFocalLoss(torch.nn.Module):
+            def __init__(self,
+                        threshold=0.5,
+                        gamma=2.0,
+                        alpha=0.25,
+                        reduction='mean',
+                        loss_weight=1.0):
+                super(ReducedFocalLoss, self).__init__()
+                self.alpha = alpha
+                self.gamma = gamma
+                self.reduction = reduction
+                self.loss_weight = loss_weight
+                self.threshold = threshold
+
+
+            def forward(self,
+                        pred,
+                        target,
+                        weight=None,
+                        avg_factor=None,
+                        reduction_override=None):
+                """Forward function.
+                Args:
+                    pred (torch.Tensor): The prediction.
+                    target (torch.Tensor): The learning target of the prediction
+                        in gaussian distribution.
+                    weight (torch.Tensor, optional): The weight of loss for each
+                        prediction. Defaults to None.
+                    avg_factor (int, optional): Average factor that is used to average
+                        the loss. Defaults to None.
+                    reduction_override (str, optional): The reduction method used to
+                        override the original reduction method of the loss.
+                        Defaults to None.
+                """
+                assert reduction_override in (None, 'none', 'mean', 'sum')
+                reduction = (
+                    reduction_override if reduction_override else self.reduction)
+                loss_reg = self.loss_weight * reduced_focal_loss(
+                    pred,
+                    target,
+                    threshold=self.threshold,
+                    alpha=self.alpha,
+                    gamma=self.gamma)
+                
+                if reduction == 'mean':
+                    return loss_reg.mean()
+                if reduction == 'sum':
+                    return loss_reg.sum()
+
+                return loss_reg
+
+
+        @LOSSES.register_module()
+        class EQLv2(torch.nn.Module):
+            def __init__(self,
+                    use_sigmoid=True,
+                    reduction='mean',
+                    class_weight=None,
+                    loss_weight=1.0,
+                    num_classes=3,  # 1203 for lvis v1.0, 1230 for lvis v0.5
+                    gamma=12,
+                    mu=0.8,
+                    alpha=4.0,
+                    vis_grad=False):
+                super().__init__()
+                self.use_sigmoid = True
+                self.reduction = reduction
+                self.loss_weight = loss_weight
+                self.class_weight = class_weight
+                self.num_classes = num_classes
+                self.group = True
+
+                # cfg for eqlv2
+                self.vis_grad = vis_grad
+                self.gamma = gamma
+                self.mu = mu
+                self.alpha = alpha
+
+                # initial variables
+                self._pos_grad = None
+                self._neg_grad = None
+                self.pos_neg = None
+
+                def _func(x, gamma, mu):
+                    return 1 / (1 + torch.exp(-gamma * (x - mu)))
+                self.map_func = partial(_func, gamma=self.gamma, mu=self.mu)
+                # logger = get_root_logger()
+                # logger.info(f"build EQL v2, gamma: {gamma}, mu: {mu}, alpha: {alpha}")
+
+            def forward(self,
+                        cls_score,
+                        label,
+                        weight=None,
+                        avg_factor=None,
+                        reduction_override=None,
+                        **kwargs):
+                self.n_i, self.n_c = cls_score.size()
+
+                self.gt_classes = label
+                self.pred_class_logits = cls_score
+
+                def expand_label(pred, gt_classes):
+                    target = pred.new_zeros(self.n_i, self.n_c)
+                    target[torch.arange(self.n_i), gt_classes] = 1
+                    return target
+
+                target = expand_label(cls_score, label)
+                pos_w, neg_w = self.get_weight(cls_score)
+                weight = pos_w * target + neg_w * (1 - target)
+                cls_loss = F.binary_cross_entropy_with_logits(cls_score, target,
+                                                            reduction='none')
+                cls_loss = torch.sum(cls_loss * weight) / self.n_i
+                self.collect_grad(cls_score.detach(), target.detach(), weight.detach())
+                return self.loss_weight * cls_loss
+
+            def get_channel_num(self, num_classes):
+                num_channel = num_classes + 1
+                return num_channel
+
+            def get_activation(self, cls_score):
+                cls_score = torch.sigmoid(cls_score)
+                n_i, n_c = cls_score.size()
+                bg_score = cls_score[:, -1].view(n_i, 1)
+                cls_score[:, :-1] *= (1 - bg_score)
+                return cls_score
+
+            def collect_grad(self, cls_score, target, weight):
+                prob = torch.sigmoid(cls_score)
+                grad = target * (prob - 1) + (1 - target) * prob
+                grad = torch.abs(grad)
+
+                # do not collect grad for objectiveness branch [:-1]
+                pos_grad = torch.sum(grad * target * weight, dim=0)[:-1]
+                neg_grad = torch.sum(grad * (1 - target) * weight, dim=0)[:-1]
+
+                # dist.all_reduce(pos_grad)
+                # dist.all_reduce(neg_grad)
+
+                self._pos_grad += pos_grad
+                self._neg_grad += neg_grad
+                self.pos_neg = self._pos_grad / (self._neg_grad + 1e-10)
+
+            def get_weight(self, cls_score):
+                # we do not have information about pos grad and neg grad at beginning
+                if self._pos_grad is None:
+                    self._pos_grad = cls_score.new_zeros(self.num_classes)
+                    self._neg_grad = cls_score.new_zeros(self.num_classes)
+                    neg_w = cls_score.new_ones((self.n_i, self.n_c))
+                    pos_w = cls_score.new_ones((self.n_i, self.n_c))
+                else:
+                    # the negative weight for objectiveness is always 1
+                    neg_w = torch.cat([self.map_func(self.pos_neg), cls_score.new_ones(1)])
+                    pos_w = 1 + self.alpha * (1 - neg_w)
+                    neg_w = neg_w.view(1, -1).expand(self.n_i, self.n_c)
+                    pos_w = pos_w.view(1, -1).expand(self.n_i, self.n_c)
+                return pos_w, neg_w
+
 
     def set_configuration(self, cfg_in):
+        self.register_new_losses()
         cfg = self.get_configuration()
         cfg.merge_config(cfg_in)
 
@@ -65,7 +257,30 @@ class MMDetDetector(ImageObjectDetector):
         from mmdet.apis import init_detector
 
         gpu_string = 'cuda:' + str(self._gpu_index)
-        self._model = init_detector(self._net_config, self._weight_file, device=gpu_string)
+        print("******************************************************************")
+        mmdet_config = mmcv.Config.fromfile(self._net_config)
+        def replace(conf, depth):
+            if depth <= 0:
+                return
+            try:
+                for k,v in conf.items():
+                    if isinstance(v, dict):
+                        replace(v, depth-1)
+                    elif isinstance(v, list):
+                        for element in v:
+                            replace(element, depth-1)
+                    else:
+                        # print(k,v)
+                        if k == 'num_classes':
+                            conf[k] = self._num_classes
+                        if k == 'CLASSES':
+                            conf[k] = self.toolset['target_dataset'].categories
+            except:
+                pass
+
+        replace(mmdet_config, 500)
+        self._model = init_detector(mmdet_config, self._weight_file, device=gpu_string)
+        print(self._model)
         with open(self._class_names, "r") as in_file:
             self._labels = in_file.read().splitlines()
 
