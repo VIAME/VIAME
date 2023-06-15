fa3746906539e1fd2f8dfb27d342ec66e71a0929
Matt Dawkins <matt.dawkins@kitware.com>
2023-06-15 10:46:57 -0400

    Implement revisions

--- cmake/add_project_learn.cmake
+++ cmake/add_project_learn.cmake
@@ -6,6 +6,8 @@ set( LEARN_DIR ${VIAME_SOURCE_DIR}/packages/learn )
 set( PYDENSECRF_DIR ${LEARN_DIR}-deps/pydensecrf )
 set( PANOPTICAPI_DIR ${LEARN_DIR}-deps/panopticapi )
 
+set( LEARN_BUILD_DIR ${VIAME_BUILD_PREFIX}/src/learn-build )
+
 set( LEARN_CLONE_CMD
   git clone https://github.com/lucasb-eyer/pydensecrf.git ${PYDENSECRF_DIR} &&
   git clone https://github.com/cocodataset/panopticapi.git ${PANOPTICAPI_DIR} &&
@@ -17,28 +19,34 @@ set( LEARN_REQ_PIP_CMD
     ${Python_EXECUTABLE} -m pip install --user )
 
 if( VIAME_SYMLINK_PYTHON )
-  set( LEARN_PIP_CMD
+  set( LEARN_BUILD_CMD
     ${CMAKE_COMMAND} -E env "${PYTHON_DEP_ENV_VARS}"
     ${Python_EXECUTABLE} -m pip install --user -e . )
+  set( LEARN_INSTALL_CMD )
 else()
   # This is only required for no symlink install without a -e with older
   # versions of pip, for never versions the above command works with no -e
-  set( LEARN_PIP_CMD
-      ${CMAKE_COMMAND} -E env "${PYTHON_DEP_ENV_VARS}"
-      ${Python_EXECUTABLE} -m pip install --user ${LEARN_DIR} )
+  set( LEARN_BUILD_CMD
+    ${CMAKE_COMMAND} -E env "${PYTHON_DEP_ENV_VARS}"
+    ${Python_EXECUTABLE} setup.py bdist_wheel -d ${LEARN_BUILD_DIR} )
+  set( LEARN_INSTALL_CMD 
+    ${CMAKE_COMMAND}
+    -DPYTHON_EXECUTABLE=${Python_EXECUTABLE}
+    -DPython_EXECUTABLE=${Python_EXECUTABLE}
+    -DWHEEL_DIR=${LEARN_BUILD_DIR}
+    -P ${VIAME_CMAKE_DIR}/install_python_wheel.cmake )
 endif()
 
 # Install required dependencies and learn
 ExternalProject_Add( learn
     DEPENDS python-deps detectron2 torchvideo
     PREFIX ${VIAME_BUILD_PREFIX}
-    SOURCE_DIR ${LEARN_DIR}
-    BUILD_IN_SOURCE 1
+    SOURCE_DIR ${VIAME_PACKAGES_DIR}
     CONFIGURE_COMMAND ${LEARN_CLONE_CMD}
     BUILD_COMMAND ${LEARN_REQ_PIP_CMD} -r ${LEARN_DIR}/requirements.txt
-          COMMAND ${LEARN_BUILD_CMD} ${LEARN_DIR}
-          COMMAND ${LEARN_BUILD_CMD} ${PYDENSECRF_DIR}
-          COMMAND ${LEARN_BUILD_CMD} ${PANOPTICAPI_DIR}
+          COMMAND cd ${PYDENSECRF_DIR} && ${LEARN_BUILD_CMD}
+          COMMAND cd ${PANOPTICAPI_DIR} && ${LEARN_BUILD_CMD}
+          COMMAND cd ${LEARN_DIR} && ${LEARN_BUILD_CMD}
     INSTALL_COMMAND ${LEARN_INSTALL_CMD}
     LIST_SEPARATOR "----"
     )
