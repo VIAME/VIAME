3d76c7e45281a789cd1312c872eb71ebb03c2945
Matt Dawkins <matt.dawkins@kitware.com>
2023-06-21 19:46:55 -0400

    Update version on dep

--- cmake/add_project_python_deps.cmake
+++ cmake/add_project_python_deps.cmake
@@ -117,7 +117,7 @@ if( VIAME_ENABLE_PYTORCH-NETHARN AND VIAME_ENABLE_GDAL )
 endif()
 
 if( VIAME_ENABLE_OPENCV OR VIAME_ENABLE_PYTORCH-NETHARN )
-  list( APPEND VIAME_PYTHON_BASIC_DEPS "ubelt" "pygments" "bezier==2020.1.14" )
+  list( APPEND VIAME_PYTHON_BASIC_DEPS "ubelt<=1.2.3" "pygments" "bezier==2020.1.14" )
   list( APPEND VIAME_PYTHON_BASIC_DEPS "ndsampler==0.6.7" "kwcoco==0.2.31" )
   list( APPEND VIAME_PYTHON_BASIC_DEPS "imageio==2.15.0" "networkx<=2.8.8" )
 endif()
