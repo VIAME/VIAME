aa39e7502d853090e8715bcc5c031717b9ce25f4
Matt Dawkins <matt.dawkins@kitware.com>
2024-02-27 00:39:37 -0500

    Do not return failure on build error

--- cmake/build_server_docker_full.sh
+++ cmake/build_server_docker_full.sh
@@ -97,7 +97,7 @@ cmake ../ -DCMAKE_BUILD_TYPE:STRING=Release \
 -DVIAME_DOWNLOAD_MODELS-MOUSS:BOOL=OFF
 
 # Perform multi-threaded build
-make -j$(nproc) > build_log.txt 2>&1
+make -j$(nproc) > build_log.txt 2>&1 || true
 
 # Below be krakens
 # (V) (°,,,°) (V)   (V) (°,,,°) (V)   (V) (°,,,°) (V)
