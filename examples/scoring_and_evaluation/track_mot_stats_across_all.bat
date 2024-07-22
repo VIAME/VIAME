2bc274b4b0c55fee406403d288dcf9f5a75a7f29
Matt Dawkins <matt.dawkins@kitware.com>
2024-07-22 00:54:57 -0400

    Rename example files

---
+++ examples/scoring_and_evaluation/track_mot_stats_across_all.bat
@@ -0,0 +1,15 @@
+@echo off
+
+REM Setup VIAME Paths (no need to set if installed to registry or already set up)
+
+SET VIAME_INSTALL=.\..\..
+
+CALL "%VIAME_INSTALL%\setup_viame.bat"
+
+REM Run score tracks on data for singular metrics
+
+python "%VIAME_INSTALL%\configs\score_results.py" ^
+ -computed detections.csv -truth groundtruth.csv ^
+ -threshold 0.10 -track-stats stats_nocls.txt
+
+pause
---
+++ examples/scoring_and_evaluation/track_mot_stats_per_category.bat
@@ -0,0 +1,16 @@
+@echo off
+
+REM Setup VIAME Paths (no need to set if installed to registry or already set up)
+
+SET VIAME_INSTALL=.\..\..
+
+CALL "%VIAME_INSTALL%\setup_viame.bat"
+
+REM Run score tracks on data for singular metrics
+
+python "%VIAME_INSTALL%\configs\score_results.py" ^
+ -computed detections.csv -truth groundtruth.csv ^
+ -threshold 0.10 -track-stats stats.txt ^
+ --per-category
+
+pause
