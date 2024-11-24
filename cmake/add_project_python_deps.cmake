b48c5499098bbe0d5b402c98b1b8e3252037b039
Matt Dawkins <matt.dawkins@kitware.com>
2024-11-24 14:45:48 -0500

    Bump version requirement

--- cmake/add_project_python_deps.cmake
+++ cmake/add_project_python_deps.cmake
@@ -50,7 +50,7 @@ list( APPEND VIAME_PYTHON_BASIC_DEPS "map_boxes" "ensemble_boxes" )
 if( Python_VERSION VERSION_LESS "3.9" )
   list( APPEND VIAME_PYTHON_BASIC_DEPS  "llvmlite==0.31.0" "numba==0.47" )
 else()
-  list( APPEND VIAME_PYTHON_BASIC_DEPS  "llvmlite==0.40.0" "numba==0.60" )
+  list( APPEND VIAME_PYTHON_BASIC_DEPS  "llvmlite==0.45.0" "numba==0.60" )
 endif()
 
 # For pytorch building
