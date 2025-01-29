540d503079ffc5246c802a17922ebf0531fe0e15
joncrall <jon.crall@kitware.com>
2025-01-29 14:40:36 -0500

    fix: use config files that exist

--- examples/object_detector_training/train_mask_rcnn_from_viame_csv.bat
+++ examples/object_detector_training/train_mask_rcnn_from_viame_csv.bat
@@ -14,7 +14,7 @@ SET KWIMAGE_DISABLE_C_EXTENSIONS=1
 REM Run pipeline
 viame_train_detector.exe ^
   -i training_data_sea_lion ^
-  -c "%VIAME_INSTALL%\configs\pipelines\train_detector_netharn_mask_rcnn.conf" ^
+  -c "%VIAME_INSTALL%\configs\pipelines\train_detector_netharn_mask_rcnn_720.conf" ^
   --threshold 0.0
 
 pause
