a5a2f7dbc33f3896defb21417062093a7fba1b6e
joncrall <jon.crall@kitware.com>
2025-01-29 14:40:36 -0500

    fix: use config files that exist

--- examples/object_detector_training/train_yolo_from_habcam_csv.sh
+++ examples/object_detector_training/train_yolo_from_habcam_csv.sh
@@ -3,7 +3,7 @@
 # Path to VIAME installation
 export VIAME_INSTALL="$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)/../.."
 
-source ${VIAME_INSTALL}/setup_viame.sh 
+source ${VIAME_INSTALL}/setup_viame.sh
 
 # Adjust log level
 export KWIVER_DEFAULT_LOG_LEVEL=info
@@ -11,5 +11,5 @@ export KWIVER_DEFAULT_LOG_LEVEL=info
 # Run pipeline
 viame_train_detector \
   -i training_data_habcam \
-  -c ${VIAME_INSTALL}/configs/pipelines/train_detector_darknet_yolo_704.adaptive.habcam.conf \
+  -c ${VIAME_INSTALL}/configs/pipelines/train_detector_darknet_yolo_704.habcam.conf \
   --threshold 0.0
