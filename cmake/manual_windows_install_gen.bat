2ae4b78e35ce6f5e0c3bf02f2b865d1ec7b95998
Matt Dawkins <matt.dawkins@kitware.com>
2025-02-06 01:31:51 -0500

    Add manual build script for windows

---
+++ cmake/manual_windows_install_gen.bat
@@ -0,0 +1,20 @@
+REM -------------------------------------------------------------------------------------------------------
+REM Setup Paths
+REM -------------------------------------------------------------------------------------------------------
+
+SET "BUILD_DIR=C:\workspace\VIAME-Windows-GPU-Release"
+
+IF EXIST %BUILD_DIR% rmdir /s /q %BUILD_DIR%
+
+GIT clone https://github.com/VIAME/VIAME.git %BUILD_DIR%
+
+CD %BUILD_DIR%
+GIT checkout next
+
+COPY %BUILD_DIR%\cmake\build_server_windows.bat build_server_windows.bat
+COPY %BUILD_DIR%\cmake\build_server_windows.cmake platform.cmake
+COPY %BUILD_DIR%\cmake\jenkins\CTestBuildOnlyPipeline jenkins_dashboard.cmake
+
+CALL build_server_windows.bat > build_log.txt
+
+PAUSE
