00623598e293b1b0e1f0993e7c2b34c2d06ce49d
Matt Dawkins <matt.dawkins@kitware.com>
2025-04-01 11:02:57 -0400

    Update example name

--- examples/object_tracking/run_user_init_tracker.bat
+++ examples/object_tracking/run_user_init_tracker.bat
@@ -8,7 +8,8 @@ CALL "%VIAME_INSTALL%\setup_viame.bat"
 
 REM Run Pipeline
 
-pipeline_runner.exe -p "%VIAME_INSTALL%\configs\pipelines\tracker_short_term.pipe" ^
-                    -s input:video_filename=input_list.txt
+kwiver.exe runner "%VIAME_INSTALL%\configs\pipelines\utility_track_selections_default_mask.pipe" ^
+  -s input:video_filename=input_list.txt ^
+  -s 
 
-pause
+PAUSE
