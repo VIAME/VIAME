b80f46a3afb2b5140adc9408298895a9146ca433
Matt Dawkins <matt.dawkins@kitware.com>
2025-04-21 23:02:46 -0400

    Update filenames

--- examples/object_tracking/run_fish_tracker.bat
+++ examples/object_tracking/run_fish_tracker.bat
@@ -8,7 +8,7 @@ CALL "%VIAME_INSTALL%\setup_viame.bat"
 
 REM Run Pipeline
 
-kwiver.exe runner "%VIAME_INSTALL%\configs\pipelines\tracker_fish.pipe" ^
+kwiver.exe runner "%VIAME_INSTALL%\configs\pipelines\tracker_default_fish.pipe" ^
                   -s input:video_filename=input_list.txt
 
 pause
