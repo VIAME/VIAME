b88266c1fbdb6369de58048db1bf84ba87999f8f
Matt Dawkins <matt.dawkins@kitware.com>
2025-05-18 00:11:15 -0400

    Update server build settings

--- cmake/build_server_rocky.sh
+++ cmake/build_server_rocky.sh
@@ -8,18 +8,19 @@ export VIAME_BUILD_DIR=$VIAME_SOURCE_DIR/build
 export VIAME_INSTALL_DIR=$VIAME_BUILD_DIR/install
 
 # Install system dependencies and use more recent compiler
-$VIAME_SOURCE_DIR/cmake/build_server_centos7_deps.sh
+$VIAME_SOURCE_DIR/cmake/build_server_deps_yum.sh
 
-source /opt/rh/devtoolset-9/enable
-
-# Install CMAKE
+# Install more modern CMAKE and OpenSSL from source
+./viame/cmake/build_server_linux_ssl.sh
 ./viame/cmake/build_server_linux_cmake.sh
 
 # Update VIAME sub git sources
+echo "Checking out VIAME submodules"
+
 cd $VIAME_SOURCE_DIR
 git submodule update --init --recursive
 mkdir build
-cd build 
+cd build
 
 # Configure Paths [should be removed when no longer necessary by fletch]
 export PATH=$VIAME_INSTALL_DIR/bin:$PATH
@@ -28,45 +29,52 @@ export C_INCLUDE_PATH=$VIAME_INSTALL_DIR/include/python3.10:$C_INCLUDE_PATH
 export CPLUS_INCLUDE_PATH=$VIAME_INSTALL_DIR/include/python3.10:$CPLUS_INCLUDE_PATH
 
 # Configure VIAME
+echo "Beginning VIAME CMake configuration"
+
 cmake ../ -DCMAKE_BUILD_TYPE:STRING=Release \
 -DVIAME_BUILD_DEPENDENCIES:BOOL=ON \
 -DVIAME_FIXUP_BUNDLE:BOOL=ON \
 -DVIAME_ENABLE_BURNOUT:BOOL=OFF \
--DVIAME_ENABLE_CUDA:BOOL=OFF \
--DVIAME_ENABLE_CUDNN:BOOL=OFF \
+-DVIAME_ENABLE_CUDA:BOOL=ON \
+-DVIAME_ENABLE_CUDNN:BOOL=ON \
 -DVIAME_ENABLE_DIVE:BOOL=ON \
 -DVIAME_ENABLE_DOCS:BOOL=OFF \
 -DVIAME_ENABLE_FFMPEG:BOOL=ON \
 -DVIAME_ENABLE_FFMPEG-X264:BOOL=ON \
--DVIAME_ENABLE_GDAL:BOOL=ON \
+-DVIAME_ENABLE_GDAL:BOOL=OFF \
 -DVIAME_ENABLE_FLASK:BOOL=OFF \
 -DVIAME_ENABLE_ITK:BOOL=OFF \
 -DVIAME_ENABLE_KWANT:BOOL=ON \
 -DVIAME_ENABLE_KWIVER:BOOL=ON \
--DVIAME_ENABLE_LEARN:BOOL=OFF \
+-DVIAME_ENABLE_LEARN:BOOL=ON \
 -DVIAME_ENABLE_MATLAB:BOOL=OFF \
+-DVIAME_ENABLE_ONNX:BOOL=ON \
 -DVIAME_ENABLE_OPENCV:BOOL=ON \
 -DVIAME_OPENCV_VERSION:STRING=3.4.0 \
+-DVIAME_ENABLE_POSTGRESQL=OFF \
 -DVIAME_ENABLE_PYTHON:BOOL=ON \
--DVIAME_PYTHON_BUILD_FROM_SOURCE:BOOL=OFF \
+-DVIAME_PYTHON_BUILD_FROM_SOURCE:BOOL=ON \
 -DVIAME_PYTHON_VERSION:STRING=3.10.4 \
 -DVIAME_ENABLE_PYTORCH:BOOL=ON \
 -DVIAME_PYTORCH_BUILD_FROM_SOURCE:BOOL=ON \
--DVIAME_PYTORCH_DISABLE_NINJA=ON \
+-DVIAME_PYTORCH_DISABLE_NINJA=OFF \
 -DVIAME_PYTORCH_VERSION:STRING=2.7.0 \
 -DVIAME_ENABLE_PYTORCH-MMDET:BOOL=ON \
 -DVIAME_ENABLE_PYTORCH-NETHARN:BOOL=ON \
--DVIAME_ENABLE_PYTORCH-PYSOT:BOOL=OFF \
+-DVIAME_ENABLE_PYTORCH-PYSOT:BOOL=ON \
+-DVIAME_ENABLE_PYTORCH-SAM:BOOL=ON \
 -DVIAME_ENABLE_SCALLOP_TK:BOOL=OFF \
 -DVIAME_ENABLE_SEAL:BOOL=OFF \
 -DVIAME_ENABLE_SMQTK:BOOL=ON \
 -DVIAME_ENABLE_TENSORFLOW:BOOL=OFF \
 -DVIAME_ENABLE_UW_PREDICTOR:BOOL=OFF \
--DVIAME_ENABLE_VIVIA:BOOL=ON \
+-DVIAME_ENABLE_VIVIA:BOOL=OFF \
 -DVIAME_ENABLE_VXL:BOOL=ON \
 -DVIAME_ENABLE_DARKNET:BOOL=ON 
 
 # Build VIAME, pipe output to file
+echo "Beginning core build, routing build info to build_log.txt"
+
 ../cmake/build_server_linux_build.sh > build_log.txt 2>&1
 
 # Output check statments
@@ -75,8 +83,8 @@ if grep -q "Built target viame" build_log.txt; then
 
   # Make zip file of install
   mv install viame
-  rm VIAME-CPU-v1.0.0-Linux-64Bit.tar.gz ||:
-  tar -zcvf VIAME-CPU-v1.0.0-Linux-64Bit.tar.gz viame
+  rm VIAME-v1.0.0-Linux-64Bit.tar.gz ||:
+  tar -zcvf VIAME-v1.0.0-Linux-64Bit.tar.gz viame
   mv viame install
 else
   echo "VIAME Build Failed"
--- cmake/manual_linux_install_gen.sh
+++ cmake/manual_linux_install_gen.sh
@@ -7,14 +7,14 @@ git clone https://github.com/VIAME/VIAME.git viame-src-clone
 cd viame-src-clone
 
 # stand up a new docker build env
-docker pull nvidia/cuda:12.3.2-cudnn9-devel-centos7
-chmod +x cmake/build_server_centos.sh
-docker run -td --runtime=nvidia --name viame_installer_zip nvidia/cuda:12.3.2-cudnn9-devel-centos7 bash
+docker pull nvidia/cuda:12.6.3-cudnn-devel-rockylinux9
+chmod +x cmake/build_server_rocky.sh
+docker run -td --runtime=nvidia --name viame_installer_zip nvidia/cuda:12.6.3-cudnn-devel-rockylinux9 bash
 cd ../
 docker cp viame-src-clone viame_installer_zip:/viame/
 
 # run the build script in the fresh docker environment
-docker exec -i viame_installer_zip ./viame/cmake/build_server_centos.sh
+docker exec -i viame_installer_zip ./viame/cmake/build_server_rocky.sh
 
 # copy out final installer and build log
 docker cp viame_installer_zip:/viame/build/VIAME-v1.0.0-Linux-64Bit.tar.gz .
