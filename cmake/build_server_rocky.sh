d3ca565added23417120be0bf560fe9d4cc69174
Matt Dawkins <matt.dawkins@kitware.com>
2025-05-20 10:28:03 -0400

    Fix spacing issue

--- cmake/build_server_rocky.sh
+++ cmake/build_server_rocky.sh
@@ -18,7 +18,7 @@ $VIAME_SOURCE_DIR/cmake/build_server_deps_yum.sh
 if [ -f /usr/include/cudnn_v9.h ] && [ ! -f /usr/include/cudnn.h ]; then
  ln -s /usr/include/cudnn_v9.h /usr/include/cudnn.h
  ln -s /usr/include/cudnn_adv_v9.h /usr/include/cudnn_adv.h
- ln -s /usr/include/cudnn_cnn_v9.h/usr/include/cudnn_cnn.h
+ ln -s /usr/include/cudnn_cnn_v9.h /usr/include/cudnn_cnn.h
  ln -s /usr/include/cudnn_ops_v9.h /usr/include/cudnn_ops.h
  ln -s /usr/include/cudnn_version_v9.h /usr/include/cudnn_version.h
  ln -s /usr/include/cudnn_backend_v9.h /usr/include/cudnn_backend.h
