34b6f719b901b76c3597dc2168936d010bfc372b
Matt Dawkins <matt.dawkins@kitware.com>
2025-06-03 15:59:16 -0400

    Check in file

---
+++ cmake/manual_linux_install_gen_cpu.sh
@@ -0,0 +1,21 @@
+
+# clean build env
+docker stop viame_installer_zip || true && docker rm --force viame_installer_zip || true
+rm -rf viame-src-clone || true
+
+git clone https://github.com/VIAME/VIAME.git viame-src-clone
+cd viame-src-clone
+
+# stand up a new docker build env
+docker pull nvidia/cuda:12.6.3-cudnn-devel-rockylinux8
+chmod +x cmake/build_server_rocky.sh
+docker run -td --runtime=nvidia --name viame_installer_zip nvidia/cuda:12.6.3-cudnn-devel-rockylinux8 bash
+cd ../
+docker cp viame-src-clone viame_installer_zip:/viame/
+
+# run the build script in the fresh docker environment
+docker exec -i viame_installer_zip ./viame/cmake/build_server_rocky_cpu.sh
+
+# copy out final installer and build log
+docker cp viame_installer_zip:/viame/build/VIAME-CPU-v1.0.0-Linux-64Bit.tar.gz .
+docker cp viame_installer_zip:/viame/build/build_log.txt .
