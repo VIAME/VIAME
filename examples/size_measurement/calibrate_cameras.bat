afbdb2b0e8e9b2e123a2c3ebdc0624dcb133a630
Matt Dawkins <matt.dawkins@kitware.com>
2025-12-10 23:36:40 -0500

    Switch example to use auto option

--- examples/size_measurement/calibrate_cameras.bat
+++ examples/size_measurement/calibrate_cameras.bat
@@ -17,8 +17,14 @@ REM   calibrate_cameras.bat --left left_video.mp4 --right right_video.mp4
 REM   calibrate_cameras.bat --left .\left_images\ --right .\right_images\
 REM   calibrate_cameras.bat --left "left\*.png" --right "right\*.png"
 REM
-REM Additional options: -x GRID_X -y GRID_Y -q SQUARE_SIZE_MM -s FRAME_STEP -g (gui)
+REM Additional options:
+REM   -a              Auto-detect grid size (recommended)
+REM   -x GRID_X       Number of inner corners in grid width (default: 6)
+REM   -y GRID_Y       Number of inner corners in grid height (default: 5)
+REM   -q SQUARE_SIZE  Width of calibration square in mm (default: 85)
+REM   -s FRAME_STEP   Process every Nth frame (default: 1)
+REM   -g              Show GUI with detection results
 
-python.exe "%VIAME_INSTALL%\tools\calibrate_cameras.py" -j calibration_matrices.json %*
+python.exe "%VIAME_INSTALL%\tools\calibrate_cameras.py" -a -o calibration_matrices.json %*
 
 pause
--- examples/size_measurement/calibrate_cameras.sh
+++ examples/size_measurement/calibrate_cameras.sh
@@ -17,6 +17,12 @@ source ${VIAME_INSTALL}/setup_viame.sh
 #   ./calibrate_cameras.sh --left ./left_images/ --right ./right_images/
 #   ./calibrate_cameras.sh --left "left/*.png" --right "right/*.png"
 #
-# Additional options: -x GRID_X -y GRID_Y -q SQUARE_SIZE_MM -s FRAME_STEP -g (gui)
+# Additional options:
+#   -a              Auto-detect grid size (recommended)
+#   -x GRID_X       Number of inner corners in grid width (default: 6)
+#   -y GRID_Y       Number of inner corners in grid height (default: 5)
+#   -q SQUARE_SIZE  Width of calibration square in mm (default: 85)
+#   -s FRAME_STEP   Process every Nth frame (default: 1)
+#   -g              Show GUI with detection results
 
-python ${VIAME_INSTALL}/tools/calibrate_cameras.py -j calibration_matrices.json "$@"
+python ${VIAME_INSTALL}/tools/calibrate_cameras.py -a -o calibration_matrices.json "$@"
