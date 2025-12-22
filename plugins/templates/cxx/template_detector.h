9f84df8c721d972926724e48562dd6f8a83991f8
Matt Dawkins <matt.dawkins@kitware.com>
2025-12-22 17:42:23 -0500

    Remove old license from every file

--- plugins/templates/cxx/template_detector.h
+++ plugins/templates/cxx/template_detector.h
@@ -1,6 +1,6 @@
-/*
- * INSERT COPYRIGHT STATEMENT OR DELETE THIS
- */
+/* This file is part of VIAME, and is distributed under an OSI-approved *
+ * BSD 3-Clause License. See either the root top-level LICENSE file or  *
+ * https://github.com/VIAME/VIAME/blob/main/LICENSE.txt for details.    */
 
 #ifndef VIAME_@TEMPLATE@_DETECTOR_H
 #define VIAME_@TEMPLATE@_DETECTOR_H
