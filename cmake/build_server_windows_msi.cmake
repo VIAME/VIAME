e0fca5168c6eec95fb83a7efaf063cbebe4d1468
Matt Dawkins <matt.dawkins@kitware.com>
2025-12-24 15:30:15 -0500

    Switch windows to use common cmake vars

--- cmake/build_server_windows_msi.cmake
+++ cmake/build_server_windows_msi.cmake
@@ -1,3 +1,9 @@
+# VIAME Windows GPU MSI Build Platform Configuration
+# Minimal build configuration for MSI installer
+#
+# Uses shared cmake preset files for common settings
+
+# CTest configuration
 set(CTEST_SITE "noctae.kitware.com")
 set(CTEST_BUILD_NAME "Windows_GPU_MSI_Main")
 set(CTEST_SOURCE_DIRECTORY "C:/VIAME-Builds/GPU-MSI")
@@ -9,29 +15,38 @@ set(CTEST_BUILD_MODEL "Nightly")
 set(CTEST_NIGHTLY_START_TIME "3:00:00 UTC")
 set(CTEST_USE_LAUNCHERS 1)
 include(CTestUseLaunchers)
-set(OPTIONS 
-  "-DCMAKE_BUILD_TYPE=Release"
-  "-DCUDA_NVCC_EXECUTABLE:PATH=C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v12.6/bin/nvcc.exe"
-  "-DCUDNN_ROOT_DIR:PATH=C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v12.6"
-  "-DVIAME_FIXUP_BUNDLE=ON"
-  "-DVIAME_ENABLE_CUDNN=ON"
-  "-DVIAME_ENABLE_CUDA=ON"
-  "-DVIAME_ENABLE_DARKNET=ON"
-  "-DVIAME_ENABLE_DIVE=OFF"
-  "-DVIAME_ENABLE_FFMPEG=ON"
-  "-DVIAME_ENABLE_GDAL=OFF"
-  "-DVIAME_ENABLE_LEARN=OFF"
-  "-DVIAME_ENABLE_OPENCV=ON"
-  "-DVIAME_OPENCV_VERSION=3.4.0"
-  "-DVIAME_ENABLE_PYTHON=ON"
-  "-DVIAME_PYTHON_BUILD_FROM_SOURCE=ON"
-  "-DVIAME_ENABLE_SCALLOP_TK=OFF"
-  "-DVIAME_ENABLE_SMQTK=ON"
-  "-DVIAME_ENABLE_PYTORCH=OFF"
-  "-DVIAME_ENABLE_VIVIA=OFF"
-  "-DVIAME_DOWNLOAD_MODELS=ON"
-  "-DVIAME_BUILD_KWIVER_DIR=C:/tmp/kv5"
-  "-DVIAME_BUILD_PLUGINS_DIR=C:/tmp/vm5"
-)
+
+# Include helper for building OPTIONS from cmake presets
+include(${CMAKE_CURRENT_LIST_DIR}/build_common_functions.cmake)
+
+# Include base cmake preset file only (MSI build is minimal)
+include_cmake_preset(build_cmake_base.cmake)
+
+# Windows-specific CUDA paths (uses CUDA 12.6)
+add_option("CUDA_NVCC_EXECUTABLE:PATH" "C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v12.6/bin/nvcc.exe")
+add_option("CUDNN_ROOT_DIR:PATH" "C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v12.6")
+
+# Windows-specific build paths
+add_option("VIAME_BUILD_KWIVER_DIR" "C:/tmp/kv5")
+add_option("VIAME_BUILD_PLUGINS_DIR" "C:/tmp/vm5")
+
+# MSI-specific settings - minimal build
+add_option("VIAME_FIXUP_BUNDLE" "ON")
+add_option("VIAME_ENABLE_CUDA" "ON")
+add_option("VIAME_ENABLE_CUDNN" "ON")
+add_option("VIAME_ENABLE_DIVE" "OFF")
+add_option("VIAME_ENABLE_FFMPEG-X264" "OFF")
+add_option("VIAME_ENABLE_LEARN" "OFF")
+add_option("VIAME_ENABLE_ONNX" "OFF")
+add_option("VIAME_ENABLE_POSTGRESQL" "OFF")
+add_option("VIAME_ENABLE_PYTORCH" "OFF")
+add_option("VIAME_ENABLE_PYTORCH-MMDET" "OFF")
+add_option("VIAME_ENABLE_PYTORCH-NETHARN" "OFF")
+add_option("VIAME_ENABLE_VIVIA" "OFF")
+add_option("VIAME_PYTHON_BUILD_FROM_SOURCE" "ON")
+add_option("VIAME_DOWNLOAD_MODELS" "ON")
+
+# Finalize OPTIONS variable
+finalize_options()
 
 set(platform Windows10)
