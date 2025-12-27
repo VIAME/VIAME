3e03eedecb668330f137458a89c4984e0fead6f2
Matt Dawkins <matt.dawkins@kitware.com>
2025-12-27 09:51:52 -0500

    Fix leading space

--- plugins/smqtk/smqtk_train_svm_models.py
+++ plugins/smqtk/smqtk_train_svm_models.py
@@ -2,7 +2,7 @@
 # BSD 3-Clause License. See either the root top-level LICENSE file or  #
 # https://github.com/VIAME/VIAME/blob/main/LICENSE.txt for details.    #
 
- from __future__ import print_function
+from __future__ import print_function
 
 import smqtk.algorithms
 import smqtk.iqr
