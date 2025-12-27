0965c301bff1faf880385daf5899a8d31bcef13e
Matt Dawkins <matt.dawkins@kitware.com>
2025-12-27 09:52:34 -0500

    Only import processes when needed at registration time

--- plugins/opencv/__init__.py
+++ plugins/opencv/__init__.py
@@ -2,9 +2,6 @@
 # BSD 3-Clause License. See either the root top-level LICENSE file or  #
 # https://github.com/VIAME/VIAME/blob/main/LICENSE.txt for details.    #
 
-from viame.processes.opencv import ocv_multimodal_registration
-from viame.processes.opencv import ocv_fft_filter_based_on_ref
-
 def __sprokit_register__():
     from kwiver.sprokit.pipeline import process_factory
 
@@ -12,6 +9,9 @@ def __sprokit_register__():
 
     if process_factory.is_process_module_loaded( module_name ):
         return
+
+    from viame.processes.opencv import ocv_multimodal_registration
+    from viame.processes.opencv import ocv_fft_filter_based_on_ref
 
     process_factory.add_process(
         'ocv_multimodal_registration',
