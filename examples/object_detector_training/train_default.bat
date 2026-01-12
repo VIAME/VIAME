ce621eccd8fc6de202de6e7da8a3fb352b83f762
Matt Dawkins <matt.dawkins@kitware.com>
2026-01-11 23:41:52 -0500

    Add example for default model

---
+++ examples/object_detector_training/train_default.bat
@@ -0,0 +1,20 @@
+@echo off
+
+REM Setup VIAME Paths (no need to set if installed to registry or already set up)
+SET VIAME_INSTALL=.\..\..
+
+CALL "%VIAME_INSTALL%\setup_viame.bat"
+
+REM Adjust log level
+SET KWIVER_DEFAULT_LOG_LEVEL=info
+
+REM Disable warnings
+SET KWIMAGE_DISABLE_C_EXTENSIONS=1
+
+REM Run pipeline
+viame_train_detector.exe ^
+  -i training_data_mouss ^
+  -c "%VIAME_INSTALL%\configs\pipelines\train_detector_default.conf" ^
+  --threshold 0.0
+
+pause
