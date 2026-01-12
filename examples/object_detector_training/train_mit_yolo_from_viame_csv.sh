ac6186e95ab0ea9d12932c90f79e99c33be21ebc
Matt Dawkins <matt.dawkins@kitware.com>
2026-01-11 21:04:31 -0500

    Check in new example pipelines

---
+++ examples/object_detector_training/train_mit_yolo_from_viame_csv.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Path to VIAME installation
+export VIAME_INSTALL="$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)/../.."
+
+source ${VIAME_INSTALL}/setup_viame.sh
+
+# Adjust log level
+export KWIVER_DEFAULT_LOG_LEVEL=info
+
+# Run pipeline
+viame_train_detector \
+  -i training_data_mouss \
+  -c ${VIAME_INSTALL}/configs/pipelines/train_detector_mit_yolo_640.conf \
+  --threshold 0.0
---
+++ examples/object_detector_training/train_netharn_rf_detr_from_viame_csv.bat
@@ -0,0 +1,17 @@
+@echo off
+
+REM Path to VIAME installation
+SET VIAME_INSTALL=.\..\..
+
+CALL "%VIAME_INSTALL%\setup_viame.bat"
+
+REM Adjust log level
+SET KWIVER_DEFAULT_LOG_LEVEL=info
+
+REM Run pipeline
+viame_train_detector.exe ^
+  -i training_data_mouss ^
+  -c "%VIAME_INSTALL%\configs\pipelines\train_detector_netharn_rf_detr.conf" ^
+  --threshold 0.0
+
+pause
