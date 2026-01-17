207cdbfd9fdeabfb466f83c7a99f924298013b2d
Matt Dawkins <matt.dawkins@kitware.com>
2026-01-17 12:42:52 -0500

    Remove traces of deprecated UI supersceded by dive

--- examples/archive_summarization/launch_timeline_interface.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export VIAME_INSTALL="$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)/../.."
-
-source ${VIAME_INSTALL}/setup_viame.sh
-
-python ${VIAME_INSTALL}/configs/launch_timeline_interface.py
