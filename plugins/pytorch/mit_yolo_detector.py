068601a745facfdd6c227c9f33129f70eca2f44e
Loic Tetrel <loic.tetrel@kitware.com>
2026-03-12 17:53:58 +0100

    fix: inference pipeline now correctly instanciated without writing config to disk

--- plugins/pytorch/mit_yolo_detector.py
+++ plugins/pytorch/mit_yolo_detector.py
@@ -106,56 +106,15 @@ class MITYoloDetector(ImageObjectDetector):
         return cfg
 
     def _build_model(self):
-        import pathlib
-        import os
         import torch
-        from hydra import compose, initialize
+        from hydra import compose, initialize_config_dir
         import yolo
         from yolo import (
             AugmentationComposer,
-            Config,
             create_converter,
             create_model,
-            # custom_logger,
-            # draw_bboxes,
+            PostProcess
         )
-        from yolo import PostProcess
-        import tempfile
-        import kwutil
-
-        weight_fpath = ub.Path(self._kwiver_config['weight'])
-
-        train_config_fpath = weight_fpath.parent / 'train_config.yaml'
-        if train_config_fpath.exists():
-            train_config = kwutil.Yaml.load(train_config_fpath)
-            # class_list = train_config['dataset']['class_list']
-        else:
-            raise Exception("Cannot introspect model config")
-
-        # Create a dummy path to keep the API happy
-        fake_image_path = tempfile.mktemp()
-
-        # temp_config_dpath = ub.Path(tempfile.mkdtemp()).ensuredir()
-        # inference_config_fpath = temp_config_dpath / 'config.yaml'
-        # train_config_fpath.copy(inference_config_fpath)
-
-        # This is annoying that we cant just specify an absolute path when it is
-        # robustly built. Furthermore, the relative path seems like it isn't even
-        # from the cwd, but the module that is currently being run.
-        # Find the path that we need to be relative to in a somewhat portable
-        # manner (i.e. will work in a Jupyter snippet).
-        try:
-            path_base = pathlib.Path(__file__).parent
-        except NameError:
-            path_base = pathlib.Path.cwd()
-        yolo_path = pathlib.Path(yolo.__file__).parent
-        rel_yolo_path = pathlib.Path(os.path.relpath(yolo_path, path_base))
-        config_path = os.fspath(yolo_path / 'config')
-        rel_config_path = pathlib.Path(os.path.relpath(config_path, path_base))
-        print(f'path_base={path_base}')
-        print(f'yolo_path={yolo_path}')
-        print(f'rel_yolo_path={rel_yolo_path}')
-        print(f'config_path={config_path}')
 
         # TODO: need to be able to read metadata with weights
         device = self._kwiver_config.device
@@ -165,50 +124,33 @@ class MITYoloDetector(ImageObjectDetector):
             else:
                 device = torch.device('cpu')
 
-        weights_fpath = self._kwiver_config.weight
-        print(f'weights_fpath={weights_fpath}')
-        config_name = 'config'
-        model = 'v9-c'
+        weight_fpath = ub.Path(self._kwiver_config.weight)
+        print(f'weights_fpath={str(weight_fpath)}')
 
-        train_config['dataset']
+        # Initialize pre-processing and inference with user train configuration
+        train_config_dir = weight_fpath.parent
+        train_config_name = "train_config.yaml"
+        print(f'train_config_path={train_config_dir / train_config_name}')
+        with initialize_config_dir(version_base=None, config_dir=str(train_config_dir), job_name="mit_yolo_detector"):
+            train_cfg = compose(config_name=train_config_name)
+            model = create_model(train_cfg.model, class_num=train_cfg.dataset.class_num, weight_path=weight_fpath).to(device)
+            transform = AugmentationComposer([], train_cfg.image_size)
+            converter = create_converter(train_cfg.model.name, model, train_cfg.model.anchor, train_cfg.image_size, device)
+        # Initialize post-processing with default nms configuration
+        import yolo.config
+        infer_config_dir = ub.Path(yolo.config.__file__).parent / "task"
+        infer_config_name = "inference.yaml"
+        with initialize_config_dir(version_base=None, config_dir=str(infer_config_dir), job_name="nms_mit_yolo_detector"):
+            infer_cfg = compose(config_name=infer_config_name)
+            post_proccess = PostProcess(converter, infer_cfg.nms)  #TODO match kwiver refiner:nms config here
 
-        # Write into the config dir a file that we can use for inference
-        # TODO: it would be very nice if we could do this outside of the yolo
-        # python module.
-        dataset_config = train_config['dataset']
-        cfgid = ub.hash_data(dataset_config, base='hex')[0:16]
-        dataset_config_name = f'dataset_config_{cfgid}'
-        dataset_config_dpath = ub.Path(config_path) / 'dataset'
-        dataset_config_fpath = dataset_config_dpath / f'{dataset_config_name}.yaml'
-        dataset_config_fpath.write_text(kwutil.Yaml.dumps(dataset_config))
-
-        with initialize(config_path=os.fspath(rel_config_path), version_base=None, job_name="mit_yolo_detector"):
-            # Use the hydra system to populate the expected configuration,
-            # but then use it to construct the model explicitly
-            cfg: Config = compose(
-                config_name=config_name,
-                overrides=[
-                    "task=inference",
-                    f"task.data.source={fake_image_path}",
-                    f"model={model}",
-                    f"weight='{weights_fpath}'",
-                    f'dataset={dataset_config_name}',
-                    # f"dataset.class_list={class_list}",
-                    # f"dataset.class_num={len(class_list)}",
-                    "use_wandb=False",
-                ]
-            )
-            model = create_model(cfg.model, class_num=cfg.dataset.class_num, weight_path=cfg.weight).to(device)
-            transform = AugmentationComposer([], cfg.image_size)
-            converter = create_converter(cfg.model.name, model, cfg.model.anchor, cfg.image_size, device)
-            post_proccess = PostProcess(converter, cfg.task.nms)
-
+        # Set the inference pipeline
         self._yolo_objects.update({
             'model': model,
             'transform': transform,
             'converter': converter,
             'post_proccess': post_proccess,
-            'classes': list(cfg.dataset.class_list),
+            'classes': list(train_cfg.dataset.class_list),
         })
 
     def set_configuration(self, cfg_in):
