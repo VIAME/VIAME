996ff8350c441614a0b1590d3785177211ba30e9
Matt Dawkins <matt.dawkins@kitware.com>
2026-08-06 14:34:21 -0400

    Move index_default.trk.pipe into the generic add-on as index_generic.trk.pipe

--- configs/prj-linux/create_index.detection_and_tracking.sh
+++ configs/prj-linux/create_index.detection_and_tracking.sh
@@ -16,6 +16,6 @@ source ${VIAME_INSTALL}/setup_viame.sh
 
 python ${VIAME_INSTALL}/configs/process_video.py --init \
   -i ${INPUT} -o database -frate ${FRAME_RATE} \
-  -p pipelines/index_default.trk.pipe -o database \
+  -p pipelines/index_generic.trk.pipe -o database \
   -gpus ${TOTAL_GPU_COUNT} -pipes-per-gpu ${PIPES_PER_GPU} \
   --build-index -install ${VIAME_INSTALL}
