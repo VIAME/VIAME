a171547ae071e9eddcbe8065beab4126364ad04a
Matt Dawkins <matt.dawkins@kitware.com>
2026-09-07 13:52:14 -0400

    Update util for training loop

--- plugins/pytorch/netharn/util/util_fname.py
+++ plugins/pytorch/netharn/util/util_fname.py
@@ -3,8 +3,6 @@
 NOTE: THIS FILE IS DEPRECATED
 
 Processing for filenames. The logic is relatively hacky.
-
-pip install pygtrie
 """
 from os.path import commonprefix, isdir, dirname
 import numpy as np  # NOQA
@@ -19,7 +17,7 @@ def shortest_unique_prefixes(items, sep=None, allow_simple=True, min_length=0, a
         sep (str): if specified, all characters between separators are treated
             as a single symbol. Makes the algo much faster.
         allow_simple (bool): if True tries to construct a simple feasible
-            solution before resorting to the optimal trie algorithm.
+            solution before resorting to the optimal algorithm.
         min_length (int): minimum length each prefix can be
         allow_end (bool): if True allows for string terminators to be
             considered in the prefix
@@ -32,11 +30,7 @@ def shortest_unique_prefixes(items, sep=None, allow_simple=True, min_length=0, a
         http://www.geeksforgeeks.org/find-all-shortest-unique-prefixes-to-represent-each-word-in-a-given-list/
         https://github.com/Briaares/InterviewBit/blob/master/Level6/Shortest%20Unique%20Prefix.cpp
 
-    Requires:
-        pip install pygtrie
-
     Example:
-        >>> # xdoctest: +REQUIRES(--pygtrie)
         >>> items = ["zebra", "dog", "duck", "dove"]
         >>> shortest_unique_prefixes(items)
         ['z', 'dog', 'du', 'dov']
@@ -70,15 +64,16 @@ def shortest_unique_prefixes(items, sep=None, allow_simple=True, min_length=0, a
         Timed for: 3 loops, best of 3
             time per loop: best=4.063 s, mean=4.063 ± 0.0 s
     """
-    import pygtrie
     if len(set(items)) != len(items):
         raise ValueError('inputs must be unique')
 
-    # construct trie
     if sep is None:
-        trie = pygtrie.CharTrie.fromkeys(items, value=0)
+        token_lists = [tuple(item) for item in items]
+
+        def join(tokens):
+            return ''.join(tokens)
     else:
-        # In some simple cases we can avoid constructing a trie
+        # In some simple cases we can avoid the general algorithm
         if allow_simple:
             tokens = [item.split(sep) for item in items]
             simple_solution = [t[0] for t in tokens]
@@ -90,52 +85,42 @@ def shortest_unique_prefixes(items, sep=None, allow_simple=True, min_length=0, a
                 if len(simple_solution) == len(set(simple_solution)):
                     return simple_solution
 
-        trie = pygtrie.StringTrie.fromkeys(items, value=0, separator=sep)
+        token_lists = [tuple(item.split(sep)) for item in items]
 
-    # Set the value (frequency) of all nodes to zero.
-    for node in _trie_iternodes(trie):
-        node.value = 0
+        def join(tokens):
+            return sep.join(tokens)
 
-    # For each item trace its path and increment frequencies
-    for item in items:
-        final_node, trace = trie._get_node(item)
-        for key, node in trace:
-            node.value += 1
+    # A prefix is unique exactly when it is longer than what the item shares
+    # with its lexicographic neighbors, so only those two need comparing.
+    order = sorted(range(len(items)), key=token_lists.__getitem__)
+    shared = [-1] * len(items)
+    for rank, index in enumerate(order):
+        toks = token_lists[index]
+        for other_rank in (rank - 1, rank + 1):
+            if 0 <= other_rank < len(order):
+                other = token_lists[order[other_rank]]
+                common = 0
+                for tok1, tok2 in zip(toks, other):
+                    if tok1 != tok2:
+                        break
+                    common += 1
+                if common > shared[index]:
+                    shared[index] = common
 
-    # if not isinstance(node.value, int):
-    #     node.value = 0
-
-    # Query for the first prefix with frequency 1 for each item.
-    # This is the shortest unique prefix over all items.
     unique = []
-    for item in items:
-        freq = None
-        for prefix, freq in trie.prefixes(item):
-            if freq == 1 and len(prefix) >= min_length:
-                break
-        if not allow_end:
-            assert freq == 1, 'item={} has no unique prefix. freq={}'.format(item, freq)
-        # print('items = {!r}'.format(items))
+    for index, toks in enumerate(token_lists):
+        size = min(shared[index] + 1, len(toks))
+        prefix = join(toks[:size])
+        while size < len(toks) and len(prefix) < min_length:
+            size += 1
+            prefix = join(toks[:size])
+        if not allow_end and size <= shared[index]:
+            freq = sum(toks[:size] == other[:size] for other in token_lists)
+            raise AssertionError(
+                'item={} has no unique prefix. freq={}'.format(
+                    items[index], freq))
         unique.append(prefix)
     return unique
-
-
-def _trie_iternodes(self):
-    """
-    Generates all nodes in the trie
-
-    # Hack into the internal structure and insert frequencies at each node
-    """
-    from collections import deque
-    stack = deque([[self._root]])
-    while stack:
-        for node in stack.pop():
-            yield node
-            try:
-                # only works in pygtrie-2.2 broken in pygtrie-2.3.2
-                stack.append(node.children.values())
-            except AttributeError:
-                stack.append([v for k, v in node.children.iteritems()])
 
 
 def shortest_unique_suffixes(items, sep=None):
@@ -144,13 +129,11 @@ def shortest_unique_suffixes(items, sep=None):
         xdoctest -m netharn.util.util_fname shortest_unique_suffixes
 
     Example:
-        >>> # xdoctest: +REQUIRES(--pygtrie)
         >>> items = ["zebra", "dog", "duck", "dove"]
         >>> shortest_unique_suffixes(items)
         ['a', 'g', 'k', 'e']
 
     Example:
-        >>> # xdoctest: +REQUIRES(--pygtrie)
         >>> items = ["aa/bb/cc", "aa/bb/bc", "aa/bb/dc", "aa/cc/cc"]
         >>> shortest_unique_suffixes(items)
     """
@@ -166,7 +149,6 @@ def dumpsafe(paths, repl='<sl>'):
     Removes common the common prefix, and replaces slashes with <sl>
 
     Ignore:
-        >>> # xdoctest: +REQUIRES(--pygtrie)
         >>> paths = ['foo/{:04d}/{:04d}'.format(i, j) for i in range(2) for j in range(20)]
         >>> list(dumpsafe(paths, '-'))
     """
@@ -240,7 +222,6 @@ def align_paths(paths1, paths2):
     paths1, paths2 = gt_paths, pred_paths
 
     Doctest:
-        >>> # xdoc: +REQUIRES(--pygtrie)
         >>> def test_gt_arrangements(paths1, paths2, paths2_):
         >>>     # no matter what order paths2_ comes in, it should align with the groundtruth
         >>>     assert align_paths(paths1, paths2_) == paths2
@@ -379,7 +360,7 @@ def check_aligned(paths1, paths2):
 if __name__ == '__main__':
     r"""
     CommandLine:
-        xdoctest -m netharn.util.util_fname all --pygtrie
+        xdoctest -m netharn.util.util_fname all
     """
     import xdoctest
     xdoctest.doctest_module(__file__)
